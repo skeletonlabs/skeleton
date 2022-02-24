b738d6c9a89842cb97ad4200763af8a22f9df18a
endigo9740 <gundamx9740@gmail.com>
2022-02-24 10:32:21 -0600

    Dark mode persist, moved toggled

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,39 +1,52 @@
 <script lang="ts">
-	import { browser } from '$app/env';
-	import Button from '$lib/button/Button.svelte';
 	import '../app.css';
+	import { browser } from '$app/env';
 
-	// Set Display Mode (dark/light)
-	// https://stackoverflow.com/questions/63259289/svelte-add-noscroll-classname-to-body-from-component
-	let mode = true; // t: dark, f: light
-	$: if (browser) document.documentElement.classList.toggle('dark', mode);
-	function toggleDisplayMode(): void { mode = !mode; }
+	let navLinks: any[] = [
+		{href: '/', label: 'Welcome'},
+		{href: '/cards', label: 'Cards'},
+		{href: '/buttons', label: 'Buttons'},
+	];
+	let darkMode: boolean;
+
+	// On load, set display mode
+	$: if (browser) {
+		darkMode = ('theme' in localStorage) && localStorage.getItem('theme') === 'dark';
+		document.documentElement.classList.toggle('dark', darkMode);
+	}
+
+	// Toggle display mode
+	function toggleDisplayMode(): void {
+		darkMode = !darkMode;
+		if (browser) { localStorage.setItem('theme', darkMode ? 'dark' : 'light'); }
+	}
 </script>
 
 <svelte:head>
 	<title>Skeleton: UI Component Library for SvelteKit</title>
 </svelte:head>
 
-<div
-	on:click="{toggleDisplayMode}"
-	class="absolute right-4 bottom-4 w-12 h-12 rounded-full bg-gray-500 text-white flex justify-center items-center cursor-pointer"
->D</div>
-
+<!-- Page Layout -->
 <div class="flex flex-row">
 
     <!-- NavBar -->
     <section class="basis-1/4 border-r-2 border-surface-200 p-10 h-screen overflow-auto dark:border-surface-800">
         <div class="flex justify-between items-center">
 			<h1 class="text-4xl font-bold">Skeleton UI</h1>
-			<a href="https://github.com/Brain-Bones/skeleton" target="_blank" class="fill-white">
-				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 32 32"><path fill-rule="evenodd" d="M 16 4 C 9.371094 4 4 9.371094 4 16 C 4 21.300781 7.4375 25.800781 12.207031 27.386719 C 12.808594 27.496094 13.027344 27.128906 13.027344 26.808594 C 13.027344 26.523438 13.015625 25.769531 13.011719 24.769531 C 9.671875 25.492188 8.96875 23.160156 8.96875 23.160156 C 8.421875 21.773438 7.636719 21.402344 7.636719 21.402344 C 6.546875 20.660156 7.71875 20.675781 7.71875 20.675781 C 8.921875 20.761719 9.554688 21.910156 9.554688 21.910156 C 10.625 23.746094 12.363281 23.214844 13.046875 22.910156 C 13.15625 22.132813 13.46875 21.605469 13.808594 21.304688 C 11.144531 21.003906 8.34375 19.972656 8.34375 15.375 C 8.34375 14.0625 8.8125 12.992188 9.578125 12.152344 C 9.457031 11.851563 9.042969 10.628906 9.695313 8.976563 C 9.695313 8.976563 10.703125 8.65625 12.996094 10.207031 C 13.953125 9.941406 14.980469 9.808594 16 9.804688 C 17.019531 9.808594 18.046875 9.941406 19.003906 10.207031 C 21.296875 8.65625 22.300781 8.976563 22.300781 8.976563 C 22.957031 10.628906 22.546875 11.851563 22.421875 12.152344 C 23.191406 12.992188 23.652344 14.0625 23.652344 15.375 C 23.652344 19.984375 20.847656 20.996094 18.175781 21.296875 C 18.605469 21.664063 18.988281 22.398438 18.988281 23.515625 C 18.988281 25.121094 18.976563 26.414063 18.976563 26.808594 C 18.976563 27.128906 19.191406 27.503906 19.800781 27.386719 C 24.566406 25.796875 28 21.300781 28 16 C 28 9.371094 22.628906 4 16 4 Z"></path></svg>
-			</a>
+			<nav class="flex justify-between items-center space-x-4">
+				<button on:click="{toggleDisplayMode}" class="fill-surface-500">
+					<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 512 512"><path d="M332.3 426.4c-93.13 17.75-178.5-53.63-178.5-147.6c0-54.25 29-104 76-130.9c7.375-4.125 5.45-15.12-2.8-16.62C108.7 109.4 0 200 0 320c0 106 85.76 192 191.8 192c59.25 0 113.2-26.79 148.9-71.04C346.1 434.5 340.3 424.8 332.3 426.4zM288 63.1l12.42 29.78c.6094 1.225 2.211 2.219 3.578 2.219s2.967-.9941 3.576-2.219l12.42-29.78l29.79-12.42C351 50.97 352 49.36 352 47.1c0-1.365-.9922-2.967-2.211-3.576l-29.79-12.42l-12.42-29.79c-.6094-1.227-2.209-2.217-3.576-2.217s-2.969 .9902-3.578 2.217l-12.42 29.79L258.2 44.42c-1.217 .6094-2.209 2.211-2.209 3.576c0 1.359 .9922 2.971 2.209 3.58L288 63.1zM507.6 216.9L448 192l-24.88-59.63C421.8 129.8 419 127.1 416 127.1s-5.75 1.75-7.125 4.375L384 192l-59.63 24.88C321.8 218.3 320 221 320 224s1.75 5.75 4.375 7.125L384 256l24.88 59.63C410.3 318.3 413 320 416 320s5.75-1.75 7.125-4.375L448 256l59.63-24.88C510.3 229.8 512 227 512 224S510.3 218.3 507.6 216.9z"/></svg>
+				</button>
+				<a href="https://github.com/Brain-Bones/skeleton" target="_blank" class="fill-surface-500">
+					<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+				</a>
+			</nav>
 		</div>
 		<hr class="border-t-1 border-surface-700 my-4">
         <nav class="space-y-4">
-			<a href="/" class="block">Welcome</a>
-			<a href="/cards" class="block">Cards</a>
-			<a href="/buttons" class="block">Buttons</a>
+			{#each navLinks as {href,label} }
+			<a {href} class="block text-xl hover:underline-offset-1">{label}</a>
+			{/each}
         </nav>
     </section>
 
