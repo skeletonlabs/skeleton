3fec9e42cdcd488a0a1c66ee0bc7491445293227
endigo9740 <gundamx9740@gmail.com>
2022-02-28 12:54:18 -0600

    Minor updates to divider component

--- src/lib/divider/Divider.svelte
+++ src/lib/divider/Divider.svelte
@@ -1,31 +1,24 @@
 <script lang="ts">
-    // Style
-    export let style: String = null;
-    // Orientation
-    export let vertical: boolean = null;
-    // Width
-    export let bold: boolean = null;
+	// Type: null | dashed | dotted
+	export let type: String = null;
+	// Orientation Vertical
+	export let vertical: boolean = null;
+	// Bold
+	export let bold: boolean = null;
 
-    const customStyle: any = [
-        style ? style : null,
-        vertical ? 'vertical' : null,
-        bold ? 'bold' : null,
-    ].filter(n => n).join('-');
-
+	const customStyle: any = [
+		type ? type : null,
+		vertical ? 'vertical' : null,
+		bold ? 'bold' : null
+    ].filter((n) => n).join('-');
 </script>
 
-<hr class="{`${customStyle} ${$$props.class}`}">
+<hr class={`${customStyle} ${$$props.class}`} />
 
 <style lang="postcss">
-    hr{
-        @apply pb-4 border-0
-    }
-
-.solid { @apply border-t border-surface-400 dark:border-surface-600; }
-.dashed { @apply border-t border-dashed border-surface-400 dark:border-surface-600; }
-.dotted { @apply border-t border-dotted border-surface-400 dark:border-surface-600; }
-
-.vertical { @apply border-l relative h-full border-surface-400 dark:border-surface-600; }
-.bold { @apply border-t-2 border-solid border-surface-400 dark:border-surface-600;}
-
+	hr { @apply pb-4 border-0 border-t border-solid border-surface-500; }
+	.dashed { @apply border-dashed; }
+	.dotted { @apply border-dotted; }
+	.bold { @apply border-t-2; }
+    .vertical { @apply border-0 border-l relative h-full; }
 </style>
--- src/routes/dividers.svelte
+++ src/routes/dividers.svelte
@@ -1,27 +1,23 @@
 <script lang="ts">
     import Divider from "$lib/Divider/Divider.svelte";
-    import Card from "$lib/card/Card.svelte";
+    import Card from "$lib/Card/Card.svelte";
 </script>
 
-<h2 class="text-3xl font-bold mb-4">
-    <span class="bg-clip-text text-transparent bg-gradient-to-br from-primary-500 to-accent-500">Dividers</span>
-</h2>
+<h2 class="text-3xl font-bold mb-4">Dividers</h2>
 
 <Card>
-    <h2 class="text-lg mb-4">Solid</h2>
-    <Divider style="solid"/>
+    <h2 class="text-lg mb-4">Default</h2>
+    <Divider />
     <h2 class="text-lg mb-4">Dashed</h2>
-    <Divider style="dashed"/>
+    <Divider type="dashed"/>
     <h2 class="text-lg mb-4">Dotted</h2>
-    <Divider style="dotted"/>
+    <Divider type="dotted"/>
     <h2 class="text-lg mb-4">Bold</h2>
     <Divider bold/>
     <h2 class="text-lg mb-4">Vertical</h2>
-    <div class="h-40 flex justify-evenly">
+    <div class="h-[100px] flex justify-evenly items-center">
         <div>CONTENT X</div>
         <Divider vertical/>
         <div>CONTENT Y</div>
-        
     </div>
-
 </Card>
