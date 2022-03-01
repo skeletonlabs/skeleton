8f78bfe7e4f381affbcae2b3a731e90d4f8963f3
endigo9740 <gundamx9740@gmail.com>
2022-03-01 14:35:00 -0600

    Moved color tw plugin, added export index

---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+export { default as Button } from "./Button/Button.svelte";
+export { default as Card } from "./Card/Card.svelte";
+export { default as Divider } from "./Divider/Divider.svelte";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,6 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	plugins: [
-		require('./src/utils/colors.cjs')
+		require('./src/lib/colors.cjs')
 	]
 };
