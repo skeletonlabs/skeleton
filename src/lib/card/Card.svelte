9055d487d5392c016ae4a81fdb3071d188c0092f
endigo9740 <gundamx9740@gmail.com>
2022-03-01 17:32:16 -0600

    Return to inline card classes

--- src/lib/card/Card.svelte
+++ src/lib/card/Card.svelte
@@ -1,10 +1,16 @@
-<div class="card {$$props.class}">
+<div class="bg-surface-200 p-5 rounded-lg dark:bg-surface-800 {$$props.class}">
     <slot />
 </div>
 
 <!-- https://github.com/svelte-add/svelte-add/issues/92 -->
-<style lang="postcss" global>
+<!-- <style lang="postcss" global>
     :local(.card) {
         @apply bg-surface-200 p-5 rounded-lg dark:bg-surface-800;
     }
-</style>
+</style> -->
+
+<!-- <style lang="postcss">
+    .card {
+        @apply bg-surface-200 p-5 rounded-lg dark:bg-surface-800;
+    }
+</style> -->
