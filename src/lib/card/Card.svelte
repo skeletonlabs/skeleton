8e83c4ca16be6ac05b672529f18387f8d21085ce
endigo9740 <gundamx9740@gmail.com>
2022-03-01 16:44:28 -0600

    Version bump, moved card styles to block

--- src/lib/card/Card.svelte
+++ src/lib/card/Card.svelte
@@ -1,3 +1,10 @@
-<div class="{`bg-surface-200 p-5 rounded-lg dark:bg-surface-800 ${$$props.class}`}">
+<div class="card {$$props.class}">
     <slot />
 </div>
+
+<!-- https://github.com/svelte-add/svelte-add/issues/92 -->
+<style lang="postcss" global>
+    :local(.card) {
+        @apply bg-surface-200 p-5 rounded-lg dark:bg-surface-800;
+    }
+</style>
