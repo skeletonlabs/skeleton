0461e986637cd8d6bd49c1ba995c3e3f9dd12ddc
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-03-03 15:41:59 -0700

    Revisions and added outlined + removed non-rounded option

--- src/lib/Avatar/Avatar.svelte
+++ src/lib/Avatar/Avatar.svelte
@@ -1,37 +1,47 @@
 <script lang="ts">
-    export let img: string = null;
-    export let rounded: boolean = false;
-    export let size: String = 'small';
-    export let cBase = 'flex justify-center items-center';
+	export let src: string = null;
+	export let size: String = 'medium';
+	export let cBase: String = 'flex justify-center items-center';
+	export let outlined: boolean = false;
 
-    export function makeStyle(){
-        let style: string = '';
-        if(rounded){
-            style += 'rounded-full';
-        }
-        if(!img){
-            style += ' bg-surface-400 dark:bg-surface-700';
-        }
-     
-        switch(size){
-            case('small'): { style += ' w-8 h-8 text-base'; break;}
-            case('medium'): { style += ' w-12 h-12 text-lg'; break; }
-            case('large'): { style += ' w-16 h-16 text-2xl'; break; }
-            case('xlarge'): { style += ' w-24 h-24 text-4xl'; break; }
-        }
-        return style;
-    }
+	export function makeStyle() {
+		let style: string = 'rounded-full';
+		if (!src) {
+			style += ' bg-surface-400 dark:bg-surface-700';
+		}
+		if (outlined) {
+			style += ' outline outline-2 outline-offset-2 outline-accent-600 dark:outline-accent-300';
+		}
 
+		switch (size) {
+			case 'small': {
+				style += ' w-8 h-8 text-base';
+				break;
+			}
+			case 'medium': {
+				style += ' w-12 h-12 text-lg';
+				break;
+			}
+			case 'large': {
+				style += ' w-16 h-16 text-2xl';
+				break;
+			}
+			case 'xlarge': {
+				style += ' w-24 h-24 text-4xl';
+				break;
+			}
+		}
+		return style;
+	}
 </script>
 
-<div on:click class="{cBase} {makeStyle()}">
-    {#if img}
-        <img src={img} alt="" class='rounded-full object-contain shadow-lg'>
-    {:else}
-        <p>A</p>
-    {/if}
+<div class="{cBase} {makeStyle()}">
+	{#if src}
+		<img class="rounded-full object-contain" {src} alt="" />
+	{:else}
+		<p data-testid="placeholder">A</p>
+	{/if}
 </div>
 
 <style lang="postcss">
-
 </style>
--- src/routes/avatars.svelte
+++ src/routes/avatars.svelte
@@ -2,10 +2,21 @@
 
 import Avatar from "$lib/Avatar/Avatar.svelte";
 import Card from "$lib/Card/Card.svelte";
+import CodeBlock from "$lib/_CodeBlock/CodeBlock.svelte";
+import Table from "$lib/_Table/Table.svelte";
 
 function allOK(){
     console.log('AllOk')
 }
+
+const tableProps: any = {
+        columns: ['Prop', 'Type', 'Values', 'Default', 'Description'],
+        data: [
+            ['src', 'string', 'URL', '-', 'The image source to be used.'],
+            ['size', 'string', 'small | medium | large | xlarge', 'medium', 'Set the size of the Avatar.'],
+            ['outline', 'boolean', 'true | false', 'false', 'Use outlining.'],
+        ],
+    };
 
 </script>
 
@@ -16,28 +27,38 @@ function allOK(){
 	</p>
 </header>
 
+<!-- Types -->
 <Card>
-    <h2>Non-interactive</h2>
+    <h2>Types</h2>
     <div class='flex justify-evenly'>
-
         <Avatar size='xlarge' />
-        <Avatar size='xlarge' img='https://i.pravatar.cc/512' />
+        <Avatar size='xlarge' outlined />
+        <Avatar size='xlarge' src='https://i.pravatar.cc/512' />
     </div>
 </Card>
 
 <div class='mb-6'></div>
 
+<!-- Sizes -->
 <Card>
     <h2>Sizes</h2>
     <div class='flex justify-evenly items-center'>
-        <Avatar rounded size='small' />
-        <Avatar rounded size='medium' />
-        <Avatar rounded size='large' />
-        <Avatar rounded size='xlarge' />
+        <Avatar size='small' />
+        <Avatar size='medium' />
+        <Avatar size='large' />
+        <Avatar size='xlarge' />
         <br>
-        <Avatar rounded size='small' img='https://i.pravatar.cc/512' />
-        <Avatar rounded size='medium' img='https://i.pravatar.cc/512' />
-        <Avatar rounded size='large' img='https://i.pravatar.cc/512' />
-        <Avatar rounded size='xlarge' img='https://i.pravatar.cc/512' />
+        <Avatar size='small' src='https://i.pravatar.cc/512' />
+        <Avatar size='medium' src='https://i.pravatar.cc/512' />
+        <Avatar size='large' src='https://i.pravatar.cc/512' />
+        <Avatar size='xlarge' src='https://i.pravatar.cc/512' />
     </div>
 </Card>
+
+<!-- Usage -->
+<h3 class="text-2xl font-bold my-4">Usage</h3>
+<CodeBlock class="mb-4">{`import {Avatar} from '@brainandbones/skeleton';\n\n<Avatar rounded img="image_source_url" size="medium">`}</CodeBlock>
+
+<!-- Properties -->
+<h3 class="text-2xl font-bold my-4">Properties</h3>
+<Table source="{tableProps}"></Table>
