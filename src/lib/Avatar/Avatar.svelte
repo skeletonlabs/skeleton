0b2f34cf413fb4875df0d0d567a8022928f0ab2f
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-03-03 16:28:32 -0700

    Added comments to the Avatar script

--- src/lib/Avatar/Avatar.svelte
+++ src/lib/Avatar/Avatar.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-	export let src: string = null;
-	export let size: String = 'medium';
-	export let cBase: String = 'flex justify-center items-center';
-	export let outlined: boolean = false;
+	export let src:         string = null;
+	export let size:        String = 'medium'; // Size for the component
+	export let cBase:       String = 'flex justify-center items-center rounded-full'; // Refers to the overlaying div container + alignment props
+	export let outlined:    boolean = false;
     export let interactive: boolean = false;
 
-	export function makeStyle() {
-		let style: string = 'rounded-full';
-		if (!src) {
+    /**
+     * Function to generate the colors, sizes, etc of the component
+     */
+	export function makeStyle(): String {
+		let style: string = '';
+		if (!src) { // Only set background if image is not provided.
 			style += ' bg-surface-300 dark:bg-surface-700';
 		}
-		if (outlined) {
+		if (outlined) { // Outlined props
 			style += ' outline outline-2 outline-offset-2 outline-accent-600 dark:outline-accent-300';
 		}
-        if(interactive) {
+        if(interactive) { // Hover state
             style += ' hover:outline hover:outline-2 outline-accent-600 outline-offset-4 dark:outline-accent-300 cursor-pointer transition-all ease-in-out';
         }
 
