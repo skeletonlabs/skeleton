66387098e9c11605e38bc4ddd4a68252907678a7
endigo9740 <gundamx9740@gmail.com>
2022-03-04 10:17:03 -0600

    Latest updates

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,4 @@ export { default as Button } from "./Button/Button.svelte";
 export { default as Card } from "./Card/Card.svelte";
 export { default as Divider } from "./Divider/Divider.svelte";
 export { default as LogoCloud } from "./LogoCloud/LogoCloud.svelte";
-export { default as Cloud } from "./LogoCloud/Cloud.svelte";
+export { default as Logo } from "./LogoCloud/Logo.svelte";
