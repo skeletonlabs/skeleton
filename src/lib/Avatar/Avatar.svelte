e35cfd5c1dccf054596338bbe73ee404651701b1
endigo9740 <gundamx9740@gmail.com>
2022-03-07 16:53:21 -0600

    git status

--- src/lib/Avatar/Avatar.svelte
+++ src/lib/Avatar/Avatar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	export let src: string = ''; // image source, default empty
-	export let size: string = 'md'; // sm | md | lg | xl
+	export let size: string = ''; // sm | md | lg | xl | 2xl | 3xl
 	export let outlined: boolean = false;
     export let hover: boolean = false;
 	export let filter: string = ''; // ex: #myFilterId
 	
-	const cBase: string = 'flex text-surface-500 font-semibold justify-center items-center rounded-full bg-surface-300 dark:bg-surface-700';
+	const cBase: string = 'flex aspect-square overflow-hidden text-surface-500 font-semibold justify-center items-center rounded-full bg-surface-300 dark:bg-surface-700';
 	let style: string = '';
 
 	// Set Outlined
@@ -22,14 +22,17 @@
 		case 'md': style += ' w-12 h-12 text-lg'; break;
 		case 'lg': style += ' w-16 h-16 text-2xl'; break;	
 		case 'xl': style += ' w-24 h-24 text-4xl'; break;
+		case '2xl': style += ' w-32 h-32 text-4xl'; break;
+		case '3xl': style += ' w-40 h-40 text-4xl'; break;
+		default: style += 'w-full text 4-xl';
 	}
 	
 	$: classes = `${cBase} ${style} ${$$props.class}`;
 </script>
 
-<div on:click class="{classes} overflow-hidden">
+<div on:click class="{classes}">
 	{#if src}
-	<img class="aspect-square object-cover" {src} style="filter: url({filter})" alt="Avatar" />
+	<img class="w-full h-full object-cover" {src} style="filter: url({filter})" alt="Avatar" />
 	{:else}
 	<p data-testid="placeholder">A</p>
 	{/if}
--- src/routes/avatars.svelte
+++ src/routes/avatars.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 
-import Avatar from "$lib/Avatar/Avatar.svelte";
-import Card from "$lib/Card/Card.svelte";
-import CodeBlock from "$lib/_CodeBlock/CodeBlock.svelte";
-import Table from "$lib/_Table/Table.svelte";
+    import Avatar from "$lib/Avatar/Avatar.svelte";
+    import Card from "$lib/Card/Card.svelte";
+    import CodeBlock from "$lib/_CodeBlock/CodeBlock.svelte";
+    import Table from "$lib/_Table/Table.svelte";
 
-const tableProps: any = {
+    const placeholder: string = 'https://i.pravatar.cc/160';
+    const tableProps: any = {
         columns: ['Prop', 'Type', 'Values', 'Default', 'Description'],
         data: [
             ['src', 'string', 'URL', '-', 'The image source to be used.'],
-            ['size', 'string', 'sm | md | lg | xl', 'md', 'Set the size of the Avatar.'],
+            ['size', 'string', 'sm | md | lg | xl | 2xl | 3xl', 'full width', 'Set the size of the Avatar.'],
             ['outline', 'boolean', 'true | false', 'false', 'Use outlining.'],
             ['hover', 'boolean', 'true | false', 'false', 'Sets Avatar to be visually hover.'],
             ['filter', 'string', 'SVG filter ID', '-' , `<a href='https://css-tricks.com/almanac/properties/f/filter/' target="_blank">Applies an SVG filter ID using filter: url(#id)</a>`],
@@ -20,55 +21,83 @@ const tableProps: any = {
 
 <header class="mb-4">
 	<h2 class="text-3xl font-bold mb-4">Avatars</h2>
-	<p class="text-base opacity-70">
-		The Avatar component is used to represent 
-	</p>
+	<p class="text-base opacity-70">Choose from a variety for avatar sizes and styles. Include clean placeholders.</p>
 </header>
 
-<!-- Types -->
-<Card>
-    <h2>Types</h2>
-    <div class='flex justify-evenly lg:flex-row'>
-        <div class='flex flex-col text-center'>
+<section class="space-y-4">
+
+    <!-- Types -->
+    <h2 class="text-base font-bold">Types</h2>
+    <Card>
+        <div class='flex justify-evenly lg:flex-row'>
+            <div class='flex flex-col text-center'>
+                <Avatar size='xl' />
+                <p class ='mt-2'>Placeholder</p>
+            </div>
+            <div class='flex flex-col text-center'>   
+                <Avatar size='xl' outlined />
+                <p class ='mt-2'>Outlined</p>
+            </div>
+            <div class='flex flex-col text-center'>
+                <Avatar size='xl' src={placeholder} />
+                <p class ='mt-2'>Image</p>
+            </div>
+            <div class='flex flex-col text-center'>
+                <Avatar size='xl' hover src={placeholder} />
+                <p class ='mt-2'>Hover</p>
+            </div>
+            <div class='flex flex-col text-center'>
+                <Avatar size='xl' filter="blur(5px)" src={placeholder} />
+                <p class ='mt-2'>Filtered</p>
+            </div>
+        </div>
+    </Card>
+
+    <!-- Sizes -->
+    <h2 class="text-base font-bold">Sizes</h2>
+    <Card class='flex justify-evenly items-center'>
+        <div>
+            <Avatar size='sm' />
+            <h3 class="text-small text-center mt-2">sm</h3>
+        </div>
+        <div>
+            <Avatar size='md' />
+            <h3 class="text-small text-center mt-2">md</h3>
+        </div>
+        <div>
+            <Avatar size='lg' />
+            <h3 class="text-small text-center mt-2">lg</h3>
+        </div>
+        <div>
             <Avatar size='xl' />
-            <p class ='mt-2'>Placeholder</p>
+            <h3 class="text-small text-center mt-2">xl</h3>
         </div>
-        <div class='flex flex-col text-center'>   
-            <Avatar size='xl' outlined />
-            <p class ='mt-2'>Outlined</p>
+        <div>
+            <Avatar size='2xl' />
+            <h3 class="text-small text-center mt-2">2xl</h3>
         </div>
-        <div class='flex flex-col text-center'>
-            <Avatar size='xl' src='https://i.pravatar.cc/512' />
-            <p class ='mt-2'>Image</p>
+        <div>
+            <Avatar size='3xl' />
+            <h3 class="text-small text-center mt-2">3xl</h3>
         </div>
-        <div class='flex flex-col text-center'>
-            <Avatar size='xl' hover src='https://i.pravatar.cc/512' />
-            <p class ='mt-2'>Hover</p>
-        </div>
-        <div class='flex flex-col text-center'>
-            <Avatar size='xl' filter="blur(5px)" src='https://i.pravatar.cc/512' />
-            <p class ='mt-2'>SVG Filter</p>
-        </div>
-    </div>
-</Card>
+    </Card>
+    <Card class='flex justify-evenly items-center'>
+        <Avatar size='sm' src={placeholder} />
+        <Avatar size='md' src={placeholder} />
+        <Avatar size='lg' src={placeholder} />
+        <Avatar size='xl' src={placeholder} />
+        <Avatar size='2xl' src={placeholder} />
+        <Avatar size='3xl' src={placeholder} />
+    </Card>
+    <Card class='grid grid-cols-5 gap-4'>
+        <Avatar src={`${placeholder}?img=1`} />
+        <Avatar src={`${placeholder}?img=2`} />
+        <Avatar src={`${placeholder}?img=3`} />
+        <Avatar src={`${placeholder}?img=4`} />
+        <Avatar src={`${placeholder}?img=5`} />
+    </Card>
 
-<div class='mb-6'></div>
-
-<!-- Sizes -->
-<Card>
-    <h2>Sizes</h2>
-    <div class='flex justify-evenly items-center'>
-        <Avatar size='sm' />
-        <Avatar size='md' />
-        <Avatar size='lg' />
-        <Avatar size='xl' />
-        <br>
-        <Avatar size='sm' src='https://i.pravatar.cc/512' />
-        <Avatar size='md' src='https://i.pravatar.cc/512' />
-        <Avatar size='lg' src='https://i.pravatar.cc/512' />
-        <Avatar size='xl' src='https://i.pravatar.cc/512' />
-    </div>
-</Card>
+</section>
 
 <!-- Usage -->
 <h3 class="text-2xl font-bold my-4">Usage</h3>
