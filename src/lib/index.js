8abe183e6777521e04cb46b45bf7fc77ea3753be
endigo9740 <gundamx9740@gmail.com>
2022-03-07 12:02:00 -0600

    ...

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ export { default as Card } from "./Card/Card.svelte";
 export { default as Divider } from "./Divider/Divider.svelte";
 export { default as LogoCloud } from "./LogoCloud/LogoCloud.svelte";
 export { default as Logo } from "./LogoCloud/Logo.svelte";
+export { default as Avatar } from "./Avatar/Avatar.svelte";
