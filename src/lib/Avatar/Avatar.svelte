dffc193f471bb28236a7709fe8378d36f471a27d
endigo9740 <gundamx9740@gmail.com>
2022-03-08 16:44:55 -0600

    Fix avatar display issue in Firefox

--- src/lib/Avatar/Avatar.svelte
+++ src/lib/Avatar/Avatar.svelte
@@ -6,33 +6,36 @@
 	export let filter: string = ''; // ex: #myFilterId
 	
 	const cBase: string = 'flex aspect-square overflow-hidden text-surface-500 font-semibold justify-center items-center rounded-full bg-surface-300 dark:bg-surface-700';
-	let style: string = '';
+	let currentClasses: string = '';
+	let currentStyles: string = '';
 
 	// Set Outlined
 	if (outlined) {
-		style += ' outline outline-2 outline-offset-2 outline-primary-600';
+		currentClasses += ' outline outline-2 outline-offset-2 outline-primary-600';
 	}
 	// Set hover enabled
-    if(hover) {
-		style += ' hover:outline hover:outline-primary-600 outline-offset-2 cursor-pointer transition-all';
+    if (hover) {
+		currentClasses += ' hover:outline hover:outline-primary-600 outline-offset-2 cursor-pointer transition-all';
 	}
 	// Set size
 	switch (size) {
-		case 'sm': style += ' w-8 h-8 text-base'; break;
-		case 'md': style += ' w-12 h-12 text-lg'; break;
-		case 'lg': style += ' w-16 h-16 text-2xl'; break;	
-		case 'xl': style += ' w-24 h-24 text-4xl'; break;
-		case '2xl': style += ' w-32 h-32 text-4xl'; break;
-		case '3xl': style += ' w-40 h-40 text-4xl'; break;
-		default: style += 'w-full text 4-xl';
+		case 'sm': currentClasses += ' w-8 h-8 text-base'; break;
+		case 'md': currentClasses += ' w-12 h-12 text-lg'; break;
+		case 'lg': currentClasses += ' w-16 h-16 text-2xl'; break;	
+		case 'xl': currentClasses += ' w-24 h-24 text-4xl'; break;
+		case '2xl': currentClasses += ' w-32 h-32 text-4xl'; break;
+		case '3xl': currentClasses += ' w-40 h-40 text-4xl'; break;
+		default: currentClasses += 'w-full text 4-xl';
 	}
+	// Set Filter
+	if (filter) { currentStyles = `filter: url(${filter})`; }
 	
-	$: classes = `${cBase} ${style} ${$$props.class}`;
+	$: classes = `${cBase} ${currentClasses} ${$$props.class}`;
 </script>
 
 <div on:click class="avatar {classes}">
 	{#if src}
-	<img class="w-full h-full object-cover" {src} style="filter: url({filter})" alt="Avatar" />
+	<img class="w-full h-full object-cover" {src} style="{currentStyles}" alt="Avatar" />
 	{:else}
 	<p data-testid="placeholder">A</p>
 	{/if}
