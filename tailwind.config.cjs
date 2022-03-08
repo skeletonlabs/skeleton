f9f3969d618ff09b15d7eda1aab2f9c8fd75ecf9
endigo9740 <gundamx9740@gmail.com>
2022-03-08 11:11:14 -0600

    Custom font Inter added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
-const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		}
+	},
 	plugins: [
 		// Utilize our Skeleton Tailwind plugin
 		require('./src/lib/tailwind.cjs')
