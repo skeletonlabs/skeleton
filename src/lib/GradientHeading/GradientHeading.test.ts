da6e82265ead03e5796295b5aab4c9d389c8e3ff
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-03-17 09:52:46 -0600

    Formatted test with Prettier + changed inst to name instance

--- src/lib/GradientHeading/GradientHeading.test.ts
+++ src/lib/GradientHeading/GradientHeading.test.ts
@@ -2,59 +2,53 @@
  * @vitest-environment jsdom
  */
 
- import { cleanup, render, screen } from '@testing-library/svelte'
- import { afterEach, describe, it, expect } from 'vitest';
- 
- import GradientHeading from '$lib/GradientHeading/GradientHeading.svelte';
+import { cleanup, render, screen } from '@testing-library/svelte';
+import { afterEach, describe, it, expect } from 'vitest';
 
- // These are used to validate that all switch cases in the component applies correctly.
+import GradientHeading from '$lib/GradientHeading/GradientHeading.svelte';
 
- const defaultDirection = 'l';
- const defaultSize      = '4xl';
+// These are used to validate that all switch cases in the component applies correctly.
 
- const sizes = [
-     'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl'
- ]
- const directions = [
-     't', 'b', 'l', 'r', 'tr', 'tl', 'br', 'bl'
- ]
- 
- describe('Card.svelte', () => {
- 
-     afterEach(() => cleanup())
-     
-     it('Renders', async () => {
-         render(GradientHeading);
-     })
+const defaultDirection = 'l';
+const defaultSize = '4xl';
 
-     it('Size style applied', ()=>{
-        sizes.forEach((item) =>{
-            const inst = render(GradientHeading, {size: item});
-            let el = screen.getByTestId('gradientHeading');
-            expect(el.getAttribute('class').includes(`font-${item}`))
-            inst.unmount();
-        })
-     })
+const sizes = ['sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl'];
+const directions = ['t', 'b', 'l', 'r', 'tr', 'tl', 'br', 'bl'];
 
-     it('Default size applied (invalid prop)', ()=>{
-         const instance = render(GradientHeading, {size: '2-xl'});
-         let el = screen.getByTestId('gradientHeading');
-         expect(el.getAttribute('class').includes(`font-${defaultSize}`));
-     })
+describe('Card.svelte', () => {
+	afterEach(() => cleanup());
 
-     it('Direction applied', ()=>{
-        directions.forEach((item) =>{
-            const inst = render(GradientHeading, {directions: item});
-            let el = screen.getByTestId('span');
-            expect(el.getAttribute('class').includes(`bg-gradient-to-${item}`));
-            inst.unmount();
-        })
-     })
+	it('Renders', async () => {
+		render(GradientHeading);
+	});
 
-     it('Default direction applied (invalid prop)', ()=>{
-        const instance = render(GradientHeading, {direction: 'top'});
-        let el = screen.getByTestId('span');
-        expect(el.getAttribute('class').includes(`bg-gradient-to-${defaultDirection}`))
-     })
- 
- })
+	it('Size style applied', () => {
+		sizes.forEach((item) => {
+			const instance = render(GradientHeading, { size: item });
+			let el = screen.getByTestId('gradientHeading');
+			expect(el.getAttribute('class').includes(`font-${item}`));
+			instance.unmount();
+		});
+	});
+
+	it('Default size applied (invalid prop)', () => {
+		const instance = render(GradientHeading, { size: '2-xl' });
+		let el = screen.getByTestId('gradientHeading');
+		expect(el.getAttribute('class').includes(`font-${defaultSize}`));
+	});
+
+	it('Direction applied', () => {
+		directions.forEach((item) => {
+			const instance = render(GradientHeading, { directions: item });
+			let el = screen.getByTestId('span');
+			expect(el.getAttribute('class').includes(`bg-gradient-to-${item}`));
+			instance.unmount();
+		});
+	});
+
+	it('Default direction applied (invalid prop)', () => {
+		const instance = render(GradientHeading, { direction: 'top' });
+		let el = screen.getByTestId('span');
+		expect(el.getAttribute('class').includes(`bg-gradient-to-${defaultDirection}`));
+	});
+});
