5289590453e4c055da9af3f3c4b218b11678cd10
endigo9740 <gundamx9740@gmail.com>
2022-03-31 10:36:54 -0500

    Divider component updates, doc sandbox added

--- src/lib/divider/Divider.svelte
+++ src/lib/divider/Divider.svelte
@@ -1,36 +1,44 @@
 <script lang="ts">
+	import { afterUpdate } from "svelte";
+
 	export let weight: number = 1;
-	export let display: string = 'solid'; // solid | dashed | dotted
+	export let variant: string = 'solid'; // solid | dashed | dotted
 	export let orientation: string = 'h'; // h (horizintal) | v (vertical)
 
 	// Define Base Classes
     const cBase = `border-surface-300 dark:border-surface-700`;
-
-	// Weight
 	const cWeight = {
 		1: {v: 'border-l', h: 'border-t'},
 		2: {v: 'border-l-2', h: 'border-t-2'},
 		4: {v: 'border-l-4', h: 'border-t-4'},
 		8: {v: 'border-l-8', h: 'border-t-8'},
 	};
+	let cVariant: string;
+	let cOrientation: string;
 
-    // Set Display
-	let cDisplay: string;
-	switch(display) {
-		case('dashed'): cDisplay = 'border-dashed'; break;
-		case('dotted'): cDisplay = 'border-dotted'; break;
-		default:        cDisplay = 'border-solid';
+    // Set Variant
+	function setVariant(): void {
+		switch(variant) {
+			case('dashed'): cVariant = 'border-dashed'; break;
+			case('dotted'): cVariant = 'border-dotted'; break;
+			default:        cVariant = 'border-solid';
+		}
 	}
 	
 	// Set Orientation
-	let cOrientation: string;
-	switch(orientation) {
-		case('v'): cOrientation = `border-0 ${cWeight[weight].v} h-full`; break;
-		default:   cOrientation = `border-0 ${cWeight[weight].h} border-t w-full`;
+	function setOrientation(): void {
+		switch(orientation) {
+			case('v'): cOrientation = `border-0 ${cWeight[weight].v} h-full`; break;
+			default:   cOrientation = `border-0 ${cWeight[weight].h} border-t w-full`;
+		}
 	}
 
-	// Maintain the current state of styles
-    $: classes = `${cBase} ${cDisplay} ${cOrientation} ${$$props.class}`;
+	// Reactive
+	afterUpdate(() => {
+		setVariant();
+		setOrientation();
+	});
+    $: classes = `${cBase} ${cVariant} ${cOrientation} ${$$props.class}`;
 </script>
 
 <hr class="divider {classes}" data-testid='divider' />
--- src/routes/components/dividers.svelte
+++ src/routes/components/dividers.svelte
@@ -8,10 +8,16 @@
     const tableProps: any = {
         columns: ['Prop', 'Type', 'Values', 'Default', 'Description'],
         data: [
+            ['variant', 'string', 'solid | dashed | dotted', 'solid', 'Visual style.'],
             ['weight', 'number', '1, 2, 4, 8', '1', 'Define the thickness.'],
-            ['display', 'string', 'solid | dashed | dotted', 'solid', 'Visual display style.'],
             ['orientation', 'string', 'h | v', 'h', 'Horizontal or vertical.'],
         ],
+    };
+
+    $: props = {
+        variant: 'solid',
+        weight: 1,
+        orientation: 'h'
     };
 </script>
 
@@ -24,33 +30,52 @@
         <CodeBlock language="javascript" code={`<script>import {Divider} from '@brainandbones/skeleton';</\script>`}></CodeBlock>
     </header>
 
-    <!-- Examples -->
-    <Card class="space-y-4">
-        <h4>Default</h4>
-        <Divider />
-        <h4>Dashed</h4>
-        <Divider display="dashed" />
-        <h4>Dotted</h4>
-        <Divider display="dotted" />
-        <h4>Weight</h4>
-        <Divider weight="{2}" />
-        <Divider weight="{4}" />
-        <Divider weight="{8}" />
-        <h4>Combined</h4>
-        <Divider weight="{4}" display="dashed" />
-        <Divider weight="{8}" display="dotted" />
-        <h4>Vertical</h4>
-        <div class="h-[100px] flex justify-evenly items-center">
-            <div>CONTENT X</div>
-            <Divider orientation="v" />
-            <div>CONTENT Y</div>
-        </div>
-    </Card>
-
-    <!-- Usage -->
+    <!-- Sandbox -->
     <section class="space-y-4">
-        <h2 class="text-2xl font-bold">Usage</h2>
-        <CodeBlock language="html" code={`<Divider weight={4} display="dashed" />`}></CodeBlock>
+        <div class="space-y-4 lg:space-y-0 lg:grid grid-cols-[2fr,1fr] gap-2">
+            <!-- Example -->
+			<Card class="space-y-4 flex justify-center items-center">
+                <div class="w-[75%] h-[100px] flex justify-evenly items-center">
+                    <svelte:component
+                        this={Divider}
+                        variant={props.variant}
+                        weight={props.weight}
+                        orientation={props.orientation}
+                    />
+                </div>
+            </Card>
+            <!-- Options -->
+            <Card class="space-y-4">
+                <!-- Variant -->
+				<label>
+                    <span>Variant</span>
+                    <select name="variant" id="variant" bind:value={props.variant}>
+                        <option value="solid">Solid</option>
+                        <option value="dashed">Dashed</option>
+                        <option value="dotted">Dotted</option>
+                    </select>
+                </label>
+                <!-- Weight -->
+				<label>
+                    <span>Weight</span>
+                    <select name="weight" id="weight" bind:value={props.weight}>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={4}>4</option>
+                        <option value={8}>8</option>
+                    </select>
+                </label>
+                <!-- Orientation -->
+				<label>
+                    <span>Orientation</span>
+                    <select name="orientation" id="orientation" bind:value={props.orientation}>
+                        <option value="h">Horizontal</option>
+                        <option value="v">Vertical</option>
+                    </select>
+                </label>
+            </Card>
+        </div>
+        <CodeBlock language="html" code={`<Divider variant="${props.variant}" weight=${props.weight} orientation=${props.orientation} />`}></CodeBlock>
     </section>
 
     <!-- Properties -->
