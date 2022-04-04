528748a70af1cbe3a44bf4ebdc3560ac76a22b2b
endigo9740 <gundamx9740@gmail.com>
2022-04-04 11:27:32 -0500

    Package index.js fixes

--- src/lib/index.js
+++ src/lib/index.js
@@ -13,6 +13,14 @@ export { default as ProgressBar } from "./Progress/ProgressBar.svelte";
 export { default as RadioGroup } from "./Radio/RadioGroup.svelte";
 export { default as RadioItem } from "./Radio/RadioItem.svelte";
 
-// Utilities
-// NOTE: use .js here as the compiled file will .js, not .ts
-export { BlueNight, Emerald, Nineties, Rustic, Summer84, XPro, GreenFall, Noir, NoirLight, filter } from "./Filters/filter.js";
+// Utility: Filters
+export { filter } from "./Filters/filter";
+export { default as BlueNight } from "./Filters/svg/BlueNight.svelte";
+export { default as Emerald } from "./Filters/svg/Emerald.svelte";
+export { default as Nineties } from "./Filters/svg/Nineties.svelte";
+export { default as Rustic } from "./Filters/svg/Rustic.svelte";
+export { default as Summer84 } from "./Filters/svg/Summer84.svelte";
+export { default as XPro } from "./Filters/svg/XPro.svelte";
+export { default as GreenFall } from "./Filters/svg/GreenFall.svelte";
+export { default as Noir } from "./Filters/svg/Noir.svelte";
+export { default as NoirLight } from "./Filters/svg/NoirLight.svelte";
