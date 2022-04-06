e86f5d97b1e39eb3aa57393504a2e4917baf1000
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-04-06 10:34:07 -0600

    Updated docs - changed warning to caution

--- src/lib/Alert/Alert.svelte
+++ src/lib/Alert/Alert.svelte
@@ -2,8 +2,6 @@
 import { onMount } from "svelte";
 
 export let dismissable: boolean = false;
-export let title: string = '';
-export let message: string = '';
 export let actionMessage: string = 'ActionMessage';
 export let outlined: boolean = false;
 export let color: string = '';
@@ -16,8 +14,8 @@ if(outlined) {
     switch(color){
         case('primary'):    { style = 'border-2 border-primary-500 bg-white-500'; break; }
         case('accent'):     { style = 'border-2 border-accent-500 bg-white-500'; break; }
-        case('warning'):    { style = 'border-2 border-orange-500 bg-white-500'; break; }
-        case('error'):      { style = 'border-2 border-warning-500 bg-white-500'; break; }
+        case('caution'):    { style = 'border-2 border-orange-500 bg-white-500'; break; }
+        case('warning'):      { style = 'border-2 border-warning-500 bg-white-500'; break; }
         default:            { style = 'border-2 border-surface-500 bg-white-500'; break; }
     }
 }
@@ -25,10 +23,10 @@ else {
     switch(color) {
     case('primary'):        { style = 'bg-primary-400'; break;}
     case('accent'):         { style = 'bg-accent-500'; break;}
-    case('warning'):        { style = 'bg-orange-400'; break;}
+    case('warning'):        { style = 'bg-orange-300'; break;}
     case('error'):          { style = 'bg-warning-400'; break;}
     default: {
-            if(!color) { style = 'bg-surface-200 dark:bg-surface-800'; break;}
+            if(!color) { style = 'bg-surface-300 dark:bg-surface-900'; break;}
             else { style = color; }
         }
     }
@@ -45,7 +43,7 @@ export function dismiss(){
     animClass = 'disappear';
     setTimeout(()=>{
             visible = false;
-    }, 450)
+    }, 250)
 }
 
 onMount(()=>{
@@ -60,29 +58,20 @@ $: classes = `${cBase} ${animClass} ${cStyle} ${$$props.class}`
 <div on:click on:mouseover on:focus class='alert {classes}'>
     {#if $$slots.icon}
     <!-- Icon slot -->
-    <div class='m-5 w-8'>
+    <div class='ml-6 mt-6 w-6 '>
         <slot name='icon'/>
     </div>
     {/if}
 
     <!-- Title + Message -->
-    <div class='flex flex-col w-full {textStyle}'>
-        <span class='m-4 ml-8'>
-            {#if title}
-            <h5>{title}</h5>
-            {:else}
+    <div class='flex flex-col w-full'>
+        <div class='m-4 ml-8 mt-6 font-semibold {textStyle}'>
             <slot name=title/>
-            {/if}
-        </span>
+        </div>
 
-        <span class='m-4 ml-8 mt-1 mb-6 {textStyle}'>
-            {#if message}
-            <p>{message}</p>
-            {:else}
-            <slot name='message' />
-            {/if}
-        </span>
-
+        <div class='m-4 ml-8 mt-2 mb-6 {textStyle}'>
+            <slot name='message'/>
+        </div>
     </div>
 
     <!-- Action Buttons -->
@@ -129,11 +118,11 @@ $: classes = `${cBase} ${animClass} ${cStyle} ${$$props.class}`
 
     .appear{
         animation: appear;
-        animation-duration: 0.2s;
+        animation-duration: 0.1s;
     }
 
     .disappear{
         animation: disappear;
-        animation-duration: 0.5s;
+        animation-duration: 0.3s;
     }
 </style>
--- src/routes/components/alerts.svelte
+++ src/routes/components/alerts.svelte
@@ -2,6 +2,7 @@
     import Alert from "$lib/Alert/Alert.svelte";
     import Card from "$lib/Card/Card.svelte";
     import Table from "$lib/_Table/Table.svelte"
+    import CodeBlock from "$lib/CodeBlock/CodeBlock.svelte"
 
     let icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z"/></svg>'
 
@@ -15,6 +16,9 @@
             ['from', 'string', '(class name)', 'from-primary-500', 'Insert the standard Tailwind "from-" class name'],
         ],
     };
+
+    let title = 'What`s new in Skeleton?';
+    let message = 'Check out the countless new features and components in the Skeleton Library.';
 
 </script>
 
@@ -30,14 +34,62 @@
     <section class="space-y-4">
         <Card class="space-y-4">
             <h6>Basic</h6>
+            <Alert>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
         </Card>
-        
+
+        <Card class="space-y-4">
+            <h6>Primary</h6>
+            <Alert color='primary'>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
+        </Card>
+
+        <Card class="space-y-4">
+            <h6>Outlined</h6>
+            <Alert color='accent' outlined>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
+        </Card>
+
+        <Card class="space-y-4">
+            <h6>Dismissable</h6>
+            <Alert color='accent' dismissable>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
+        </Card>
+
+        <Card class="space-y-4">
+            <h6>CustomAction</h6>
+            <Alert color='error' actionMessage='Check it out' actionFunction={()=>{window.location.href = '/'}}>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
+        </Card>
+
+        <Card class="space-y-4">
+            <h6>Icon</h6>
+            <Alert>
+                <svelte:fragment slot='icon'><span class='fill-slate-900 dark:fill-slate-300'>{@html icon}</span></svelte:fragment>
+                <svelte:fragment slot='title'>{title}</svelte:fragment>
+                <svelte:fragment slot='message'>{message}</svelte:fragment>
+            </Alert>
+        </Card>
+
     </section>
     
     <!-- Usage -->
     <section class="space-y-4">
         <h2>Usage</h2>
-        
+        <h6>Script</h6>
+        <CodeBlock language="javascript" code="{`<script>import Alert from '@brainandbones/skeleton';</\script>`}"></CodeBlock>
+        <h6>HTML</h6>
+        <CodeBlock language="html" code="{`<Alert color='primary' actionMessage='Go here' actionFunction={function}>\n   <svelte:fragment slot='title'>{title}</svelte:fragment> \n   <svelte:fragment slot='message'>{body}</svelte:fragment>\n</Alert>`}"></CodeBlock>
     </section>
     
     <!-- Properties -->
