c504379ebd616b81ed016ef47dcd6a3a79c5ac2e
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-04-13 10:30:42 -0600

    Revisions and improvements

--- src/lib/Badge/Badge.svelte
+++ src/lib/Badge/Badge.svelte
@@ -1,61 +1,65 @@
 <script lang="ts">
+import { compute_rest_props } from "svelte/internal";
+
+
 	export let variant: string = '';
 	export let background: string = 'bg-surface-400';
 	export let textColor: string = 'text-surface-900';
 	export let radius: string = 'rounded-lg';
+	export let icon: boolean = false;
 	
-	$: cBase = `${background} ${textColor} ${radius} m-2 text-sm flex flex-row self-center font-semibold w-min pl-2 pr-2`;
+	$: cBase = `${background} ${textColor} ${$$props.class} ${radius} m-2 p-[2px] text-xs flex flex-row self-center font-semibold`;
+	$: cTrail = 'w-3 self-center ml-2';
+	$: cLead = 'w-3 self-center mr-2';
+
+	$: cStyle = (icon) ? 'w-4 h-4 rounded-full justify-center items-center truncate p-[2px]' : 'pl-2 pr-2';
 
 </script>
-
+<!--Sub-script variant-->
 {#if variant == 'sub'}
-	<sub class="whitespace-nowrap {cBase} h-min">
+	<sub class="whitespace-nowrap {cBase} {cStyle} h-min">
 		{#if $$slots.lead}
-			<div class="w-3 self-center mr-2">
+			<div class="{cLead}">
 				<slot name="lead" />
 			</div>
 		{/if}
 		<slot />
 		{#if $$slots.trail}
-		<div class='ml-2'>
+		<div class="{cTrail}">
 			<slot name="trail" />
 		</div>
 		{/if}
 	</sub>
+<!--Super-script variant-->
 {:else if variant == 'sup'}
-	<sup class="whitespace-nowrap {cBase}">
+	<sup class="whitespace-nowrap {cBase} {cStyle}">
 		{#if $$slots.lead}
-			<div class="w-3 self-center mr-2">
+			<div class="{cLead}">
 				<slot name="lead" />
 			</div>
-		{/if}
+		{/if}		
 		<slot />
 		{#if $$slots.trail}
-		<div class='ml-2'>
+		<div class="{cTrail}">
 			<slot name="trail" />
 		</div>
 		{/if}
 	</sup>
-
-{:else if variant == 'icon'}
-<div>
-	<div class='flex rounded-full w-8 h-8 justify-center overflow-clip {background}'>
-		<div class='w-6 self-center'><slot /></div>
-	</div>
-</div>
+<!--Regular variant-->
 {:else}
-	<span class="whitespace-nowrap {cBase}">
+	<span class="whitespace-nowrap {cBase} {cStyle}">
 		{#if $$slots.lead}
-			<div class="w-3 self-center mr-2">
+			<div class="{cLead}">
 				<slot name="lead" />
 			</div>
 		{/if}
 		<slot />
 		{#if $$slots.trail}
-			<div class='ml-2'>
+			<div class="{cTrail}">
 				<slot name="trail" />
 			</div>		
 		{/if}
 	</span>
 {/if}
+
 
--- src/routes/components/badges.svelte
+++ src/routes/components/badges.svelte
@@ -2,8 +2,9 @@
 <script>
     import Card from "$lib/Card/Card.svelte";
     import Badge from "$lib/Badge/Badge.svelte";
-    $: showBadge = true;
-    let icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z"/></svg>'
+    import Avatar from "$lib/Avatar/Avatar.svelte";
+    import Button from "$lib/Button/Button.svelte"
+    let icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z"/></svg>'
 </script>
 
 <div class='space-y-8'>
@@ -12,38 +13,49 @@
         <div class='flex-row flex'>
             <Badge variant=''>Avatar</Badge>
             <Badge background='bg-primary-200'>Button</Badge>
-            <Badge background='bg-accent-300'>Card</Badge>
-            <Badge background='bg-purple-400'>CodeBlock</Badge>
+            <Badge background='bg-blue-400'>Card</Badge>
+            <Badge background='bg-warning-400'>CodeBlock</Badge>
         </div>
+   
+    </Card>
 
+    <Card class='space-y-4'>
         <h6>Leading slot</h6>
         <div class='flex-row flex'>
             <Badge variant=''>
                 Avatar
                 <svelte:fragment slot='lead'>{@html icon}</svelte:fragment>
             </Badge>
-            <Badge background='bg-primary-200'>Button</Badge>
-            <Badge background='bg-accent-300'>Card</Badge>
-            <Badge background='bg-purple-400'>CodeBlock</Badge>
         </div>
+    </Card>
+
+    <Card class='space-y-4'>
         <h6>Trailing slot</h6>
         <div class='flex-row flex'>
             <Badge background='bg-yellow-400'>Badge
                 <svelte:fragment slot='trail'>
-                    <button>X</button>
+                    {@html icon}
                 </svelte:fragment>
             </Badge>
         </div>
-        
-        <h6>Icon Variant</h6>
-        <div class='flex-row flex'>
-            <Badge variant='icon' background='bg-accent-400'>{@html icon}</Badge>
-        </div>
+    </Card>
 
+    <Card class='space-y-4'>
         <h6>Super/Subscript Position</h6>
-        <div class='flex-row flex'>
-            <h1>Hello</h1> <Badge variant='icon'>Badge</Badge>
+        <div class='flex-row flex items-center'>
+            <h4>Badge</h4> <Badge class='-ml-1' background='bg-accent-200' variant='sub'>New</Badge>
+            <h4>Hello</h4> <Badge class='-ml-1' background='bg-primary-200' variant='sup'>New</Badge>
+            <Button size='sm' variant='filled-accent'>Component</Button>
+            <Badge variant='sub' class='-ml-4 -mb-4'>5k</Badge>
+            
         </div>
+    </Card>
 
+    <Card class='space-y-4'>
+        <h6>Icon prop + added to components with custom margins</h6>
+        <div class='flex-row flex'>
+            <Avatar size='md' /> <Badge class='-ml-4 -mt-4' icon variant='sup' background='bg-warning-400'>2</Badge>
+            <Avatar size='md' /> <Badge class='-ml-4 -mb-4' icon variant='sub' background='bg-primary-400'>{@html icon}</Badge>
+        </div>
     </Card>
 </div>
