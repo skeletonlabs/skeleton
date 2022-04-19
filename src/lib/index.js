76212c4c4ea6f74917f6fb34d1bdb2aacff2f407
endigo9740 <gundamx9740@gmail.com>
2022-04-19 09:57:33 -0500

    Added new components to index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,20 @@
 export { default as Avatar } from "./Avatar/Avatar.svelte";
+export { default as Badge } from "./Badge/Badge.svelte";
 export { default as Breadcrumb } from "./Breadcrumb/Breadcrumb.svelte";
 export { default as Crumb } from "./Breadcrumb/Crumb.svelte";
+export { default as ConicGradient } from "./ConicGradient/ConicGradient.svelte";
 export { default as Button } from "./Button/Button.svelte";
 export { default as Card } from "./Card/Card.svelte";
 export { default as Divider } from "./Divider/Divider.svelte";
+export { default as Drawer } from "./Drawer/Drawer.svelte";
 export { default as GradientHeading } from "./GradientHeading/GradientHeading.svelte";
+export { default as LightSwitch } from "./LightSwitch/LightSwitch.svelte";
 export { default as List } from "./List/List.svelte";
 export { default as ListItem } from "./List/ListItem.svelte";
 export { default as LogoCloud } from "./LogoCloud/LogoCloud.svelte";
 export { default as Logo } from "./LogoCloud/Logo.svelte";
 export { default as ProgressBar } from "./Progress/ProgressBar.svelte";
+export { default as ProgressRadial } from "./Progress/ProgressRadial.svelte";
 export { default as RadioGroup } from "./Radio/RadioGroup.svelte";
 export { default as RadioItem } from "./Radio/RadioItem.svelte";
 
