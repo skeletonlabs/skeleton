37b1b052c569e7a302a3c325c5b114d137267ec0
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-04-19 17:08:42 -0600

    Added tick marks, custom color, front/behind value, etc

--- src/lib/RangeSlider/RangeSlider.svelte
+++ src/lib/RangeSlider/RangeSlider.svelte
@@ -4,9 +4,9 @@
     export let max: number = 1000;
     export let min: number = 0;
     export let step: number = 1;
+
     export let label: string = '';
     export let accent: string = '';
-    export let ticklist: string = '';
     export let size = '';
     export let showValueFront: boolean = false;
 
@@ -19,26 +19,45 @@
         default: { styleSize = 'h-[6px]'; break;}
     }
 
-    $: cBase = `${accent} ${styleSize} ${$$props.class}`;
+    $: cBase = `${accent} ${styleSize}`;
     $: flexStyle = showValueFront ? 'flex' : 'flex flex-row-reverse';
+
+    export let tickmarks = null;
 
 </script>
 
-<div>
+<div class="${$$props.class}">
     <label for="">{label}</label>
     <div class="flex gap-4 ${flexStyle}">
-        <p class='w-8'>{value}</p>     
-        <input
-        type="range"
-        class="rangeSlider {cBase} w-full h-[px] self-center"
-        list={ticklist} 
-        step={step}
-        bind:value={value}
-        min={min}
-        max={max} 
-        on:click 
-        on:change 
-        on:blur  
-        >
+        <p class='w-8'>{value}</p>
+        <div class='rangeContainer w-full'>
+            <input
+            type="range"
+            class="{cBase} rangeSlider rounding-full w-full h-[px] self-center" 
+            step={step}
+            bind:value={value}
+            min={min}
+            max={max} 
+            on:click 
+            on:change 
+            on:blur  
+            >
+            {#if tickmarks}
+            <div class="flex justify-between w-auto mt-1">
+                {#each tickmarks as item}
+                <span class="text-xs text-center">
+                    <div class="w-4">
+                        {item}
+                    </div></span>
+                {/each}
+            </div>
+            {/if}
+        </div> 
     </div>
 </div>
+
+<style lang="postcss">
+
+    .rangeSlider::-webkit-slider-runnable-track{ @apply appearance-none}
+    
+</style>
--- src/routes/components/range-sliders.svelte
+++ src/routes/components/range-sliders.svelte
@@ -3,14 +3,45 @@
 import Card from "$lib/Card/Card.svelte";
 import RangeSlider from "$lib/RangeSlider/RangeSlider.svelte";
 
+let tickLabels = ['0','20', "40", "60", "80", "100"];
+let ticks = ["|", "|", "|", "|", "|", "|"];
 </script>
 
 <div class="space-y-4">
+    <h1>TESTING</h1>
+
+    <p>DEFAULT</p>
 
     <section class="space-y-4">
         <Card>
-            <RangeSlider size="md" label="Label" accent="accent-pink-400" showValueFront>
+            <RangeSlider size="md" label="Label" showValueFront>
             </RangeSlider>
+        </Card>
+    </section>
+
+    <p>Custom Color + Value After</p>
+    <section class="space-y-4">
+        <Card>
+            <RangeSlider min={0} max={100} step={20} size="md" label="Label" accent="accent-pink-400">
+                
+            </RangeSlider>
+        </Card>
+    </section>
+
+    <p>Tickmarks/tick labels</p>
+    <section class="space-y-4">
+        <Card>
+            <RangeSlider tickmarks={tickLabels} min={0} max={100} step={20} size="md" label="Label" accent="accent-primary-400" showValueFront />
+            <RangeSlider tickmarks={ticks} min={0} max={100} step={20} size="md" label="Label" accent="accent-primary-400" showValueFront />
+        </Card>
+    </section>
+
+    <p>Sizes</p>
+    <section class="space-y-4">
+        <Card>
+            <RangeSlider size='sm' min={0} max={100} step={20} label="Small" accent="accent-primary-400" showValueFront />
+            <RangeSlider size='md' min={0} max={100} step={20} label="Medium" accent="accent-warning-400" showValueFront />
+            <RangeSlider size='lg' min={0} max={100} step={20} label="Large" accent="accent-primary-400" showValueFront />
         </Card>
     </section>
 
