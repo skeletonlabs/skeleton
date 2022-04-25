7299986f9bd4808d53f302bb7351294bf77d705a
Thomas Bunk Jespersen <52718757+thomasbjespersen@users.noreply.github.com>
2022-04-25 14:27:26 -0600

    Class changes and improvements

--- src/lib/SliderToggle/SliderToggle.svelte
+++ src/lib/SliderToggle/SliderToggle.svelte
@@ -2,31 +2,41 @@
     export let checked: boolean = false;
     export let offLabel: string = 'OFF';
     export let onLabel: string = 'ON';
+    export let color: string = 'bg-accent-700';
+    export let labelColor: string = '!text-white-100'
 
-    $: cSwitch = 'dark:bg-surface-700 bg-surface-300'
+    $: cSwitch = 'transition duration-[0.3s]'
     $: cSlider = 'dark:bg-surface-300 bg-surface-100'
 
 </script>
 
-<label class="switch flex relative {cSwitch} {checked ? 'on' : ''} transition duration-[0.3s]">
-
+<div class="wrapper flex">
+<label class="switch flex relative {cSwitch} {checked ? `${color}` : 'dark:bg-surface-700 bg-surface-300'}">
     <!-- Slider -->
-    <input type="checkbox" bind:checked class="hidden">
+    <input type="checkbox" 
+    bind:checked 
+    class="hidden ${$$props.class}"
+    on:click
+    on:mouseover
+    on:focus
+
+    >
     <span class="slider {cSlider}"></span>
 
     <!-- Inner labels -->
     {#if offLabel || onLabel}
     <div class="w-full flex {checked ? '' : 'flex-row-reverse'} transition">
-        <p class="text-surface-800 w-1/2 text-xs text-center self-center">{checked ? onLabel : offLabel}</p>
+        <p class="{labelColor} w-1/2 text-xs text-center self-center">{checked ? onLabel : offLabel}</p>
     </div>
     {/if}
-
 </label>
-
+<div class="self-center ml-4">
+    <slot/>
+</div>
+</div>
 
 <style lang="postcss">
     .switch { @apply w-16 h-8 rounded-full; }
-    .on { @apply bg-accent-500; }
     .slider { @apply cursor-pointer absolute top-0 left-0 right-0 bottom-0 w-[50%] scale-[75%] h-full rounded-full transition duration-[0.5s] }
     input:checked + .slider { @apply translate-x-8; }
 </style>
--- src/routes/components/slider-toggles.svelte
+++ src/routes/components/slider-toggles.svelte
@@ -3,13 +3,24 @@
 import SliderToggle from "$lib/SliderToggle/SliderToggle.svelte";
 import Card from "$lib/Card/Card.svelte";
 
+let toggleOnMes = 'Thanks!';
+let toggleOffMes = 'Toggle me to agree!';
+
+let checked = false;
+
 </script>
 
 <h1>TESTING</h1>
 <div>
 
     <Card>
-        <SliderToggle />
+        <SliderToggle bind:checked>
+        {#if checked}
+        {toggleOnMes}
+        {:else}
+        {toggleOffMes}
+        {/if}
+        </SliderToggle>
     </Card>
     
 </div>
