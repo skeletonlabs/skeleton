b82402062c7726adc446aa58f986ddfb8eea5a95
endigo9740 <gundamx9740@gmail.com>
2022-05-12 17:08:28 -0500

    Fix whitespace

--- src/lib/Accordion/AccordionItem.svelte
+++ src/lib/Accordion/AccordionItem.svelte
@@ -50,22 +50,22 @@
 {#if value === undefined}<p class="!text-warning-500">Item missing value property.</p>{:else}
 <div on:click class="accordion-item {classesAccordian} {$$props.class}" data-testid="accordion-item">
 
-        <!-- Title -->
-        <dt class="{classesTitle}" on:click={onToggle}>
-            <!-- Slot: Lead -->
-            {#if $$slots.lead}<div><slot name="lead"/></div>{/if}
-            <!-- Slot: Title -->
-            <div class="flex-auto"><slot name="title"/></div>
-            <!-- Caret -->
-            <div class="{classesIcon}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>
-            </div>
-        </dt>
-    
-        <!-- Description -->
-        {#if $active.includes(value)}
-        <dd class="{classesDesc}" transition:slide|local><slot name="description"/></dd>
-        {/if}
+    <!-- Title -->
+    <dt class="{classesTitle}" on:click={onToggle}>
+        <!-- Slot: Lead -->
+        {#if $$slots.lead}<div><slot name="lead"/></div>{/if}
+        <!-- Slot: Title -->
+        <div class="flex-auto"><slot name="title"/></div>
+        <!-- Caret -->
+        <div class="{classesIcon}">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>
+        </div>
+    </dt>
 
-    </div>
+    <!-- Description -->
+    {#if $active.includes(value)}
+    <dd class="{classesDesc}" transition:slide|local><slot name="description"/></dd>
+    {/if}
+
+</div>
 {/if}
