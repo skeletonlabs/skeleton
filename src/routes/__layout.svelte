79bb4dadae0d9e20611920a27968436a931baa0f
endigo9740 <gundamx9740@gmail.com>
2022-05-18 15:29:18 -0500

    Remove customization

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -85,7 +85,7 @@
 </svelte:head>
 
 <!-- Notification Components -->
-<Dialog backdrop="bg-primary-500/50" blur="backdrop-blur-sm" card="bg-primary-500" duration={250} />
+<Dialog />
 
 <!-- Filter SVG Components -->
 <Apollo />
