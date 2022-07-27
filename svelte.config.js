8cb3bf1b54852c90d53214acdba75e68c5aaec77
endigo9740 <gundamx9740@gmail.com>
2022-07-27 11:39:59 -0500

    Updated deps, migrate sveltekit to vitest config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import { configDefaults } from 'vitest/config'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -21,11 +20,7 @@ const config = {
 			fallback:'index.html', // index.html (SPA) | null (SSR)
 			precompress: false
 		}),
-		vite: {
-			test: {
-				exclude: [...configDefaults.exclude, './package', './build']
-			}
-		}
+		// vite: { ...moved to vite.config.js... }
 	}
 };
 
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { configDefaults } from 'vitest/config'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+    test: {
+        exclude: [...configDefaults.exclude, './package', './build']
+    }
+};
+
+export default config;
