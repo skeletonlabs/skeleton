00d4a0a45a812eeff7f0b4f033b70296ca0445e6
RubenPX <rubenmarchante@gmail.com>
2022-08-14 15:49:30 +0200

    document theme switcher

--- src/routes/utilities/themeswitcher.svelte
+++ src/routes/utilities/themeswitcher.svelte
@@ -1,29 +1,44 @@
 <script lang="ts">
     import CodeBlock from '$lib/CodeBlock/CodeBlock.svelte';
     import Card from "$lib/Card/Card.svelte";
-    import LightSwitch from "$lib/LightSwitch/LightSwitch.svelte";
+    import ThemeSwitcher from "$lib/ThemeSwitcher/ThemeSwitcher.svelte";
+
+    const presets = [
+        { name: 'Skeleton', colors: ['#10b981', '#6366f1', '#f43f5e'], surface: '#111827' }
+    ]
 </script>
 
 <div class="space-y-8">
 
     <!-- Header -->
     <header class="space-y-4">
-        <h1>Lightswitch</h1>
-        <p>Allows users to toggle their preferred theme preference between light, dark, and system. This uses <a href="https://tailwindcss.com/docs/dark-mode" target="_blank">Tailwind's dark mode</a> feature.</p>
-        <CodeBlock language="javascript" code={`import { LightSwitch } from '@brainandbones/skeleton';`}></CodeBlock>
+        <h1>ThemeSwitcher</h1>
+        <p>Allows users to toggle their preferred theme, color scheme. This uses <a href="https://tailwindcss.com/docs/dark-mode" target="_blank">Tailwind's dark mode</a> feature.</p>
+        <CodeBlock language="javascript" code={`import { ThemeSwitcher } from '@brainandbones/skeleton';`}></CodeBlock>
     </header>
 
     <!-- Examples -->
     <Card class="text-center py-10 overflow-visible">
-        <LightSwitch />
+        <ThemeSwitcher variants={presets} />
     </Card>
 
     <!-- Usage -->
     <section class="space-y-4">
         <h2>Usage</h2>
-        <CodeBlock language="html" code={`<LightSwitch select={true} open={false} origin="tl" />`}></CodeBlock>
-        <p>This modifies the class on your document's HTML tag.</p>
+        
+        <p>Theme files should stay in <code>static/themes/theme-{"{name}"}.css</code></p>
+        <CodeBlock language="js" code={`const presets = [
+    { name: 'Skeleton', colors: ['#10b981', '#6366f1', '#f43f5e'], surface: '#111827' }
+]`}></CodeBlock>
+        <CodeBlock language="html" code={`<ThemeSwitcher variants={presets} select={true} open={false} origin="tl" />`}></CodeBlock>
+
+        <p>Slide togle change en html body class</p>
         <CodeBlock language="html" code={`<html class="dark">`}></CodeBlock>
+
+        <p>Theme selector will load a css file in header</p>
+        <CodeBlock language="html" code={`<link rel="stylesheet" href='/themes/theme-name.css' />`}></CodeBlock>
+        
+
         <p>Please refer to Tailwind's official instructions to setup and configure your project for <a href="https://tailwindcss.com/docs/dark-mode" target="_blank">dark mode</a>.</p>
     </section>
     
