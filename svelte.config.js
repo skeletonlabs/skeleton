ded1efa595393f1f5a59705ffded70fc194f123e
Nik <nik@niktek.io>
2022-08-20 11:01:33 +1000

    Stripped tests

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import mm from 'micromatch';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
@@ -19,8 +21,13 @@ const config = {
 			assets: 'build',
 			fallback: 'index.html', // index.html (SPA) | null (SSR)
 			precompress: false
-		})
-		// vite: { ...moved to vite.config.js... }
+		}),
+		package: {
+			// strip test files from packaging
+			files: (filepath) => {
+				return !mm.contains(filepath, 'test');
+			}
+		}
 	}
 };
 
