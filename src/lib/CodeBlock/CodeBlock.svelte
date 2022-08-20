0cdf955e5eac01bcf3a9e47dd22c56c174ed1954
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-19 21:52:28 -0500

    Update CodeBlock.svelte
    Removed highlight.js and refactored to be functional with prismjs
    
    Additionally added the editable property

--- src/lib/CodeBlock/CodeBlock.svelte
+++ src/lib/CodeBlock/CodeBlock.svelte
@@ -1,29 +1,24 @@
 <script lang="ts">
-	import hljs from 'highlight.js';
+	import Prism from 'prismjs';
+	import loadLanguages from 'prismjs/components/';
 
 	export let language: string = 'plaintext';
 	export let code: string = '';
 	export let background: string = 'bg-neutral-900';
+	export let editable: boolean = false;
 
 	// Base Classes
 	let cBaseBlock: string = `text-surface-50 p-4 rounded`;
 	let cBaseHeader: string = 'text-xs opacity-50 pb-2';
 
-	function languageFormatter(lang: string): string {
-		if (lang === 'js') {
-			return 'javascript';
-		}
-		return lang;
-	}
+	if (language !== 'plaintext') loadLanguages([language]);
 
 	// Reactive Classes
 	$: classesBlock = `${cBaseBlock} ${background}`;
 </script>
 
-{#if language && code}
-	<div class="codeblock {classesBlock} {$$props.class}" data-testid="codeblock">
-		<header class={cBaseHeader}>{languageFormatter(language)}</header>
-		<!-- prettier-ignore -->
-		<pre class="whitespace-pre-wrap break-all text-sm"><code class="language-{language} outline-none" contenteditable spellcheck="false">{@html hljs.highlight(code, { language }).value || code}</code></pre>
-	</div>
-{/if}
+<div class="codeblock {classesBlock} {$$props.class}" data-testid="codeblock">
+	<header class={cBaseHeader}>{language}</header>
+	<!-- prettier-ignore -->
+	<pre class="whitespace-pre-wrap break-all text-sm"><code class="language-{language} outline-none" contenteditable={editable} spellcheck="false">{@html Prism.highlight(code, Prism.languages[language], language) || code}</code></pre>
+</div>
