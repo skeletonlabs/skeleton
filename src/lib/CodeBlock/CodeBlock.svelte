7806fdfa1ecd054a0c056516946d66a2df3ec746
endigo9740 <gundamx9740@gmail.com>
2022-09-04 12:34:05 -0500

    Added codeblock highlighter agnostic state var

--- src/lib/CodeBlock/CodeBlock.svelte
+++ src/lib/CodeBlock/CodeBlock.svelte
@@ -9,31 +9,32 @@
 	let cBaseBlock: string = `text-surface-50 p-4 rounded`;
 	let cBaseHeader: string = 'text-xs opacity-50 pb-2';
 
+	// Notify the template to use @html injection
+	let formatted: boolean = false;
+
 	// Allow shorthand 'js' alias for Javascript
 	function languageFormatter(lang: string): string {
-		if (lang === 'js') {
-			return 'javascript';
-		}
+		if (lang === 'js') { return 'javascript'; }
 		return lang;
 	}
 
 	// Trigger syntax highlighting if highlight.js is available
 	$: if ($storeHighlightJs !== undefined) {
-		// Apply Highlight.js syntaxt highlighting
 		code = $storeHighlightJs.highlight(code, { language }).value;
+		formatted = true;
 	}
 
 	// Reactive Classes
 	$: classesBlock = `${cBaseBlock} ${background}`;
 </script>
 
-<!-- NOTE: don't allow linting on this page as pre/code tags are particular about whitespace -->
+<!-- NOTE: don't allow linting as pre/code tags are particular about whitespace -->
 <!-- prettier-ignore-start -->
 
 {#if language && code}
 <div class="codeblock {classesBlock} {$$props.class}" data-testid="codeblock">
 <header class={cBaseHeader}>{languageFormatter(language)}</header>
-<pre class="whitespace-pre-wrap break-all text-sm"><code class="language-{language} outline-none" contenteditable spellcheck="false">{#if $storeHighlightJs !== undefined}{@html code}{:else}{code}{/if}</code></pre>
+<pre class="whitespace-pre-wrap break-all text-sm"><code class="language-{language} outline-none" contenteditable spellcheck="false">{#if formatted}{@html code}{:else}{code}{/if}</code></pre>
 </div>
 {/if}
 
