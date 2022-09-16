d86b89846328eb9ed497b3ad188458772b385dc4
Ak4zh <agwl.akash@gmail.com>
2022-09-16 10:19:33 +0530

    fixes store exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -45,11 +45,13 @@ export { default as CodeBlock } from './utilities/CodeBlock/CodeBlock.svelte';
 export { default as Drawer } from './utilities/Drawer/Drawer.svelte';
 
 // Dialog
-export { dialogStore } from './utilities/Dialog/stores.js';
+import { dialogStore } from './utilities/Dialog/stores.js';
+export { type DialogAlert, type DialogConfirm, type DialogPrompt } from './utilities/Dialog/stores';
 export { default as Dialog } from './utilities/Dialog/Dialog.svelte';
 
 // Toast
 export { toastStore } from './utilities/Toast/stores.js';
+export { type ToastMessage } from './utilities/Toast/stores';
 export { default as Toast } from './utilities/Toast/Toast.svelte';
 
 // LightSwitch
