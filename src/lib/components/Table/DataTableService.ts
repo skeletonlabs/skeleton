74afbd747df86191a3b93aa207cf462fd20ea668
Rhys <36913739+ryceg@users.noreply.github.com>
2022-09-24 17:33:11 +1000

    initial

--- src/lib/components/Table/DataTableService.ts
+++ src/lib/components/Table/DataTableService.ts
@@ -13,27 +13,28 @@ export function mapTableSource(keys: any[], object: any[]): any[] {
 }
 
 // Sort Handlers ---
-
-export function sortAscNumber(arr: any[], key: string): any[] {
-	return arr.sort((x, y) => x[key] - y[key]);
+type StringOrNumberKey<Type> = keyof {
+    [Key in keyof Type as Type[Key] extends string | number ? Key : never]: Type[Key];
 }
 
-export function sortDescNumber(arr: any[], key: string): any[] {
-	return arr.sort((x, y) => y[key] - x[key]);
+export function sort<Obj, Key extends StringOrNumberKey<Obj>>(arr: Obj[], key: Key, isDescending = false): Obj[] {
+    if (isDescending) return sortDesc(arr, key);
+    return sortAsc(arr, key);
 }
 
-export function sortAscString(arr: any[], key: any): any[] {
-	return arr.sort((x, y) => {
-		let a = String(x[key]).toUpperCase(),
-			b = String(y[key]).toUpperCase();
-		return a == b ? 0 : a > b ? 1 : -1;
-	});
+export function sortAsc<Obj, Key extends StringOrNumberKey<Obj>>(arr: Obj[], key: Key): Obj[] {
+    return arr
+        .sort((x, y) =>
+            typeof x[key] === "string" && typeof y[key] === "string"
+                ? (String(x[key])).localeCompare(String(y[key]))
+                : (x[key] as number) - (y[key] as number));
 }
 
-export function sortDescString(arr: any[], key: any): any[] {
-	return arr.sort((x, y) => {
-		let a = String(x[key]).toUpperCase(),
-			b = String(y[key]).toUpperCase();
-		return a == b ? 0 : a > b ? -1 : 1;
-	});
+export function sortDesc<Obj, Key extends StringOrNumberKey<Obj>>(arr: Obj[], key: Key): Obj[] {
+    return arr
+        .sort((x, y) =>
+            typeof y[key] === "string" && typeof x[key] === "string"
+                ? (String(y[key])).localeCompare(String(x[key]))
+                : (y[key] as number) - (x[key] as number));
 }
+
