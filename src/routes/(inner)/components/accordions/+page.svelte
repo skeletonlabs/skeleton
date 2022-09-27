fa23456f27d87ffaa95390e695058372300366d3
Benzara Tahar <benzara.tahar@gmail.com>
2022-09-27 22:34:27 +0100

    Add Accordion a11y keyboard shortcuts documentation

--- src/routes/(inner)/components/accordions/+page.svelte
+++ src/routes/(inner)/components/accordions/+page.svelte
@@ -36,6 +36,14 @@
 			['contentId', 'Provide semantic ID for ARIA content element.']
 		]
 	};
+	const tableKeys: any = {
+		headings: ['Keys', 'Description'],
+		source: [
+			['<kbd>Tab</kbd>', 'Moves focus to the next focusable element.<br>All focusable elements in the accordion are included in the page <kbd>Tab</kbd> sequence.'],
+			['<kbd>Shift + Tab</kbd> ', 'Moves focus to the previous focusable element.<br>All focusable elements in the accordion are included in the page <kbd>Tab</kbd> sequence.'],
+			['<kbd>Space</kbd> or <kbd>Enter</kbd>', 'When focus is on the accordion header of a collapsed section, expands the section.']
+		]
+	};
 </script>
 
 <div class="space-y-8">
@@ -160,5 +168,7 @@
 			<a href="https://www.w3.org/WAI/ARIA/apg/example-index/accordion/accordion" target="_blank">ARIA Guidelines</a>
 		</div>
 		<DataTable headings={tableA11yItem.headings} source={tableA11yItem.source} />
+		<h3>Keyboard Interactions</h3>
+		<DataTable headings={tableKeys.headings} source={tableKeys.source} />
 	</section>
 </div>
