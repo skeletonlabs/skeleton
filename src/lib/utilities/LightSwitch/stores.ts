e5b0cbbc1ed8b47e3d6d3c5e1d4f0465d18a0da8
endigo9740 <gundamx9740@gmail.com>
2022-10-04 13:54:17 -0500

    Documented new LightSwitch stores

--- src/lib/utilities/LightSwitch/stores.ts
+++ src/lib/utilities/LightSwitch/stores.ts
@@ -1,5 +1,8 @@
 import type { Writable } from 'svelte/store';
 import { localStorageStore } from '$lib/utilities/LocalStorageStore/LocalStorageStore';
 
-// Persist Dark Mode - false: light | true: dark
+// OS Prefers Dark Scheme - TRUE: dark | FALSE: light
+export const storePrefersDarkScheme: Writable<boolean> = localStorageStore('storePrefersDarkScheme', false);
+
+// User Selected Mode - TRUE: dark | FALSE: light | undefined: use system preference
 export const storeLightSwitch: Writable<boolean | undefined> = localStorageStore('storeLightSwitch', undefined);
