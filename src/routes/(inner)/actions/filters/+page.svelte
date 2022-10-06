1c12ee7c90922de69722877e539eb1eeaf798022
endigo9740 <gundamx9740@gmail.com>
2022-10-06 17:46:52 -0500

    Svelte Action docs convertted

--- src/routes/(inner)/actions/filters/+page.svelte
+++ src/routes/(inner)/actions/filters/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	// Utilities
+	import DocsShell from '$docs/DocsShell/DocsShell.svelte';
+	import { DocsFeature, type DocsShellSettings, type DocsShellTable } from '$docs/DocsShell/types';
+
 	import CodeBlock from '$lib/utilities/CodeBlock/CodeBlock.svelte';
 	// Actions
 	import { filter } from '$lib/actions/Filters/filter';
@@ -14,14 +16,29 @@
 	import Summer84 from '$lib/actions/Filters/svg-filters/Summer84.svelte';
 	import XPro from '$lib/actions/Filters/svg-filters/XPro.svelte';
 
-	// Placeholder Image
+	// Docs Shell
+	const settings: DocsShellSettings = {
+		feature: DocsFeature.Action,
+		name: 'Filters',
+		description:
+			'Applies a set of unique <a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/filter" target="_blank">SVG filters</a>. Best used with images, but also available to select components, such as Avatars.',
+		imports: ['filter'],
+		source: 'actions/Filters'
+		// dependencies: [{ label: 'Highlight.js', url: 'https://highlightjs.org/' }],
+	};
+	const properties: DocsShellTable[] = [
+		{
+			label: 'Parameter Keys',
+			headings: ['Key', 'Type', 'Required', 'Description'],
+			source: [['<code>default</code>', 'string', '&check;', 'The name of the filter, such as "Apollo".']]
+		}
+	];
+
+	// Local
 	let imgPlaceholder = 'https://i.pravatar.cc/512?img=5';
 </script>
 
-<!--
-Filter SVG Components
-NOTE: Keep these localized
--->
+<!-- NOTE: Keep Filter SVG Components localized -->
 <Apollo />
 <BlueNight />
 <Emerald />
@@ -32,100 +49,72 @@ NOTE: Keep these localized
 <Summer84 />
 <XPro />
 
-<div class="!mt-0 space-y-8">
-	<!-- Header -->
-	<header class="space-y-4">
-		<h1>Filters</h1>
-		<p>
-			Applies a set of unique <a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/filter" target="_blank">SVG filters</a>. Best used with images, but also available to select components,
-			such as Avatars.
-		</p>
-		<CodeBlock language="javascript" code={`import { filter } from '@brainandbones/skeleton';`} />
-	</header>
+<DocsShell {settings} {properties}>
+	<!-- Slot: Sandbox -->
+	<svelte:fragment slot="sandbox">
+		<section class="card card-body grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} alt="example" />
+				<code>Original</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'Emerald'} alt="example" />
+				<code>Emerald</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'BlueNight'} alt="example" />
+				<code>BlueNight</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'XPro'} alt="example" />
+				<code>XPro</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'Summer84'} alt="example" />
+				<code>Summer84</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'Rustic'} alt="example" />
+				<code>Rustic</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'Apollo'} alt="example" />
+				<code>Apollo</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'GreenFall'} alt="example" />
+				<code>GreenFall</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'Noir'} alt="example" />
+				<code>Noir</code>
+			</div>
+			<div class="flex flex-col items-center space-y-4 overflow-hidden">
+				<img class="bg-surface-900 aspect-square w-full rounded-lg" src={imgPlaceholder} use:filter={'NoirLight'} alt="example" />
+				<code>NoirLight</code>
+			</div>
+		</section>
+	</svelte:fragment>
 
-	<!-- Examples -->
-	<div class="card card-body grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
-		<h2 class="sr-only">Examples</h2>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} alt="example" />
-			<code>Original</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'Emerald'} alt="example" />
-			<code>Emerald</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'BlueNight'} alt="example" />
-			<code>BlueNight</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'XPro'} alt="example" />
-			<code>XPro</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'Summer84'} alt="example" />
-			<code>Summer84</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'Rustic'} alt="example" />
-			<code>Rustic</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'Apollo'} alt="example" />
-			<code>Apollo</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'GreenFall'} alt="example" />
-			<code>GreenFall</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'Noir'} alt="example" />
-			<code>Noir</code>
-		</div>
-		<div class="flex flex-col items-center space-y-4">
-			<img class="bg-surface-900 aspect-square w-full rounded-lg overflow-hidden" src={imgPlaceholder} use:filter={'NoirLight'} alt="example" />
-			<code>NoirLight</code>
-		</div>
-	</div>
-
-	<!-- Usage -->
-	<section class="space-y-4">
-		<h2>Usage</h2>
-		<h4>Import Filter Components</h4>
-		<p>Add within scope of your filtered element. For global scope, add the following to your app's root component. Only import the filters you plan to use.</p>
-		<CodeBlock language="javascript" code={`import { Emerald, BlueNight, /* ... */  } from '@brainandbones/skeleton';`} />
-		<CodeBlock language="html" code={`<Emerald />\n<BlueNight />`} />
-		<p>You'll then need to apply the filter using one of the two techniques listed below.</p>
-		<h3>Option 1: Use the Svelte Action</h3>
-		<p>
-			Use the following <a href="https://svelte.dev/tutorial/actions" target="_blank">Svelte action</a> to to filter any element.
-		</p>
-		<CodeBlock language="html" code={`<img src={myImageSrc} use:filter={'BlueNight'}>`} />
-		<h3>Option 2: Using Only CSS</h3>
-		<p>Alternatively you may apply filters using CSS. This is what the action does for you. Note the hash mark is required.</p>
-		<CodeBlock language="html" code={`<img src={myImageSrc} style="filter: url({'#Emerald'})">`} />
-	</section>
-
-	<!-- Browser support -->
-	<section class="space-y-4">
-		<h2>Browser Support</h2>
-		<p>
-			Please be aware that <a href="https://caniuse.com/css-filters" target="_blank">browser support</a> varies. Some filters are not functional in Safari, while Firefox is not supported at all. We recommend
-			using progressive enhancement when using filters in non-supported browsers.
-		</p>
-		<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-			<div class="card card-body !bg-emerald-500 text-center">
-				<h4>Chrome: Full</h4>
-			</div>
-			<div class="card card-body !bg-emerald-500 text-center">
-				<h4>Edge: Full</h4>
-			</div>
-			<div class="card card-body !bg-orange-500 text-center">
-				<h4>Safari: Partial</h4>
-			</div>
-			<div class="card card-body !bg-rose-500 text-center">
-				<h4>Firefox: None</h4>
-			</div>
-		</div>
-	</section>
-</div>
+	<!-- Slot: Usage -->
+	<svelte:fragment slot="usage">
+		<section class="space-y-4">
+			<h2>Import Filter Components</h2>
+			<p>Add within scope of your filtered element. For global scope, add the following to your app's root component. Only import the filters you plan to use.</p>
+			<CodeBlock language="javascript" code={`import { Emerald, BlueNight, /* ... */  } from '@brainandbones/skeleton';`} />
+			<CodeBlock language="html" code={`<Emerald />\n<BlueNight />`} />
+		</section>
+		<section class="space-y-4">
+			<h2>Applying a Filter</h2>
+			<p>Use of of the two menthods below to apply your desired filter.</p>
+			<h3>Via the Svelte Action</h3>
+			<p>
+				Use the following <a href="https://svelte.dev/tutorial/actions" target="_blank">Svelte action</a> to to filter any element.
+			</p>
+			<CodeBlock language="html" code={`<img src={myImageSrc} use:filter={'BlueNight'}>`} />
+			<h3>Via Style Attribute</h3>
+			<p>Alternatively you may apply filters using CSS. This is what the action does for you. Note the hash mark is required.</p>
+			<CodeBlock language="html" code={`<img src={myImageSrc} style="filter: url({'#Emerald'})">`} />
+		</section>
+	</svelte:fragment>
+</DocsShell>
