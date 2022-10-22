27dfca6d0deb5dfd17a16e14af7b0bf71850d9db
Nik <nik@niktek.io>
2022-10-23 10:00:31 +1100

    Added JSDocs Tag and Description parser

--- src/docs/DocsShell/sveldMapper.ts
+++ src/docs/DocsShell/sveldMapper.ts
@@ -52,3 +52,51 @@ export function sveldeMapperEvents(sveld: Sveld): any {
 		})
 	};
 }
+
+export function parseDescriptionAndTagsFromJSDocs(jsDocs: string) {
+	let description: string = '';
+	let tags: { [key: string]: string } = {};
+	let capturingDescription = true;
+	let currentTag = '';
+	let currentCapture = '';
+	let currentChar = '';
+
+	for (let x = 0; x < jsDocs.length; x++) {
+		currentChar = jsDocs[x];
+		switch (currentChar) {
+			case '@':
+				//tag at front wipes out description
+				if (capturingDescription && x < 1) {
+					capturingDescription = false;
+				}
+				// normal capture of leading description till a tag
+				if (capturingDescription && currentCapture.length > 1) {
+					description = currentCapture;
+					currentCapture = '';
+					capturingDescription = false;
+				}
+				//two tags in a row, stash the first before capturing name of second below.
+				if (currentTag.length > 1) {
+					tags[currentTag] = currentCapture;
+					currentTag = '';
+					currentCapture = '';
+				}
+				//at start of tag, seek till space char for tag name
+				while (x != jsDocs.length && currentChar != ' ') {
+					currentTag += currentChar;
+					x++;
+					currentChar = jsDocs[x];
+				}
+				break;
+			default:
+				// capturing value of description or tag
+				currentCapture += currentChar;
+				break;
+		}
+	}
+	//if a tag finishes the jsDoc string
+	if (currentTag.length) {
+		tags[currentTag] = currentCapture;
+	}
+	return [description, tags];
+}
