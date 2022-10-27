10f6c5f90ff875e7ffb8a8bf218d39527b52ef7f
endigo9740 <gundamx9740@gmail.com>
2022-10-27 13:05:51 -0500

    Revised and expanded the Documentation contributor guide

--- src/routes/(inner)/docs/contributions/documentation/+page.svelte
+++ src/routes/(inner)/docs/contributions/documentation/+page.svelte
@@ -15,91 +15,165 @@
 	<!-- Header -->
 	<header class="space-y-4">
 		<div class="grid grid-cols-[1fr_auto] gap-4">
-			<div class="space-y-2">
+			<div class="space-y-4">
 				<h1>Documentation</h1>
+				<p>
+					Below is an introduction to automated documentation features, using tools like <a href="https://github.com/carbon-design-system/sveld" target="_blank" rel="noreferrer">Sveld</a>. First we'll
+					learn how to document component props, slots, and events. Then we'll cover how to provide this information to the documentation pages themselves.
+				</p>
 			</div>
-			<div class="place-self-end">
-				<a class="btn btn-filled-primary" href="https://github.com/carbon-design-system/sveld" target="_blank" rel="noreferrer">Sveld</a>
+			<div class="place-self-end grid grid-cols-1 gap-2">
+				<a class="btn btn-filled-primary" href="https://github.com/carbon-design-system/sveld" target="_blank" rel="noreferrer">Sveld Documentation</a>
 			</div>
 		</div>
-		<p>Good documentation on both the website and via Intellisense makes for a better developer experience and also helps to reduce the support load for our community.</p>
-		<p>
-			Fortunately, through tools like <a href="https://github.com/carbon-design-system/sveld" target="_blank" rel="noreferrer">Sveld</a> and
-			<a href="https://tsdoc.org" target="_blank" rel="noreferrer">TSDoc</a>
-			an increasing amount of this is automated for you for things like documenting the props, events and slots of your component. So lets jump into getting your component documented!
-		</p>
 	</header>
+
 	<hr />
-	<!-- TSDocs comments -->
+
+	<!-- Intro -->
 	<section class="space-y-4">
-		<h2>Adding TSDoc Annotations</h2>
+		<h2>Components</h2>
 		<p>
-			The simplest way to start documenting your component is to add TSDoc style comment blocks immediately prior to your exported props. Where the intializer value of your prop is computed or derived
-			from a context, an additional <code>@type</code> tag is needed to help Sveld determine the type.
+			Sveld makes use of <a href="https://tsdoc.org" target="_blank" rel="noreferrer">TSDoc tags</a> (a superset of JSDocs) to to generate component documenting from within our component code itself.
+			This comes with the added benefit of providing additional <a href="https://code.visualstudio.com/docs/editor/intellisense" target="_blank" rel="noreferrer">Intellesense</a> features to aid developers
+			implementing Skeleton features in tools like VS Code.
 		</p>
-		<Alert>Sveld doesn't support generics at this point, so please note the alternate way we have specified the type within the <code>@type</code> tag.</Alert>
+	</section>
+
+	<!-- TSDocs -->
+	<section class="space-y-4">
+		<h3>Props</h3>
+		<p><a href="https://github.com/carbon-design-system/sveld#type" target="_blank" rel="noreferrer">Sveld Reference</a></p>
+		<p>
+			To document component props, simply add TSDoc description comments using the <code>/** ... */</code> format. In most use cases Sveld will automatically parse all relevant information - including
+			the prop name, type, value, and your provided description.
+		</p>
 		<CodeBlock
 			language="js"
-			code={`// Props (Listbox)
-/** Provide a writable store to maintain list selection.
- * @type {writeable(any) | writable(any[])}
- */
-export let selected: Writable<any> = writable(undefined);
+			code={`
 /** Provide classes to set vertical item spacing. */
-export let space: string = 'space-y-1';`}
+export let space: string = 'space-y-1';
+			`}
 		/>
 		<p>
-			Slot documentation is provided by placing a TSDoc comment block at the top (by convention) of your script block. The default slot is not able to have a description applied at this point, but if
-			it would be helpful to end developers to describe it, consider adding it to the description of the component.
+			For non-primitive or custom types, you may need to specify this information. This can be accomplished using the <code>@type</code> tag with block-style comments. Specify the type in curly brackets
+			immediately following the tag.
 		</p>
-		<Alert
-			>The hyphen between the name of the named slot and the description as well as the preceding <code>//Slots</code> comment block, are both critical for the Sveld parser to successfully extract the
-			information.</Alert
-		>
 		<CodeBlock
 			language="js"
-			code={`//Slots
+			code={`
+/**
+ * Bind this to your form data, represents the "files" data from the input.
+ * @type {FileList}
+ */
+export let files: FileList;
+			`}
+		/>
+		<p>
+			Ensure you document Context API <code>getContext</code> values to provide Intellesense. However, we intentionally exclude these values from the Props table.
+		</p>
+		<CodeBlock
+			language="js"
+			code={`
+/** Provide classes to set the hover background color. */
+export let hover: string = getContext('hover');
+			`}
+		/>
+		<p>For additonal examples, reference reference any existing components in the project.</p>
+	</section>
+
+	<!-- Slots -->
+	<section class="space-y-4">
+		<h3>Slots</h3>
+		<p><a href="https://github.com/carbon-design-system/sveld#slot" target="_blank" rel="noreferrer">Sveld Reference</a></p>
+		<p>
+			Slot documentation is provided by placing a TSDoc comment block at the top (by convention) of your script block. Note that Sveld does not currently support descriptions for the <code
+				>default</code
+			> slot at this time. Instead, we recommend you opt for Usage tab example and instruction to illustrate the use case of this slot.
+		</p>
+		<CodeBlock
+			language="js"
+			code={`
+// Slots
 /**
  * @slot lead - Provide a leading element, such as an icon.
  * @slot content - Provide the alert message text.
  */`}
 		/>
+		<Alert><strong>NOTE:</strong> The leading <code>// ...</code> comment is required for Sveld to successfully parse the slot descriptions. This is not optional.</Alert>
 	</section>
-	<p>Event documentation is created by adding a TSDoc block immediately prior to the dispatch of your event. Sveld will automatically detect forwarded events.</p>
-	<CodeBlock
-		language="js"
-		code={`/** @event {{ event: DragEvent }} dragover - When a file is dragged over the component. */
+
+	<!-- Events -->
+	<section class="space-y-4">
+		<h3>Events</h3>
+		<p><a href="https://github.com/carbon-design-system/sveld#event" target="_blank" rel="noreferrer">Sveld Reference</a></p>
+		<p>
+			Sveld will automatically document forwarded events. You should not attempt to document these. However, dispatched events may be documented similar to props - with a TSDocs comment applied
+			directly above the <code>dispatch()</code> method. Provide the event response in curly brackets, followed by the event name, a dash, and then the event description.
+		</p>
+		<CodeBlock
+			language="js"
+			code={`
+/** @event {{ event: DragEvent }} dragover - When a file is dragged over the component. */
 dispatch('dragover', event);`}
-		<hr
-	/>
-	<section class="space-y-4">
-		<h2>Creating the doc page</h2>
-		<p>Now that we have added all the information to the component, it's time to create the doc page that displays all of the information derived by Sveld.</p>
-		<p>
-			Grab the boilerplate template for a new documentation page from here <code>/src/routes/(inner)/template/+page.svelte</code> and copy it to the appropriate place with lowercase names and dashes
-			for the enclosing folder e.g. <code>/routes/components/your-new-component/+page.svelte</code>.
-		</p>
-		<p>
-			The key to getting all the Sveld information is to adjust the import of your component at the top of the file, ensuring that you preserve the <code>?raw&sveld</code> url params. Now we can pass
-			it into the
-			<code>DocsShellSettings.components</code> array.
-		</p>
-		<Alert>Sveld silently drops props that don't meet it's spec. Double check that they are all coming through as expected</Alert>
-		<p>
-			Unfortunately not everything can be automated, so for the rest of the information, you need to fill out the props on the <code>DocsShellSettings</code> be replacing the template defaults. It's
-			also a good idea to check other doc pages to see a concrete implementation. You can get info on the <code>DocsShellSettings</code> props from <kbd>Ctrl/⌘ + i</kbd> in the code, or from
-			referencing the
-			<a href="https://github.com/Brain-Bones/skeleton/blob/dev/src/docs/DocsShell/types.ts" target="_blank" rel="noreferrer">source</a>.
-		</p>
+		/>
 	</section>
+
+	<hr />
+
+	<!-- Documentation Pages -->
 	<section class="space-y-4">
-		<h2>Creating examples</h2>
+		<h2>Documentation Pages</h2>
+		<p>Now that our components are ready, it's time to create the documentation page that displays all of the information derived by Sveld.</p>
 		<p>
-			Examples generally come in two forms: a basic show of a component being used in various configurations e.g. <a href="components/accordions" target="_blank" rel="noreferrer">Accordion</a> OR
-			dynamic demos e.g.
-			<a href="/components/range-sliders">Range Sliders</a>. Which you choose is somewhat dependant on the component you have made, but at a minimum, you should show the common use cases, but
-			preferably look into maintaining the same level of examples as similar components.
+			We provide a boilerplate template here:<br /><code>/src/routes/(inner)/template/+page.svelte</code>.
 		</p>
-		<p>You should also have <code>CodeBlock</code> snippets showing actual code to pair with your examples to help end developers understand how to use your component.</p>
+		<p>Copy this to the appropriate file route location and use our recommend naming convention:<br />e.g. <code>/routes/components/your-new-component/+page.svelte</code>.</p>
+		<!-- Documentation Tables -->
+		<h3>Documentation Tables</h3>
+		<p>To populate each documentation table we'll need to import our Sveld documentation data. Follow the instruction below:</p>
+		<ol class="list-decimal list-inside space-y-1">
+			<li>Create a duplicate of your component import statement, e.g. <code>import Avatar from '$lib/components/Avatar/Avatar.svelte';</code></li>
+			<li>Implement a <code>// @ts-ignore</code> comment above the new import to silence any warnings.</li>
+			<li>Rename the import reference using the convention: <code>Avatar</code> -> <code>sveldAvatar</code>.</li>
+			<li>Append the following URL parameters to the end of your import statement, ex: <code>.../Accordion.svelte?raw&sveld</code>.</li>
+			<li>Finally, pass the import reference your DocShell settings like so: <code>components: [&#123; sveld: sveldAvatar &#125;]</code></li>
+		</ol>
+		<!-- DocShell Settings -->
+		<h3>DocShell Settings</h3>
+		<p>
+			We can provide settings to our DocShell component using <code>const settings: DocsShellSettings</code>. This allows you to populate all relevant settings on the page.
+		</p>
+		<section class="card card-body flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
+			<div class="space-y-2">
+				<p>Reference all available settings from the Typescript interface defintion.</p>
+			</div>
+			<a class="btn btn-filled-accent" href="https://github.com/Brain-Bones/skeleton/blob/dev/src/docs/DocsShell/types.ts#L42" target="_blank" rel="noreferrer">View Available Settings</a>
+		</section>
+		<p>
+			When adding a new keys, tap <kbd>Ctrl/⌘ + i</kbd> to see suggested values. Below are existing documentation pages we recommend you reference:
+		</p>
+		<ul class="list-disc list-inside space-y-1">
+			<li><a href="/elements/button">Buttons</a> showcases how to document Tailwind Element classes.</li>
+			<li><a href="/components/accordions">Accordion</a> makes use of most Component settings utilizing Sveld.</li>
+			<li><a href="/components/pagination">Accordion</a> uses Dipatched Event documentation.</li>
+			<li><a href="/utilities/tooltips">Tooltips</a> provides an excellent reference for Action params.</li>
+		</ul>
+		<!-- Examples -->
+		<h3>Examples</h3>
+		<p>When showcasing examples of new features we typically handle this by one of two methods:</p>
+		<ul class="list-disc list-inside space-y-1">
+			<li><strong>Sandbox</strong> (e.g. <a href="/components/range-sliders">Range Sliders</a>) - which provide an dynamic and interactive example that can be adjusted live.</li>
+			<li><strong>Static</strong> (e.g. <a href="components/accordions">Accordion</a>) - with multiple static examples displaying various configurations.</li>
+		</ul>
+		<p>Dynamic examples are preferred, but remember the overall goal is to showcase how the feature operates.</p>
+		<!-- Usage -->
+		<h3>Usage</h3>
+		<p>
+			In addition to examples, you should provide multiple use case demonstrations using <code>CodeBlock</code> snippets to help end developers understand how to make use of your new components and features.
+		</p>
+		<!-- Keyboard Interactions -->
+		<h3>Keyboard Interactions</h3>
+		<p>Finally, don't forget to document any relevant keyboard interactions for accessability. These can easily be provided via the DocShell settings.</p>
 	</section>
 </div>
