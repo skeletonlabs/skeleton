8a7c42bd955e477453cf3940a82234f2fa0a681f
endigo9740 <gundamx9740@gmail.com>
2022-11-09 17:48:23 -0600

    Minor patches

--- src/routes/(inner)/elements/tables/+page.svelte
+++ src/routes/(inner)/elements/tables/+page.svelte
@@ -10,7 +10,7 @@
 
 	// Docs Shell
 	const settings: DocsShellSettings = {
-		feature: DocsFeature.Component,
+		feature: DocsFeature.Element,
 		name: 'Tables',
 		description: 'Provides a set of styles for native HTML table elements.',
 		stylesheetIncludes: ['all', 'elements'],
@@ -29,12 +29,12 @@
 		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
 		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-		{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-		{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-		{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-		{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-		{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
+		// { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+		// { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+		// { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+		// { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+		// { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
 	];
 	const totalWeight = tableArr.reduce((accumulator, obj) => accumulator + obj.weight, 0);
 </script>
