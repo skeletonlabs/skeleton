7a520147e35174e7720f39b09290c533870a71c8
Rhys <36913739+ryceg@users.noreply.github.com>
2022-11-17 14:22:10 +1100

    Add keywords, closes #566

--- src/docs/DocsNavigation/links.ts
+++ src/docs/DocsNavigation/links.ts
@@ -1,6 +1,19 @@
 // Navigation Links & Sitemap
 
-export const menuNavLinks: any = [
+export type MenuNavLink = {
+	href: string;
+	label: string;
+	badge?: string;
+	keywords?: string;
+};
+
+export type MenuNavCategory = {
+	id: string;
+	title: string;
+	list: MenuNavLink[];
+};
+
+export const menuNavLinks: MenuNavCategory[] = [
 	// Docs ---
 
 	{
@@ -8,11 +21,11 @@ export const menuNavLinks: any = [
 		title: 'Docs',
 		list: [
 			{ href: '/docs/why', label: 'Why Skeleton' },
-			{ href: '/docs/faqs', label: 'FAQs', badge: 'New' },
-			{ href: '/docs/comparisons', label: 'Comparisons' },
-			{ href: '/docs/features', label: 'Features' },
-			{ href: '/docs/recommended', label: 'Recommended' },
-			{ href: '/docs/contributions', label: 'Contributions' }
+			{ href: '/docs/faqs', label: 'FAQs', badge: 'New', keywords: 'typescript package manager monorepo roadmap' },
+			{ href: '/docs/comparisons', label: 'Comparisons', keywords: 'mantine flowbite daisy material mui' },
+			{ href: '/docs/features', label: 'Features', keywords: 'benefits' },
+			{ href: '/docs/recommended', label: 'Recommended', keywords: '3rd party plugins' },
+			{ href: '/docs/contributions', label: 'Contributions', keywords: 'contribute pull request pr' }
 		]
 	},
 
@@ -22,11 +35,11 @@ export const menuNavLinks: any = [
 		id: 'guides',
 		title: 'Guides',
 		list: [
-			{ href: '/guides/install', label: 'Installation' },
-			{ href: '/guides/tailwind', label: 'Tailwind CSS' },
-			{ href: '/guides/themes', label: 'Themes' },
-			{ href: '/guides/styling', label: 'Styling' },
-			{ href: '/guides/frameworks', label: 'Frameworks' }
+			{ href: '/guides/install', label: 'Installation', keywords: 'tutorial create' },
+			{ href: '/guides/tailwind', label: 'Tailwind CSS', keywords: 'tutorial plugin' },
+			{ href: '/guides/themes', label: 'Themes', keywords: 'generator styling tutorial' },
+			{ href: '/guides/styling', label: 'Styling', keywords: 'tutorial customize customise override' },
+			{ href: '/guides/frameworks', label: 'Frameworks', keywords: 'tutorial sveltekit vite astro' }
 		]
 	},
 
@@ -41,21 +54,21 @@ export const menuNavLinks: any = [
 		id: 'base',
 		title: 'Base',
 		list: [
-			{ href: '/elements/core', label: 'Core' },
-			{ href: '/elements/typography', label: 'Typography' },
-			{ href: '/elements/forms', label: 'Forms' }
+			{ href: '/elements/core', label: 'Core', keywords: 'scrollbar horizontal rule divider' },
+			{ href: '/elements/typography', label: 'Typography', keywords: 'paragraph anchor blockquote pre code kbd ins del' },
+			{ href: '/elements/forms', label: 'Forms', keywords: 'kitchen sink preview' }
 		]
 	},
 	{
 		id: 'elements',
 		title: 'Elements',
 		list: [
-			{ href: '/elements/badges', label: 'Badges' },
-			{ href: '/elements/buttons', label: 'Buttons' },
-			{ href: '/elements/cards', label: 'Cards' },
-			{ href: '/elements/lists', label: 'Lists' },
-			{ href: '/elements/logo-clouds', label: 'Logo Clouds' },
-			{ href: '/elements/placeholders', label: 'Placeholders' },
+			{ href: '/elements/badges', label: 'Badges', keywords: 'chips heart icon' },
+			{ href: '/elements/buttons', label: 'Buttons', keywords: 'cta chips' },
+			{ href: '/elements/cards', label: 'Cards', keywords: 'info' },
+			{ href: '/elements/lists', label: 'Lists', keywords: 'select input' },
+			{ href: '/elements/logo-clouds', label: 'Logo Clouds', keywords: 'grid sponsors' },
+			{ href: '/elements/placeholders', label: 'Placeholders', keywords: 'loading' },
 			{ href: '/elements/tables', label: 'Tables' }
 		]
 	},
@@ -66,8 +79,8 @@ export const menuNavLinks: any = [
 		id: 'actions',
 		title: 'Actions',
 		list: [
-			{ href: '/actions/clipboard', label: 'Clipboard' },
-			{ href: '/actions/filters', label: 'Filters' }, // badge: 'Experimental'
+			{ href: '/actions/clipboard', label: 'Clipboard', keywords: 'copy paste' },
+			{ href: '/actions/filters', label: 'Filters', keywords: 'image img' }, // badge: 'Experimental'
 			{ href: '/actions/focus-trap', label: 'Focus Trap' }
 		]
 	},
@@ -75,27 +88,27 @@ export const menuNavLinks: any = [
 		id: 'components',
 		title: 'Components',
 		list: [
-			{ href: '/components/accordions', label: 'Accordions' },
-			{ href: '/components/alerts', label: 'Alerts' },
+			{ href: '/components/accordions', label: 'Accordions', keywords: 'details summary collapsible' },
+			{ href: '/components/alerts', label: 'Alerts', keywords: 'error warning info pop up' },
 			{ href: '/components/app-bar', label: 'App Bar' },
-			{ href: '/components/app-rail', label: 'App Rail' },
+			{ href: '/components/app-rail', label: 'App Rail', keywords: 'discord navigation vertical' },
 			{ href: '/components/app-shell', label: 'App Shell' },
-			{ href: '/components/avatars', label: 'Avatars' },
-			{ href: '/components/breadcrumbs', label: 'Breadcrumbs' },
-			{ href: '/components/conic-gradients', label: 'Conic Gradients' },
-			{ href: '/components/dividers', label: 'Dividers' },
-			{ href: '/components/file-buttons', label: 'File Buttons' },
-			{ href: '/components/file-dropzone', label: 'File Dropzone' },
+			{ href: '/components/avatars', label: 'Avatars', keywords: 'profile picture image img' },
+			{ href: '/components/breadcrumbs', label: 'Breadcrumbs', keywords: 'navigation link seperator' },
+			{ href: '/components/conic-gradients', label: 'Conic Gradients', keywords: 'charts circle loading pie spinner' },
+			{ href: '/components/dividers', label: 'Dividers', keywords: 'spacer seperator vertical' },
+			{ href: '/components/file-buttons', label: 'File Buttons', keywords: 'upload' },
+			{ href: '/components/file-dropzone', label: 'File Dropzone', keywords: 'upload drag drop' },
 			{ href: '/components/gradient-headings', label: 'Gradient Headings' },
-			{ href: '/components/listboxes', label: 'Listboxes' },
+			{ href: '/components/listboxes', label: 'Listboxes', keywords: 'select input' },
 			{ href: '/components/paginators', label: 'Paginators' },
-			{ href: '/components/progress-bars', label: 'Progress Bars' },
-			{ href: '/components/progress-radials', label: 'Progress Radials' },
-			{ href: '/components/radio-groups', label: 'Radio Groups' },
-			{ href: '/components/range-sliders', label: 'Range Sliders' },
-			{ href: '/components/slide-toggles', label: 'Slide Toggles' },
-			{ href: '/components/steppers', label: 'Steppers' },
-			{ href: '/components/tabs', label: 'Tabs' },
+			{ href: '/components/progress-bars', label: 'Progress Bars', keywords: 'charts loading indeterminate' },
+			{ href: '/components/progress-radials', label: 'Progress Radials', keywords: 'spinner charts loading indeterminate' },
+			{ href: '/components/radio-groups', label: 'Radio Groups', keywords: 'select input' },
+			{ href: '/components/range-sliders', label: 'Range Sliders', keywords: 'select input' },
+			{ href: '/components/slide-toggles', label: 'Slide Toggles', keywords: 'select input switch on off' },
+			{ href: '/components/steppers', label: 'Steppers', keywords: 'progress tutorial' },
+			{ href: '/components/tabs', label: 'Tabs', keywords: 'navigation link' },
 			{ href: '/components/tables', label: 'Tables' }
 			// Work in Progress
 			// { href: '/components/svg-icons', label: 'SVG Icons' }, // keep disabled until further notice
@@ -108,15 +121,15 @@ export const menuNavLinks: any = [
 		id: 'utilities',
 		title: 'Utilities',
 		list: [
-			{ href: '/utilities/codeblocks', label: 'Code Blocks' },
-			{ href: '/utilities/data-tables', label: 'Data Tables' },
-			{ href: '/utilities/modals', label: 'Modals', keywords: 'dialog' },
-			{ href: '/utilities/drawers', label: 'Drawers' },
-			{ href: '/utilities/lightswitches', label: 'Lightswitch' },
-			{ href: '/utilities/local-storage-stores', label: 'Local Storage Stores' },
-			{ href: '/utilities/menus', label: 'Menus' },
-			{ href: '/utilities/toasts', label: 'Toasts' },
-			{ href: '/utilities/tooltips', label: 'Tooltips' }
+			{ href: '/utilities/codeblocks', label: 'Code Blocks', keywords: 'format coding js ts python html' },
+			{ href: '/utilities/data-tables', label: 'Data Tables', keywords: 'sort' },
+			{ href: '/utilities/modals', label: 'Modals', keywords: 'dialog pop up confirm alert prompt' },
+			{ href: '/utilities/drawers', label: 'Drawers', keywords: 'overlay pop up tray' },
+			{ href: '/utilities/lightswitches', label: 'Lightswitch', keywords: 'dark mode' },
+			{ href: '/utilities/local-storage-stores', label: 'Local Storage Stores', keywords: 'persistent pub sub storage save' },
+			{ href: '/utilities/menus', label: 'Menus', keywords: 'navigation link' },
+			{ href: '/utilities/toasts', label: 'Toasts', keywords: 'pop up alert notification queue' },
+			{ href: '/utilities/tooltips', label: 'Tooltips', keywords: 'info pop up hint helper' }
 		]
 	}
 ];
