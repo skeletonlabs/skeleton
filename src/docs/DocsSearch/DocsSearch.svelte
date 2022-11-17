2ea7d771c0924dcf2139d3d706b6e8ce8848eaa3
Rhys <36913739+ryceg@users.noreply.github.com>
2022-11-17 16:06:05 +1100

    add recent search

--- src/docs/DocsSearch/DocsSearch.svelte
+++ src/docs/DocsSearch/DocsSearch.svelte
@@ -2,12 +2,12 @@
 	import { menuNavLinks, type MenuNavCategory, type MenuNavLink } from '$docs/DocsNavigation/links';
 	import { modalStore } from '$lib/utilities/Modal/stores';
 	import SvgIcon from '$lib/components/SvgIcon/SvgIcon.svelte';
-
+	import { storeRecentlySearched, storeFavSearch } from '../stores';
 	// Local
 	let searchTerm: string = '';
 	let navigationOriginal = Object.values(menuNavLinks);
 	let navigation = navigationOriginal;
-	let highlighted;
+	let highlighted: HTMLAnchorElement;
 	function filterList(list: MenuNavLink[]) {
 		return list.filter((rowObj) => {
 			const formattedSearchTerm = searchTerm.toLowerCase() || '';
@@ -33,12 +33,21 @@
 
 	/** Sets the aria-selected attribute */
 	function onMouseOver(e: MouseEvent | FocusEvent): void {
+		// set every element to false to reset only when a new valid element is hovered
 		searchResults.querySelectorAll('[aria-selected="true"]')?.forEach((el) => el.setAttribute('aria-selected', 'false'));
-		const target = e.target as HTMLElement;
+		const target = e.target as HTMLAnchorElement;
 		if (target.tagName === 'A') {
 			highlighted = target;
 			highlighted.setAttribute('aria-selected', 'true');
 		}
+	}
+
+	function onClick(link: MenuNavLink) {
+		modalStore.close();
+		storeRecentlySearched.update((list) => {
+			if (list.includes(link)) return list;
+			return [link, ...list];
+		});
 	}
 
 	let searchResults: HTMLDivElement;
@@ -58,26 +67,66 @@
 	</header>
 	<hr />
 	<div bind:this={searchResults} class="docs-search-categories {cList}">
-		{#each navigation as category, i}
+		{#if searchTerm.length === 0}
 			<div class="space-y-4">
-				<strong>{category.title}</strong>
+				<strong>Recently Searched</strong>
 				<nav>
 					<ul class="list-none space-y-2">
-						{#each category.list as link, i}
+						{#each $storeRecentlySearched as link, i}
 							<li>
-								<!-- prettier-ignore -->
-								<a aria-selected="false" on:mouseover={onMouseOver} on:focus={onMouseOver} class="{cCard}" href={link.href} on:click={() => { modalStore.close(); }}>
-								<div>
-									<p>{link.label}</p>
-									<small class="opacity-75">{link.href}</small>
-								</div>
-								<span class="opacity-75">&rarr;</span>
+								<a
+									aria-selected="false"
+									on:mouseover={onMouseOver}
+									on:focus={onMouseOver}
+									class={cCard}
+									href={link.href}
+									on:click={() => onClick(link)}
+								>
+									<div>
+										<p>{link.label}</p>
+										<small class="opacity-75">{link.href}</small>
+									</div>
+									<button
+										on:click|preventDefault={() => {
+											$storeRecentlySearched = $storeRecentlySearched.filter((_, index) => index !== i);
+										}}
+									>
+										<SvgIcon class="w-10 h-10" name="close" />
+									</button>
 								</a>
 							</li>
 						{/each}
 					</ul>
 				</nav>
 			</div>
-		{/each}
+		{:else}
+			{#each navigation as category, i}
+				<div class="space-y-4">
+					<strong>{category.title}</strong>
+					<nav>
+						<ul class="list-none space-y-2">
+							{#each category.list as link, i}
+								<li>
+									<a
+										aria-selected="false"
+										on:mouseover={onMouseOver}
+										on:focus={onMouseOver}
+										class={cCard}
+										href={link.href}
+										on:click={() => onClick(link)}
+									>
+										<div>
+											<p>{link.label}</p>
+											<small class="opacity-75">{link.href}</small>
+										</div>
+										<span class="opacity-75">&rarr;</span>
+									</a>
+								</li>
+							{/each}
+						</ul>
+					</nav>
+				</div>
+			{/each}
+		{/if}
 	</div>
 </div>
--- src/docs/stores.ts
+++ src/docs/stores.ts
@@ -1,18 +1,25 @@
 import { writable, type Writable } from 'svelte/store';
 import { localStorageStore } from '$lib/utilities/LocalStorageStore/LocalStorageStore';
+import type { MenuNavLink } from './DocsNavigation/links';
 
 // Svelte Writable Stores ---
 
-// Set within root layout, persists current SvelteKit $page.url.pathname
+/** Set within root layout, persists current SvelteKit $page.url.pathname */
 export const storeCurrentUrl: Writable<string | undefined> = writable(undefined);
 
-// Persists the mobile-only nav drawer 'open' state
+/** Persists the mobile-only nav drawer 'open' state */
 export const storeMobileDrawer: Writable<boolean> = writable(false);
 
 // Local Storage Stores ---
 
-// Persists select preset theme
+/** Persists select preset theme */
 export const storeTheme: Writable<string> = localStorageStore('storeTheme', 'skeleton');
 
-// Persists the tab selection for the user's preferred framework
+/** Persists the tab selection for the user's preferred framework */
 export const storeFramework: Writable<string> = localStorageStore('storeFramework', 'sveltekit');
+
+/** Persists recently searched  */
+export const storeRecentlySearched: Writable<MenuNavLink[]> = localStorageStore('storeRecentlySearched', []);
+
+/** Persists user-saved searched */
+export const storeFavSearch: Writable<MenuNavLink[]> = localStorageStore('storeFavSearch', []);
