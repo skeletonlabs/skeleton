70f4031e552ec9e3b4f5a2702b4fd0db2246b3a5
Rhys <36913739+ryceg@users.noreply.github.com>
2022-11-17 14:22:40 +1100

    Add close icon

--- src/lib/components/SvgIcon/icons.ts
+++ src/lib/components/SvgIcon/icons.ts
@@ -1,10 +1,10 @@
-/* 
+/*
     Font Awesome @fontawesome - https://fontawesome.com
     License - https://fontawesome.com/license (Commercial License)
     Copyright 2022 Fonticons, Inc
 */
 
-export let icons: any = {
+export const icons = {
 	// Default Placeholder
 	// Source: https://fontawesome.com/icons/image?s=solid
 	image: {
@@ -198,5 +198,11 @@ export let icons: any = {
 	search: {
 		viewBox: '0 0 512 512',
 		path: 'M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z'
+	},
+
+	// Source: https://fontawesome.com/icons/close?s=solid&f=classic
+	close: {
+		viewBox: '0 0 320 512',
+		path: 'M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z'
 	}
 };
