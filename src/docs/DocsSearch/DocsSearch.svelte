9c666d0a54e0f5f7091330b2c6fafeb46b3adb2e
Rhys <36913739+ryceg@users.noreply.github.com>
2022-11-17 14:23:16 +1100

    Add unordered list, and enter navigation, closes #568 and closes #567

--- src/docs/DocsSearch/DocsSearch.svelte
+++ src/docs/DocsSearch/DocsSearch.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { menuNavLinks } from '$docs/DocsNavigation/links';
+	import { menuNavLinks, type MenuNavCategory, type MenuNavLink } from '$docs/DocsNavigation/links';
 	import { modalStore } from '$lib/utilities/Modal/stores';
 	import SvgIcon from '$lib/components/SvgIcon/SvgIcon.svelte';
 
 	// Local
 	let searchTerm: string = '';
-	let navigationOriginal: any[] = Object.values(menuNavLinks);
-	let navigation: any[] = navigationOriginal;
-
-	function filterList(list: any[]): any[] {
-		return list.filter((rowObj: any) => {
+	let navigationOriginal = Object.values(menuNavLinks);
+	let navigation = navigationOriginal;
+	let highlighted;
+	function filterList(list: MenuNavLink[]) {
+		return list.filter((rowObj) => {
 			const formattedSearchTerm = searchTerm.toLowerCase() || '';
 			return Object.values(rowObj).join(' ').toLowerCase().includes(formattedSearchTerm);
 		});
@@ -17,41 +17,65 @@
 
 	function onSearch(): void {
 		let navDeepCopy = JSON.parse(JSON.stringify(navigationOriginal));
-		navigation = navDeepCopy.filter((category: any) => {
+		navigation = navDeepCopy.filter((category: MenuNavCategory) => {
 			category.list = filterList(category.list);
 			if (category.list.length) return category;
 		});
+		searchResults.getElementsByTagName('a')[0].setAttribute('aria-selected', 'true');
 	}
+
+	function onKey(e: KeyboardEvent): void {
+		if (e.key === 'Enter') {
+			const firstEl = searchResults.querySelector('[aria-selected="true"]');
+			if (firstEl instanceof HTMLAnchorElement) firstEl.click();
+		}
+	}
+
+	/** Sets the aria-selected attribute */
+	function onMouseOver(e: MouseEvent | FocusEvent): void {
+		searchResults.querySelectorAll('[aria-selected="true"]')?.forEach((el) => el.setAttribute('aria-selected', 'false'));
+		const target = e.target as HTMLElement;
+		if (target.tagName === 'A') {
+			highlighted = target;
+			highlighted.setAttribute('aria-selected', 'true');
+		}
+	}
+
+	let searchResults: HTMLDivElement;
 
 	const cBase: string = '';
 	const cHeader: string = 'flex items-center space-x-4 p-4';
 	const cList: string = 'pr-1 space-y-4 rounded-container-token max-h-[60vh] md:max-h-[75vh] p-4 space-y-4 overflow-y-auto';
-	const cCard: string = 'card card-body py-2 bg-surface-200-700-token hover:bg-primary-500 flex justify-between items-center';
+	const cCard: string = 'card card-body py-2 bg-surface-200-700-token aria-selected:bg-primary-500 flex justify-between items-center';
 </script>
 
 <div class="docs-search {cBase}">
 	<header class="docs-search-header {cHeader}">
 		<SvgIcon name="search" />
-		<input bind:value={searchTerm} type="search" placeholder="Search..." on:input={onSearch} />
+		<input bind:value={searchTerm} type="search" placeholder="Search..." on:input={onSearch} on:keypress={onKey} />
 		<!-- prettier-ignore -->
-		<button class="btn btn-ghost-surface btn-sm" on:click={() => { modalStore.close(); }}>✕</button>
+		<button class="btn-icon" on:click={() => { modalStore.close(); }}><kbd>Esc</kbd></button>
 	</header>
 	<hr />
-	<div class="docs-search-categories {cList}">
+	<div bind:this={searchResults} class="docs-search-categories {cList}">
 		{#each navigation as category, i}
 			<div class="space-y-4">
 				<strong>{category.title}</strong>
-				<nav class="space-y-2">
-					{#each category.list as link, i}
-						<!-- prettier-ignore -->
-						<a class="{cCard}" href={link.href} on:click={() => { modalStore.close(); }}>
-							<div>
-								<p>{link.label}</p>
-								<small class="opacity-75">{link.href}</small>
-							</div>
-							<span class="opacity-75">&rarr;</span>
-						</a>
-					{/each}
+				<nav>
+					<ul class="list-none space-y-2">
+						{#each category.list as link, i}
+							<li>
+								<!-- prettier-ignore -->
+								<a aria-selected="false" on:mouseover={onMouseOver} on:focus={onMouseOver} class="{cCard}" href={link.href} on:click={() => { modalStore.close(); }}>
+								<div>
+									<p>{link.label}</p>
+									<small class="opacity-75">{link.href}</small>
+								</div>
+								<span class="opacity-75">&rarr;</span>
+								</a>
+							</li>
+						{/each}
+					</ul>
 				</nav>
 			</div>
 		{/each}
