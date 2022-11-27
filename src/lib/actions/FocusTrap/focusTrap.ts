839c5b022549ec531331f9ba8d4baed74586f841
bennymi <xbenmmusicx@gmail.com>
2022-11-28 00:03:34 +0100

    update focus trap action to also focus trap with shift + tab keydown events

--- src/lib/actions/FocusTrap/focusTrap.ts
+++ src/lib/actions/FocusTrap/focusTrap.ts
@@ -6,9 +6,16 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 	let elemLast: HTMLElement;
 
 	function onElemLastKeydown(e: KeyboardEvent): void {
-		if (e.code === 'Tab') {
+		if (!e.shiftKey && e.code === 'Tab') {
 			e.preventDefault();
 			elemFirst.focus();
+		}
+	}
+
+	function onElemFirstKeydown(e: KeyboardEvent): void {
+		if (e.shiftKey && e.code === 'Tab') {
+			e.preventDefault();
+			elemLast.focus();
 		}
 	}
 
@@ -22,7 +29,8 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 			elemLast = focusableElems[focusableElems.length - 1];
 			// Auto-focus first focusable element
 			elemFirst.focus();
-			// Listen for keydown on last element
+			// Listen for keydown on first & last element
+			elemFirst.addEventListener('keydown', onElemFirstKeydown);
 			elemLast.addEventListener('keydown', onElemLastKeydown);
 		}
 	};
