57628ecd80ab602e6123277cfd1904a4de3452c7
endigo9740 <gundamx9740@gmail.com>
2022-12-06 15:18:30 -0600

    Updates to theme gen layout

--- src/docs/DocsThemer/DocsThemer.svelte
+++ src/docs/DocsThemer/DocsThemer.svelte
@@ -22,9 +22,6 @@
 	import { fonts } from '$docs/DocsThemer/fonts';
 	import SvgIcon from '$lib/components/SvgIcon/SvgIcon.svelte';
 
-	// Local
-	let showCode = false;
-
 	// Reactive ---
 
 	// Set the current palette based on Tailwind/Hex mode
@@ -66,11 +63,19 @@
 	{@html headStyleCSS}
 </svelte:head>
 
-<!-- prettier-ignore -->
 <div class="themer space-y-4">
-
-	<!-- Color Selection -->
-	<div class="card card-body flex flex-col space-y-4">
+	<!-- Preview -->
+	<!-- class="col-span-2 place-self-center" -->
+	<div class="card card-body !bg-accent-500/5 flex justify-between items-center">
+		<SlideToggle bind:checked={$storePreview}>Edit Mode</SlideToggle>
+		<div />
+		<div>
+			<button class="btn btn-filled" on:click={resetSettings} disabled={!$storePreview}>Reset Theme</button>
+		</div>
+	</div>
+	{#if $storePreview}
+		<!-- Color Selection -->
+		<div class="card card-body flex flex-col space-y-4">
 			<!-- Header -->
 			<header class="flex justify-between items-center gap-4">
 				<div class="flex justify-between items-center space-x-4">
@@ -78,7 +83,9 @@
 						<RadioItem value={true}>Tailwind</RadioItem>
 						<RadioItem value={false}>Hex</RadioItem>
 					</RadioGroup>
-					<button class="btn btn-ghost-surface" on:click={onRandomize} disabled={!$storeMode}>Randomize</button>
+					{#if $storeMode}
+						<button class="btn btn-ghost-surface" on:click={onRandomize} title="Randomize Tailwind Colors">Randomize</button>
+					{/if}
 				</div>
 				<div>
 					<LightSwitch />
@@ -88,6 +95,7 @@
 			<!-- Color Inputs -->
 			{#each semanticNames as colorKey}
 				<div class="grid grid-cols-1 md:grid-cols-[120px_1fr] gap-2 md:gap-4">
+					<!-- prettier-ignore -->
 					<label class="w-full">
 						<span class="capitalize">{colorKey}</span>
 						{#if $storeMode}
@@ -104,192 +112,175 @@
 					<Swatches palette={currentPalette[colorKey].shades} />
 				</div>
 			{/each}
-	</div>
+		</div>
 
-	<!-- On-Colors -->
-	<div class="card card-body space-y-4">
-		<label for=""><span>Text/Fill Accessibility</span></label>
-		<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-			<div class="space-y-2">
-				<div class="bg-primary-500 text-on-primary-token text-center p-2">
-					<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
-					Primary
+		<!-- On-Colors -->
+		<div class="card card-body space-y-4">
+			<label for=""><span>Adjust Text and Fill Accessibility</span></label>
+			<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+				<div class="space-y-2">
+					<div class="bg-primary-500 text-on-primary-token text-center p-2">
+						<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
+						Primary
+					</div>
+					<select bind:value={$storeFormData.onPrimary}>
+						<option value="#000">Black</option>
+						<option value="#fff">White</option>
+					</select>
 				</div>
-				<select bind:value={$storeFormData.onPrimary}>
-					<option value='#000'>Black</option>
-					<option value='#fff'>White</option>
-				</select>
-			</div>
-			<div class="space-y-2">
-				<div class="bg-accent-500 text-on-accent-token text-center p-2">
-					<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
-					Accent
+				<div class="space-y-2">
+					<div class="bg-accent-500 text-on-accent-token text-center p-2">
+						<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
+						Accent
+					</div>
+					<select bind:value={$storeFormData.onAccent}>
+						<option value="#000">Black</option>
+						<option value="#fff">White</option>
+					</select>
 				</div>
-				<select bind:value={$storeFormData.onAccent}>
-					<option value='#000'>Black</option>
-					<option value='#fff'>White</option>
-				</select>
-			</div>
-			<div class="space-y-2">
-				<div class="bg-tertiary-500 text-on-tertiary-token text-center p-2">
-					<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
-					Tertiary
+				<div class="space-y-2">
+					<div class="bg-tertiary-500 text-on-tertiary-token text-center p-2">
+						<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
+						Tertiary
+					</div>
+					<select bind:value={$storeFormData.onTertiary}>
+						<option value="#000">Black</option>
+						<option value="#fff">White</option>
+					</select>
 				</div>
-				<select bind:value={$storeFormData.onTertiary}>
-					<option value='#000'>Black</option>
-					<option value='#fff'>White</option>
-				</select>
-			</div>
-			<div class="space-y-2">
-				<div class="bg-warning-500 text-on-warning-token text-center p-2">
-					<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
-					Warning
+				<div class="space-y-2">
+					<div class="bg-warning-500 text-on-warning-token text-center p-2">
+						<SvgIcon name="skull" width="w-4" height="h-4" class="mr-1 -translate-y-0.5" />
+						Warning
+					</div>
+					<select bind:value={$storeFormData.onWarning}>
+						<option value="#000">Black</option>
+						<option value="#fff">White</option>
+					</select>
 				</div>
-				<select bind:value={$storeFormData.onWarning}>
-					<option value='#000'>Black</option>
-					<option value='#fff'>White</option>
-				</select>
 			</div>
 		</div>
-	</div>
 
-	<!-- Settings -->
-	<div class="card card-body grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-		<!-- --theme-font-family-heading -->
-		<label>
-			<span>Font Family <small>(headings)</small></span>
-			<select name="background" id="background" bind:value={$storeFormData.fontFamilyHeading}>
-				<option value="sans">Sans Serif</option>
-				<option value="serif">Serif</option>
-				<option value="mono">Monospace</option>
-				<option value="system">System UI</option>
-			</select>
-		</label>
-		<!-- --theme-font-family-base -->
-		<label>
-			<span>Font Family <small>(base)</small></span>
-			<select name="background" id="background" bind:value={$storeFormData.fontFamilyBase}>
-				<option value="sans">Sans Serif</option>
-				<option value="serif">Serif</option>
-				<option value="mono">Monospace</option>
-				<option value="system">System UI</option>
-			</select>
-		</label>
-		<!-- --theme-font-color-base -->
-		<label>
-			<span>Font Color <small>(light mode)</small></span>
-			<select name="background" id="background" bind:value={$storeFormData.fontColorBase}>
-				<!-- Black -->
-				<option value="0 0 0">Black</option>
-				<!-- Surface -->
-				<option value="var(--color-surface-500)">Surface 500</option>
-				<option value="var(--color-surface-600)">Surface 600</option>
-				<option value="var(--color-surface-700)">Surface 700</option>
-				<option value="var(--color-surface-800)">Surface 800</option>
-				<option value="var(--color-surface-900)">Surface 900</option>
-				<!-- Primary -->
-				<option value="var(--color-primary-500)">Primary 500</option>
-				<option value="var(--color-primary-600)">Primary 600</option>
-				<option value="var(--color-primary-700)">Primary 700</option>
-				<option value="var(--color-primary-800)">Primary 800</option>
-				<option value="var(--color-primary-900)">Primary 900</option>
-				<!-- Accent -->
-				<option value="var(--color-accent-500)">Accent 500</option>
-				<option value="var(--color-accent-600)">Accent 600</option>
-				<option value="var(--color-accent-700)">Accent 700</option>
-				<option value="var(--color-accent-800)">Accent 800</option>
-				<option value="var(--color-accent-900)">Accent 900</option>
-			</select>
-		</label>
-		<!-- --theme-font-color-dark -->
-		<label>
-			<span>Font Color <small>(dark mode)</small></span>
-			<select name="background" id="background" bind:value={$storeFormData.fontColorDark}>
-				<!-- White -->
-				<option value="255 255 255">White</option>
-				<!-- Surface -->
-				<option value="var(--color-surface-400)">Surface 400</option>
-				<option value="var(--color-surface-300)">Surface 300</option>
-				<option value="var(--color-surface-200)">Surface 200</option>
-				<option value="var(--color-surface-100)">Surface 100</option>
-				<option value="var(--color-surface-50)">Surface 50</option>
-				<!-- Primary -->
-				<option value="var(--color-primary-400)">Primary 400</option>
-				<option value="var(--color-primary-300)">Primary 300</option>
-				<option value="var(--color-primary-200)">Primary 200</option>
-				<option value="var(--color-primary-100)">Primary 100</option>
-				<option value="var(--color-primary-50)">Primary 50</option>
-				<!-- Accent -->
-				<option value="var(--color-accent-400)">Accent 400</option>
-				<option value="var(--color-accent-300)">Accent 300</option>
-				<option value="var(--color-accent-200)">Accent 200</option>
-				<option value="var(--color-accent-100)">Accent 100</option>
-				<option value="var(--color-accent-50)">Accent 50</option>
-			</select>
-		</label>
-		<!-- --theme-rounded-base -->
-		<label>
-			<span>Rounded</span>
-			<select name="background" id="background" bind:value={$storeFormData.roundedBase}>
-				<option value="0px">0px</option>
-				<option value="2px">2px</option>
-				<option value="4px">4px</option>
-				<option value="6px">6px</option>
-				<option value="8px">8px</option>
-				<option value="12px">12px</option>
-				<option value="16px">16px</option>
-				<option value="24px">24px</option>
-				<option value="9999px">9999px</option>
-			</select>
-		</label>
-		<!-- --theme-rounded-container -->
-		<label>
-			<span>Rounded <small>(container)</small></span>
-			<select name="background" id="background" bind:value={$storeFormData.roundedContainer}>
-				<option value="0px">0px</option>
-				<option value="2px">2px</option>
-				<option value="4px">4px</option>
-				<option value="6px">6px</option>
-				<option value="8px">8px</option>
-				<option value="12px">12px</option>
-				<option value="16px">16px</option>
-				<option value="24px">24px</option>
-			</select>
-		</label>
-		<!-- --theme-border-base -->
-		<label>
-			<span>Border</span>
-			<select name="background" id="background" bind:value={$storeFormData.borderBase}>
-				<option value="0px">0px</option>
-				<option value="1px">1px</option>
-				<option value="2px">2px</option>
-				<option value="4px">4px</option>
-			</select>
-		</label>
-	</div>
-
-	<!-- Preview / Generate Actions -->
-	<div class="card card-body flex justify-between items-center space-x-4">
-		<!-- Preview -->
-		<div class="col-span-2 place-self-center">
-			<SlideToggle bind:checked={$storePreview}>Live Preview</SlideToggle>
+		<!-- Font Settings -->
+		<div class="card card-body grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			<!-- --theme-font-family-heading -->
+			<label>
+				<span>Headings Font</span>
+				<select name="background" id="background" bind:value={$storeFormData.fontFamilyHeading}>
+					<option value="sans">Sans Serif</option>
+					<option value="serif">Serif</option>
+					<option value="mono">Monospace</option>
+					<option value="system">System UI</option>
+				</select>
+			</label>
+			<!-- --theme-font-family-base -->
+			<label>
+				<span>Base Font</span>
+				<select name="background" id="background" bind:value={$storeFormData.fontFamilyBase}>
+					<option value="sans">Sans Serif</option>
+					<option value="serif">Serif</option>
+					<option value="mono">Monospace</option>
+					<option value="system">System UI</option>
+				</select>
+			</label>
+			<!-- --theme-font-color-base -->
+			<label>
+				<span>Light Mode Text Color</span>
+				<select name="background" id="background" bind:value={$storeFormData.fontColorBase}>
+					<!-- Black -->
+					<option value="0 0 0">Black</option>
+					<!-- Surface -->
+					<option value="var(--color-surface-500)">Surface 500</option>
+					<option value="var(--color-surface-600)">Surface 600</option>
+					<option value="var(--color-surface-700)">Surface 700</option>
+					<option value="var(--color-surface-800)">Surface 800</option>
+					<option value="var(--color-surface-900)">Surface 900</option>
+					<!-- Primary -->
+					<option value="var(--color-primary-500)">Primary 500</option>
+					<option value="var(--color-primary-600)">Primary 600</option>
+					<option value="var(--color-primary-700)">Primary 700</option>
+					<option value="var(--color-primary-800)">Primary 800</option>
+					<option value="var(--color-primary-900)">Primary 900</option>
+					<!-- Accent -->
+					<option value="var(--color-accent-500)">Accent 500</option>
+					<option value="var(--color-accent-600)">Accent 600</option>
+					<option value="var(--color-accent-700)">Accent 700</option>
+					<option value="var(--color-accent-800)">Accent 800</option>
+					<option value="var(--color-accent-900)">Accent 900</option>
+				</select>
+			</label>
+			<!-- --theme-font-color-dark -->
+			<label>
+				<span>Dark Mode Text Color</span>
+				<select name="background" id="background" bind:value={$storeFormData.fontColorDark}>
+					<!-- White -->
+					<option value="255 255 255">White</option>
+					<!-- Surface -->
+					<option value="var(--color-surface-400)">Surface 400</option>
+					<option value="var(--color-surface-300)">Surface 300</option>
+					<option value="var(--color-surface-200)">Surface 200</option>
+					<option value="var(--color-surface-100)">Surface 100</option>
+					<option value="var(--color-surface-50)">Surface 50</option>
+					<!-- Primary -->
+					<option value="var(--color-primary-400)">Primary 400</option>
+					<option value="var(--color-primary-300)">Primary 300</option>
+					<option value="var(--color-primary-200)">Primary 200</option>
+					<option value="var(--color-primary-100)">Primary 100</option>
+					<option value="var(--color-primary-50)">Primary 50</option>
+					<!-- Accent -->
+					<option value="var(--color-accent-400)">Accent 400</option>
+					<option value="var(--color-accent-300)">Accent 300</option>
+					<option value="var(--color-accent-200)">Accent 200</option>
+					<option value="var(--color-accent-100)">Accent 100</option>
+					<option value="var(--color-accent-50)">Accent 50</option>
+				</select>
+			</label>
 		</div>
-		<!-- Code Toggle -->
-		<button class="col-span-2 btn btn-filled-primary btn-lg" on:click={()=>{showCode=!showCode}}>
-			{!showCode ? 'Show' : 'Hide'} Theme CSS <span class="text-xs ml-4">{@html showCode ? '&#9650;' : '&#9660;'}</span>
-		</button>
-	</div>
 
-	<!-- Preview Enabled Alert -->
-	<Alert visible={$storePreview} class="animate-pulse hover:animate-none">
-		<svelte:fragment slot="title">Live Preview Enabled</svelte:fragment>
-		<span>While the preview is active you can browse the entire site. Don't worry, your settings will not be lost. When you're done, tap the reset button to return to your default theme.</span>
-		<svelte:fragment slot="trail">
-			<button class="btn btn-filled" on:click={resetSettings}>Reset Theme</button>
-		</svelte:fragment>
-	</Alert>
+		<!-- Radius and Border Settings -->
+		<div class="card card-body grid grid-cols-1 md:grid-cols-3 gap-4">
+			<!-- --theme-rounded-base -->
+			<label>
+				<span>Rounded</span>
+				<select name="background" id="background" bind:value={$storeFormData.roundedBase}>
+					<option value="0px">0px</option>
+					<option value="2px">2px</option>
+					<option value="4px">4px</option>
+					<option value="6px">6px</option>
+					<option value="8px">8px</option>
+					<option value="12px">12px</option>
+					<option value="16px">16px</option>
+					<option value="24px">24px</option>
+					<option value="9999px">9999px</option>
+				</select>
+			</label>
+			<!-- --theme-rounded-container -->
+			<label>
+				<span>Container Rounded</span>
+				<select name="background" id="background" bind:value={$storeFormData.roundedContainer}>
+					<option value="0px">0px</option>
+					<option value="2px">2px</option>
+					<option value="4px">4px</option>
+					<option value="6px">6px</option>
+					<option value="8px">8px</option>
+					<option value="12px">12px</option>
+					<option value="16px">16px</option>
+					<option value="24px">24px</option>
+				</select>
+			</label>
+			<!-- --theme-border-base -->
+			<label>
+				<span>Border</span>
+				<select name="background" id="background" bind:value={$storeFormData.borderBase}>
+					<option value="0px">0px</option>
+					<option value="1px">1px</option>
+					<option value="2px">2px</option>
+					<option value="4px">4px</option>
+				</select>
+			</label>
+		</div>
 
-
-	<!-- Code -->
-	{#if showCode}<CodeBlock language="css" code={themeCSS} />{/if}
+		<!-- Generated CSS Code -->
+		<CodeBlock language="css" code={themeCSS} />
+	{/if}
 </div>
