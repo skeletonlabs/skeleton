7b30c6b826ca21e832aac90a540cb8f51987eda5
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-12-23 16:38:53 -0500

    fixed flashbang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,6 +25,9 @@
 	import DocsDrawer from '$docs/DocsNavigation/DocsDrawer.svelte';
 	import DocsFooter from '$docs/DocsFooter/DocsFooter.svelte';
 
+	// Default Theme
+	import skeleton from '$lib/themes/theme-skeleton.css?inline';
+
 	// Skeleton Stylesheets
 	import '$lib/styles/all.css';
 	// The Skeleton blog stylesheet
@@ -70,7 +73,7 @@
 
 <!-- Select Preset Theme CSS DO NOT REMOVE ESCAPES-->
 <svelte:head>
-	{@html `\<style\>${currentTheme}}\</style\>`}
+	{@html `\<style\>${currentTheme ?? skeleton}}\</style\>`}
 </svelte:head>
 
 <!-- Overlays -->
