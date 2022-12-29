a6f167926bfa66a90ce857f5bd758d2a8aba9389
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-12-29 17:07:38 -0500

    more fitting name for the plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,5 +10,5 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [require('@tailwindcss/forms'), require('./src/lib/tailwind/theme.cjs'), require('./src/lib/tailwind/all.cjs')]
+	plugins: [require('@tailwindcss/forms'), require('./src/lib/tailwind/theme.cjs'), require('./src/lib/tailwind/intellisense.cjs')]
 };
