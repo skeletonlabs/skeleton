334b9f7c22dd05644ccf50f185ac312fda9d6c19
endigo9740 <gundamx9740@gmail.com>
2022-12-29 12:13:32 -0600

    InputChip flushed out

--- src/lib/components/InputChip/InputChip.svelte
+++ src/lib/components/InputChip/InputChip.svelte
@@ -1,44 +1,84 @@
 <script lang="ts">
-	export let source: any[] = ['vanilla', 'chocolate', 'strawberry'];
-	export let whitelist: any = [...source];
-	export let value: string = '';
+	import { createEventDispatcher } from 'svelte/internal';
+
+	// Event Dispatcher
+	const dispatch = createEventDispatcher();
+
+	/** Optionally provide an descriptive label. */
+	export let label: string = '';
+	/** Optionally provide an descriptive label. */
+	export let placeholder: string = 'Enter Value...';
+	/**
+	 * Binds an array of values.
+	 * @type {string[]}
+	 */
+	export let value: string[] = [];
+	/**
+	 * Provide a whitelist of accepted values.
+	 * @type {string[]}
+	 */
+	export let whitelist: string[] = [];
+	/** When enabled, will format entered values as lowercase. */
+	export let lowercase: boolean = true;
+
+	// Local
+	let inputValue: string = '';
+	let inputInvalid: boolean = false;
 
 	// Classes
 	// focus:border-secondary-500
 	const cBase = 'unstyled border-token flex flex-wrap gap-2 items-center p-2 rounded';
 	const cChip = 'chip-primary hover:chip-error';
-	const cInput = 'unstyled flex-auto bg-transparent border-transparent px-2 p-[1px] rounded text-sm';
+	const cInput =
+		'unstyled flex-auto border-transparent bg-transparent p-0 px-1 rounded text-base focus:ring-transparent focus:border-transparent min-h-[30px]';
 
 	function onKeyDown(event: KeyboardEvent): void {
-		if (event.code === 'Enter') {
-			// Not already included, whitelist allows it
-			if (!source.includes(value) && whitelist.length > 0 && whitelist.includes(value)) {
-				source = [...source, value];
-				value = '';
+		inputInvalid = false;
+		if (event.code === 'Enter' || event.keyCode === 13) {
+			// Validate whitelist (if available)
+			if (whitelist.length > 0 && !whitelist.includes(inputValue)) {
+				inputInvalid = true;
+				return;
+			}
+			// Validate value is unique
+			if (!value.includes(inputValue)) {
+				// Format lowercase (if enabled)
+				inputValue = lowercase ? inputValue.toLowerCase() : inputValue;
+				// Append value
+				value = [...value, inputValue];
+				// Clear input value
+				inputValue = '';
+				/** @event {{ event: KeyboardEvent }} add - When a chip is added. */
+				dispatch('add', event);
+			} else {
+				inputInvalid = true;
+				return;
 			}
 		}
 	}
 
-	function remove(chipIndex: number): void {
-		source = source.filter((_, i) => i !== chipIndex);
-		console.log(source);
+	function removeChip(chipIndex: number): void {
+		/** @event {{ chipIndex: number }} remove - When a chip is removed. */
+		dispatch('remove', chipIndex);
+		value = value.filter((_, i) => i !== chipIndex);
 	}
 </script>
 
 <label class="input-chip {cBase}">
-	<strong>Flavors</strong>
-	{#each source as chip, i}
+	{#if label}<strong>{label}</strong>{/if}
+	{#each value as chip, i}
 		<!-- prettier-ignore -->
-		<span class="chip {cChip}" on:click={() => {remove(i)}} on:keypress>
+		<span class="chip {cChip}" on:click={() => {removeChip(i)}} on:keypress>
 			<span>{chip}</span>
 			<span>✕</span>
 		</span>
 	{/each}
-	<input class="input-chip-field {cInput}" type="text" placeholder="Enter Value..." bind:value on:keydown={onKeyDown} />
+	<input
+		type="text"
+		{placeholder}
+		bind:value={inputValue}
+		class="input-chip-field {cInput}"
+		class:input-invalid={inputInvalid}
+		on:keydown={onKeyDown}
+	/>
 </label>
-
-<style>
-	input:focus {
-		outline: 10 !important;
-	}
-</style>
--- src/routes/(inner)/components/input-chips/+page.svelte
+++ src/routes/(inner)/components/input-chips/+page.svelte
@@ -17,30 +17,36 @@
 		name: 'Input Chips',
 		description: 'Allows input of multiple values.',
 		imports: ['InputChip'],
-		// types: ['Template'],
-		// stylesheetIncludes: ['all', 'elements'],
-		// stylesheets: ['elements/template'],
+		stylesheetIncludes: ['all', 'elements'],
+		stylesheets: ['elements/forms', 'elements/chips'],
 		source: 'components/InputChip',
-		// aria: 'https://www.w3.org/WAI/ARIA/apg/'
-		// dependencies: [{ label: 'Highlight.js', url: 'https://highlightjs.org/' }],
 		components: [{ sveld: sveldInputChip }]
-		// classes: [
-		// 	['<code>.foo</code>', '-', '...'],
-		// 	['<code>.bar</code>', '-', '...']
-		// ],
-		// keyboard: [
-		// 	['<code>foo</code>', '-', '-', '-', '...'],
-		// 	['<code>bar</code>', '-', '-', '-', '...']
-		// ]
+		// aria: 'https://www.w3.org/WAI/ARIA/apg/'
 	};
+
+	// Local
+	let emails = ['john@email.com', 'jane@email.com', 'sally@email.com'];
+	let emailsWhitelist = ['john@email.com', 'jane@email.com', 'sally@email.com'];
+	let flavors = ['vanilla', 'chocolate', 'strawberry'];
 </script>
 
 <DocsShell {settings}>
 	<!-- Slot: Sandbox -->
 	<svelte:fragment slot="sandbox">
-		<div class="card p-4">
-			<InputChip />
-		</div>
+		<section class="space-y-4">
+			<div class="card p-4 space-y-4">
+				<!-- Flavors -->
+				<p>Type a value then hit ENTER to apply it.</p>
+				<InputChip label="Flavors" placeholder="Add flavor..." bind:value={flavors} />
+				<code class="inline-block">{flavors.length ? flavors : 'No flavors set.'}</code>
+			</div>
+			<div class="card p-4 space-y-4">
+				<!-- Emails -->
+				<p>Emails whitelisted to <em>{emailsWhitelist.join(', ')}</em>.</p>
+				<InputChip label="Emails" placeholder="Enter emails..." bind:value={emails} whitelist={emailsWhitelist} />
+				<code class="inline-block">{emails.length ? emails : 'No emails set.'}</code>
+			</div>
+		</section>
 	</svelte:fragment>
 
 	<!-- Slot: Usage -->
