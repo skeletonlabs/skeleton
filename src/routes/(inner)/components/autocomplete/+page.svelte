2c24f60bbc6bbf5e1d60c6f00d10d8694ec95a8a
Hunter Johnston <johnstonhuntera@gmail.com>
2022-12-31 02:49:41 -0500

    initial stuff in place - need to refine

---
+++ src/routes/(inner)/components/autocomplete/+page.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import DocsShell from '$docs/DocsShell/DocsShell.svelte';
+	import { DocsFeature, type DocsShellSettings } from '$docs/DocsShell/types';
+
+	// Components
+	import AccordionGroup from '$lib/components/Accordion/AccordionGroup.svelte';
+	import AccordionItem from '$lib/components/Accordion/AccordionItem.svelte';
+	import Avatar from '$lib/components/Avatar/Avatar.svelte';
+	// Utilities
+	import CodeBlock from '$lib/utilities/CodeBlock/CodeBlock.svelte';
+
+	// @ts-expect-error sveld import
+	import sveldAccordionGroup from '$lib/components/Accordion/AccordionGroup.svelte?raw&sveld';
+	// @ts-expect-error sveld import
+	import sveldAccordionItem from '$lib/components/Accordion/AccordionItem.svelte?raw&sveld';
+	import Autocomplete from '$lib/components/Autocomplete/Autocomplete.svelte';
+	import type { AutocompleteConfig, AutocompleteOptions } from '$lib/components/Autocomplete/types';
+
+	// Docs Shell
+	const settings: DocsShellSettings = {
+		feature: DocsFeature.Component,
+		name: 'Autocomplete',
+		description: 'Divide content into collapsible sections.',
+		imports: ['AccordionGroup', 'AccordionItem'],
+		source: 'components/Accordion',
+		aria: 'https://www.w3.org/WAI/ARIA/apg/example-index/accordion/accordion',
+		components: [
+			{ label: 'AccordionGroup', sveld: sveldAccordionGroup },
+			{ label: 'AccordionItem', sveld: sveldAccordionItem, overrideProps: ['hover', 'spacing', 'padding', 'rounded'] }
+		],
+		keyboard: [
+			['<kbd>Tab</kbd>', 'Moves focus to the next focusable element.'],
+			['<kbd>Shift + Tab</kbd> ', 'Moves focus to the previous focusable element.'],
+			['<kbd>Space</kbd> or <kbd>Enter</kbd>', 'When focus is on the accordion header toggles the collapsable region open/closed.']
+		]
+	};
+
+	export let items = [
+		{
+			id: 1,
+			name: 'Red',
+			value: 'RED'
+		},
+		{
+			id: 2,
+			name: 'Green',
+			value: 'GREEN'
+		},
+		{
+			id: 3,
+			name: 'Blue',
+			value: 'BLUE'
+		},
+		{
+			id: 4,
+			name: 'Yellow',
+			value: 'YELLOW'
+		},
+		{
+			id: 5,
+			name: 'Gold',
+			value: 'GOLD'
+		}
+	];
+
+	const autocompleteOptions: AutocompleteOptions = {
+		labelField: 'name',
+		valueField: 'value'
+	};
+</script>
+
+<DocsShell {settings}>
+	<!-- Slot: Sandbox -->
+	<svelte:fragment slot="sandbox">
+		<section class="card p-4">
+			<Autocomplete {items} options={autocompleteOptions} />
+		</section>
+	</svelte:fragment>
+
+	<!-- Slot: Usage -->
+	<svelte:fragment slot="usage">
+		<p>Autocomplete (work-in-progress)</p>
+		<CodeBlock
+			language="html"
+			code={`
+`}
+		/>
+
+		<p>
+			When you don't want an AccordionItem to auto-collapse the other AccordionItems, just set <code>collapse</code> to <code>false</code>.
+		</p>
+		<CodeBlock
+			language="html"
+			code={`
+<AccordionGroup collapse={false}>
+	<!-- Open -->
+	<AccordionItem open>
+		<svelte:fragment slot="lead">(lead)</svelte:fragment>
+		<svelte:fragment slot="summary">(summary)</svelte:fragment>
+		<svelte:fragment slot="content">(content)</svelte:fragment>
+	</AccordionItem>
+	<!-- Closed -->
+	<AccordionItem>
+		<svelte:fragment slot="summary">(summary)</svelte:fragment>
+		<svelte:fragment slot="content">(content)</svelte:fragment>
+	</AccordionItem>
+</AccordionGroup>`}
+		/>
+	</svelte:fragment>
+</DocsShell>
