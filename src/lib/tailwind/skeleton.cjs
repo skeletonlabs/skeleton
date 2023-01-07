3b7265fc7f82385ef13165d14496efe41a4344c9
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-01-06 20:34:30 -0500

    cleaned up

--- src/lib/tailwind/skeleton.cjs
+++ src/lib/tailwind/skeleton.cjs
@@ -14,17 +14,35 @@ const tokensFills = require('./tokens/fills.cjs');
 const tokensText = require('./tokens/text.cjs');
 const tokensRings = require('./tokens/rings.cjs');
 
-module.exports = plugin(({ addUtilities }) => {
-	addUtilities({
-		// Implement Skeleton design token classes
-		...tokensBackgrounds(),
-		...tokensBorders(),
-		...tokensBorderRadius(),
-		...tokensFills(),
-		...tokensText(),
-		...tokensRings(),
-	})
-},
+module.exports = plugin(
+	({ addUtilities, addComponents }) => {
+		addUtilities({
+			// Implement Skeleton design token classes
+			...tokensBackgrounds(),
+			...tokensBorders(),
+			...tokensBorderRadius(),
+			...tokensFills(),
+			...tokensText(),
+			...tokensRings()
+		});
+		// Don't want to load all components in production
+		if (process.env.NODE_ENV !== 'production') {
+			// try/catch because it will throw when allComponents.cjs isn't generated yet
+			try {
+				const all = require('./generated/allComponents.cjs');
+				// Tokens are already loaded as utilities so we'll delete them to prevent duplicate rules
+				for (const [key, _] of Object.entries(all)) {
+					if (key.includes('token')) {
+						delete all[key];
+					}
+				}
+				addComponents(all, {
+					respectImportant: true,
+					respectPrefix: true
+				});
+			} catch {}
+		}
+	},
 	{
 		theme: {
 			extend: {
