fcc57fc3018b968ff28a6f2c6fbc231383a7ef42
endigo9740 <gundamx9740@gmail.com>
2023-01-09 11:31:51 -0600

    Context comment added

--- src/lib/tailwind/skeleton.cjs
+++ src/lib/tailwind/skeleton.cjs
@@ -25,22 +25,22 @@ module.exports = plugin(
 			...tokensText(),
 			...tokensRings()
 		});
-		// Don't want to load all components in production
+		// The following will generate the non-token classes PURELY for Intellisense.
+		// These are excluded from production, which means we still need to lean into
+		// using the `all.css` stylesheet to import non-token styles.
 		if (process.env.NODE_ENV !== 'production') {
 			// try/catch because it will throw when allComponents.cjs isn't generated yet
 			try {
 				const all = require('./generated/allComponents.cjs');
 				// Tokens are already loaded as utilities so we'll delete them to prevent duplicate rules
 				for (const [key, _] of Object.entries(all)) {
-					if (key.includes('token')) {
-						delete all[key];
-					}
+					if (key.includes('token')) delete all[key];
 				}
 				addComponents(all, {
 					respectImportant: true,
 					respectPrefix: true
 				});
-			} catch {}
+			} catch { }
 		}
 	},
 	{
