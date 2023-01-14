bf0c12c95e7b7c2ba9832db549aebcede6230802
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-01-14 18:49:27 -0500

    remove any selector that's not a class selector

--- scripts/generate-jss.js
+++ scripts/generate-jss.js
@@ -59,6 +59,9 @@ async function removeDuplicateClasses(cssInJs) {
 		// if it's a background color, delete it
 		if (key.includes('bg-')) delete cssInJs[key];
 
+		// if it's not a class selector, delete it (only want classes in the intellisense)
+		if (key[0] !== '.' && key) delete cssInJs[key];
+
 		// if it's a default tailwind class, delete it
 		if (twClasses[key]) delete cssInJs[key];
 	}
