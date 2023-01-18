b959364a81a440ab5403fe4770e88c4420924ad2
endigo9740 <gundamx9740@gmail.com>
2023-01-18 11:58:12 -0600

    Breadrumb improvements

--- src/routes/(inner)/elements/breadcrumbs/+page.svelte
+++ src/routes/(inner)/elements/breadcrumbs/+page.svelte
@@ -33,7 +33,7 @@
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
 					<li class="crumb"><a href="/">Elements</a></li>
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-					<li>breadcrumb</li>
+					<li>Breadcrumbs</li>
 				</ol>
 			</div>
 			<!-- 2 -->
@@ -41,33 +41,29 @@
 				<ol class="breadcrumb">
 					<li class="crumb"><a href="/">Home</a></li>
 					<li class="crumb-separator" aria-hidden>/</li>
-					<li class="crumb"><a href="/">Articles</a></li>
+					<li class="crumb"><a href="/">Blog</a></li>
 					<li class="crumb-separator" aria-hidden>/</li>
-					<li>Current</li>
+					<li>Article</li>
 				</ol>
 			</div>
 			<!-- 3 -->
 			<div class="card variant-glass p-4">
 				<ol class="breadcrumb">
-					<li class="crumb"><a href="/"><i class="fa-solid fa-house" /></a></li>
+					<li class="crumb"><a href="/" class="btn btn-sm variant-soft-primary">Users</a></li>
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-					<li class="crumb"><a href="/"><i class="fa-solid fa-skull" /></a></li>
+					<li class="crumb">
+						<a href="/"><Avatar src="https://i.pravatar.cc/?img=5" width="w-8" /></a>
+					</li>
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-					<li>Current</li>
+					<li>Settings</li>
 				</ol>
 			</div>
 			<!-- 4 -->
 			<div class="card variant-glass p-4">
-				<ol class="breadcrumb">
-					<li class="crumb">
-						<i class="fa-solid fa-house" />
-						<a href="/">Home</a>
-					</li>
+				<ol class="breadcrumb mt-1">
+					<li class="crumb"><i class="fa-solid fa-house text-xl" /></li>
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-					<li class="crumb">
-						<i class="fa-solid fa-skull" />
-						<a href="/">About</a>
-					</li>
+					<li class="crumb"><i class="fa-solid fa-gears text-xl" /></li>
 					<li class="crumb-separator" aria-hidden>&rsaquo;</li>
 					<li>Current</li>
 				</ol>
@@ -106,6 +102,7 @@
 		<section class="space-y-4">
 			<h2>Using Icons</h2>
 			<p>Add your icon before or after your anchor tag within the <code>.crumb</code> list item.</p>
+			<CodeBlock language="html" code={`<li class="crumb-separator" aria-hidden>(icon)</li>`} />
 			<CodeBlock
 				language="html"
 				code={`
@@ -127,6 +124,39 @@
 			<h2>Current Page</h2>
 			<p>We recommend the last item not include an anchor link, as this indicates the page the user is currently on.</p>
 			<CodeBlock language="html" code={`<li>Current</li>`} />
+		</section>
+		<section class="space-y-4">
+			<h2>Using #Each Loops</h2>
+			<p>
+				Use the following technique to ensure the <em>current</em> item is formatted properly and prevent an extra seperator at the end.
+			</p>
+			<CodeBlock
+				language="ts"
+				code={`
+const myBreadcrumbs = [
+	{ label: 'Foo', link: '/foo' },
+	{ label: 'Bar', link: '/bar' },
+	{ label: 'Fizz', link: '/fizz' },
+	{ label: 'Buzz', link: '/buzz' }
+];
+			`}
+			/>
+			<CodeBlock
+				language="html"
+				code={`
+<ol class="breadcrumb">
+	{#each myBreadcrumbs as crumb, i}
+		<!-- If crumb index is less than the breadcrumb length minus 1 -->
+		{#if i < myBreadcrumbs.length - 1}
+			<li class="crumb"><a href={crumb.link}>{crumb.label}</a></li>
+			<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+		{:else}
+			<li class="crumb">{crumb.label}</li>
+		{/if}
+	{/each}
+</ol>
+			`}
+			/>
 		</section>
 	</svelte:fragment>
 </DocsShell>
