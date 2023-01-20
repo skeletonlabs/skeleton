c1796f08d811280167daee62e914900172189e3a
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-01-19 19:04:11 -0500

    change plugin import on docs

--- src/routes/(inner)/guides/tailwind/+page.svelte
+++ src/routes/(inner)/guides/tailwind/+page.svelte
@@ -95,8 +95,7 @@ const config = {
 	// ...
 	plugins: [
 		// Keep any existing plugins present and append the following:
-		require('@skeletonlabs/skeleton/tailwind/skeleton.cjs'),
-		require('@skeletonlabs/skeleton/tailwind/intellisense.cjs')
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
 	]
 }
 `}
