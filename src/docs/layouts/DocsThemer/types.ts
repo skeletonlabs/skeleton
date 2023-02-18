0af0d9e0025149aae92bbe4a9c24c69ef7165c40
Rhys <36913739+ryceg@users.noreply.github.com>
2023-02-18 16:18:21 +1100

    fix import

--- src/docs/layouts/DocsThemer/types.ts
+++ src/docs/layouts/DocsThemer/types.ts
@@ -1,4 +1,4 @@
-import type { SemanticNames } from '$lib/components/ConicGradient/settings';
+import type { SemanticNames } from '$lib/types';
 
 export interface ColorSettings {
 	key: SemanticNames;
