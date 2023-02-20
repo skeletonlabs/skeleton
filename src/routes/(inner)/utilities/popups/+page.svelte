b8e94432b4ce26c81878f9ceae103c1d622ca194
endigo9740 <gundamx9740@gmail.com>
2023-02-20 15:59:29 -0600

    Quick popup fixes

--- src/routes/(inner)/utilities/popups/+page.svelte
+++ src/routes/(inner)/utilities/popups/+page.svelte
@@ -88,13 +88,18 @@
 					<span>Menu</span>
 					<span class="badge bg-white/10 dark:bg-black/10">Tap</span>
 				</button>
-				<div class="card variant-filled-secondary p-4 w-72 shadow-xl space-y-4" data-popup="exampleMenu">
-					<p class="font-bold">This is a <strong>Menu</strong> example.</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta amet nam hic aspernatur cum porro praesentium. Voluptates velit
-						ex ad eius sit! Sit deserunt ex accusamus quod fugit enim in?
-					</p>
-					<button class="btn variant-filled">Close Me</button>
+				<div class="card variant-filled-secondary p-4 w-72 shadow-xl" data-popup="exampleMenu">
+					<!-- NOTE: Keep this wrapper, .space-y will affect the arrow -->
+					<div class="space-y-4">
+						<p class="font-bold">This is a <strong>Menu</strong> example.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta amet nam hic aspernatur cum porro praesentium. Voluptates
+							velit ex ad eius sit! Sit deserunt ex accusamus quod fugit enim in?
+						</p>
+						<button class="btn variant-filled">Close Me</button>
+					</div>
+					<!-- Arrow -->
+					<div class="arrow variant-filled-secondary" />
 				</div>
 			</div>
 			<!-- Combobox -->
