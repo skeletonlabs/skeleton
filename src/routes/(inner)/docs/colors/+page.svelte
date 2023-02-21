85ce68413339f8609cf703ef51f5449538f3bfa9
endigo9740 <gundamx9740@gmail.com>
2023-02-21 14:48:45 -0600

    Blog page adjustments

--- src/routes/(inner)/docs/colors/+page.svelte
+++ src/routes/(inner)/docs/colors/+page.svelte
@@ -8,50 +8,19 @@
 
 	const cSwatch = 'w-4 aspect-square rounded-full';
 	const tableProps: TableSource = {
-		head: ['Color', 'ID', 'Example', 'Description'],
+		head: ['Color', 'ID', 'Example'],
 		body: [
-			[
-				`<div class="${cSwatch} bg-primary-500" />`,
-				'<code>primary</code>',
-				'[style]-primary-[50-900]',
-				'Typically your primary brand color.'
-			],
-			[
-				`<div class="${cSwatch} bg-secondary-500" />`,
-				'<code>secondary</code>',
-				'[style]-secondary-[50-900]',
-				'A secondary branding color for supplementary value.'
-			],
-			[
-				`<div class="${cSwatch} bg-tertiary-500" />`,
-				'<code>tertiary</code>',
-				'[style]-tertiary-[50-900]',
-				'A third and additional branding color.'
-			],
+			[`<div class="${cSwatch} bg-primary-500" />`, '[style]-primary-[50-900]', 'Typically your primary brand color.'],
+			[`<div class="${cSwatch} bg-secondary-500" />`, '[style]-secondary-[50-900]', 'A secondary branding color for supplementary value.'],
+			[`<div class="${cSwatch} bg-tertiary-500" />`, '[style]-tertiary-[50-900]', 'A third and additional branding color.'],
 			[
 				`<div class="${cSwatch} bg-success-500" />`,
-				'<code>success</code>',
 				'[style]-success-[50-900]',
 				'Used for successful or positive actions, such as a form submit alert.'
 			],
-			[
-				`<div class="${cSwatch} bg-warning-500" />`,
-				'<code>warning</code>',
-				'[style]-warning-[50-900]',
-				'Used for negative or harmful actions, such as errors.'
-			],
-			[
-				`<div class="${cSwatch} bg-error-500" />`,
-				'<code>error</code>',
-				'[style]-error-[50-900]',
-				'May be used for errors, alerts, and invalid inputs.'
-			],
-			[
-				`<div class="${cSwatch} bg-surface-500" />`,
-				'<code>surface</code>',
-				'[style]-surface-[50-900]',
-				'The base level colors, using for your background, cards, and other layered elements.'
-			]
+			[`<div class="${cSwatch} bg-warning-500" />`, '[style]-warning-[50-900]', 'Used for negative or harmful actions, such as errors.'],
+			[`<div class="${cSwatch} bg-error-500" />`, '[style]-error-[50-900]', 'May be used for errors, alerts, and invalid inputs.'],
+			[`<div class="${cSwatch} bg-surface-500" />`, '[style]-surface-[50-900]', 'The base level colors, used for backgrounds.']
 		]
 	};
 </script>
