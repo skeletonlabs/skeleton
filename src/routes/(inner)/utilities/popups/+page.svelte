5bddd688c942e0d4b17a033b9284c3173dab0037
Francis Nimick <fnimick@gmail.com>
2023-02-22 15:22:03 -0500

    Fix `middleware` typo in Popups docs

--- src/routes/(inner)/utilities/popups/+page.svelte
+++ src/routes/(inner)/utilities/popups/+page.svelte
@@ -30,7 +30,7 @@
 			['<code>target</code>', 'string', '-', '-', 'Match the popup data value of <code>[data-popup]</code>'],
 			['<code>placement</code>', 'string', '-', 'bottom', 'Set the placement position.'],
 			['<code>closeQuery</code>', 'string', 'a[href], button', '-', 'Query list of elements that will close the popup.'],
-			['<code>middelware</code>', 'object', '-', '-', 'Provide Floating UI middleware settings.'],
+			['<code>middleware</code>', 'object', '-', '-', 'Provide Floating UI middleware settings.'],
 			['<code>state</code>', 'function', '-', '-', 'Provide an optional callback function to monitor open/close state.']
 		],
 		keyboard: [
