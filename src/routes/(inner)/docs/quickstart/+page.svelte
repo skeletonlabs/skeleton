ec4cf523cd8f303ab00b13e26852b46d918f8cdd
Prasad Joshi <94825413+predatorprasad@users.noreply.github.com>
2023-02-26 17:40:12 +0530

    Fixes: #1037
    I have removed the closing tag and now it reads `<section>` insted of ~~</section>~~ in **line 77**

--- src/routes/(inner)/docs/quickstart/+page.svelte
+++ src/routes/(inner)/docs/quickstart/+page.svelte
@@ -74,7 +74,7 @@ npm run dev
 	<h1>Hello Skeleton</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-	</section>
+	<section>
 		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/" target="_blank" rel="noreferrer">SvelteKit</a>
 		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind</a>
 		<a class="btn variant-filled-tertiary" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
