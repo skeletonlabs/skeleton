b631d5126a0a8f4eade832ec2f195e8f01bed140
endigo9740 <gundamx9740@gmail.com>
2023-02-27 16:25:42 -0600

    Initial change

--- src/docs/components/DocsFooter/DocsFooter.svelte
+++ src/docs/components/DocsFooter/DocsFooter.svelte
@@ -24,7 +24,7 @@
 			<div class="space-y-4">
 				<h6>About</h6>
 				<ul class="space-y-2">
-					<li><a href="/docs/why">Why Skeleton</a></li>
+					<li><a href="/docs/introduction">Introduction</a></li>
 					<li><a href="/docs/comparisons">Comparisons</a></li>
 					<li><a href="/docs/contributing">Contribute</a></li>
 				</ul>
