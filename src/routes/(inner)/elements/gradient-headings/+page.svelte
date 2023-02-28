a72de241ebd2a43a19595280a042f0957bca3bb7
Logan <logan@firefly.llc>
2023-02-28 10:13:56 -0700

    Updated sandbox codeblock to match the preview.

--- src/routes/(inner)/elements/gradient-headings/+page.svelte
+++ src/routes/(inner)/elements/gradient-headings/+page.svelte
@@ -38,9 +38,13 @@
 					language="html"
 					code={`
 <h1>
-	<span class="bg-gradient-to-br from-primary-500 via-tertiary-500 to-secondary-500 bg-clip-text text-transparent box-decoration-clone">
-		Skeleton
-	</span>
+	<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">Design.</span>
+</h1>
+<h1>
+	<span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Build.</span>
+</h1>
+<h1>
+	<span class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone">Deploy.</span>
 </h1>
 				`}
 				/>
