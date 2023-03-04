aa3ea0315204a9e7e36f90b69545fe91d39ecc44
Nik <nik@niktek.io>
2023-03-04 15:21:57 +1100

    Logging stripped

--- src/routes/(inner)/redirect/[destination]/+page.ts
+++ src/routes/(inner)/redirect/[destination]/+page.ts
@@ -4,8 +4,6 @@ import { redirect } from '@sveltejs/kit';
 
 // This exists because JSDoc doesn't like multiple '-' characters in a line and a lot of our url's have hyphen's, so we do redirects to the natural destination
 export const load = (({ params }) => {
-	console.log(params.destination);
-	
 	switch (params.destination) {
 		case "appbar":
 			throw redirect(302, "/components/app-bar")
