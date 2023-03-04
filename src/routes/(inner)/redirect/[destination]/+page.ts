62a3537de77e0368370f0130de69b0ef331b89fd
Nik <nik@niktek.io>
2023-03-04 15:21:29 +1100

    Simple redirect

---
+++ src/routes/(inner)/redirect/[destination]/+page.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+// This exists because JSDoc doesn't like multiple '-' characters in a line and a lot of our url's have hyphen's, so we do redirects to the natural destination
+export const load = (({ params }) => {
+	console.log(params.destination);
+	
+	switch (params.destination) {
+		case "appbar":
+			throw redirect(302, "/components/app-bar")
+	}
+}) satisfies PageLoad;
