cc39a7b5ec86958f958db6d40a091975aa337d7d
endigo9740 <gundamx9740@gmail.com>
2023-03-05 15:50:09 -0600

    Sponsors and Contributors sections ready

--- src/routes/home-partials/HomeContributors.svelte
+++ src/routes/home-partials/HomeContributors.svelte
@@ -9,37 +9,39 @@
 		<h2>Built by Awesome People.</h2>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 	</div>
-	<div class="grid grid-cols-1 lg:grid-cols-3 gap-10 items-start">
-		<!-- Skeleton Labs -->
-		<div class="col-span-2">
-			<div class="card variant-glass-secondary p-4 grid grid-cols-2 gap-4 md:gap-10 items-center">
-				<!-- Info -->
-				<div class="space-y-4 text-right">
-					<h2>Skeleton Labs</h2>
-					<p>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta perspiciatis quisquam impedit cupiditate rerum error. Eaque
-						voluptate reprehenderit non, odio officia, rerum commodi saepe vero odit omnis facere.
-					</p>
-					<a href={'#'} class="btn variant-filled">Learn More</a>
+	<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
+		<!-- Premium Sponsors -->
+		<div class="card variant-soft p-10 space-y-10">
+			<div class="grid grid-cols-[1fr_auto] gap-4 items-center">
+				<h2>Skeleton Labs</h2>
+				<a class="btn variant-filled" href="https://skeletonlabs.co/" target="_blank" rel="noreferrer">
+					<span>Learn More</span>
+					<i class="fa-solid fa-arrow-up-right-from-square" />
+				</a>
+			</div>
+			<hr />
+			<div class="grid grid-cols-2 gap-4">
+				<div class="flex flex-col items-center gap-2">
+					<Avatar src="https://avatars.githubusercontent.com/u/1509726?v=4" width="w-32" />
+					<h4>Chris Simmons</h4>
+					<p class="opacity-75">Co-founder and core maintainer.</p>
 				</div>
-				<!-- Members -->
-				<div class="space-y-4">
-					<div class="card variant-soft p-4 md:py-16 text-center">
-						<p>Chris Simmons</p>
-					</div>
-					<div class="card variant-soft p-4 md:py-16 text-center">
-						<p>Trey Weir</p>
-					</div>
+				<div class="flex flex-col items-center gap-2">
+					<Avatar src="https://avatars.githubusercontent.com/u/10255430?v=4" width="w-32" />
+					<h4>Trey Weir</h4>
+					<p class="opacity-75">Co-founder and business relations.</p>
 				</div>
 			</div>
 		</div>
-		<!-- Contributors -->
-		<div class="grid grid-cols-6 gap-2 max-w-[480px] mx-auto">
-			{#each contributors as c}
-				<a href={c.html_url} target="_blank" rel="noreferrer" title={c.login}>
-					<Avatar src={c.avatar_url} width="w-full" />
-				</a>
-			{/each}
+		<!-- Info -->
+		<div class="space-y-4 text-center lg:text-left">
+			<div class="grid grid-cols-6 gap-2 max-w-[480px] mx-auto">
+				{#each contributors as c}
+					<a href={c.html_url} target="_blank" rel="noreferrer" title={c.login}>
+						<Avatar src={c.avatar_url} width="w-full" />
+					</a>
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
--- src/routes/home-partials/HomeSponsors.svelte
+++ src/routes/home-partials/HomeSponsors.svelte
@@ -14,13 +14,13 @@
 		<Avatar src="https://i.imgur.com/ySBlLmQ.png" width="w-full max-w-[180px]" shadow="shadow-xl" />
 	</div>
 	<!-- Info -->
-	<div class="space-y-4">
+	<div class="space-y-4 text-center lg:text-left">
 		<h2>Help Support Skeleton.</h2>
 		<p class="max-w-[480px]">
 			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque dolorem provident quasi illo, nostrum autem. Culpa eius error
-			modi odit veritatis? Quaerat exercitationem repellat.
+			modi odit veritatis? Quaerat exercitationem.
 		</p>
-		<div class="flex gap-4">
+		<div class="flex justify-center lg:justify-start gap-4">
 			<a class="btn variant-filled" href="https://ko-fi.com/skeletonlabs" target="_blank" rel="noreferrer">Ko-Fi</a>
 			<a class="btn variant-filled" href="https://github.com/sponsors/skeletonlabs" target="_blank" rel="noreferrer">GitHub</a>
 			<a class="btn variant-filled" href="https://patreon.com/user?u=83786276" target="_blank" rel="noreferrer">Patreon</a>
