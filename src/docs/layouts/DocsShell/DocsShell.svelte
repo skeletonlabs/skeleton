69164125e1760850008c6291a101f8c9bdd6544b
Chris Simmons <gundamx9740@gmail.com>
2023-03-09 19:53:46 -0600

    Chore/responsive design audit (#1114)
    * Adjust browser support grid
    
    * Logo cloud grid breakpoints
    
    * Minimum breakpoint audit fixes
    
    * Homepage contributor minor adjustment

--- src/docs/layouts/DocsShell/DocsShell.svelte
+++ src/docs/layouts/DocsShell/DocsShell.svelte
@@ -25,7 +25,7 @@
 
 	// Classes
 	const cBase = 'relative';
-	const cTabs = 'max-w-5xl mx-auto'; // sticky top-0 z-[1]
+	const cTabs = 'max-w-5xl mx-auto hide-scrollbar'; // sticky top-0 z-[1]
 	const cPanels = 'space-y-10';
 
 	// Local
