b74d6eb8bb6b4aa1f7d9219ad4afa96cb8ccc428
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2023-03-17 06:47:21 -0400

    Rewrote filtering functionality. Items now remain in the llist even when the exact value is type in.

--- src/lib/components/Autocomplete/Autocomplete.svelte
+++ src/lib/components/Autocomplete/Autocomplete.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { stringify } from "postcss";
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 
@@ -35,58 +36,107 @@
 
 	function isMatch(obj: Record<string, unknown>) {
 		const keys = Object.keys(obj);
+		const inputFormatted = input.toLowerCase();
 		let matched = false;
+
 		keys.forEach((key) => {
-			const stringyValue = JSON.stringify(obj[key]);
-			matched = stringyValue.includes(input);
+			const stringyValue = JSON.stringify(obj[key]).replaceAll('"','').toLowerCase();
+			matched = stringyValue.includes(inputFormatted);
 		});
+
 		return matched;
 	}
 
-	function filterDefault(input: string, whitelist: Record<string, unknown>[]): Record<string, unknown>[] {
+	function filter(input: string, whitelist: Record<string, unknown>[], mode?: string): Record<string, unknown>[] {
 		return options.filter((row) => {
 			/** Searching using options instead of row, so it doesn't matter what kind of KvP naming convetion is used. */
-			if (options.some(isMatch) && whitelist.some(isMatch)) {
-				/** Stringify adds "" to the start and end of the new string. Those add difficulty to the search, so we remove them. */
-				const stringyValue = JSON.stringify(row.value).replaceAll('"', '');
-				/**
-				 * Grabs the first set of chars, that match the length of the input
-				 * this prevents things like 'bar' matching 'bar' and 'foobar' when it should
-				 * only match 'bar'.
-				 */
-				if (stringyValue.substring(0, input.length) === input) return row;
+			if(options.some(isMatch) && whitelist.some(isMatch)){
+				const keys = Object.keys(row);
+				let matched = false;
+	
+				keys.map((key) => {
+					/** Stringify adds "" to the start and end of the new string. Those add difficulty to the search, so we remove them. */
+					const value = JSON.stringify(row[key]).replaceAll('"','').toLowerCase();
+	
+					switch (mode) {
+						case 'exclude':
+							/** Prevents matching when words from letters that aren't at from the start of the string to the inputs length */
+							if(value.substring(0, input.length) !== input || value.substring(input.length) !== '') 
+								matched = true;
+						break;
+						default:
+							/** Prevents matching when words from letters that aren't at from the start of the string to the inputs length */
+							if(value.substring(0, input.length) === input) 
+								matched = true;
+						break;
+					}
+					
+				});
+	
+				if(matched) return row;
 			}
-		});
+		});	
 	}
 
-	function filterExclude(input: string, whitelist: Record<string, unknown>[]): Record<string, unknown>[] {
-		return options.filter((row) => {
-			/** Return rows that don't match input, or return nothing if nothing matches. */
-			if (options.some(isMatch) && whitelist.some(isMatch)) {
-				/** Stringify adds "" to the start and end of the new string. Those add difficulty to the search, so we remove them. */
-				const stringyValue = JSON.stringify(row.value).replaceAll('"', '');
-
-				if (!stringyValue.includes(input) || stringyValue.substring(input.length) !== '') return row;
-			} else return [];
-		});
-	}
+	//NOTE: Chris - I feel like we can remove these individual functions? I have the functionality so similar for filtering, the only difference is if the conditional check that determines when it needs returned.
+	// function filterDefault(input: string, whitelist: Record<string, unknown>[]): Record<string, unknown>[] {
+	// 	return options.filter((row) => {
+	// 		/** Searching using options instead of row, so it doesn't matter what kind of KvP naming convetion is used. */
+	// 		if(options.some(isMatch) && whitelist.some(isMatch)){
+	// 			const keys = Object.keys(row);
+	// 			let matched = false;
+	
+	// 			keys.map((key) => {
+	// 				/** Stringify adds "" to the start and end of the new string. Those add difficulty to the search, so we remove them. */
+	// 				const value = JSON.stringify(row[key]).replaceAll('"','').toLowerCase();
+	
+	// 				/**
+	// 				 * Grabs the first set of chars, that match the length of the input
+	// 				 * this prevents things like 'bar' matching 'bar' and 'foobar' when it should
+	// 				 * only match 'bar'.
+	// 				*/
+	// 				if(value.substring(0, input.length) === input) matched = true;
+	// 			});
+	
+	// 			if(matched) return row;
+	// 		}
+	// 	});
+	// }
+	
+	// function filterExclude(input: string, whitelist: Record<string, unknown>[]): Record<string, unknown>[] {
+	// 	return options.filter((row) => {
+	// 		/** Searching using options instead of row, so it doesn't matter what kind of KvP naming convetion is used. */
+	// 		if (options.some(isMatch) && whitelist.some(isMatch)) {
+	// 			const keys = Object.keys(row);
+	// 			let matched = false;
+	
+	// 			keys.map((key) => {
+	// 				/** Stringify adds "" to the start and end of the new string. Those add difficulty to the search, so we remove them. */
+	// 				const value = JSON.stringify(row[key]).replaceAll('"','').toLowerCase();
+	
+	// 				/**
+	// 				 * Grabs the first set of chars, that match the length of the input
+	// 				 * this prevents things like 'bar' matching 'bar' and 'foobar' when it should
+	// 				 * only match 'bar'.
+	// 				*/
+	// 				if(value.substring(0, input.length) !== input || value.substring(input.length) !== '') matched = true;
+	// 			});
+	
+	// 			if(matched) return row;
+	// 		} else return [];
+	// 	});
+	// }
 
 	$: filteredOptions = () => {
 		if (!input) return [...options];
 		const inputFormatted = input.toLowerCase();
-		// Filter based on mode
-		switch (mode) {
-			case 'exclude':
-				return filterExclude(inputFormatted, whitelist);
-			default:
-				return filterDefault(inputFormatted, whitelist);
-		}
+
+		return filter(inputFormatted, whitelist, mode);
 	};
 
 	// Reactive
 	$: classesSelect = `${cSelect} ${element} ${$$props.class ?? ''}`;
 	$: classesOption = `${cOption} ${maxHeight}`;
-	// $: reactiveSize = filteredOptions().length < 4 ? filteredOptions.length : 4;
 
 	// RestProps
 	function prunedRestProps(): any {
