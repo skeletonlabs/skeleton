efbc2cf14a455cf6456ad185f36b04782de00035
endigo9740 <gundamx9740@gmail.com>
2023-03-31 13:53:42 -0500

    Added AppRailTile to import

--- src/routes/(inner)/components/app-rail/+page.svelte
+++ src/routes/(inner)/components/app-rail/+page.svelte
@@ -21,7 +21,7 @@
 		feature: DocsFeature.Component,
 		name: 'App Rail',
 		description: 'A side navigation rail component',
-		imports: ['AppRail'],
+		imports: ['AppRail', 'AppRailTile'],
 		source: 'components/AppRail',
 		// aria: 'https://www.w3.org/WAI/ARIA/apg/',
 		components: [
