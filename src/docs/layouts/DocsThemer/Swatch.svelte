93f2a9f3b1de1a121b243113a7b42eeec3c940af
Rhys <36913739+ryceg@users.noreply.github.com>
2023-04-04 20:09:51 +1000

    Add hex, updates on change

--- src/docs/layouts/DocsThemer/Swatch.svelte
+++ src/docs/layouts/DocsThemer/Swatch.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	import type { SemanticNames, tailwindNumbers, TailwindNumbers } from '$lib/types/tailwind';
+	import type { SemanticNames } from '$lib/types/tailwind';
 	import { clipboard, toastStore, type ToastSettings } from '@skeletonlabs/skeleton';
-	import { handleStringColor } from './colors';
+	import chroma from 'chroma-js';
+	import { generateA11yHex, handleStringColor } from './colors';
 	import { swatchColorClasses } from './settings';
-
 	/** Pass the color key name. */
 	export let color: SemanticNames = 'primary';
-	function getHex(semanticName: SemanticNames, tailwindNumber: string) {
-		if (typeof window === 'undefined') return;
+	export let hex = getHex(color, '500');
+	$: hex = getHex(color, '500');
 
-		return handleStringColor(
-			window?.getComputedStyle(document.documentElement).getPropertyValue(`--color-${semanticName}-${tailwindNumber}`).trim()
-		);
+	/**
+	 * Get the hex value of a color from the CSS variable.
+	 * @param color - The color key name.
+	 * @param tailwindNumber - The tailwind number.
+	 * @param hex - This is a weird but necessary evil; it's a dependency that forces the re-render when the hex (i.e. the master color) changes.
+	 */
+	function getHex(color: SemanticNames, tailwindNumber: string, hex?: string) {
+		if (typeof window === 'undefined') return;
+		console.log(color, tailwindNumber);
+		// we get the color from the CSS variable to ensure we're operating from a single source of truth
+		const cssVar = window.getComputedStyle(document.documentElement).getPropertyValue(`--color-${color}-${tailwindNumber}`);
+		return chroma(handleStringColor(cssVar)).hex();
 	}
 
 	function onClickHandler(text?: string): void {
@@ -29,11 +38,19 @@
 			}
 		);
 	}
+
+	function createHoverAccessibleText(shade: string) {
+		const hexCode = getHex(color, shade, hex);
+		if (!hexCode) return 'hover:text-white/100';
+		const blackOrWhite = generateA11yHex(hexCode);
+		if (blackOrWhite === '#000000') return 'hover:text-black/100';
+		return 'hover:text-white/100';
+	}
 </script>
 
 <div class="w-full grid grid-cols-11 gap-0 hover:grid-cols-[repeat(13,_minmax(0,_1fr))] transition-all duration-1000">
 	{#each Object.entries(swatchColorClasses[color]) as [shade, shadeClasses]}
-		{@const accessibleText = parseInt(shade) < 500 ? 'hover:text-black/100' : 'hover:text-white/100'}
+		{@const accessibleText = createHoverAccessibleText(shade)}
 		<div
 			class="grid grid-rows-[1fr_40px] hover:col-span-3 transition-all duration-1000 text-center"
 			class:col-span-2={shade === '500'}
@@ -47,18 +64,22 @@
 			<div class="bg-black/5 dark:bg-white/5">
 				<div class="h-full flex justify-center items-center {shadeClasses} ">
 					{#if shade === '500'}
-						<div class="grid grid-cols-[auto_1fr] hover:col-span-4 gap-1.5 place-items-center">
+						<button
+							on:click={() => onClickHandler(getHex(color, shade, hex))}
+							on:keypress={() => onClickHandler(getHex(color, shade, hex))}
+							class="grid grid-cols-[auto_1fr] hover:col-span-4 gap-1.5 place-items-center"
+						>
 							<i class="fa-solid fa-skull" />
-							<span class="hidden xl:inline-block">Text</span>
-						</div>
+							<span class="hidden xl:inline-block">{getHex(color, shade, hex)}</span>
+						</button>
 					{:else}
 						<button
-							on:click={() => onClickHandler(getHex(color, shade))}
-							on:keypress={() => onClickHandler(getHex(color, shade))}
-							use:clipboard={getHex(color, shade)}
+							on:click={() => onClickHandler(getHex(color, shade, hex))}
+							on:keypress={() => onClickHandler(getHex(color, shade, hex))}
+							use:clipboard={getHex(color, shade, hex)}
 							class="hover:block w-full h-full place-items-center text-black/0 select-none hover:select-text {accessibleText}"
 						>
-							{getHex(color, shade)}
+							{getHex(color, shade, hex)}
 						</button>
 					{/if}
 				</div>
