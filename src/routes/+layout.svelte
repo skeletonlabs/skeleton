0b6de9d8f8ca5a89a25115873c0b81b335605fc1
endigo9740 <gundamx9740@gmail.com>
2023-04-26 14:02:51 -0500

    Update typography structure to format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 <script lang="ts">
 	// Dependency: Highlight JS
 	import hljs from 'highlight.js';
-	import '$lib/styles/highlight-js.css'; // was: 'highlight.js/styles/github-dark.css';
+	import '$docs/styles/highlight-js.css'; // was: 'highlight.js/styles/github-dark.css';
 	import { storeHighlightJs } from '$lib/utilities/CodeBlock/stores';
 	storeHighlightJs.set(hljs);
 
@@ -41,7 +41,12 @@
 	import ModalExampleImage from '$docs/modals/examples/ModalExampleImage.svelte';
 
 	// Skeleton Stylesheets
-	import '$lib/styles/all.css';
+	// import '$lib/styles/all.css';
+	import '$lib/styles/skeleton.css';
+	// import '$lib/styles/skeleton-next.css';
+	// import '$lib/styles/skeleton-minimal.css';
+	// import '$lib/styles/partials/core.css';
+	// import '$lib/styles/partials/typography-next.css';
 	// The Skeleton blog stylesheet
 	import '$docs/styles/blog.css';
 	// Global Stylesheets
