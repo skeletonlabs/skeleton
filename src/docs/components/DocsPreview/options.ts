b056f166bcc95f72520da5602feae0c92bd2991f
Chris Simmons <gundamx9740@gmail.com>
2023-04-27 14:15:43 -0500

    Implemented gradient variants and docs (#1388)

--- src/docs/components/DocsPreview/options.ts
+++ src/docs/components/DocsPreview/options.ts
@@ -3,19 +3,31 @@
 export const backgrounds: Record<string, string> = {
 	'bg-transparent': 'bg-transparent',
 	neutral: 'bg-white/10 dark:bg-black/10',
+	'neutral-opaque': 'bg-white/50 dark:bg-black/50',
 	// Surfaces
 	'bg-surface-50': 'bg-surface-50 text-surface-900',
 	'bg-surface-900': 'bg-surface-900 text-surface-50',
 	// Filled
+	'variant-filled-surface': `variant-filled-surface`,
 	'variant-filled-primary': `variant-filled-primary`,
 	'variant-filled-secondary': `variant-filled-secondary`,
 	'variant-filled-tertiary': `variant-filled-tertiary`,
 	'variant-filled-success': `variant-filled-success`,
 	'variant-filled-warning': `variant-filled-warning`,
 	'variant-filled-error': `variant-filled-error`,
-	'variant-filled-surface': `variant-filled-surface`,
 	// Gradient
-	'primary-to-secondary': 'bg-gradient-to-br from-primary-500 to-secondary-500 text-on-primary-token'
+	'primary-to-secondary': 'bg-gradient-to-br variant-gradient-primary-secondary',
+	'secondary-to-tertiary': 'bg-gradient-to-br variant-gradient-secondary-tertiary',
+	'tertiary-to-primary': 'bg-gradient-to-br variant-gradient-tertiary-primary',
+	'secondary-to-primary': 'bg-gradient-to-br variant-gradient-secondary-primary',
+	'tertiary-to-secondary': 'bg-gradient-to-br variant-gradient-tertiary-secondary',
+	'primary-to-tertiary': 'bg-gradient-to-br variant-gradient-primary-tertiary',
+	'success-to-warning': 'bg-gradient-to-br variant-gradient-success-warning',
+	'warning-to-error': 'bg-gradient-to-br variant-gradient-warning-error',
+	'error-to-success': 'bg-gradient-to-br variant-gradient-error-success',
+	'warning-to-success': 'bg-gradient-to-br variant-gradient-warning-success',
+	'error-to-warning': 'bg-gradient-to-br variant-gradient-error-warning',
+	'success-to-error': 'bg-gradient-to-br variant-gradient-success-error'
 };
 
 export const variants: any[] = [
@@ -77,6 +89,24 @@ export const variants: any[] = [
 			'variant-ringed-success',
 			'variant-ringed-warning',
 			'variant-ringed-error'
+		]
+	},
+	// gradient
+	{
+		label: 'Gradient Variants',
+		list: [
+			'bg-gradient-to-br variant-gradient-primary-secondary',
+			'bg-gradient-to-br variant-gradient-secondary-tertiary',
+			'bg-gradient-to-br variant-gradient-tertiary-primary',
+			'bg-gradient-to-br variant-gradient-secondary-primary',
+			'bg-gradient-to-br variant-gradient-tertiary-secondary',
+			'bg-gradient-to-br variant-gradient-primary-tertiary',
+			'bg-gradient-to-br variant-gradient-success-warning',
+			'bg-gradient-to-br variant-gradient-warning-error',
+			'bg-gradient-to-br variant-gradient-error-success',
+			'bg-gradient-to-br variant-gradient-warning-success',
+			'bg-gradient-to-br variant-gradient-error-warning',
+			'bg-gradient-to-br variant-gradient-success-error'
 		]
 	}
 ];
