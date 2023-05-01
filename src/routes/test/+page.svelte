95368773d4967cf7b4cde9be1f9d0c0964a1b5e3
Hugos68 <hugokorteapple@gmail.com>
2023-05-01 13:33:40 +0200

    Added additonal tests for nested forms

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -2,8 +2,25 @@
 	import { enhance } from "$app/forms";
 	import { InputChip } from "@skeletonlabs/skeleton";
 </script>
+    
+<div class="card p-4">
+    <p>Nested forms:</p>
+    <form class="outline p-2 m-2" method="post" use:enhance id="form-outer">
+        <InputChip name="test-outer" required form="form-outer"/>
+        <form class="outline p-2 m-2" method="post"  use:enhance id="form-inner">
+            <InputChip name="test-inner" required form="form-inner" />
+            <button>Inner Submit</button>
+        </form>
+        <button>Outer Submit</button>
+    </form>
+</div>
 
-<form method="post" use:enhance>
-    <InputChip name="test" required value={['some', 'default', 'values']} restToInitialValue={true} />
-    <button>Submit</button>
-</form>
+<div class="card p-4 Nested form test" >
+    <p>Singular form:</p>
+    <form class="outline p-2 m-2" method="post" use:enhance id="form">
+        <InputChip name="test" required form="form"/>
+        <button>Submit</button>
+    </form>
+</div>
+
+
