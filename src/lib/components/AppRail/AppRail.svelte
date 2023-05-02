a49d527e2a4c3d3e0cd759c17782338919fa8a59
unloco <amxmodx.unloco@gmail.com>
2023-05-02 10:43:33 +0100

    Add AppRail test

--- src/lib/components/AppRail/AppRail.svelte
+++ src/lib/components/AppRail/AppRail.svelte
@@ -53,7 +53,7 @@
 
 <!-- @component A vertical navigation rail component. -->
 
-<div class="app-rail {classesBase}">
+<div class="app-rail {classesBase}" data-testid="app-rail">
 	<!-- Slot: lead -->
 	<div class="app-bar-lead {regionLead}"><slot name="lead" /></div>
 	<!-- Slot: Default -->
