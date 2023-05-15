32e198ca8e59ac5d4a699c3a38f37a1433a3187c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-05-15 01:37:11 -0400

    add CSA to workspace

--- scripts/checkout.cjs
+++ scripts/checkout.cjs
@@ -5,5 +5,5 @@ if (!shell.which('git')) {
 	shell.exit(1);
 }
 
-shell.exec('git clone https://github.com/skeletonlabs/create-skeleton-app packages/create-skeleton-app')
-shell.exec('git clone https://github.com/skeletonlabs/skeletonlabs.co sites/skeletonlabs.co')
+shell.exec('git clone https://github.com/skeletonlabs/create-skeleton-app packages/create-skeleton-app');
+shell.exec('git clone https://github.com/skeletonlabs/skeletonlabs.co sites/skeletonlabs.co');
