9ad58f85b359a0e045656cd2083ac7b6af56c05e
Bert Vanhooff <bert.vanhooff@zinnonym.com>
2023-05-15 18:42:59 +0200

    #1453 Clipboard now uses text/html when copying a node. (#1454)
    * #1453 Clipboard now uses text/html when copying a node.
    
    Also: an event is fired when copying is ready.
    
    * #1453 Adds ts typing for custom event.
    
    Without this, svelte-check will fail.
    
    * #1453 Adds ts typing for custom event.
    
    Without this, svelte-check will fail.
    
    * #1453 Fixes an issue where pasting html content in a 'simple' editor doesn't work.
    
    Puts the content both as text/html and text/plain on the clipboard. The 'paste client' can than choose which content it can handle.

--- src/lib/actions/Clipboard/clipboard.ts
+++ src/lib/actions/Clipboard/clipboard.ts
@@ -1,24 +1,27 @@
 // Action: Clipboard
 
 export function clipboard(node: HTMLElement, args: any) {
+	const fireCopyCompleteEvent = () => {
+		node.dispatchEvent(new CustomEvent('copyComplete'));
+	};
 	const onClick = () => {
 		// Handle `data-clipboard` target based on object key
 		if (typeof args === 'object') {
 			// Element Inner HTML
 			if (Object.prototype.hasOwnProperty.call(args, 'element')) {
 				const element: HTMLElement | null = document.querySelector(`[data-clipboard="${args.element}"]`);
-				copyToClipboard(element?.innerHTML);
+				copyToClipboard(element?.innerHTML, 'text/html').then(fireCopyCompleteEvent);
 				return;
 			}
 			// Form Input Value
 			if (Object.prototype.hasOwnProperty.call(args, 'input')) {
 				const input: HTMLInputElement | null = document.querySelector(`[data-clipboard="${args.input}"]`);
-				copyToClipboard(input?.value);
+				copyToClipboard(input?.value).then(fireCopyCompleteEvent);
 				return;
 			}
 		}
 		// Handle everything else.
-		copyToClipboard(args);
+		copyToClipboard(args).then(fireCopyCompleteEvent);
 	};
 	// Event Listener
 	node.addEventListener('click', onClick);
@@ -34,6 +37,22 @@ export function clipboard(node: HTMLElement, args: any) {
 }
 
 // Shared copy method
-function copyToClipboard(data: any): void {
-	navigator.clipboard.writeText(String(data));
+async function copyToClipboard(data: any, mimeType = 'text/plain') {
+	if (navigator.clipboard.write) {
+		await navigator.clipboard.write([
+			new ClipboardItem({
+				[mimeType]: new Blob([data], {
+					type: mimeType
+				}),
+				['text/plain']: new Blob([data], {
+					type: 'text/plain'
+				})
+			})
+		]);
+	} else {
+		// fallback since .writeText has wider browser support
+		await new Promise((resolve) => {
+			resolve(navigator.clipboard.writeText(String(data)));
+		});
+	}
 }
