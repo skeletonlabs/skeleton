7760c97abcd410dfd15bc9e2498121003178e326
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2023-05-26 12:06:10 +0200

    add paginator controls

--- packages/skeleton/src/lib/components/Paginator/Paginator.svelte
+++ packages/skeleton/src/lib/components/Paginator/Paginator.svelte
@@ -15,6 +15,8 @@
 	export let disabled = false;
 	/** Show First and Last buttons. */
 	export let showFirstLastButtons = false;
+	/** Show page controls */
+	export let showPageControls = false;
 
 	// Props (styles)
 	/** Provide classes to style the select input. */
@@ -25,8 +27,14 @@
 	export let text = 'text-xs';
 	/** Set the text for the amount selection input. */
 	export let amountText = 'Items';
+	/** Provide arbitrary classes to the controls. */
+	export let controlsClasses: CssClasses = 'flex space-x-1';
 	/** Provide arbitrary classes to the next/previous buttons. */
-	export let buttonClasses: CssClasses = 'btn-icon variant-filled';
+	export let buttonClasses: CssClasses = 'btn-icon btn-icon-sm variant-filled';
+	/** Provide arbitrary classes to the page buttons group. */
+	export let pageButtonsGroupClasses: CssClasses = 'btn-group variant-filled h-8';
+	/** Provide arbitrary classes to the active page buttons. */
+	export let activePageButtonClasses: CssClasses = 'bg-primary-600 pointer-events-none';
 	/** Set the text label for the Previous button. */
 	export let buttonTextPrevious: CssClasses = '&larr;';
 	/** Set the text label for the Next button. */
@@ -41,28 +49,58 @@
 	const cLabel = 'w-full md:w-auto';
 	const cPageText = 'whitespace-nowrap';
 
+	// Local
+	let lastPage = Math.ceil(settings.size / settings.limit - 1);
+	let controlPages: number[] = getControlPages();
+
 	// Functionality
 	function onChangeLength(): void {
 		settings.offset = 0;
 		/** @event {{ length: number }} amount - Fires when the amount selection input changes.  */
 		dispatch('amount', settings.limit);
+
+		lastPage = Math.ceil(settings.size / settings.limit - 1);
+		controlPages = getControlPages();
 	}
-	function onPrev(): void {
-		settings.offset--;
+	function gotoPage(page: number) {
+		settings.offset = page;
 		/** @event {{ offset: number }} page Fires when the next/back buttons are pressed. */
 		dispatch('page', settings.offset);
+		controlPages = getControlPages();
 	}
-	function onNext(): void {
-		settings.offset++;
-		dispatch('page', settings.offset);
-	}
-	function onFirst(): void {
-		settings.offset = 0;
-		dispatch('page', settings.offset);
-	}
-	function onLast(): void {
-		settings.offset = Math.ceil(settings.size / settings.limit - 1);
-		dispatch('page', settings.offset);
+	function getControlPages() {
+		const maxPagesToShow = 5;
+		const pages = [];
+
+		if (lastPage <= maxPagesToShow) {
+			for (let index = 0; index <= lastPage; index++) {
+				pages.push(index);
+			}
+			return pages;
+		}
+
+		const isWithinLeftSection = settings.offset < maxPagesToShow - 2;
+		const isWithinRightSection = settings.offset > lastPage - (maxPagesToShow - 2);
+		pages.push(0);
+		if (!isWithinLeftSection) {
+			pages.push(-1);
+		}
+		if (!isWithinLeftSection && !isWithinRightSection) {
+			for (let i = settings.offset - 1; i <= settings.offset + 1; i++) {
+				pages.push(i);
+			}
+		} else {
+			const start = isWithinLeftSection ? 1 : lastPage - (maxPagesToShow - 1);
+			const end = isWithinRightSection ? lastPage - 1 : maxPagesToShow - 1;
+			for (let i = start; i <= end; i++) {
+				pages.push(i);
+			}
+		}
+		if (!isWithinRightSection) {
+			pages.push(-1);
+		}
+		pages.push(lastPage);
+		return pages;
 	}
 
 	// Reactive Classes
@@ -70,6 +108,8 @@
 	$: classesLabel = `${cLabel}`;
 	$: classesSelect = `${select}`;
 	$: classesPageText = `${cPageText} ${text}`;
+	$: classesControls = `${controlsClasses}`;
+	$: classesPageButtons = `${pageButtonsGroupClasses}`;
 </script>
 
 <!-- prettier-ignore -->
@@ -84,21 +124,31 @@
 	<span class="paginator-details {classesPageText}">
 		{settings.offset * settings.limit + 1} - {Math.min(settings.offset * settings.limit + settings.limit, settings.size)} <span class="opacity-50 px-2">/</span> <strong>{settings.size}</strong>
 	</span>
-	<!-- Arrows -->
-	<div class="paginator-arrows space-x-2">
+	<!-- Controls -->
+	<div class="paginator-controls {classesControls}">
 		{#if showFirstLastButtons}
-		<button type="button" class="{buttonClasses}" on:click={() => { onFirst() }} disabled={disabled || settings.offset === 0}>
+		<button type="button" class="{buttonClasses}" on:click={() => { gotoPage(0) }} disabled={disabled || settings.offset === 0}>
 			{@html buttonTextFirst}
 		</button>
 		{/if}
-		<button type="button" class="{buttonClasses}" on:click={() => { onPrev() }} disabled={disabled || settings.offset === 0}>
+		<button type="button" class="{buttonClasses}" on:click={() => { gotoPage(settings.offset - 1) }} disabled={disabled || settings.offset === 0}>
 			{@html buttonTextPrevious}
 		</button>
-		<button type="button" class="{buttonClasses}" on:click={() => { onNext() }} disabled={disabled || (settings.offset + 1) * settings.limit >= settings.size}>
+		{#if showPageControls}
+		<div class={classesPageButtons}>
+			{#each controlPages as page}
+				<button type='button' class={page === settings.offset ? activePageButtonClasses : page < 0 ? 'pointer-events-none' : ''} 
+					on:click={() => gotoPage(page)}>
+					{page >= 0 ? page : '...'}
+				</button>
+			{/each}
+		</div>
+		{/if}
+		<button type="button" class="{buttonClasses}" on:click={() => { gotoPage(settings.offset + 1) }} disabled={disabled || (settings.offset + 1) * settings.limit >= settings.size}>
 			{@html buttonTextNext}
 		</button>
 		{#if showFirstLastButtons}
-		<button type="button" class="{buttonClasses}" on:click={() => { onLast() }} disabled={disabled || (settings.offset + 1) * settings.limit >= settings.size}>
+		<button type="button" class="{buttonClasses}" on:click={() => { gotoPage(lastPage) }} disabled={disabled || (settings.offset + 1) * settings.limit >= settings.size}>
 			{@html buttonTextLast}
 		</button>
 		{/if}
