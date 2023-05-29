c9fdbf7fb0108ae4f62fe5b8e3f60ccf44627841
Nik <fakenik@niktek.io>
2023-05-29 17:06:05 +1000

    Trim down the files being copied

--- packages/create-skeleton-app/scripts/prepare-templates.mjs
+++ packages/create-skeleton-app/scripts/prepare-templates.mjs
@@ -6,7 +6,10 @@ function copyTemplates(){
     //get all folders in ../../templates directory
     fs.readdirSync('../../templates').forEach(file => {
         console.log(file);
-        fs.cpSync(`../../templates/${file}/src/`, `./templates/${file}/src/`, {recursive: true, filter: (src, dest) => {console.log("src:", src, "dest:", dest); }});
+        fs.cpSync(`../../templates/${file}/src/`, `./templates/${file}/src/`, {recursive: true});
+        fs.rmSync(`./templates/${file}/src/app.d.ts`);
+        fs.rmdirSync(`./templates/${file}/src/lib/`, {recursive: true});
+
         fs.cpSync(`../../templates/${file}/static/`, `./templates/${file}/static/`, {recursive: true});
         fs.cpSync(`../../templates/${file}/csa-meta.json`, `./templates/${file}/csa-meta.json`);
     });
