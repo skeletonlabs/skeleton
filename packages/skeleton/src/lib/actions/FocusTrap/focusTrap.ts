99c065e2e3e2870b483ece4642caa18a151235eb
Hugo <hugokorteapple@gmail.com>
2023-06-01 08:40:49 +0200

    Extracted logic from logic trap that was resuseable (rather than creating functions for every sentence, all focus trap now can use the same function to get children

--- packages/skeleton/src/lib/actions/FocusTrap/focusTrap.ts
+++ packages/skeleton/src/lib/actions/FocusTrap/focusTrap.ts
@@ -1,11 +1,9 @@
 // Action: Focus Trap
 export function focusTrap(node: HTMLElement, enabled: boolean) {
-	const elemWhitelist = 'a[href], button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])';
 	let firstFocusableChild: HTMLElement;
 	let lastFocusableChild: HTMLElement;
 
 	function keydownHandler(event: KeyboardEvent) {
-
 		// We do not care for any keys other than tab so we return early, also return if focustrap is disabled
 		// Note: All code after this statement is with 'Tab is pressed' context in mind.
 		if (event.key !== 'Tab' || !enabled) return;
@@ -14,7 +12,7 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 		if (event.shiftKey && document.activeElement === firstFocusableChild) {
 			event.preventDefault();
 			lastFocusableChild.focus();
-		} 
+		}
 
 		// If the currently active element is the lastFocusableChild, focus the first child
 		else if (!event.shiftKey && document.activeElement === lastFocusableChild) {
@@ -30,18 +28,10 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 	}
 	document.addEventListener('keydown', keydownHandler);
 
-	// Get first and last focusable (and therefor tabbable) child from element
-	function getFirstAndLastFocusableChild(element: HTMLElement) {
-		const focusableChilds: HTMLElement[] = Array.from(element.querySelectorAll(elemWhitelist));
-		const first = focusableChilds.at(0);
-		const last = focusableChilds.at(-1);
-		return { first, last };
-	}
-
-	function determineFocusableChilds(fromObserver: boolean)  {
+	function determineFocusableChilds(fromObserver: boolean) {
 		if (!enabled) return;
 		// Gather all focusable elements
-		const focusableElems: HTMLElement[] = Array.from(node.querySelectorAll(elemWhitelist));
+		const focusableElems: HTMLElement[] = Array.from(node.querySelectorAll(focusableElementSelector));
 		if (focusableElems.length) {
 			// Get first and last focusable child from the node
 			const { first, last } = getFirstAndLastFocusableChild(node);
@@ -51,9 +41,8 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 			// Auto-focus first focusable element only when not called from observer
 			if (!fromObserver) firstFocusableChild.focus();
 		}
-	};
+	}
 	determineFocusableChilds(false);
-	
 
 	// When children of node are changed (added or removed)
 	const onObservationChange = (mutationRecords: MutationRecord[], observer: MutationObserver) => {
@@ -67,11 +56,21 @@ export function focusTrap(node: HTMLElement, enabled: boolean) {
 	return {
 		update(newArgs: boolean) {
 			enabled = newArgs;
-			if (newArgs) determineFocusableChilds(false)
+			if (newArgs) determineFocusableChilds(false);
 		},
 		destroy() {
 			document.removeEventListener('keydown', keydownHandler);
 			observer.disconnect();
 		}
 	};
+}
+
+const focusableElementSelector = 'a[href], button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])';
+
+// Get first and last focusable (and therefor tabbable) child from element
+function getFirstAndLastFocusableChild(element: HTMLElement) {
+	const focusableChilds: HTMLElement[] = Array.from(element.querySelectorAll(focusableElementSelector));
+	const first = focusableChilds.at(0);
+	const last = focusableChilds.at(-1);
+	return { first, last };
 }
