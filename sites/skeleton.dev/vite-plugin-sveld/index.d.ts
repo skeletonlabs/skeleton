a42b78c6b225953bb7d247454bc0489bdb77bba1
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-07 15:02:00 -0400

    add vite-plugin-sveld to support typescript >5.0

---
+++ sites/skeleton.dev/vite-plugin-sveld/index.d.ts
@@ -0,0 +1,68 @@
+declare module '*.svelte?raw&sveld' {
+	interface SveldProp {
+		name: string;
+		kind: string;
+		type?: string;
+		isFunction: boolean;
+		isFunctionDeclaration: boolean;
+		isRequired: boolean;
+		constant: boolean;
+		reactive: boolean;
+	}
+
+	interface SveldSlot {
+		name: string;
+		default: boolean;
+		fallback: string;
+		slot_props: string;
+		description?: string;
+	}
+
+	interface SveldEvent {
+		type: string;
+		name: string;
+		element: string;
+		description?: string;
+	}
+
+	interface SveldRestProps {
+		type: string;
+		name: string;
+	}
+
+	interface SveldTypedefs {
+		type: string;
+		name: string;
+		ts: string;
+	}
+
+	interface SveldModuleExport {
+		name: string;
+		kind: string;
+		type?: string;
+		value: string;
+		isFunction: boolean;
+		isFunctionDeclaration: boolean;
+		isRequired: boolean;
+		constant: boolean;
+		reactive: boolean;
+	}
+
+	interface SveldJson {
+		props: SveldProp[];
+		slots: SveldSlot[];
+		events: SveldEvent[];
+		typedefs: SveldTypedefs[];
+		rest_props: SveldRestProps[];
+		moduleExports: SveldModuleExport[];
+		componentComment?: string;
+		extends?: {
+			interface: string;
+			import: string;
+		};
+	}
+
+	const json: SveldJson;
+
+	export default json;
+}
