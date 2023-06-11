e599b9688f5bb3e91cee2571eb27c01f27be1b3d
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-11 10:53:18 -0400

    esm modules

--- sites/skeleton.dev/tailwind.config.js
+++ sites/skeleton.dev/tailwind.config.js
@@ -1,9 +1,11 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import skeleton from '../../packages/plugin/src/tailwind/skeleton.js';
+import { skeleton } from '../../packages/plugin/dist/index.js';
 
 const SKELETON_PATH = '../../packages/skeleton/src/lib';
+
+/** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', `${SKELETON_PATH}/**/*.{html,js,svelte,ts}`],
