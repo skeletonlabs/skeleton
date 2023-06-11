75f19ebbc12113388d70fa7672d34667bcd25dcf
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-11 18:20:07 -0400

    added docs for config options

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,9 +1,22 @@
 import plugin from 'tailwindcss/plugin.js';
 import { coreConfig, coreUtilities, getSkeletonClasses } from './tailwind/core.js';
+
 type ConfigOptions = {
-	intellisense?: boolean;
-	theme?: string;
+	/**
+	 * Prefix Skeleton component classes with a specified string. Ideally used when
+	 * there are Skeleton class names that conflict with other plugins.
+	 * @example
+	 * // Skeleton classes will be prefixed with 'sk-' (ex: sk-btn)
+	 * prefix = 'sk-'
+	 * @default ''
+	 */
 	prefix?: string;
+	/**
+	 * Whether to include Skeleton's base styles.
+	 * @default true
+	 */
+	base?: boolean;
+	theme?: string;
 };
 
 const { components, base } = getSkeletonClasses()!;
@@ -12,7 +25,13 @@ const skeleton = plugin.withOptions<ConfigOptions>(
 	// Plugin Creator
 	(options) => {
 		return ({ addBase, addComponents, addUtilities, config }) => {
-			addBase(base);
+			const userConfig = config();
+			const skeletonConfig = userConfig['skeleton'];
+
+			if (skeletonConfig) console.dir(skeletonConfig);
+
+			console.dir({ options, config: skeletonConfig });
+			if (options?.base !== false) addBase(base);
 			addUtilities(coreUtilities);
 			addComponents(components);
 		};
