bef4cdbcae6414a91104fea15e9cc92da98b8cf1
endigo9740 <gundamx9740@gmail.com>
2023-06-12 16:48:19 -0500

    Doc and minor updates to ratings

--- packages/skeleton/src/lib/components/Ratings/Ratings.svelte
+++ packages/skeleton/src/lib/components/Ratings/Ratings.svelte
@@ -10,22 +10,26 @@
 	import type { CssClasses } from '../../index.js';
 
 	// Props
-	/** Maximum rating value */
-	export let max = 5;
-	/** Current rating value */
+	/** Current rating value. */
 	export let value = 0;
+	/** Maximum rating value. */
+	export let max = 5;
 
 	// Props (styles)
-	/** Set the justification for the rating shapes */
+	/** Provide classes to set the text color. */
+	export let text: CssClasses = 'text-token';
+	/** Provide classes to set the SVG fill color. */
+	export let fill: CssClasses = 'fill-token';
+	/** Provide classes to set the flexbox justification. */
 	export let justify: CssClasses = 'justify-center';
-	/** Provide class to set the horizontal spacing style. */
-	export let spacing: CssClasses = 'space-x-1';
+	/** Provide classes to set the horizontal spacing style. */
+	export let spacing: CssClasses = 'space-x-2';
 
 	// Classes
 	const cBase = 'w-full flex';
 
 	// Reactive
-	$: classesBase = `${cBase} ${justify} ${spacing} ${$$props.class ?? ''}`;
+	$: classesBase = `${cBase} ${text} ${fill} ${justify} ${spacing} ${$$props.class ?? ''}`;
 </script>
 
 <div class="rating-bar {classesBase}" data-testid="rating-bar">
--- sites/skeleton.dev/src/routes/(inner)/components/ratings/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/components/ratings/+page.svelte
@@ -3,36 +3,39 @@
 	import { DocsFeature, type DocsShellSettings } from '$lib/layouts/DocsShell/types';
 	import DocsPreview from '$lib/components/DocsPreview/DocsPreview.svelte';
 	// Components
-	import { Ratings, CodeBlock } from '@skeletonlabs/skeleton';
+	import { Ratings, CodeBlock, RangeSlider } from '@skeletonlabs/skeleton';
 	// Sveld
 	import sveldRatings from '@skeletonlabs/skeleton/components/Ratings/Ratings.svelte?raw&sveld';
-	import { emptyStar, fullStar, halfStar } from './icons';
+	import { icons } from './icons';
 
 	// Docs Shell
 	const settings: DocsShellSettings = {
 		feature: DocsFeature.Component,
 		name: 'Ratings',
-		description: 'Display a rating bar, also known as <code class="code">Star Rating</code>',
+		description: 'Create an visual representation of a numeric range.',
 		imports: ['Ratings'],
 		source: 'components/Ratings',
 		components: [{ label: 'Ratings', sveld: sveldRatings }]
 	};
+
+	// Local
+	let value = { current: 3.5, max: 5 };
 </script>
 
 <DocsShell {settings}>
 	<!-- Slot: Sandbox -->
 	<svelte:fragment slot="sandbox">
-		<DocsPreview>
+		<DocsPreview regionFooter="grid grid-cols-[1fr_auto] gap-4 place-items-center">
 			<svelte:fragment slot="preview">
-				<Ratings class="fill-on-primary-token" value={3.5}>
+				<Ratings fill="fill-on-primary-token" value={value.current} max={value.max}>
 					<svelte:fragment slot="empty">
-						{@html emptyStar}
+						{@html icons.starEmpty}
 					</svelte:fragment>
 					<svelte:fragment slot="half">
-						{@html halfStar}
+						{@html icons.starHalf}
 					</svelte:fragment>
 					<svelte:fragment slot="full">
-						{@html fullStar}
+						{@html icons.starFull}
 					</svelte:fragment>
 				</Ratings>
 			</svelte:fragment>
@@ -41,24 +44,100 @@
 					language="html"
 					code={`
 <Ratings value={3.5} max={5}>
-    <svelte:fragment slot="empty">
-        (empty icon)
-    </svelte:fragment>
-    <svelte:fragment slot="half">
-        (half full icon)
-    </svelte:fragment>
-    <svelte:fragment slot="full">
-        (full icon)
-    </svelte:fragment>
+    <svelte:fragment slot="empty">(icon)</svelte:fragment>
+    <svelte:fragment slot="half">(con)</svelte:fragment>
+    <svelte:fragment slot="full">(icon)</svelte:fragment>
 </Ratings>
 			`}
 				/>
+			</svelte:fragment>
+			<svelte:fragment slot="footer">
+				<RangeSlider name="range-slider" bind:value={value.current} max={value.max} step={0.5} ticked class="w-full" />
+				<select class="select" name="max" id="max" bind:value={value.max}>
+					<option value={3}>Max 3</option>
+					<option value={5}>Max 5</option>
+					<option value={10}>Max 10</option>
+				</select>
 			</svelte:fragment>
 		</DocsPreview>
 	</svelte:fragment>
 
 	<!-- Slot: Usage -->
 	<svelte:fragment slot="usage">
-		<section class="space-y-4" />
+		<section class="space-y-4">
+			<p>
+				Perfect for creating a stars ratings for movies or ecommerce ratings. Supply any type of image, text, or icon for the <em>empty</em
+				>, <em>half</em>, and <em>full</em> slots. Then adjust the <code class="code">value</code>
+				and <code class="code">max</code> properties as desired. Note that SVG icons displayed verbatim, so you may need to adjust the fill color
+				and responsive sizing.
+			</p>
+		</section>
+		<section class="space-y-4">
+			<h2 class="h2">Binding</h2>
+			<DocsPreview background="neutral">
+				<svelte:fragment slot="preview">
+					<Ratings class="fill-token" value={value.current} max={value.max}>
+						<svelte:fragment slot="empty">
+							{@html icons.circleEmpty}
+						</svelte:fragment>
+						<svelte:fragment slot="half">
+							{@html icons.circleHalf}
+						</svelte:fragment>
+						<svelte:fragment slot="full">
+							{@html icons.circleFull}
+						</svelte:fragment>
+					</Ratings>
+				</svelte:fragment>
+				<svelte:fragment slot="source">
+					<CodeBlock language="ts" code={`let value = { current: 3.5, max: 5 };`} />
+					<CodeBlock
+						language="html"
+						code={`
+<Ratings bind:value={value.current} max={value.max}>
+	<svelte:fragment slot="empty">(icon)</svelte:fragment>
+	<svelte:fragment slot="half">(con)</svelte:fragment>
+	<svelte:fragment slot="full">(icon)</svelte:fragment>
+</Ratings>
+			`}
+					/>
+				</svelte:fragment>
+			</DocsPreview>
+		</section>
+		<section class="space-y-4">
+			<h2 class="h2">Emoji</h2>
+			<DocsPreview background="neutral">
+				<svelte:fragment slot="preview">
+					<Ratings class="fill-token" value={value.current} max={value.max}>
+						<svelte:fragment slot="empty">
+							<span class="text-3xl">⚪</span>
+						</svelte:fragment>
+						<svelte:fragment slot="half">
+							<span class="text-3xl">☠️</span>
+						</svelte:fragment>
+						<svelte:fragment slot="full">
+							<span class="text-3xl">💀</span>
+						</svelte:fragment>
+					</Ratings>
+				</svelte:fragment>
+				<svelte:fragment slot="source">
+					<CodeBlock
+						language="html"
+						code={`
+<Ratings value={3.5} max={5}>
+	<svelte:fragment slot="empty">
+		<span class="text-3xl">⚪</span>
+	</svelte:fragment>
+	<svelte:fragment slot="half">
+		<span class="text-3xl">☠️</span>
+	</svelte:fragment>
+	<svelte:fragment slot="full">
+		<span class="text-3xl">💀</span>
+	</svelte:fragment>
+</Ratings>
+			`}
+					/>
+				</svelte:fragment>
+			</DocsPreview>
+		</section>
 	</svelte:fragment>
 </DocsShell>
