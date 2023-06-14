3e536324ecc68447401b3d61a2becfe8a6db9a89
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-14 16:00:11 -0400

    fixed flickering when reduced motion was on

--- packages/skeleton/src/lib/internal/transitions.ts
+++ packages/skeleton/src/lib/internal/transitions.ts
@@ -6,7 +6,11 @@ export function dynamicTransition<T extends Transition>(node: Element, dynParams
 
 	if (enabled) return transition(node, params);
 
-	return {};
+	// it's better to just set the `duration` to 0 to prevent flickering
+	if ('duration' in params) return transition(node, { duration: 0 });
+
+	// if the transition doesn't provide a `duration` prop, then we'll just return this as a last resort
+	return { duration: 0 };
 }
 
 type DynamicTransitionParams<T extends Transition> = {
