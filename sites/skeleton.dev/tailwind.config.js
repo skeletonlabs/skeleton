ecd00317494ddfba5a6d15294676d5daa0950ee8
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-17 11:17:23 -0400

    resolve the skeleton package

--- sites/skeleton.dev/tailwind.config.js
+++ sites/skeleton.dev/tailwind.config.js
@@ -2,16 +2,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import { join } from 'path';
 
 // Must be imported directly so that the tailwind extension can detect file changes
 import { skeleton } from '../../packages/plugin/dist/index.js';
 
-const SKELETON_PATH = '../../packages/skeleton/src/lib';
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', `${SKELETON_PATH}/**/*.{html,js,svelte,ts}`],
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {
 			fontFamily: {
