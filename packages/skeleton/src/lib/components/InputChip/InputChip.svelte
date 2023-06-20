f651c941d22b530b54af2ff05accfbad052943e1
Dominik Röschke <dominik.roeschke@cloud37.io>
2023-06-20 09:07:42 +0200

    Added nullsafe access and nullish coalescense to InputChips to handle value being undefined.

--- packages/skeleton/src/lib/components/InputChip/InputChip.svelte
+++ packages/skeleton/src/lib/components/InputChip/InputChip.svelte
@@ -63,9 +63,9 @@
 
 	// Local
 	let inputValid = true;
-	let chipValues: Array<{ val: (typeof value)[0]; id: number }> = value.map((val) => {
+	let chipValues: Array<{ val: (typeof value)[0]; id: number }> = value?.map((val) => {
 		return { val: val, id: Math.random() };
-	});
+	}) || [];
 
 	// Reset Form
 	function resetFormHandler() {
@@ -152,10 +152,10 @@
 	$: classesInterface = `${cInterface}`;
 	$: classesChipList = `${cChipList}`;
 	$: classesInputField = `${cInputField}`;
-	$: chipValues = value.map((val, i) => {
+	$: chipValues = value?.map((val, i) => {
 		if (chipValues[i]?.val === val) return chipValues[i];
 		return { id: Math.random(), val: val };
-	});
+	}) || [];
 </script>
 
 <div class="input-chip {classesBase}" class:opacity-50={$$restProps.disabled}>
