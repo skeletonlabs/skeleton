f242a3fb5668832ac631bbe663efdd26750c07e6
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-06-23 11:08:10 -0400

    add comment

--- packages/skeleton/src/lib/actions/Filters/filter.ts
+++ packages/skeleton/src/lib/actions/Filters/filter.ts
@@ -1,6 +1,8 @@
 // Action: Filter
 
 type Filters = ['Apollo', 'BlueNight', 'Emerald', 'GreenFall', 'Noir', 'NoirLight', 'Rustic', 'Summer84', 'XPro'];
+// This type allows users to get the autocomplete option for our
+// filter presets while also permitting them into adding their own filter.
 type FilterName = `#${Filters[number]}` | (string & {});
 
 export function filter(node: HTMLElement, filterName: FilterName) {
