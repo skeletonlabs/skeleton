08c7418cc2d078e2483d0e6188abcac8d4ec68f5
endigo9740 <gundamx9740@gmail.com>
2023-06-23 15:37:02 -0500

    ...

--- sites/skeleton.dev/src/routes/(inner)/elements/scroll-containers/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/elements/scroll-containers/+page.svelte
@@ -70,7 +70,7 @@
 			<svelte:fragment slot="preview">
 				<div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
 					{#each Array.from({ length: 8 }) as _, i}
-						<div class="card {currentSnap} shrink-0 w-40 md:w-80 text-center py-20">{i + 1}</div>
+						<div class="{currentSnap} shrink-0 card py-20 w-40 md:w-80 text-center">{i + 1}</div>
 					{/each}
 				</div>
 			</svelte:fragment>
@@ -80,7 +80,7 @@
 					code={`
 <div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
 	{#each Array.from({ length: 8 }) as _, i}
-		<div class="card ${currentSnap} shrink-0 w-40 md:w-80 text-center py-20">{i + 1}</div>
+		<div class="${currentSnap} shrink-0 card py-20 w-40 md:w-80 text-center">{i + 1}</div>
 	{/each}
 </div>
 `}
