cb449a44be093bccfb0eab12271315c211904554
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2023-06-30 10:59:36 +0200

    deleted unrelated code

--- sites/skeleton.dev/src/routes/(inner)/components/app-shell/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/components/app-shell/+page.svelte
@@ -31,10 +31,6 @@
 	function toggle(key: keyof typeof state): void {
 		state[key] = !state[key];
 	}
-	function scrollHandler(event: CustomEvent<HTMLElement>) {
-		const targetElement = event.detail;
-		console.log(targetElement.scrollTop);
-	}
 </script>
 
 <DocsShell {settings}>
@@ -47,7 +43,6 @@
 				</svelte:fragment>
 				<svelte:fragment slot="preview">
 					<AppShell
-						on:scroll={scrollHandler}
 						class="card h-full p-1"
 						slotSidebarLeft="grid grid-cols-1"
 						slotSidebarRight="grid grid-cols-1"
