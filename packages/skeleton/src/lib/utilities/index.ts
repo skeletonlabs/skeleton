0350fd73d775fab5ad0d450c638068c3364599a6
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-08-06 17:55:27 -0400

    simplify initializer

--- packages/skeleton/src/lib/utilities/index.ts
+++ packages/skeleton/src/lib/utilities/index.ts
@@ -2,14 +2,7 @@ import { initializeModalStore } from './Modal/stores.js';
 import { initializeToastStore } from './Toast/stores.js';
 import { initializeDrawerStore } from './Drawer/stores.js';
 
-type Opts = {
-	modal: boolean;
-	toast: boolean;
-	drawer: boolean;
-	popup: boolean;
-	lightswitch: boolean;
-};
-export function initializeStores(opts: Opts = { modal: true, toast: true, drawer: true, popup: true, lightswitch: true }) {
+export function initializeStores() {
 	const modalStore = initializeModalStore();
 	const toastStore = initializeToastStore();
 	const drawerStore = initializeDrawerStore();
