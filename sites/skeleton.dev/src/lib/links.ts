667ff5233b20fe2d8fba73e428ca24e37d34b993
endigo9740 <gundamx9740@gmail.com>
2023-08-11 15:17:20 -0500

    Add NEW badge to PurgeCSS link

--- sites/skeleton.dev/src/lib/links.ts
+++ sites/skeleton.dev/src/lib/links.ts
@@ -31,7 +31,7 @@ export const menuNavLinks: Record<string, Array<{ title: string; list: List }>> 
 			title: 'Resources',
 			list: [
 				{ href: '/docs/generator', label: 'Theme Generator', keywords: 'create, custom, style, css, design' },
-				{ href: '/docs/purgecss', label: 'PurgeCSS', keywords: 'purgecss, purge, css, vite, tree, shaking, bundle, optimize' },
+				{ href: '/docs/purgecss', label: 'PurgeCSS', keywords: 'purgecss, vite, tree, shaking, bundle, optimize', badge: 'New' },
 				{ href: '/docs/contributing', label: 'Contributing', keywords: 'branch, pr' }
 			]
 		}
