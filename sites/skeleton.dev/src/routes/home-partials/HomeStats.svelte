1a22f5bced3f6ffda7e20e404d3d4807f483e217
endigo9740 <gundamx9740@gmail.com>
2023-08-29 10:01:42 -0500

    Remove homepage banner and updated stats

--- sites/skeleton.dev/src/routes/home-partials/HomeStats.svelte
+++ sites/skeleton.dev/src/routes/home-partials/HomeStats.svelte
@@ -1,13 +1,13 @@
 <div class="flex flex-col lg:flex-row justify-center items-center gap-10">
 	<div class="space-y-1">
-		<span class="block font-heading-token text-8xl font-bold">45k+</span>
+		<span class="block font-heading-token text-8xl font-bold">60k+</span>
 		<p class="block opacity-50">Monthly Downloads</p>
 	</div>
 	<!-- --- -->
 	<div class="border-t lg:border-t-0 lg:border-l border-surface-500/50 w-20 lg:w-1 lg:h-20" />
 	<!-- --- -->
 	<div class="space-y-1">
-		<span class="block font-heading-token text-8xl font-bold">3k+</span>
+		<span class="block font-heading-token text-8xl font-bold">3.5k+</span>
 		<p class="block opacity-50">GitHub Stars</p>
 	</div>
 	<!-- --- -->
