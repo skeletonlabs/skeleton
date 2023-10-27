ad601981d2fc74ed941c3e248378ebf2a1319225
james <james@bubbleye.com>
2023-10-27 23:38:56 +0800

    Simplify to only needed code

--- sites/skeleton.dev/src/routes/(inner)/elements/lists/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/elements/lists/+page.svelte
@@ -196,21 +196,7 @@
 			</DocsPreview>
 			<p>To highlight active state, we recommend conditionally applying a background color to the anchor tag.</p>
 			<CodeBlock language="ts" code={`$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');`} />
-			<CodeBlock
-				language="html"
-				code={`
-<nav class="list-nav">
-    <ul>
-        <li>
-            <a href="/elements/lists" class={classesActive("/elements/lists")}>
-                <span class="badge bg-primary-500">📋</span>
-                <span class="flex-auto">Lists</span>
-            </a>
-        </li>
-    </ul>
-</nav>
-`}
-			/>
+			<CodeBlock language="html" code={`<a href={href} class="{classesActive(href)}">Page</a>`} />
 		</section>
 	</svelte:fragment>
 </DocsShell>
