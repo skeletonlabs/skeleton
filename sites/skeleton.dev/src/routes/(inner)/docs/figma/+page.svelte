621ca265ae1c01c6b433a83cc423cd9dd694c68e
endigo9740 <gundamx9740@gmail.com>
2023-12-03 11:50:51 -0600

    Minor text edits

--- sites/skeleton.dev/src/routes/(inner)/docs/figma/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/docs/figma/+page.svelte
@@ -4,22 +4,15 @@
 
 <LayoutPage>
 	<!-- Header -->
-	<header class="space-y-4">
-		<h1 class="h1">Figma</h1>
-		<!-- prettier-ignore -->
-		<div>
-			<div class="!flex flex-col md:flex-row justify-between items-start space-y-4 md:space-y-0 md:space-x-4">
-                <!-- prettier-ignore -->
-				<p>
-                    Design kit for Skeleton built with Figma. You can install it from the Figma Community.
-                </p>
-				
-				<a class="btn variant-filled-secondary" href="https://www.figma.com/community/file/1266774078142216791" target="_blank">
-					<i class="fa-brands fa-figma" />
-					<span>View on Figma</span>
-				</a>
-			</div>
+	<header class="!flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
+		<div class="space-y-4">
+			<h1 class="h1">Figma</h1>
+			<p>A community-maintained Figma design kit for Skeleton.</p>
 		</div>
+		<a class="btn variant-filled-secondary" href="https://www.figma.com/community/file/1266774078142216791" target="_blank">
+			<i class="fa-brands fa-figma" />
+			<span>View on Figma Community</span>
+		</a>
 	</header>
 
 	<hr />
@@ -27,8 +20,8 @@
 	<section class="space-y-4">
 		<h2 class="h2">Introduction</h2>
 		<p>
-			The Figma kit is designed to match the structure of the components and properties in Skeleton. This helps designers & developers
-			create stunning user interfaces faster.
+			The Figma kit is designed to match the structure of the components and properties in Skeleton. This can assist both designers and
+			developers in create and mocking stunning user interfaces.
 		</p>
 		<h3 class="h3">Features</h3>
 		<ol class="list">
@@ -47,7 +40,7 @@
 				</li>
 				<li>
 					<span>•</span>
-					<span class="flex-auto">Tutorial (Only Alerts at the moment)</span>
+					<span class="flex-auto">Tutorial (limited to Alerts at the moment)</span>
 				</li>
 				<li>
 					<span>•</span>
@@ -63,11 +56,8 @@
 		<h2 class="h2">Attribution</h2>
 		<!-- prettier-ignore -->
 		<p>
-			This <a class="anchor" href="https://www.figma.com/community/file/1266774078142216791" target="_blank" rel="noreferrer">UI kit</a> is maintained by <a class="anchor" href="https://github.com/mmailaender" target="_blank" rel="noreferrer">Micha</a> and <a class="anchor" href="https://github.com/bohdank1995" target="_blank" rel="noreferrer">Bohdan</a>.
-		</p>
-		<p>
-			If you have any questions or feedback, please feel free to share it in Discord with a ping to <code class="code">@Micha</code> or
-			<code class="code">@Bodya</code>
+			This <a class="anchor" href="https://www.figma.com/community/file/1266774078142216791" target="_blank" rel="noreferrer">Figma UI kit</a> is maintained by <a class="anchor" href="https://github.com/mmailaender" target="_blank" rel="noreferrer">Micha</a> and <a class="anchor" href="https://github.com/bohdank1995" target="_blank" rel="noreferrer">Bohdan</a>. If you have any questions or feedback, please feel free ping these members on the Skeleton community discord via <code class="code">@Micha</code> or
+			<code class="code">@Bodya</code>.
 		</p>
 	</section>
 </LayoutPage>
