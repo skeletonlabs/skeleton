627b5ed65a4687e11aed2df96781def0f860bc7a
jwatts777 <watts.jordan777@gmail.com>
2023-12-03 17:57:52 -0600

    format + lint

--- sites/skeleton.dev/src/routes/(inner)/docs/tokens/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/docs/tokens/+page.svelte
@@ -81,8 +81,8 @@
 			[
 				'<code class="code">.divide-[color]-[pairings]-token</code>',
 				`${vColorsAll}<br>${vPairings}`,
-				`\nImplements a <a class='anchor' href='https://tailwindcss.com/docs/text-decoration' target="_blank">text-decoration</a> color. ` + descPairings
-					
+				`\nImplements a <a class='anchor' href='https://tailwindcss.com/docs/text-decoration' target="_blank">text-decoration</a> color. ` +
+					descPairings
 			]
 		]
 	};
