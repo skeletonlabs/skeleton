858acaba8fb5d834c5e8f84d13b75be55d0dfda5
endigo9740 <gundamx9740@gmail.com>
2023-12-20 16:32:59 -0600

    ...

--- sites/skeleton.dev/src/routes/(inner)/components/tables/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/components/tables/+page.svelte
@@ -53,14 +53,6 @@
 		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
 	];
 
-	let sourceDataUpdated = [
-		{ position: 1, name: 'Hydrogen XXX', weight: 1.0079, symbol: 'H' },
-		{ position: 2, name: 'Helium XXX', weight: 4.0026, symbol: 'He' },
-		{ position: 3, name: 'Lithium XXX', weight: 6.941, symbol: 'Li' },
-		{ position: 4, name: 'Beryllium XXX', weight: 9.0122, symbol: 'Be' },
-		{ position: 5, name: 'Boron XXX', weight: 10.811, symbol: 'B' }
-	];
-
 	// Table Simple
 	const tableSimple: TableSource = {
 		head: ['Symbol', 'Name', 'weight'],
