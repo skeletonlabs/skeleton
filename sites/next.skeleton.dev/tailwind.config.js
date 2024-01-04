d52fcf870488b03c587a37be919af9ac75304b1a
endigo9740 <gundamx9740@gmail.com>
2024-01-04 15:54:58 -0600

    Skeleton theme added

--- sites/next.skeleton.dev/tailwind.config.js
+++ sites/next.skeleton.dev/tailwind.config.js
@@ -1,27 +1,30 @@
 import { skeleton } from "@skeletonlabs/skeleton-next/plugin";
 
+// TODO: remove this
+const enhancements = false;
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		skeleton({
-			themes: {
-				preset: [
-					{ name: "crimson", enhancements: true },
-					{ name: "gold-nouveau", enhancements: true },
-					{ name: "hamlindigo", enhancements: true },
-					{ name: "modern", enhancements: true },
-					{ name: "rocket", enhancements: true },
-					{ name: "sahara", enhancements: true },
-					{ name: "seafoam", enhancements: true },
-					{ name: "skeleton", enhancements: true },
-					{ name: "vintage", enhancements: true },
-					{ name: "wintry", enhancements: true },
-				],
-			},
-		}),
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    skeleton({
+      themes: {
+        preset: [
+          { name: "crimson", enhancements },
+          { name: "gold-nouveau", enhancements },
+          { name: "hamlindigo", enhancements },
+          { name: "modern", enhancements },
+          { name: "rocket", enhancements },
+          { name: "sahara", enhancements },
+          { name: "seafoam", enhancements },
+          { name: "skeleton", enhancements },
+          { name: "vintage", enhancements },
+          { name: "wintry", enhancements },
+        ],
+      },
+    }),
+  ],
 };
