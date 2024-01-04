2c5b7a8bb577ff7f2da512222c197f13a187db62
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-01-04 16:37:51 -0500

    hooked up the tw-plugin

--- sites/next.skeleton.dev/tailwind.config.js
+++ sites/next.skeleton.dev/tailwind.config.js
@@ -1,9 +1,27 @@
+import { skeleton } from "@skeletonlabs/skeleton-next/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				preset: [
+					{ name: "crimson", enhancements: true },
+					{ name: "gold-nouveau", enhancements: true },
+					{ name: "hamlindigo", enhancements: true },
+					{ name: "modern", enhancements: true },
+					{ name: "rocket", enhancements: true },
+					{ name: "sahara", enhancements: true },
+					{ name: "seafoam", enhancements: true },
+					{ name: "skeleton", enhancements: true },
+					{ name: "vintage", enhancements: true },
+					{ name: "wintry", enhancements: true },
+				],
+			},
+		}),
+	],
+};
