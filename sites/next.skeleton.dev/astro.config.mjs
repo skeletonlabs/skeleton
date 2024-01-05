ca6e444fc82fc22ba27a78599c388453e5dfedd8
niktek <nik@niktek.io>
2024-01-06 10:33:04 +1100

    Added adapter cloudflare

--- sites/next.skeleton.dev/astro.config.mjs
+++ sites/next.skeleton.dev/astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import svelte from "@astrojs/svelte";
 import react from '@astrojs/react';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-    react({
-      // https://docs.astro.build/en/guides/integrations-guide/react/
-      experimentalReactChildren: true
-    })
-  ]
+  integrations: [tailwind(), svelte(), react({
+    // https://docs.astro.build/en/guides/integrations-guide/react/
+    experimentalReactChildren: true
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
