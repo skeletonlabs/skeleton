4f59fb6a051a64669f13c253312686dbf21d122f
Chris Simmons <gundamx9740@gmail.com>
2024-01-05 10:19:26 -0600

    Setup the Astro project for next.skeleton.dev (#2401)
    Co-authored-by: AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>

---
+++ sites/next.skeleton.dev/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import svelte from "@astrojs/svelte";
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    svelte(),
+    react({
+      // https://docs.astro.build/en/guides/integrations-guide/react/
+      experimentalReactChildren: true
+    })
+  ]
+});
