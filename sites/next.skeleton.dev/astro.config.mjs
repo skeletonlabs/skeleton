844912fc69287d9c4d3b2aae26624590f8ceed26
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-01-05 13:58:57 -0500

    adds tw-plugin watcher

--- sites/next.skeleton.dev/astro.config.mjs
+++ sites/next.skeleton.dev/astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import skeletonPluginWatcher from "./skeleton-plugin-watcher.js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-    react({
-      // https://docs.astro.build/en/guides/integrations-guide/react/
-      experimentalReactChildren: true
-    })
-  ]
+	integrations: [
+		tailwind(),
+		svelte(),
+		react({
+			// https://docs.astro.build/en/guides/integrations-guide/react/
+			experimentalReactChildren: true,
+		}),
+	],
+	vite: {
+		plugins: [skeletonPluginWatcher()],
+	},
 });
---
+++ sites/next.skeleton.dev/skeleton-plugin-watcher.js
@@ -0,0 +1,41 @@
+import { Minimatch } from "minimatch";
+import { exec } from "node:child_process";
+import { join, resolve, basename } from "node:path";
+
+/**
+ * @returns {NonNullable<import('astro').ViteUserConfig['plugins']>[number]}
+ */
+export default function skeletonPluginWatcher() {
+	const pluginSrcPath = resolve(
+		".",
+		join("..", "..", "packages", "skeleton-next", "src", "plugin")
+	);
+	const mm = new Minimatch("**/packages/skeleton-next/src/plugin/**/*");
+	let locked = false;
+
+	return {
+		name: "skeleton-plugin-watcher",
+		configureServer(vite) {
+			vite.watcher.add(pluginSrcPath);
+			vite.watcher.on("all", async (event, path) => {
+				if (mm.match(path) && !path.includes("generated")) {
+					console.log(`[TW Plugin]: File Updated: ${basename(path)}`);
+					if (!locked) {
+						locked = true;
+						const now = Date.now();
+						exec("pnpm -F @skeletonlabs/skeleton-next build", () => {
+							console.log(`[TW Plugin]: Completed in ${Date.now() - now}ms`);
+							locked = false;
+						});
+					}
+				}
+			});
+		},
+		async buildStart() {
+			const now = Date.now();
+			exec("pnpm -F @skeletonlabs/skeleton-next build", () => {
+				console.log(`[TW Plugin]: Completed in ${Date.now() - now}ms`);
+			});
+		},
+	};
+}
