bd2b80e297ac076faef49e32ba553aec2d9e4f78
endigo9740 <gundamx9740@gmail.com>
2024-01-17 16:48:36 -0600

    Next Theme Generator

---
+++ sites/themes.skeleton.dev/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
+
+module.exports = config;
