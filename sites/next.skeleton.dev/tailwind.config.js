20e0c91f3be5773b505238d01d641f9593489f5d
endigo9740 <gundamx9740@gmail.com>
2024-01-17 14:53:08 -0600

    Next theme and plugin refactor

--- sites/next.skeleton.dev/tailwind.config.js
+++ sites/next.skeleton.dev/tailwind.config.js
@@ -1,8 +1,5 @@
 import { skeleton } from "@skeletonlabs/skeleton/plugin";
 
-// TODO: remove this
-const enhancements = false;
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -13,16 +10,7 @@ export default {
     skeleton({
       themes: {
         preset: [
-          { name: "crimson", enhancements },
-          { name: "gold-nouveau", enhancements },
-          { name: "hamlindigo", enhancements },
-          { name: "modern", enhancements },
-          { name: "rocket", enhancements },
-          { name: "sahara", enhancements },
-          { name: "seafoam", enhancements },
-          { name: "skeleton", enhancements },
-          { name: "vintage", enhancements },
-          { name: "wintry", enhancements },
+          { name: "next" },
         ],
       },
     }),
