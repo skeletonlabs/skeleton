6d410246c971bdb3a094d92a7546f3083e207b07
endigo9740 <gundamx9740@gmail.com>
2024-02-01 17:45:08 -0600

    Framework picker looped

--- sites/next.skeleton.dev/src/lib/components/react/FrameworkPicker.tsx
+++ sites/next.skeleton.dev/src/lib/components/react/FrameworkPicker.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-export const FrameworkPicker: React.FC<any> = ({ selected, svelte, react }) => {
-    const [activeTab, setActiveTab] = React.useState(selected)
-    const baseClasses = 'font-bold py-2 px-4 border-b-[3px] border-transparent'
+export const FrameworkPicker: React.FC<any> = (props) => {
+    const [activeTab, setActiveTab] = React.useState(props.selected)
+    const baseClasses = 'font-bold py-2 px-4 border-b-[3px] border-transparent capitalize'
+
+    // prettier-ignore
+    const tabs = [
+        props.react,
+        props.svelte,
+        props.vue
+    ].filter((framework) => framework !== undefined)
 
     function activeClass(framework: string) {
         if (activeTab === framework) return '!border-surface-950-50'
@@ -11,16 +18,22 @@ export const FrameworkPicker: React.FC<any> = ({ selected, svelte, react }) => {
     return (
         <div className="space-y-4">
             <nav className="flex gap-4 border-b-[1px] border-surface-200-800">
-                <button className={`${baseClasses} ${activeClass('svelte')}`} onClick={() => setActiveTab('svelte')}>
-                    Svelte
-                </button>
-                <button className={`${baseClasses} ${activeClass('react')}`} onClick={() => setActiveTab('react')}>
-                    React
-                </button>
+                {tabs.map((tab: any) => {
+                    return (
+                        <button
+                            key={tab.props.name}
+                            className={`${baseClasses} ${activeClass(tab.props.name)}`}
+                            onClick={() => setActiveTab(tab.props.name)}
+                        >
+                            {tab.props.name}
+                        </button>
+                    )
+                })}
             </nav>
             <div className="docs-card-outlined-centered p-8">
-                {activeTab === 'svelte' && <div>{svelte}</div>}
-                {activeTab === 'react' && <div>{react}</div>}
+                {tabs.map((tab: any) => {
+                    return activeTab === tab.props.name && <div key={tab.props.name}>{props[tab.props.name]}</div>
+                })}
             </div>
         </div>
     )
