2c875969767eb94c6b6fa5715f1c16b94314e11e
endigo9740 <gundamx9740@gmail.com>
2024-02-08 12:56:58 -0600

    Fix astro check lint issue for JSX components

--- sites/next.skeleton.dev/src/examples/components/accordions/Example.tsx
+++ sites/next.skeleton.dev/src/examples/components/accordions/Example.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { Accordion, AccordionItem } from '@skeletonlabs/skeleton-react';
 
-export const Page: React.FC<any> = (props) => {
+export const Page: React.FC<any> = () => {
     return <p>(React Accordion Example)</p>
 }
