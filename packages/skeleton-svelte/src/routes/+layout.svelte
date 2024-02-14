abc3bc18c96a297eda6bedce2e77b0ab5acd3f1b
endigo9740 <gundamx9740@gmail.com>
2024-02-14 13:39:40 -0600

    Svelte package app layout with nav

--- packages/skeleton-svelte/src/routes/+layout.svelte
+++ packages/skeleton-svelte/src/routes/+layout.svelte
@@ -2,7 +2,23 @@
 	import '../app.pcss';
 </script>
 
-<main class="p-10">
-	<h1 class="h1">Skeleton-Svelte</h1>
-	<slot />
-</main>
+<div class="h-screen grid grid-cols-[320px_minmax(0,_1fr)]">
+	<!-- Nav -->
+	<div class="bg-surface-100-900 p-8 overflow-y-auto space-y-8">
+		<a class="type-scale-3 font-bold font-mono" href="/">skeleton-svelte</a>
+		<hr class="hr" />
+		<!-- Components -->
+		<div class="space-y-8">
+			<span class="font-bold">Components</span>
+			<nav class="flex flex-col gap-2 type-scale-2">
+				<a class="anchor" href="/components/test">Test</a>
+				<a class="anchor" href="/components/accordions">Accordions</a>
+				<a class="anchor" href="/components/app-bars">App Bars</a>
+			</nav>
+		</div>
+	</div>
+	<!-- Page -->
+	<main class="p-8 overflow-y-auto">
+		<slot />
+	</main>
+</div>
--- packages/skeleton-svelte/src/routes/+page.svelte
+++ packages/skeleton-svelte/src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<script>
-	import Test from '$lib/components/Test/Test.svelte';
-</script>
-
-<Test />
+<div class="w-full h-full flex justify-center items-center">
+	<div class="max-w-[600px] text-balance text-center">
+		<p>
+			This is a sandbox for <code class="code">@skeletonlabs/skeleton-svelte</code>. Select a
+			feature from the list to preview.
+		</p>
+	</div>
+</div>
