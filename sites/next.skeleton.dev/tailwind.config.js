79e5f5120bdf43bbd789b964990d5678120e9db3
hugos68 <hugokorteapple@gmail.com>
2024-02-28 11:53:32 +0100

    Lint is now passing

--- sites/next.skeleton.dev/tailwind.config.js
+++ sites/next.skeleton.dev/tailwind.config.js
@@ -1,30 +1,28 @@
 // import { skeleton } from '../../packages/skeleton/dist/plugin/index.cjs'
 // NOTE: do not delete the above comment. It's required for local HMR on plugin changes.
 
-import { join } from 'path';
-import { skeleton } from "@skeletonlabs/skeleton/plugin";
-import forms from '@tailwindcss/forms';
+import { join } from 'path'
+import { skeleton } from '@skeletonlabs/skeleton/plugin'
+import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    join(require.resolve('@skeletonlabs/skeleton-react'), '../**/*.{html,js,ts,jsx,tsx}'),
-    join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,ts,svelte}'),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    forms,
-    // typography,
-    skeleton({
-      themes: {
-        preset: [
-          { name: "next" },
-        ],
-      },
-    }),
-  ],
-};
+    darkMode: 'class',
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        join(require.resolve('@skeletonlabs/skeleton-react'), '../**/*.{html,js,ts,jsx,tsx}'),
+        join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,ts,svelte}'),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        forms,
+        // typography,
+        skeleton({
+            themes: {
+                preset: [{ name: 'next' }],
+            },
+        }),
+    ],
+}
