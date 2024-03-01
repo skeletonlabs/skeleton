2f80aa09243c7c2efcc6f1eaf632df5e6cb76610
endigo9740 <gundamx9740@gmail.com>
2024-03-01 12:52:45 -0600

    CodeBlock comp via Shiki

--- sites/themes.skeleton.dev/src/lib/components/previews/CodeGen.svelte
+++ sites/themes.skeleton.dev/src/lib/components/previews/CodeGen.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { codeToHtml } from 'shiki';
 	// State
 	import {
 		stateFormColors,
@@ -9,19 +10,33 @@
 	} from '$lib/state.svelte';
 	// Utils
 	import { genColorPalette } from '$lib/generator';
+	// Components
+	import CodeBlock from '../utilities/CodeBlock.svelte';
 
 	// Local
+	const theme = {
+		properties: {
+			foo: 'bar'
+		}
+	};
+
+	// Reactive State
 	let colorPalette = $derived(genColorPalette(stateFormColors));
 </script>
 
-<div class="space-y-4 px-4 md:space-y-8 md:px-8">
+<div class="px-4 md:px-8 space-y-4 md:space-y-8">
+	<!-- Template Code Block -->
+	<CodeBlock code={theme.properties} lang="ts" />
+
+	<hr class="hr" />
+
 	<!-- Palette Preview -->
 	<div class="grid grid-cols-7 gap-0.5">
 		{#each Object.values(colorPalette) as color}
 			<div class="grid grid-rows-11 gap-0.5">
 				{#each Object.values(color || {}) as shade}
 					<div class="p-2 pt-6 text-center" style:background="rgb({shade})">
-						<div class="bg-black/30">{shade}</div>
+						<div class="bg-black/30 text-white">{shade}</div>
 					</div>
 				{/each}
 			</div>
@@ -30,33 +45,33 @@
 
 	<section class="space-y-4">
 		<h2 class="h4">Color Palette</h2>
-		<pre class="pre">{JSON.stringify(colorPalette, null, 2)}</pre>
+		<CodeBlock code={colorPalette} lang="ts" />
 	</section>
 
 	<hr class="hr" />
 
 	<section class="space-y-4">
 		<h2 class="h4">Colors</h2>
-		<pre class="pre">{JSON.stringify(stateFormColors, null, 2)}</pre>
+		<CodeBlock code={stateFormColors} lang="ts" />
 	</section>
 
 	<section class="space-y-4">
 		<h2 class="h4">Backgrounds</h2>
-		<pre class="pre">{JSON.stringify(stateFormBackgrounds, null, 2)}</pre>
+		<CodeBlock code={stateFormBackgrounds} lang="ts" />
 	</section>
 
 	<section class="space-y-4">
 		<h2 class="h4">Typography</h2>
-		<pre class="pre">{JSON.stringify(stateFormTypography, null, 2)}</pre>
+		<CodeBlock code={stateFormTypography} lang="ts" />
 	</section>
 
 	<section class="space-y-4">
 		<h2 class="h4">Spacing</h2>
-		<pre class="pre">{JSON.stringify(stateFormSpacing, null, 2)}</pre>
+		<CodeBlock code={stateFormSpacing} lang="ts" />
 	</section>
 
 	<section class="space-y-4">
 		<h2 class="h4">Edges</h2>
-		<pre class="pre">{JSON.stringify(stateFormEdges, null, 2)}</pre>
+		<CodeBlock code={stateFormEdges} lang="ts" />
 	</section>
 </div>
---
+++ sites/themes.skeleton.dev/src/lib/components/utilities/CodeBlock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { codeToHtml } from 'shiki';
+
+	interface PropsCodeBlock {
+		code: any;
+		lang?: string;
+		theme?: string;
+	}
+
+	// prettier-ignore
+	const {
+        code,
+        lang = 'plaintext',
+        theme = 'min-dark'
+    } = $props<PropsCodeBlock>();
+
+	// Generate Code Block HTML
+	const genCodeBlock = codeToHtml(JSON.stringify(code, null, 2), { lang, theme });
+</script>
+
+{#await genCodeBlock then code}
+	<div class="[&>.shiki]:type-scale-1 [&>.shiki]:p-4 [&>.shiki]:rounded-2xl">
+		{@html code}
+	</div>
+{/await}
