9cfbbb9a19b35e62ee8d30472d082c6ca525fc42
endigo9740 <gundamx9740@gmail.com>
2024-03-01 15:46:05 -0600

    Color seed simple validation

--- sites/themes.skeleton.dev/src/lib/generator.ts
+++ sites/themes.skeleton.dev/src/lib/generator.ts
@@ -2,18 +2,19 @@ import chroma from 'chroma-js';
 
 // Generates a color ramp with default settings
 function genColorRamp(colorSettings: Record<string, string>) {
+	const validatedSeed = colorSettings.seed.length === 7 ? colorSettings.seed : '#CCCCCC';
 	return {
-		50: chroma(colorSettings.seed).brighten(2.25).rgb(),
-		100: chroma(colorSettings.seed).brighten(1.9).rgb(),
-		200: chroma(colorSettings.seed).brighten(1.5).rgb(),
-		300: chroma(colorSettings.seed).brighten(0.75).rgb(),
-		400: chroma(colorSettings.seed).brighten(0.5).rgb(),
-		500: chroma(colorSettings.seed).rgb(), // *
-		600: chroma(colorSettings.seed).darken(0.5).rgb(),
-		700: chroma(colorSettings.seed).darken(0.75).rgb(),
-		800: chroma(colorSettings.seed).darken(1.5).rgb(),
-		900: chroma(colorSettings.seed).darken(1.9).rgb(),
-		950: chroma(colorSettings.seed).darken(2.25).rgb()
+		50: chroma(validatedSeed).brighten(2.25).rgb(),
+		100: chroma(validatedSeed).brighten(1.9).rgb(),
+		200: chroma(validatedSeed).brighten(1.5).rgb(),
+		300: chroma(validatedSeed).brighten(0.75).rgb(),
+		400: chroma(validatedSeed).brighten(0.5).rgb(),
+		500: chroma(validatedSeed).rgb(), // *
+		600: chroma(validatedSeed).darken(0.5).rgb(),
+		700: chroma(validatedSeed).darken(0.75).rgb(),
+		800: chroma(validatedSeed).darken(1.5).rgb(),
+		900: chroma(validatedSeed).darken(1.9).rgb(),
+		950: chroma(validatedSeed).darken(2.25).rgb()
 	};
 }
 
