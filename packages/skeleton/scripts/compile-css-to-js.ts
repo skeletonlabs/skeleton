64d82061912720207b58796eb6c4c27875e33212
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-13 17:03:09 -0400

    only process imports and compile directly to css-in-js

--- packages/skeleton/scripts/compile-css-to-js.ts
+++ packages/skeleton/scripts/compile-css-to-js.ts
@@ -1,47 +1,16 @@
-import postcss, { type PluginCreator } from 'postcss';
+import postcss from 'postcss';
 import postcssJs from 'postcss-js';
 import { readFileSync } from 'node:fs';
 import postcssImport from 'postcss-import';
-import tw from 'tailwindcss';
-import type { Config } from 'tailwindcss';
-import { corePlugin } from '../src/plugin/core.js';
-
-// tailwind needs to fix their exports :]
-const tailwindcss = tw as unknown as PluginCreator<string | Config | { config: string | Config }>;
 
 // Transpiles all of our library's CSS to JS
-export async function transpileCssToJs(cssEntryPath: string, plugins: Config['plugins'] = []) {
-	const selectors: string[] = [];
-
-	// We'll first get all the custom class names,
-	// then we can feed that into the TW preprocessor as the `content`
-	// so that TW can detect and generate them properly
-
+export async function transpileCssToJs(cssEntryPath: string) {
 	const css = readFileSync(cssEntryPath, 'utf8');
-	const processor = postcss([postcssImport()]);
-	const result = await processor.process(css, { from: cssEntryPath });
 
-	result.root.walk((node) => {
-		if (node.type === 'rule') {
-			selectors.push(...node.selectors);
-		}
-	});
+	const result = await postcss([postcssImport()]).process(css, { from: cssEntryPath });
+	if (result.root.type === 'document') throw Error('This should never happen');
 
-	// Custom tailwind config so that we only use the necessities
-	const twConfig = {
-		darkMode: 'class',
-		content: [{ raw: selectors.join(' ') }],
-		plugins: [
-			corePlugin,
-			// add skeleton component classes for the base styles
-			...plugins
-		]
-	} satisfies Config;
-
-	const result2 = await postcss([postcssImport(), tailwindcss(twConfig)]).process(css, { from: cssEntryPath });
-	if (result2.root.type === 'document') throw Error('This should never happen');
-
-	const cssInJs = postcssJs.objectify(result2.root);
+	const cssInJs = postcssJs.objectify(result.root);
 
 	return cssInJs;
 }
