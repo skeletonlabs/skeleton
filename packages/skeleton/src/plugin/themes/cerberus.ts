585a227c4967b73bdfb2e01ef1608b1a3c6245ca
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-13 12:58:04 -0400

    no longer needed

--- packages/skeleton/src/plugin/themes/cerberus.ts
+++ packages/skeleton/src/plugin/themes/cerberus.ts
@@ -256,9 +256,7 @@ const cerberus = {
 		'--color-surface-contrast-800': 'var(--color-surface-contrast-light)',
 		'--color-surface-contrast-900': 'var(--color-surface-contrast-light)',
 		'--color-surface-contrast-950': 'var(--color-surface-contrast-light)'
-	},
-	properties_dark: {}, // TODO: REMOVE
-	enhancements: {} // TODO: REMOVE
+	}
 }; // satisfies PresetTheme;
 
 export default cerberus;
