8394878eaa5b2dcb4d598a4a3b95e0d5bb361f70
hugos68 <hugokorteapple@gmail.com>
2024-03-19 16:30:39 +0100

    Removed memo, formatted

--- packages/skeleton-react/src/components/Progress/Progress.tsx
+++ packages/skeleton-react/src/components/Progress/Progress.tsx
@@ -1,52 +1,56 @@
-import { useEffect, useMemo } from 'react';
-import { ProgressProps } from './types';
+import { useEffect } from "react";
+import { ProgressProps } from "./types";
 
 export default function Progress({
-		value,
-		min = 0,
-		max = 100,
-		ariaLabelledby = '',
-		// Root
-		base = 'overflow-x-hidden',
-		bg = 'bg-surface-200-800',
-		height = 'h-2',
-		rounded = 'rounded',
-		classes = '',
-		// Meter
-		meterBase = 'h-full',
-		meterBg = 'bg-primary-500',
-		meterRounded = 'rounded',
-		meterTransition = 'transition-[width]',
-		meterIndeterminateAnim = 'indeterminate',
-		meterClasses = ''
-	}: ProgressProps) {
+	value,
+	min = 0,
+	max = 100,
+	ariaLabelledby = "",
+	// Root
+	base = "overflow-x-hidden",
+	bg = "bg-surface-200-800",
+	height = "h-2",
+	rounded = "rounded",
+	classes = "",
+	// Meter
+	meterBase = "h-full",
+	meterBg = "bg-primary-500",
+	meterRounded = "rounded",
+	meterTransition = "transition-[width]",
+	meterIndeterminateAnim = "indeterminate",
+	meterClasses = "",
+}: ProgressProps) {
+	useEffect(() => {
+		if (min > max) {
+			console.error(
+				`The minimum value: "${min}" is greater than the maximum value: "${max}"`,
+			);
+		}
+	}, [min, max]);
 
-    useEffect(() => {
-        if (min > max) {
-            console.error(`The minimum value: "${min}" is greater than the maximum value: "${max}"`);
-        }
-    }, [min, max]);
+	const indeterminate = value === undefined;
+	const fillPercentage = indeterminate ? 50 : ((value! - min) / (max - min)) * 100;
+	const width = `${fillPercentage}%`;
 
-    const indeterminate = useMemo(() => value === undefined, [value]);
-    const fillPercentage = useMemo(() => (indeterminate ? 50 : ((value! - min) / (max - min)) * 100), [value, min, max, indeterminate]);
-    const width = useMemo(() =>`${fillPercentage}%`, [fillPercentage]);
+	const rxIndeterminate = indeterminate ? meterIndeterminateAnim : "";
 
-	const rxIndeterminate = useMemo(() => indeterminate ? meterIndeterminateAnim : '', [indeterminate, meterIndeterminateAnim]);
+	return (
+		<>
+			<div
+				role="progressbar"
+				aria-labelledby={ariaLabelledby}
+				aria-valuenow={value}
+				aria-valuemin={min}
+				aria-valuemax={max}
+				className={`${base} ${bg} ${height} ${rounded} ${classes}`}
+			>
+				<div
+					className={`${meterBase} ${meterBg} ${meterRounded} ${meterTransition} ${rxIndeterminate} ${meterClasses}`}
+					style={{ width }}
+				></div>
+			</div>
 
-    return (
-        <>
-        <div
-            role="progressbar"
-            aria-labelledby={ariaLabelledby}
-            aria-valuenow={value}
-            aria-valuemin={min}
-            aria-valuemax={max}
-            className={`${base} ${bg} ${height} ${rounded} ${classes}`}
-        >
-            <div className={`${meterBase} ${meterBg} ${meterRounded} ${meterTransition} ${rxIndeterminate} ${meterClasses}`} style={{ width }}></div>
-        </div>
-
-            <style>{`
+			<style>{`
                 .indeterminate {
                     animation: anim-progress-indeterminate 2s linear infinite;
                 }
@@ -59,6 +63,6 @@ export default function Progress({
                     }
                 }
             `}</style>
-        </>
-    )
-} 
+		</>
+	);
+}
