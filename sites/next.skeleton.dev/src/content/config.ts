8842473554a4bda75414e6c8a5323dc094881035
endigo9740 <gundamx9740@gmail.com>
2024-03-29 14:35:17 -0500

    Fixed component meta frontmatter in page

--- sites/next.skeleton.dev/src/content/config.ts
+++ sites/next.skeleton.dev/src/content/config.ts
@@ -4,6 +4,11 @@ const docs = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string().optional().default('(Title)'),
+		description: z.string().optional().default('(Description)'),
+		srcSvelte: z.string().optional(),
+		srcReact: z.string().optional(),
+		srcAlly: z.string().optional(),
+		showDocsUrl: z.boolean().optional().default(false),
 		order: z.number().optional().default(0),
 	}),
 });
