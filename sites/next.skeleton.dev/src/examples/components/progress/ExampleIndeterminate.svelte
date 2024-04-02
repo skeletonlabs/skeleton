6a0d5f5d05bc5aa95d433f2fc1695cfdbdd3148d
hugos68 <hugokorteapple@gmail.com>
2024-04-02 17:38:44 +0200

    Added doc page, content is severely lacking, but atleast its there :), also added Progres exports for all packages (was missing previously)

---
+++ sites/next.skeleton.dev/src/examples/components/progress/ExampleIndeterminate.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { Progress } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<Progress value={undefined} ariaLabelledby="A halfway filled progress bar" classes="w-full"  />
---
+++ sites/next.skeleton.dev/src/examples/components/progress/ExampleIndeterminate.tsx
@@ -0,0 +1,5 @@
+import { Progress } from '@skeletonlabs/skeleton-react';
+
+export default () => {
+	return <Progress value={undefined} ariaLabelledby="A halfway filled progress bar" classes="w-full" />;
+};
