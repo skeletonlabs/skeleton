192c5477c807e46aedc8eedb9de1bc708d066b25
hugos68 <hugokorteapple@gmail.com>
2024-04-10 23:05:25 +0200

    Fix react animation playing on initial render, added props to allow for two way binding

--- packages/skeleton-react/src/components/Accordion/Accordion.tsx
+++ packages/skeleton-react/src/components/Accordion/Accordion.tsx
@@ -1,179 +1,177 @@
 "use client";
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import {
-  AccordionContextState,
-  AccordionControlProps,
-  AccordionItemProps,
-  AccordionPanelProps,
-  AccordionProps,
+	AccordionContextState,
+	AccordionControlProps,
+	AccordionItemProps,
+	AccordionPanelProps,
+	AccordionProps,
 } from "./types";
 import { motion, AnimatePresence } from "framer-motion";
 
 // Context ---
 
 export const AccordionContext = createContext<AccordionContextState>({
-  animDuration: 0.2,
-  open: () => {},
-  close: () => {},
-  toggle: () => {},
-  isOpen: () => false,
+	animDuration: 0.2,
+	open: () => {},
+	close: () => {},
+	toggle: () => {},
+	isOpen: () => false,
 });
 
 // Components ---
 
 const AccordionRoot: React.FC<AccordionProps> = ({
-  multiple = false,
-  opened = [],
-  animDuration = 0.2,
-  // Root
-  base = "",
-  padding = "",
-  spaceY = "space-y-2",
-  rounded = "rounded",
-  width = "w-full",
-  classes = "",
-  // Events
-  ontoggle = () => {},
-  // Children
-  children,
+	multiple = false,
+	opened: openedInit = [],
+	animDuration = 0.2,
+	// Root
+	base = "",
+	padding = "",
+	spaceY = "space-y-2",
+	rounded = "rounded",
+	width = "w-full",
+	classes = "",
+	// Events
+	onToggle = () => {},
+	onOpenedChange = () => {},
+	// Children
+	children,
 }) => {
-  // State
-  const [openedState, setOpenedState] = useState<string[]>(
-    multiple ? opened : [opened[0]]
-  );
+	// State
+	const [opened, setOpened] = useState<string[]>(multiple ? openedInit : [openedInit[0]]);
 
-  // Functions
-  function open(id: string) {
-    setOpenedState((openedState) => (multiple ? [...openedState, id] : [id]));
-  }
-  function close(id: string) {
-    setOpenedState((openedState) => openedState.filter((_id) => _id !== id));
-  }
-  function toggle(id: string) {
-    isOpen(id) ? close(id) : open(id);
-    ontoggle(new CustomEvent("toggle", { detail: { id, open: isOpen(id) } }));
-  }
-  function isOpen(id: string) {
-    return openedState.includes(id);
-  }
+	// Functions
+	function open(id: string) {
+		setOpened((opened) => (multiple ? [...opened, id] : [id]));
+	}
+	function close(id: string) {
+		setOpened((opened) => opened.filter((_id) => _id !== id));
+	}
+	function toggle(id: string) {
+		isOpen(id) ? close(id) : open(id);
+		onToggle(new CustomEvent("toggle", { detail: { id, open: isOpen(id) } }));
+	}
+	function isOpen(id: string) {
+		return opened.includes(id);
+	}
 
-  return (
-    <div
-      className={`${base} ${padding} ${spaceY} ${rounded} ${width} ${classes}`}
-      data-testid="accordion"
-    >
-      <AccordionContext.Provider
-        value={{
-          animDuration,
-          open,
-          close,
-          toggle,
-          isOpen,
-        }}
-      >
-        {children}
-      </AccordionContext.Provider>
-    </div>
-  );
+	// Effect
+	useEffect(() => {
+		onOpenedChange(opened);
+	}, [onOpenedChange, opened]);
+
+	// Context
+	const ctx = {
+		animDuration,
+		open,
+		close,
+		toggle,
+		isOpen,
+	};
+
+	return (
+		<div
+			className={`${base} ${padding} ${spaceY} ${rounded} ${width} ${classes}`}
+			data-testid="accordion"
+		>
+			<AccordionContext.Provider value={ctx}>{children}</AccordionContext.Provider>
+		</div>
+	);
 };
 
 const AccordionItem: React.FC<AccordionItemProps> = ({
-  base = "",
-  spaceY = "",
-  classes = "",
-  // Children
-  children,
+	base = "",
+	spaceY = "",
+	classes = "",
+	// Children
+	children,
 }) => {
-  return (
-    <div
-      className={`${base} ${spaceY} ${classes}`}
-      data-testid="accordion-item"
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div className={`${base} ${spaceY} ${classes}`} data-testid="accordion-item">
+			{children}
+		</div>
+	);
 };
 
 const AccordionControl: React.FC<AccordionControlProps> = ({
-  controls,
-  disabled = false,
-  // Control
-  base = "flex text-start items-center space-x-4 w-full",
-  hover = "hover:preset-tonal-primary",
-  padding = "py-2 px-4",
-  rounded = "rounded",
-  classes = "",
-  // Children
-  iconOpen = "-",
-  iconClosed = "+",
-  lead,
-  children,
+	controls,
+	disabled = false,
+	// Control
+	base = "flex text-start items-center space-x-4 w-full",
+	hover = "hover:preset-tonal-primary",
+	padding = "py-2 px-4",
+	rounded = "rounded",
+	classes = "",
+	// Children
+	iconOpen = "-",
+	iconClosed = "+",
+	lead,
+	children,
 }) => {
-  const ctx = useContext<AccordionContextState>(AccordionContext);
-  return (
-    <button
-      type="button"
-      className={`${base} ${hover} ${padding} ${rounded} ${classes}`}
-      aria-expanded={ctx.isOpen(controls)}
-      aria-controls={`accordion-panel-${controls}`}
-      onClick={() => ctx.toggle(controls)}
-      disabled={disabled}
-      data-testid="accordion-control"
-    >
-      {/* Lead */}
-      {lead && <div>{lead}</div>}
-      {/* Content */}
-      <div className="flex-1">{children}</div>
-      {/* State Indicator */}
-      <div>{ctx.isOpen(controls) ? iconOpen : iconClosed}</div>
-    </button>
-  );
+	const ctx = useContext<AccordionContextState>(AccordionContext);
+	return (
+		<button
+			type="button"
+			className={`${base} ${hover} ${padding} ${rounded} ${classes}`}
+			aria-expanded={ctx.isOpen(controls)}
+			aria-controls={`accordion-panel-${controls}`}
+			onClick={() => ctx.toggle(controls)}
+			disabled={disabled}
+			data-testid="accordion-control"
+		>
+			{/* Lead */}
+			{lead && <div>{lead}</div>}
+			{/* Content */}
+			<div className="flex-1">{children}</div>
+			{/* State Indicator */}
+			<div>{ctx.isOpen(controls) ? iconOpen : iconClosed}</div>
+		</button>
+	);
 };
 
 const AccordionPanel: React.FC<AccordionPanelProps> = ({
-  id,
-  // Panel
-  base = "",
-  padding = "py-2 px-4",
-  rounded = "",
-  classes = "",
-  // Children
-  children,
+	id,
+	// Panel
+	base = "",
+	padding = "py-2 px-4",
+	rounded = "",
+	classes = "",
+	// Children
+	children,
 }) => {
-  const ctx = useContext<AccordionContextState>(AccordionContext);
-  return (
-    <div
-      role="region"
-      aria-hidden={ctx.isOpen(id)}
-      aria-labelledby={id}
-      data-testid="accordion-panel"
-    >
-      <AnimatePresence>
-        {ctx.isOpen(id) && (
-          <motion.div
-            className="overflow-hidden"
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            transition={{ duration: ctx.animDuration && 0.2 }}
-          >
-            <div className={`${base} ${padding} ${rounded} ${classes}`}>
-              {children}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
+	const ctx = useContext<AccordionContextState>(AccordionContext);
+	return (
+		<div
+			role="region"
+			aria-hidden={ctx.isOpen(id)}
+			aria-labelledby={id}
+			data-testid="accordion-panel"
+		>
+			<AnimatePresence initial={false}>
+				{ctx.isOpen(id) && (
+					<motion.div
+						className="overflow-hidden"
+						initial="collapsed"
+						animate="open"
+						exit="collapsed"
+						variants={{
+							open: { opacity: 1, height: "auto" },
+							collapsed: { opacity: 0, height: 0 },
+						}}
+						transition={{ duration: ctx.animDuration && 0.2 }}
+					>
+						<div className={`${base} ${padding} ${rounded} ${classes}`}>{children}</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</div>
+	);
 };
 
 export const Accordion = Object.assign(AccordionRoot, {
-  Item: AccordionItem,
-  Control: AccordionControl,
-  Panel: AccordionPanel,
+	Item: AccordionItem,
+	Control: AccordionControl,
+	Panel: AccordionPanel,
 });
--- packages/skeleton-react/src/routes/components/accordions.tsx
+++ packages/skeleton-react/src/routes/components/accordions.tsx
@@ -2,54 +2,42 @@ import { useState } from "react";
 import { Accordion } from "../../components/Accordion/Accordion.tsx";
 
 export function Component() {
-  const lorem =
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore.";
-  // const ontoggle = (e: Event) => console.log(e);
+	const lorem =
+		"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore.";
+	// const ontoggle = (e: Event) => console.log(e);
 
-  // FIXME: feed useState to the prop, not just the current state
-  const [opened, _] = useState<string[]>(["reactItem1"]);
+	// FIXME: feed useState to the prop, not just the current state
+	const [opened, setOpened] = useState(["reactItem1"]);
 
-  return (
-    <>
-      <pre className="pre mb-10">Open: {opened.join(", ")}</pre>
+	return (
+		<>
+			<pre className="pre mb-10">Open: {opened.join(", ")}</pre>
 
-      <Accordion opened={opened} ontoggle={console.log}>
-        <Accordion.Item>
-          <Accordion.Control controls="reactItem1" lead="💀">
-            React Control 1
-          </Accordion.Control>
-          <Accordion.Panel id="reactItem1">
-            React Panel 1 - {lorem}
-          </Accordion.Panel>
-        </Accordion.Item>
-        <hr className="hr" />
-        <Accordion.Item>
-          <Accordion.Control controls="reactItem2">
-            React Control 2
-          </Accordion.Control>
-          <Accordion.Panel id="reactItem2">
-            React Panel 2 - {lorem}
-          </Accordion.Panel>
-        </Accordion.Item>
-        <hr className="hr" />
-        <Accordion.Item>
-          <Accordion.Control controls="reactItem3" disabled>
-            React Control 3 (Disabled)
-          </Accordion.Control>
-          <Accordion.Panel id="reactItem3">
-            React Panel 3 - {lorem}
-          </Accordion.Panel>
-        </Accordion.Item>
-        <hr className="hr" />
-        <Accordion.Item>
-          <Accordion.Control controls="reactItem4">
-            React Control 4
-          </Accordion.Control>
-          <Accordion.Panel id="reactItem4">
-            React Panel 4 - {lorem}
-          </Accordion.Panel>
-        </Accordion.Item>
-      </Accordion>
-    </>
-  );
+			<Accordion multiple opened={opened} onOpenedChange={setOpened} onToggle={console.log}>
+				<Accordion.Item>
+					<Accordion.Control controls="reactItem1" lead="💀">
+						React Control 1
+					</Accordion.Control>
+					<Accordion.Panel id="reactItem1">React Panel 1 - {lorem}</Accordion.Panel>
+				</Accordion.Item>
+				<hr className="hr" />
+				<Accordion.Item>
+					<Accordion.Control controls="reactItem2">React Control 2</Accordion.Control>
+					<Accordion.Panel id="reactItem2">React Panel 2 - {lorem}</Accordion.Panel>
+				</Accordion.Item>
+				<hr className="hr" />
+				<Accordion.Item>
+					<Accordion.Control controls="reactItem3" disabled>
+						React Control 3 (Disabled)
+					</Accordion.Control>
+					<Accordion.Panel id="reactItem3">React Panel 3 - {lorem}</Accordion.Panel>
+				</Accordion.Item>
+				<hr className="hr" />
+				<Accordion.Item>
+					<Accordion.Control controls="reactItem4">React Control 4</Accordion.Control>
+					<Accordion.Panel id="reactItem4">React Panel 4 - {lorem}</Accordion.Panel>
+				</Accordion.Item>
+			</Accordion>
+		</>
+	);
 }
