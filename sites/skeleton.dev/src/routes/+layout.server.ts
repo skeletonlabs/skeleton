6ec4871accd66ba51aeebf3252334fd17e8ed39b
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-04-30 18:41:07 -0400

    use dynamic env instead

--- sites/skeleton.dev/src/routes/+layout.server.ts
+++ sites/skeleton.dev/src/routes/+layout.server.ts
@@ -1,8 +1,6 @@
 import type { LayoutServerLoad } from './$types';
-
-// @ts-ignore only available on deployment to Vercel:
-import { VERCEL_ENV } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const load: LayoutServerLoad = async () => {
-	return { vercelEnv: VERCEL_ENV };
+	return { vercelEnv: env.VERCEL_ENV };
 };
