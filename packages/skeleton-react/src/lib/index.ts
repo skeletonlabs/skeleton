86a0850953a03ced829708ce82c1fd10e29ded96
endigo9740 <gundamx9740@gmail.com>
2024-05-16 14:25:13 -0500

    Updated schema generation, created ApiTable component

--- packages/skeleton-react/src/lib/index.ts
+++ packages/skeleton-react/src/lib/index.ts
@@ -5,8 +5,12 @@ export { Accordion } from "../components/Accordion/Accordion.jsx";
 export { Avatar } from "../components/Avatar/Avatar.jsx";
 export { AppBar, ToolBar } from "../components/AppBar/AppBar.jsx";
 export { Progress } from "../components/Progress/Progress.jsx";
-export { Tabs } from "../components/Tab/Tabs.jsx";
+export { Tabs } from "../components/Tabs/Tabs.jsx";
 
 // Typescript Schema
+// REMINDER: append `Schema` as the suffix
 export { default as AccordionSchema } from "../components/Accordion/schema.json";
 export { default as AvatarSchama } from "../components/Avatar/schema.json";
+export { default as AppBarSchema } from "../components/AppBar/schema.json";
+export { default as ProgressSchema } from "../components/Progress/schema.json";
+export { default as TabsSchema } from "../components/Tabs/schema.json";
--- sites/next.skeleton.dev/src/components/docs/ApiTable.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-/** Create a Table to display API Reference information. */
-export const ApiTable: React.FC<any> = (props) => {
-	return <pre className="pre">{JSON.stringify(props.schema, null, 2)}</pre>;
-};
