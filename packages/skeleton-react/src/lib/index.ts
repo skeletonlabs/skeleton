00455a01db670d62c5c1b1ceff21e11630c324f4
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-05-17 18:34:00 -0400

    move schemas into it's own export file

--- packages/skeleton-react/src/lib/index.ts
+++ packages/skeleton-react/src/lib/index.ts
@@ -6,11 +6,3 @@ export { Avatar } from "./components/Avatar/Avatar.js";
 export { AppBar, ToolBar } from "./components/AppBar/AppBar.js";
 export { Progress } from "./components/Progress/Progress.js";
 export { Tabs } from "./components/Tabs/Tabs.js";
-
-// Typescript Schema
-// REMINDER: append `Schema` as the suffix
-export { default as AccordionSchema } from "./components/Accordion/schema.json";
-export { default as AvatarSchema } from "./components/Avatar/schema.json";
-export { default as AppBarSchema } from "./components/AppBar/schema.json";
-export { default as ProgressSchema } from "./components/Progress/schema.json";
-export { default as TabsSchema } from "./components/Tabs/schema.json";
