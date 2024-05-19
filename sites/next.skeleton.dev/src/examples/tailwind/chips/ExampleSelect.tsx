677bc104802cd7acd14287898a4e9ae43150d303
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-05-18 21:34:53 -0400

    add key for map

--- sites/next.skeleton.dev/src/examples/tailwind/chips/ExampleSelect.tsx
+++ sites/next.skeleton.dev/src/examples/tailwind/chips/ExampleSelect.tsx
@@ -10,11 +10,12 @@ export const Page: React.FC = () => {
 		<div className="flex gap-2">
 			{/* Loop through the available colors */}
 			{color &&
-				colors.map((c: string) => (
+				colors.map((c) => (
 					// On selection, set the color state, dynamically update classes
 					<button
 						className={`chip capitalize ${color === c ? 'preset-filled' : 'preset-tonal'}`}
 						onClick={() => setColor(c)}
+						key={c}
 					>
 						<span>{c}</span>
 					</button>
