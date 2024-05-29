dbd5278dadd6520cf80f69404e9f70486fed2718
endigo9740 <gundamx9740@gmail.com>
2024-05-29 16:56:35 -0500

    Refinement

--- packages/skeleton-svelte/src/lib/components/Switch/Switch.svelte
+++ packages/skeleton-svelte/src/lib/components/Switch/Switch.svelte
@@ -23,17 +23,21 @@
 		thumbStateActive = 'bg-surface-50 text-surface-contrast-50',
 		thumbRounded = 'rounded-full',
 		thumbTranslateX = 'translate-x-4',
-		thumbTransition = 'transition-transform',
+		thumbTransition = 'transition',
+		thumbEase = 'ease-in-out',
 		thumbDuration = 'duration-200',
 		thumbClasses = '',
+		// Events
+		onclick = () => {},
 		// Snippets
 		inactiveChild,
 		activeChild
 	}: SwitchProps = $props();
 
-	function onToggle() {
+	function toggle(event: MouseEvent | TouchEvent) {
 		if (disabled) return;
 		checked = !checked;
+		onclick(event);
 	}
 
 	const rxTrackState = $derived(checked ? stateActive : stateInactive);
@@ -47,12 +51,12 @@
 	role="switch"
 	aria-checked={checked}
 	tabindex="0"
-	onclick={onToggle}
+	onclick={toggle}
 >
-	<!-- Hidden Input -->
+	<!-- Input (note: this is never visible) -->
 	<input type="checkbox" {id} {name} bind:checked class="hidden" {disabled} />
 	<!-- Thumb -->
-	<div class="thumb {thumbBase} {rxThumbState} {thumbRounded} {thumbTransition} {thumbDuration} {thumbClasses}">
+	<div class="thumb {thumbBase} {rxThumbState} {thumbRounded} {thumbTransition} {thumbEase} {thumbDuration} {thumbClasses}">
 		{#if !checked && inactiveChild}<span>{@render inactiveChild()}</span>{/if}
 		{#if checked && activeChild}<span>{@render activeChild()}</span>{/if}
 	</div>
--- packages/skeleton-svelte/src/lib/components/Switch/types.ts
+++ packages/skeleton-svelte/src/lib/components/Switch/types.ts
@@ -1,9 +1,9 @@
 import type { Snippet } from 'svelte';
 
 export interface SwitchProps {
-	/** ... */
+	/** Set a unique ID for the switch input. */
 	id: string;
-	/** ... */
+	/** Set a unique name for the switch input. */
 	name: string;
 	/** Set the checked state. */
 	checked?: boolean;
@@ -13,21 +13,21 @@ export interface SwitchProps {
 	// Root ---
 	/** Set base classes for the root element. */
 	base?: string;
-	/** ... */
+	/** Set inactive state classes for the root element. */
 	stateInactive?: string;
-	/** ... */
+	/** Set active state classes for the root element. */
 	stateActive?: string;
-	/** ... */
+	/** Set disabled state classes for the root element. */
 	stateDisabled?: string;
-	/** ... */
+	/** Set width classes for the root element. */
 	width?: string;
-	/** ... */
+	/** Set height classes for the root element. */
 	height?: string;
-	/** ... */
+	/** Set padding classes for the root element. */
 	padding?: string;
-	/** ... */
+	/** Set rounded classes for the root element. */
 	rounded?: string;
-	/** ... */
+	/** Set hover classes for the root element. */
 	hover?: string;
 	/** Provide arbitrary classes to the root element. */
 	classes?: string;
@@ -35,20 +35,26 @@ export interface SwitchProps {
 	// Thumb ---
 	/** Set base classes for the thumb element. */
 	thumbBase?: string;
-	/** ... */
+	/** Set inactive classes for the thumb element. */
 	thumbStateInactive?: string;
-	/** ... */
+	/** Set active classes for the thumb element. */
 	thumbStateActive?: string;
-	/** ... */
+	/** Set rounded classes for the thumb element. */
 	thumbRounded?: string;
-	/** ... */
+	/** Set animation X-axis translate classes for the thumb element. */
 	thumbTranslateX?: string;
-	/** ... */
+	/** Set animation transition classes for the thumb element. */
 	thumbTransition?: string;
-	/** ... */
+	/** Set animation easing classes for the thumb element. */
+	thumbEase?: string;
+	/** Set animation duration classes for the thumb element. */
 	thumbDuration?: string;
 	/** Provide arbitrary classes to the thumb element. */
 	thumbClasses?: string;
+
+	// Events ---
+	/** Triggers when clicked. */
+	onclick?: (event: MouseEvent | TouchEvent) => void;
 
 	// Snippets ---
 	/** The inactive state children. */
