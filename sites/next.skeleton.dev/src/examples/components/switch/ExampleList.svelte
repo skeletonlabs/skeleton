d90951466ed104763f25c884db6d954491cb7b2f
endigo9740 <gundamx9740@gmail.com>
2024-05-30 16:08:11 -0500

    Doc refinement

--- sites/next.skeleton.dev/src/examples/components/switch/ExampleList.svelte
+++ sites/next.skeleton.dev/src/examples/components/switch/ExampleList.svelte
@@ -2,19 +2,19 @@
 	import { Switch } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<section class="card p-4 space-y-4 w-full">
+<section class="w-full space-y-4">
 	<label class="label flex justify-between items-center gap-4" for="inactive">
-		<p>Defaults to inactive state.</p>
+		<p>Default to the inactive state.</p>
 		<Switch id="inactive" name="inactive" />
 	</label>
 	<hr class="hr" />
 	<label class="label flex justify-between items-center gap-4" for="active">
-		<p>Defaults to active state.</p>
+		<p>Default to the active state.</p>
 		<Switch id="active" name="active" checked />
 	</label>
 	<hr class="hr" />
 	<label class="label flex justify-between items-center gap-4" for="disabled">
-		<p>Set to disabled.</p>
+		<p>Shown in disabled mode.</p>
 		<Switch id="disabled" name="disabled" disabled />
 	</label>
 </section>
