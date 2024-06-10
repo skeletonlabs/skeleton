1a45a9b657d8c22dba2d9b479de6a6bb2ba791b1
endigo9740 <gundamx9740@gmail.com>
2024-06-10 15:34:24 -0500

    Example modifications

--- sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleIcons.svelte
+++ sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleIcons.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import { ProgressRing } from '@skeletonlabs/skeleton-svelte';
 	import IconThermometer from 'lucide-svelte/icons/thermometer-sun';
-
-	let value = $state(60);
-	let max = 100;
 </script>
 
-<ProgressRing {value} {max} meterStroke="stroke-error-500" strokeLinecap="butt">
+<ProgressRing value={60} max={100} meterStroke="stroke-error-500" strokeLinecap="butt">
 	<IconThermometer size={48} />
 </ProgressRing>
--- sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleIcons.tsx
+++ sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleIcons.tsx
@@ -3,11 +3,8 @@ import { ProgressRing } from '@skeletonlabs/skeleton-react';
 import { ThermometerSun as IconThermometer } from 'lucide-react';
 
 export const Page = () => {
-	const [value] = useState(60);
-	const [max] = useState(100);
-
 	return (
-		<ProgressRing value={value} max={max} meterStroke="stroke-error-500" strokeLinecap="butt">
+		<ProgressRing value={60} max={100} meterStroke="stroke-error-500" strokeLinecap="butt">
 			<IconThermometer size={48} />
 		</ProgressRing>
 	);
