f43ff175c65f107935b9ecd4d816c0559a2c6dc4
endigo9740 <gundamx9740@gmail.com>
2024-06-10 15:15:22 -0500

    Added schema

--- packages/skeleton-react/src/lib/schemas.ts
+++ packages/skeleton-react/src/lib/schemas.ts
@@ -4,5 +4,6 @@ export { default as AccordionSchema } from "./components/Accordion/schema.json";
 export { default as AvatarSchema } from "./components/Avatar/schema.json";
 export { default as AppBarSchema } from "./components/AppBar/schema.json";
 export { default as ProgressSchema } from "./components/Progress/schema.json";
+export { default as ProgressRingSchema } from "./components/ProgressRing/schema.json";
 export { default as SwitchSchema } from "./components/Switch/schema.json";
 export { default as TabsSchema } from "./components/Tabs/schema.json";
