7137cc1116989c892856b628c5d809ede655b2b7
Seth Murphy <3085408+eagerestwolf@users.noreply.github.com>
2024-06-16 17:44:10 -0500

    Moved Prettier and ESLint to workspace root

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	root: true,
+	env: {
+		es2020: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier"
+	],
+	overrides: [
+		{
+			files: ["*.svelte"],
+			extends: ["plugin:svelte/recommended", "plugin:svelte/prettier"],
+			env: {
+				browser: true
+			},
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser"
+			},
+			plugins: ["svelte"]
+		},
+		{
+			files: ["*.tsx"],
+			env: {
+				browser: true,
+				node: false
+			},
+			extends: [
+				"plugin:jsx-a11y/recommended",
+				"plugin:react/recommended",
+				"plugin:react/jsx-runtime",
+				"plugin:react-hooks/recommended"
+			],
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			},
+			plugins: [
+				"jsx-a11y",
+				"react",
+				"react-compiler",
+				"react-hooks",
+				"react-refresh"
+			],
+			rules: {
+				"react-compiler/react-compiler": "error",
+				"react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
+			},
+			settings: {
+				react: {
+					version: "detect"
+				}
+			}
+		}
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"],
+		sourceType: "module"
+	},
+	plugins: ["@typescript-eslint"],
+	reportUnusedDisableDirectives: true,
+	rules: {
+		'no-empty-function': 'off',
+		'no-useless-escape': 'off',
+		'@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }]
+	}
+}
