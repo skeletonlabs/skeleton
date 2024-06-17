4c0677a645ded9694ee7266dbf01a9bf44787bf6
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-06-17 16:58:10 -0400

    placeholder tests

--- packages/skeleton-svelte/tests/test.ts
+++ packages/skeleton-svelte/tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('placeholder test', async ({ page }) => {
+test('placeholder playwright test', async ({ page }) => {
 	await page.goto('/');
 	expect(true).toBe(true);
 });
