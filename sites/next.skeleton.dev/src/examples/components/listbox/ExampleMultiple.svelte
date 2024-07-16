060109dd44f0eead01897795820466481ad12e3d
hugos68 <hugokorteapple@gmail.com>
2024-07-17 00:26:37 +0200

    Added doc examples for svelte

---
+++ sites/next.skeleton.dev/src/examples/components/listbox/ExampleMultiple.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Listbox } from '@skeletonlabs/skeleton-svelte';
+
+	const fruits = $state(['Apple', 'Banana', 'Orange', 'Pineapple']);
+
+	let selectedFruits = $state('');
+</script>
+
+<Listbox name="fruits" multiple bind:value={selectedFruits}>
+	{#each fruits as fruit}
+		<Listbox.Item value={fruit.toLowerCase()}>{fruit}</Listbox.Item>
+	{/each}
+</Listbox>
---
+++ sites/next.skeleton.dev/src/examples/components/listbox/ExampleSingle.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Listbox } from '@skeletonlabs/skeleton-svelte';
+
+	const fruits = $state(['Apple', 'Banana', 'Orange', 'Pineapple']);
+
+	let selectedFruit = $state('');
+</script>
+
+<Listbox name="fruit" bind:value={selectedFruit}>
+	{#each fruits as fruit}
+		<Listbox.Item value={fruit.toLowerCase()}>{fruit}</Listbox.Item>
+	{/each}
+</Listbox>
