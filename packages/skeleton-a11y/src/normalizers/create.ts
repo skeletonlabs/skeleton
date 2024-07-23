5863ebf06b08b8a8a11e9eaf9b86b7ccad213c08
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-07-23 12:52:56 -0400

    remove unnecessary jsdoc types

--- packages/skeleton-a11y/src/normalizers/create.ts
+++ packages/skeleton-a11y/src/normalizers/create.ts
@@ -1,17 +1,17 @@
 /**
  * Create a normalizer function that normalizes a set of attributes based on a given dictionary.
  *
- * @param {Record<string, string>} dictionary - The dictionary to use for the normalization.
- * @return {(attributes: Record<string, unknown>): Record<string, unknown>} The normalizer, which takes in an object of attributes and returns an object of normalized attributes.
+ * @param dictionary - The dictionary to use for the normalization.
+ * @return The normalizer, which takes in an object of attributes and returns an object of normalized attributes.
  */
-const createNormalizer = (dictionary: Record<string, string>) => {
-	return (attributes: Record<string, unknown>): Record<string, unknown> => {
+function createNormalizer(dictionary: Record<string, string>): (attributes: Record<string, unknown>) => Record<string, unknown> {
+	return (attributes) => {
 		const output: Record<string, unknown> = {};
 		for (const [key, value] of Object.entries(attributes)) {
 			output[dictionary[key] ?? key] = value;
 		}
 		return output;
 	};
-};
+}
 
 export { createNormalizer };
