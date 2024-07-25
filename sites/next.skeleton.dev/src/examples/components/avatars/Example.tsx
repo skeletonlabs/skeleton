437644ceeef993bd9c33d7a94c59cf6be73abc7a
endigo9740 <gundamx9740@gmail.com>
2024-07-25 13:52:58 -0500

    Update Avatar docs, add Fallback example

--- sites/next.skeleton.dev/src/examples/components/avatars/Example.tsx
+++ sites/next.skeleton.dev/src/examples/components/avatars/Example.tsx
@@ -5,5 +5,5 @@ export const Page: React.FC = () => {
 	const imgSrc =
 		'https://images.unsplash.com/photo-1620122303020-87ec826cf70d?q=80&w=256&h=256&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
-	return <Avatar src={imgSrc} alt="skeleton" />;
+	return <Avatar src={imgSrc} name="skeleton" />;
 };
