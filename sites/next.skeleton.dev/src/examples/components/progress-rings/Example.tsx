9a54b0dd4f967eb248cfb37a244d4ea1120577cc
endigo9740 <gundamx9740@gmail.com>
2024-08-02 13:20:17 -0500

    Firefox style fix for Progress Ring examples

--- sites/next.skeleton.dev/src/examples/components/progress-rings/Example.tsx
+++ sites/next.skeleton.dev/src/examples/components/progress-rings/Example.tsx
@@ -6,11 +6,11 @@ export const Page = () => {
 	const [max] = useState(100);
 
 	return (
-		<div className="grid grid-cols-1 gap-4">
+		<div className="grid grid-cols-1 gap-4 justify-center">
 			{/* Progress */}
 			<ProgressRing value={value} max={max} />
 			{/* Range Input */}
-			<input type="range" className="input" value={value} onInput={(e) => setValue(e.currentTarget.valueAsNumber)} max={max} />
+			<input type="range" className="input max-w-32" value={value} onInput={(e) => setValue(e.currentTarget.valueAsNumber)} max={max} />
 		</div>
 	);
 };
