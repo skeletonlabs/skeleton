8735a49c8ee2a1ae8a1b4bfcad985a093daf4127
Chris Simmons <gundamx9740@gmail.com>
2024-08-15 14:27:51 -0500

    NEXT: Resolve Unit Tests - Part 2 (#2814)

--- packages/skeleton-react/src/lib/components/Switch/Switch.test.tsx
+++ packages/skeleton-react/src/lib/components/Switch/Switch.test.tsx
@@ -12,16 +12,14 @@ describe('<Switch>', () => {
 
 	it('should render the component in the off state', () => {
 		const { getByTestId } = render(<Switch name="test" checked={false} />);
-		const component = getByTestId('switch');
-		const ariaChecked = component.getAttribute('aria-checked');
-		expect(ariaChecked).toBeFalsy;
+		const component = getByTestId('switch-input');
+		expect(component).not.toHaveAttribute('checked');
 	});
 
 	it('should render the component in the on state', () => {
 		const { getByTestId } = render(<Switch name="test" checked={true} />);
-		const component = getByTestId('switch');
-		const ariaChecked = component.getAttribute('aria-checked');
-		expect(ariaChecked).toBeTruthy;
+		const component = getByTestId('switch-input');
+		expect(component).toHaveAttribute('checked');
 	});
 
 	it('should render the component with an inactive icon', () => {
@@ -42,14 +40,27 @@ describe('<Switch>', () => {
 
 	it('should render the component in the disabled state', () => {
 		const { getByTestId } = render(<Switch name="test" disabled />);
-		const component = getByTestId('switch-control');
-		expect(component).toHaveClass('opacity-50');
-		expect(component).toHaveClass('cursor-not-allowed');
+		const component = getByTestId('switch-input');
+		expect(component).toHaveAttribute('disabled');
 	});
 
 	it('should render the component in the compact mode', () => {
 		const { getByTestId } = render(<Switch name="test" compact />);
 		const component = getByTestId('switch-control');
 		expect(component).toHaveClass('aspect-square');
+	});
+
+	it('should render base classes on root', () => {
+		const testClass = 'bg-green-500';
+		const { getByTestId } = render(<Switch name="test" base={testClass} />);
+		const component = getByTestId('switch');
+		expect(component).toHaveClass(testClass);
+	});
+
+	it('should render arbitrary classes on root', () => {
+		const testClass = 'bg-green-500';
+		const { getByTestId } = render(<Switch name="test" classes={testClass} />);
+		const component = getByTestId('switch');
+		expect(component).toHaveClass(testClass);
 	});
 });
