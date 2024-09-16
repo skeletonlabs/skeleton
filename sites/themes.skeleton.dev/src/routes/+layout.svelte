f8397395e92745c8cefe51f1003c8d3290e53cb6
endigo9740 <gundamx9740@gmail.com>
2024-09-16 13:28:31 -0500

    Strip to basics

--- sites/themes.skeleton.dev/src/routes/+layout.svelte
+++ sites/themes.skeleton.dev/src/routes/+layout.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
 	import '../app.pcss';
-	import { genCssCode } from '$lib/generator.svelte';
-	import type { Snippet } from 'svelte';
 
-	let {
-		children
-	}: {
-		children: Snippet;
-	} = $props();
-
-	// FIXME: this will need a major refactor
-	let cssCode = $derived(genCssCode());
+	let { children } = $props();
 </script>
-
-<!-- Live Preview CSS -->
-<svelte:head>
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html `\<style\>${cssCode}\</style\>`}
-</svelte:head>
 
 {@render children?.()}
---
+++ sites/themes.skeleton.dev/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="h2">Homepage</h1>
+<p>this page is working</p>
