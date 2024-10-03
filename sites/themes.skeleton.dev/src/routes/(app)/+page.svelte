109e51a714ec1cc8ce8c733be45871f3b2d06532
endigo9740 <gundamx9740@gmail.com>
2024-10-03 14:02:57 -0500

    Server side redirect for Home route

--- sites/themes.skeleton.dev/src/routes/(app)/+page.svelte
+++ sites/themes.skeleton.dev/src/routes/(app)/+page.svelte
@@ -1,12 +1,4 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		// NOTE: Redirect to Generator page
-		goto('/themes/create');
-	});
-</script>
+<!-- <script lang="ts"></script> -->
 
 <div class="card preset-tonal-warning p-4 text-center">
 	<p>Future home of the <u>browse</u> page.</p>
