829fc67bba7680d8ca1db6860a792d80de3decf0
endigo9740 <gundamx9740@gmail.com>
2024-10-31 14:36:23 -0500

    Toast integration guide started, React example added

---
+++ sites/next.skeleton.dev/src/examples/integrations/toasts/Example.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+export const Page: React.FC = () => {
+	function notify() {
+		// toast('Here is your toast.');
+		toast.custom(() => <div className={`card preset-filled-primary-500 p-4 shadow-md`}>Hello World! 👋</div>);
+	}
+
+	return (
+		<div>
+			<button className="btn preset-tonal" onClick={notify}>
+				Show Toast
+			</button>
+			<Toaster position="bottom-right" />
+		</div>
+	);
+};
