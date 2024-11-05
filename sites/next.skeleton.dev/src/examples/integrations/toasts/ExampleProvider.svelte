18204a38e23c964c3aeb112fdc93358e04719389
Chris Simmons <gundamx9740@gmail.com>
2024-11-05 12:45:29 -0600

    NEXT Toast Integration Guide and Svelte Component (#2922)

---
+++ sites/next.skeleton.dev/src/examples/integrations/toasts/ExampleProvider.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
+	import ExamplePageContent from './ExamplePageContent.svelte';
+</script>
+
+<ToastProvider>
+	<!-- In a real SvelteKit app, insert the layout slot here: -->
+	<!-- {@render children()} -->
+
+	<!-- We're replicate this with a component in this example. -->
+	<ExamplePageContent />
+</ToastProvider>
