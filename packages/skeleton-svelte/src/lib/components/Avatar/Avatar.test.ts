cc4518e3464bad44dce03797329fa32dadc96a70
endigo9740 <gundamx9740@gmail.com>
2024-11-14 13:54:57 -0600

    Adds Avatar tests

--- packages/skeleton-svelte/src/lib/components/Avatar/Avatar.test.ts
+++ packages/skeleton-svelte/src/lib/components/Avatar/Avatar.test.ts
@@ -1,15 +1,65 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 
+import { mockSnippet } from '$lib/testing.js';
 import { Avatar } from '$lib/index.js';
 
 describe('Avatar', () => {
-	const imgSrc =
-		'https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop';
+	const testIds = {
+		root: 'avatar',
+		image: 'avatar-image',
+		fallback: 'avatar-fallback'
+	};
+	const test = {
+		src: 'https://picsum.photos/100',
+		name: 'John Doe',
+		initials: 'JD'
+	};
 
 	it('Renders the component', () => {
-		render(Avatar, { src: imgSrc, name: 'basic' });
-		const component = screen.getByTestId('avatar');
+		render(Avatar, { src: test.src, name: test.name });
+		const component = screen.getByTestId(testIds.root);
 		expect(component).toBeInTheDocument();
+	});
+
+	for (const prop of ['base', 'background', 'size', 'font', 'border', 'rounded', 'shadow', 'classes']) {
+		it(`Correctly applies the \`${prop}\` prop`, () => {
+			const value = 'bg-green-500';
+			render(Avatar, { name: test.name, [prop]: value });
+			const component = screen.getByTestId(testIds.root);
+			expect(component).toHaveClass(value);
+		});
+	}
+
+	describe('Image', () => {
+		it('Renders the image element', () => {
+			render(Avatar, { src: test.src, name: test.name });
+			const component = screen.getByTestId(testIds.image);
+			expect(component).toBeInTheDocument();
+		});
+
+		for (const prop of ['imageBase', 'imageClasses']) {
+			it(`Correctly applies the \`${prop}\` prop`, () => {
+				const value = 'foo';
+				render(Avatar, { src: test.src, name: test.name, [prop]: value });
+				const component = screen.getByTestId(testIds.image);
+				expect(component).toHaveClass(value);
+			});
+		}
+	});
+
+	describe('Fallback', () => {
+		it('Renders fallback initials', () => {
+			render(Avatar, { name: test.name });
+			const component = screen.getByTestId(testIds.fallback);
+			expect(component).toHaveTextContent(test.initials);
+		});
+
+		it('Renders children snippet', () => {
+			const testValue = 'childrenSnippet';
+			render(Avatar, { name: test.name, children: mockSnippet(testValue) });
+			const component = screen.getByTestId(testIds.fallback);
+			expect(component).toHaveTextContent(testValue);
+		});
 	});
 });
