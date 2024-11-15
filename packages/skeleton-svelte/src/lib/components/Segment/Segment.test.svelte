f371868a076b89336f40b1b45973fef4fe86c799
endigo9740 <gundamx9740@gmail.com>
2024-11-15 15:47:54 -0600

    Added Segment tests

---
+++ packages/skeleton-svelte/src/lib/components/Segment/Segment.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Segment } from '$lib/index.js';
+	let { childProps = {} } = $props();
+	let chilPropsSpread = childProps ?? {};
+</script>
+
+<Segment name="align" value="left">
+	<Segment.Item value="left" {...chilPropsSpread}>left</Segment.Item>
+	<Segment.Item value="center" {...chilPropsSpread}>center</Segment.Item>
+	<Segment.Item value="right" {...chilPropsSpread}>right</Segment.Item>
+	<Segment.Item value="justify" disabled {...chilPropsSpread}>justify</Segment.Item>
+</Segment>
