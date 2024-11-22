2872722655f028657d6e2cf8f05865466225646f
hugos68 <hugokorteapple@gmail.com>
2024-11-22 15:26:15 +0100

    Reduced needless complexity in folder structure

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,13 @@
-import { program } from './internal/program.js';
+import { Command } from 'commander';
+import migrate from './commands/migrate/migrate.js';
+
+const program = new Command();
+
+// Info
+program.name('Skeleton');
+program.description('A CLI tool for Skeleton related functionality');
+
+// Commands
+program.command('migrate').action(migrate).argument('<migration>', 'Migration to run');
 
 program.parse(process.argv);
