b17647b4553a5de5b417949d1a514ddd7e84adc2
longph <longphamduy2002@gmail.com>
2024-11-23 10:41:12 +0200

    Simplify examples

--- sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleLabel.svelte
+++ sites/next.skeleton.dev/src/examples/components/progress-rings/ExampleLabel.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import { ProgressRing } from '@skeletonlabs/skeleton-svelte';
-
-	let value = $state(25);
-	let max = 100;
 </script>
 
 <div class="grid grid-cols-1 gap-4 justify-center">
-	<ProgressRing {value} {max} showLabel />
-	<input type="range" class="input max-w-32" bind:value {max} />
+	<ProgressRing value={25} max={100} showLabel />
 </div>
