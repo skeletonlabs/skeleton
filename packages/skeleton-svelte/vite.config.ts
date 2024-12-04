a8e4b1192383ce7426882d7530a16a5b7eba3f1e
endigo9740 <gundamx9740@gmail.com>
2024-12-04 15:34:08 -0600

    Svelte package Vite config ignore fix

--- packages/skeleton-svelte/vite.config.ts
+++ packages/skeleton-svelte/vite.config.ts
@@ -7,7 +7,12 @@ import path from 'node:path';
 
 export default defineConfig({
 	plugins: [
+		// NOTE: confirmed the following with @CokaKola on the Skeleton Discord after the Vite 6 upgrade:
+		// There is a known Vitest issue where they reverted their peer dep of vite 6 so the types are now mismatched.
+		// He advised reverting back to Vitest 2.1.6 or adding an ignore comment, with emphasis on the latter.
+		// @ts-expect-error due to vitest peer dependency issue per Vite 6 update
 		sveltekit(),
+		// @ts-expect-error due to vitest peer dependency issue per Vite 6 update
 		svelteTesting(),
 		skeletonPluginWatcher(path.resolve(path.join('..', '..', 'packages', 'skeleton', 'src', 'plugin')))
 	],
