f3b662f98145de48ddb88ae4073209091f28405b
bohdank1995 <bohdan.karpliak@dechea.com>
2024-12-05 14:50:49 +0200

    Create wireframes v1
    Co-authored-by: Micha Mailänder <mmailaender@users.noreply.github.com>

---
+++ sites/next.skeleton.dev/src/components/academy/InsertComponent.svelte
@@ -0,0 +1,15 @@
+<script module>
+	import insertAssets from '@images/tutorials/figma/mini-tutorials/insertAssets.gif';
+</script>
+
+<div class="flex flex-col gap-2 w-full">
+	<img src={insertAssets.src} width={insertAssets.width} height={insertAssets.height} class="rounded-container" alt="Insert assets (components) into the canvas" />
+	
+	
+	<h3 class="h3"> Insert assets (components) into the canvas </h3>
+	<ul class="list-inside list-decimal space-y-2">
+		<li>On the left sidebar, click on “Assets”;</li>
+		<li>Type name of the component that you want to use in the search field below;</li>
+		<li>Drag component from the ‘Assets’ panel to the canvas.</li>
+	</ul>
+</div>
---
+++ sites/next.skeleton.dev/src/components/academy/MiniTutorial.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+
+	let { Component } = $props();
+	let drawerState = $state(false);
+
+	function drawerClose() {
+		drawerState = false;
+	}
+
+	$effect(() => {
+		console.log('Component received:', Component);
+	});
+</script>
+
+<Modal
+	bind:open={drawerState}
+	triggerBase="btn preset-tonal"
+	contentBase="bg-surface-100-900 p-4 space-y-4 shadow-xl w-[480px] h-screen"
+	positionerJustify="justify-start"
+	positionerAlign=""
+	positionerPadding=""
+	transitionsPositionerIn={{ x: -480, duration: 200 }}
+	transitionsPositionerOut={{ x: -480, duration: 200 }}
+>
+	{#snippet trigger()}
+		Open Drawer
+	{/snippet}
+	{#snippet content()}
+		{#if Component}
+			<Component />
+		{:else}
+			<p>Component not received</p>
+		{/if}
+		<p>Debug: Component type: {typeof Component}</p>
+		<footer>
+			<button type="button" class="btn preset-filled" onclick={drawerClose}>
+				Close Drawer
+			</button>
+		</footer>
+	{/snippet}
+</Modal>
