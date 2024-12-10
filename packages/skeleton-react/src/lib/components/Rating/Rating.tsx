67e6918609d87ec964abe51baf41c15cae35e143
Long Pham <69810988+phamduylong@users.noreply.github.com>
2024-12-11 00:32:36 +0200

    Set unique key on outmost element for Rating (#3043)

--- packages/skeleton-react/src/lib/components/Rating/Rating.tsx
+++ packages/skeleton-react/src/lib/components/Rating/Rating.tsx
@@ -65,7 +65,6 @@ export const Rating: FC<RatingProps> = ({
 				className={`${controlBase} ${controlGap} ${rxInteractive} ${rxReadOnly} ${rxDisabled} ${controlClasses}`}
 				data-testid="rating-control"
 			>
-				{/* FIXME: `item` is causing React key error; is not unique on re-render? */}
 				{api.items.map((item) => {
 					const itemState = api.getItemState({ index: item });
 					const icon = (() => {
@@ -78,12 +77,10 @@ export const Rating: FC<RatingProps> = ({
 						}
 					})();
 					return (
-						<>
+						<span key={item} {...api.getItemProps({ index: item })} className={`${itemBase} ${itemClasses}`} data-testid="rating-item">
 							{/* Item */}
-							<span key={item} {...api.getItemProps({ index: item })} className={`${itemBase} ${itemClasses}`} data-testid="rating-item">
-								{icon}
-							</span>
-						</>
+							{icon}
+						</span>
 					);
 				})}
 			</div>
