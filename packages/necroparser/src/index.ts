22119094051fcac75d62863fb1fd637c3eecc874
hugos68 <hugokorteapple@gmail.com>
2024-12-19 21:05:01 +0100

    cleanup

--- packages/necroparser/src/index.ts
+++ packages/necroparser/src/index.ts
@@ -1,28 +1,13 @@
-import { randomUUID } from 'node:crypto';
 import { readFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
-import {
-	createCompilerHost,
-	createProgram,
-	createSourceFile,
-	forEachChild,
-	ScriptTarget,
-	Node,
-	Type,
-	TypeFlags,
-	ObjectType,
-	ObjectFlags,
-	isInterfaceDeclaration,
-	SymbolFlags,
-	displayPartsToString
-} from 'typescript';
+import * as ts from 'typescript';
 
 interface Interface {
 	name: string;
 	properties: {
 		name: string;
 		type: string;
-		typeKind: 'intersection' | 'union' | 'array' | 'function' | 'object' | 'primitive';
+		typeKind: 'function' | 'array' | 'object' | 'primitive';
 		required: boolean;
 		documentation: {
 			text: string | null;
@@ -34,30 +19,24 @@ interface Interface {
 	}[];
 }
 
-function walk(node: Node, callback: (node: Node) => void) {
-	forEachChild(node, (node) => {
+function walk(node: ts.Node, callback: (node: ts.Node) => void) {
+	ts.forEachChild(node, (node) => {
 		callback(node);
 		walk(node, callback);
 	});
 }
 
-function getTypeKind(type: Type): Interface['properties'][number]['typeKind'] {
+function getTypeKind(type: ts.Type): Interface['properties'][number]['typeKind'] {
 	if (type.getCallSignatures().length > 0) {
 		return 'function';
-	}
-	if (type.flags & TypeFlags.Intersection) {
-		return 'intersection';
-	}
-	if (type.flags & TypeFlags.Union) {
-		return 'union';
 	}
 	if (type.symbol?.name === 'Array' || type.symbol?.name === 'ReadonlyArray') {
 		return 'array';
 	}
-	if (type.flags & TypeFlags.Object) {
-		const objectType = type as ObjectType;
+	if (type.flags & ts.TypeFlags.Object) {
+		const objectType = type as ts.ObjectType;
 		if (
-			objectType.objectFlags & (ObjectFlags.Reference | ObjectFlags.Interface | ObjectFlags.Anonymous) ||
+			objectType.objectFlags & (ts.ObjectFlags.Reference | ts.ObjectFlags.Interface | ts.ObjectFlags.Anonymous) ||
 			type.getProperties().length > 0
 		) {
 			return 'object';
@@ -67,60 +46,52 @@ function getTypeKind(type: Type): Interface['properties'][number]['typeKind'] {
 }
 
 function getInterfaces(path: string) {
-	const code = readFileSync(path, { encoding: 'utf-8' });
-	const host = createCompilerHost({});
+	const code = readFileSync(path, 'utf-8');
+	const host = ts.createCompilerHost({});
 	const originalGetSourceFile = host.getSourceFile;
 	host.getCurrentDirectory = () => dirname(path);
-	const virtualFileName = `${randomUUID()}.tsx`;
+	const virtualFileName = 'virtual.ts';
 	host.getSourceFile = (fileName, scriptTarget) => {
 		if (fileName === virtualFileName) {
-			return createSourceFile(fileName, code, ScriptTarget.Latest, true);
+			return ts.createSourceFile(fileName, code, ts.ScriptTarget.Latest, true);
 		}
 		return originalGetSourceFile(resolve(dirname(path), fileName), scriptTarget);
 	};
-	const program = createProgram({
+	const program = ts.createProgram({
 		rootNames: [virtualFileName],
 		options: {},
 		host: host
 	});
+	const typeChecker = program.getTypeChecker();
 	const root = program.getSourceFile(virtualFileName)!;
 	const interfaces: Interface[] = [];
 	walk(root, (node) => {
-		if (!isInterfaceDeclaration(node)) {
+		if (!ts.isInterfaceDeclaration(node)) {
 			return;
 		}
-		const name = node.name.text;
-		const typeChecker = program.getTypeChecker();
 		const nodeType = typeChecker.getTypeAtLocation(node);
-		const properties = nodeType.getProperties().map((property) => {
-			const name = property.name;
-			const propertyType = typeChecker.getTypeOfSymbolAtLocation(property, node);
-			const type = typeChecker.typeToString(propertyType);
-			const typeKind = getTypeKind(propertyType);
-			const required = !(property.flags & SymbolFlags.Optional);
-			const text = displayPartsToString(property.getDocumentationComment(typeChecker)) || null;
-			const tags = property.getJsDocTags(typeChecker).map((tag) => {
-				const name = tag.name;
-				const value = displayPartsToString(tag.text);
-				return {
-					name: name,
-					value: value === '' ? null : value
-				};
-			});
-			return {
-				name: name,
-				type: type,
-				typeKind: typeKind,
-				required: required,
-				documentation: {
-					text: text,
-					tags: tags
-				}
-			};
-		});
 		interfaces.push({
-			name: name,
-			properties: properties
+			name: node.name.getText(),
+			properties: nodeType.getProperties().map((property) => {
+				const propertyType = typeChecker.getTypeOfSymbolAtLocation(property, node);
+				return {
+					name: property.getName(),
+					type: typeChecker.typeToString(propertyType),
+					typeKind: getTypeKind(propertyType),
+					required: !(property.flags & ts.SymbolFlags.Optional),
+					documentation: {
+						text: ts.displayPartsToString(property.getDocumentationComment(typeChecker)) || null,
+						tags: property.getJsDocTags(typeChecker).map((tag) => {
+							const name = tag.name;
+							const value = ts.displayPartsToString(tag.text);
+							return {
+								name: name,
+								value: value === '' ? null : value
+							};
+						})
+					}
+				};
+			})
 		});
 	});
 	return interfaces;
