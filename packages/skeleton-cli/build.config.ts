526e6629ac85ad4c112284247c8feb89e2c9b2ba
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2025-01-13 20:30:07 +0100

    Bugfix: Add `bin` back to package.json (#3103)

--- packages/skeleton-cli/build.config.ts
+++ packages/skeleton-cli/build.config.ts
@@ -3,8 +3,5 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
 	entries: ['./src/index.ts'],
 	declaration: true,
-	clean: true,
-	rollup: {
-		emitCJS: true
-	}
+	clean: true
 });
