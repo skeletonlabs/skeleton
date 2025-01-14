8f4ed9c16bbba91b6281e1982b018ff5cedaf558
Hugos68 <hugokorteapple@gmail.com>
2025-01-14 15:13:56 +0100

    Replaced `estree-walker` with `zimmerframe` due to more readable code.

--- packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.ts
+++ packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.ts
@@ -1,27 +1,32 @@
-import { AST, parse } from 'svelte/compiler';
-import { walk, type Node } from 'estree-walker';
+import { type AST, parse } from 'svelte/compiler';
 import MagicString from 'magic-string';
+import { walk } from 'zimmerframe';
 
 function transformApp(code: string, theme: string) {
 	const s = new MagicString(code);
 	const ast = parse(code, {
 		modern: true
 	});
-	walk(ast.fragment as unknown as Node, {
-		enter(node: AST.SvelteNode) {
-			if (node.type === 'RegularElement' && node.name === 'body') {
-				const dataThemeAttribute = node.attributes.find((attribute) => {
-					return attribute.type === 'Attribute' && attribute.name === 'data-theme';
-				});
-				const newDataThemeAttribute = `data-theme="${theme}"`;
-				if (dataThemeAttribute) {
-					s.update(dataThemeAttribute.start, dataThemeAttribute.end, newDataThemeAttribute);
-				} else {
-					s.appendLeft(node.start + '<body'.length, ` ${newDataThemeAttribute}`);
+	walk(
+		ast.fragment as AST.SvelteNode,
+		{},
+		{
+			RegularElement(node, ctx) {
+				if (node.name === 'body') {
+					const dataThemeAttribute = node.attributes.find((attribute) => {
+						return attribute.type === 'Attribute' && attribute.name === 'data-theme';
+					});
+					const newDataThemeAttribute = `data-theme="${theme}"`;
+					if (dataThemeAttribute) {
+						s.update(dataThemeAttribute.start, dataThemeAttribute.end, newDataThemeAttribute);
+					} else {
+						s.appendLeft(node.start + '<body'.length, ` ${newDataThemeAttribute}`);
+					}
 				}
+				ctx.next();
 			}
 		}
-	});
+	);
 	return {
 		code: s.toString()
 	};
--- packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-svelte.ts
+++ packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-svelte.ts
@@ -1,5 +1,6 @@
 import { type AST, parse } from 'svelte/compiler';
-import { walk, type Node } from 'estree-walker';
+import type { Node } from 'estree';
+import { walk } from 'zimmerframe';
 import MagicString from 'magic-string';
 import { transformClasses } from './transform-classes.js';
 
@@ -9,40 +10,46 @@ function hasRange(node: Node | AST.SvelteNode): node is (Node | AST.SvelteNode) 
 
 function transformSvelte(code: string) {
 	const s = new MagicString(code);
-	const ast = parse(code, {
+	const root = parse(code, {
 		modern: true
 	});
-	const roots = [ast.instance, ast.module, ast.fragment].filter((node) => !!node);
-	for (const root of roots) {
-		walk(root as unknown as Node, {
-			enter(node: Node | AST.SvelteNode, parent: Node | AST.SvelteNode | null) {
-				if (
-					node.type === 'ImportDeclaration' &&
-					node.source.type === 'Literal' &&
-					node.source.value === '@skeletonlabs/skeleton' &&
-					hasRange(node.source)
-				) {
-					// Add 1 to the start and subtract 1 from the end to exclude (and thus preserve) the quotes
-					s.update(node.source.start + 1, node.source.end - 1, '@skeletonlabs/skeleton-svelte');
-				}
-				if (
-					!(parent && parent.type === 'ImportDeclaration') &&
-					node.type === 'Literal' &&
-					typeof node.value === 'string' &&
-					hasRange(node)
-				) {
-					// Add 1 to the start and subtract 1 from the end to exclude (and thus preserve) the quotes
-					s.update(node.start + 1, node.end - 1, transformClasses(node.value).code);
-				}
-				if (node.type === 'Text' && hasRange(node)) {
+	const asts = [root.instance, root.module, root.fragment].filter((node) => !!node);
+	for (const ast of asts) {
+		walk(
+			ast as Node | AST.SvelteNode,
+			{},
+			{
+				ImportDeclaration(node, ctx) {
+					if (node.source.type === 'Literal' && node.source.value === '@skeletonlabs/skeleton' && hasRange(node.source)) {
+						// Add 1 to the start and subtract 1 from the end to exclude (and thus preserve) the quotes
+						s.update(node.source.start + 1, node.source.end - 1, '@skeletonlabs/skeleton-svelte');
+					}
+					ctx.next();
+				},
+				Literal(node, ctx) {
+					const parent = ctx.path.at(-1);
+					if (typeof node.value === 'string' && !(parent && parent.type === 'ImportDeclaration') && hasRange(node)) {
+						// Add 1 to the start and subtract 1 from the end to exclude (and thus preserve) the quotes
+						s.update(node.start + 1, node.end - 1, transformClasses(node.value).code);
+					}
+					ctx.next();
+				},
+				Text(node, ctx) {
 					s.update(node.start, node.end, transformClasses(node.data).code);
-				}
-				if (node.type === 'ClassDirective' && !(node.expression.type === 'Identifier' && !('loc' in node.expression) && node.name === node.expression.name) && hasRange(node)) {
-					const adjustedStart = node.start + 'class:'.length;
-					s.update(adjustedStart, adjustedStart + node.name.length, transformClasses(node.name).code);
+					ctx.next();
+				},
+				ClassDirective(node, ctx) {
+					if (
+						!(node.expression.type === 'Identifier' && !('loc' in node.expression) && node.name === node.expression.name) &&
+						hasRange(node)
+					) {
+						const adjustedStart = node.start + 'class:'.length;
+						s.update(adjustedStart, adjustedStart + node.name.length, transformClasses(node.name).code);
+					}
+					ctx.next();
 				}
 			}
-		});
+		);
 	}
 	return {
 		code: s.toString()
