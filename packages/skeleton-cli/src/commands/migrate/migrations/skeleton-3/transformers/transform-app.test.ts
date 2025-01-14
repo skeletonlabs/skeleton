cef470530bcc9ac32940b2afee425430747f2176
Hugos68 <hugokorteapple@gmail.com>
2025-01-14 17:24:18 +0100

    Add additional test cases for all preset themes for the app tests.

--- packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.test.ts
+++ packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.test.ts
@@ -1,54 +1,57 @@
 import { describe, expect, it } from 'vitest';
 import { transformApp } from './transform-app.js';
+import { THEME_MAPPINGS } from '../utility/theme-mappings.js';
 
 describe('transformsApp', () => {
-	it('transforms the `data-them` attribute when present', () => {
-		expect(
-			transformApp(
-				`
+	for (const [oldTheme, newTheme] of Object.entries(THEME_MAPPINGS)) {
+		it(`transforms the \`data-theme="${oldTheme}"\` attribute to \`data-theme="${newTheme}"\``, () => {
+			expect(
+				transformApp(
+					`
 <html lang="en">
 <head><title>foo</title></head>
-<body data-theme="skeleton"></body>
+<body data-theme="${oldTheme}"></body>
 </html>
 
 		`,
-				'cerberus'
-			)
-				.code.trim()
-				.replace(/\r\n|\r|\n/g, '\n')
-		).toBe(
-			`
+					newTheme
+				)
+					.code.trim()
+					.replace(/\r\n|\r|\n/g, '\n')
+			).toBe(
+				`
 <html lang="en">
 <head><title>foo</title></head>
-<body data-theme="cerberus"></body>
+<body data-theme="${newTheme}"></body>
 </html>
 		`
-				.trim()
-				.replace(/\r\n|\r|\n/g, '\n')
-		);
-	});
-	it('adds the `data-theme` attribute', () => {
-		expect(
-			transformApp(
-				`
+					.trim()
+					.replace(/\r\n|\r|\n/g, '\n')
+			);
+		});
+		it(`adds the \`data-theme="${newTheme}"\` attribute`, () => {
+			expect(
+				transformApp(
+					`
 <html lang="en">
 <head><title>foo</title></head>
 <body></body>
 </html>
 		`,
-				'cerberus'
-			)
-				.code.trim()
-				.replace(/\r\n|\r|\n/g, '\n')
-		).toBe(
-			`
+					newTheme
+				)
+					.code.trim()
+					.replace(/\r\n|\r|\n/g, '\n')
+			).toBe(
+				`
 <html lang="en">
 <head><title>foo</title></head>
-<body data-theme="cerberus"></body>
+<body data-theme="${newTheme}"></body>
 </html>
 		`
-				.trim()
-				.replace(/\r\n|\r|\n/g, '\n')
-		);
-	});
+					.trim()
+					.replace(/\r\n|\r|\n/g, '\n')
+			);
+		});
+	}
 });
