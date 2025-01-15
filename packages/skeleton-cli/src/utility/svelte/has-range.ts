3c95f9c075f24c2daac65595ccf888500e8ae4de
Hugos68 <hugokorteapple@gmail.com>
2025-01-15 22:22:56 +0100

    Bugfix: Fixed longstanding bug of svelte files not being transformed properly.

--- packages/skeleton-cli/src/utility/svelte/has-range.ts
+++
@@ -1,8 +0,0 @@
-import type { Node } from 'estree';
-import type { AST } from 'svelte/compiler';
-
-function hasRange(node: Node | AST.SvelteNode): node is (Node | AST.SvelteNode) & { start: number; end: number } {
-	return 'start' in node && 'end' in node && typeof node.start === 'number' && typeof node.end === 'number';
-}
-
-export { hasRange };
