59298564bba76577f825d9c4c3d2c08b4e9b4156
endigo9740 <gundamx9740@gmail.com>
2025-01-21 15:17:14 -0600

    Home components abstracted

---
+++ sites/next.skeleton.dev/src/components/homepage/HomeDesignSystem.svelte
@@ -0,0 +1,24 @@
+<div class="grid grid-cols-1 xl:grid-cols-[1fr_2fr] gap-10">
+	<!-- Text -->
+	<div class="self-center space-y-8">
+		<h2 class="h2">Design System</h2>
+		<nav class="grid grid-cols-2 gap-4">
+			<button type="button" class="btn preset-filled">Figma UI Kit</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Themes</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Colors</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Presets</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Typography</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Spacing</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Iconography</button>
+			<button type="button" class="btn preset-outlined-surface-400-600 hover:preset-tonal">Motion</button>
+		</nav>
+		<p class="opacity-60">
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum id, aperiam vel consectetur odio animi fugiat eveniet? Vel totam
+			similique pariatur iusto esse dignissimos.
+		</p>
+	</div>
+	<!-- Carousel -->
+	<div class="self-center card bg-noise preset-filled-primary-500 aspect-video overflow-hidden shadow-xl">
+		<img src="https://i.imgur.com/KOrl47B.png" alt="figma" class="w-full h-auto" />
+	</div>
+</div>
---
+++ sites/next.skeleton.dev/src/components/homepage/HomeFuncComps.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import IconChevron from 'lucide-svelte/icons/chevron-right';
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
+
+	let state = $state(true);
+</script>
+
+<div class="grid grid-cols-1 xl:grid-cols-2 gap-10">
+	<div class="space-y-10">
+		<div class="text-center space-y-2">
+			<h2 class="h2">Tailwind Components</h2>
+			<p class="text-balance opacity-60">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae doloribus quaerat placeat optio fuga est incidunt.
+			</p>
+		</div>
+		<div class="card bg-noise preset-filled-secondary-500 aspect-video shadow-xl flex justify-center items-center">
+			<a href="/" class="btn preset-filled scale-150">
+				<span>Button</span>
+				<IconChevron className="size-4" />
+			</a>
+		</div>
+	</div>
+	<div class="space-y-10">
+		<div class="text-center space-y-2">
+			<h2 class="h2">Functional Components</h2>
+			<p class="text-balance opacity-60">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae doloribus quaerat placeat optio fuga est incidunt.
+			</p>
+		</div>
+		<div class="card bg-noise preset-filled-secondary-500 aspect-video shadow-xl flex justify-center items-center">
+			<Switch name="example" bind:checked={state} controlActive="bg-surface-200" controlInactive="bg-surface-800" classes="scale-[2.0]" />
+		</div>
+	</div>
+</div>
