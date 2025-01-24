88cd2a34c6e76dd7ac2195d96cf8dd17c58cfb17
Hugos68 <hugokorteapple@gmail.com>
2025-01-24 13:38:53 +0100

    Use AtProto to grab author feed on page load.

---
+++ sites/next.skeleton.dev/src/components/docs/BlueskyFeed.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { blueSkyAgent } from '../../lib/bluesky';
+
+	const postsPromise = blueSkyAgent
+		.getAuthorFeed({
+			actor: 'skeletonlabs.bsky.social',
+			limit: 15,
+			filter: 'posts_no_replies'
+		})
+		.then((result) =>
+			result.data.feed
+				.filter((item) => {
+					return item.post.author.handle === 'skeletonlabs.bsky.social';
+				})
+				.map((item) => item.post)
+		);
+</script>
+
+{#await postsPromise}
+	{#each { length: 15 } as _}
+		<div class="card bg-surface-100-900 opacity-50 animate-pulse h-96"></div>
+	{/each}
+{:then posts}
+	{#each posts as post (post.cid)}
+		<a href={post.uri} target="_blank" class="block">
+			<article class="card preset-outlined-surface-200-800 hover:preset-tonal p-4 xl:p-10 space-y-4">
+				<header class="flex justify-between items-center">
+					<img
+						class="w-10 rounded-full overflow-hidden"
+						src="https://cdn.bsky.app/img/avatar_thumbnail/plain/did:plc:whtgi5zx7ylmdw2i76vq7vq4/bafkreicyyaq6y2iqhow3vknjyvenqqpxoxls63miv5traflivi44kkp4gq@jpeg"
+						alt="avatar"
+					/>
+					<small class="opacity-60">{new Date(post.record.createdAt).toLocaleString()}</small>
+				</header>
+				<p class="xl:type-scale-4">{post.record.text}</p>
+				<footer class="space-y-4">
+					{#if post.embed}
+						{#if post.embed.$type.includes('external')}
+							<div class="block">
+								{#if post.embed.external.thumb}
+									<img
+										class="w-full bg-surface-500/10 hover:bg-surface-500 p-0.5 rounded-container overflow-hidden shadow"
+										src={post.embed.external.thumb}
+										alt={post.embed.external.title}
+									/>
+								{:else}
+									<div class="card preset-outlined-surface-200-800 p-4 text-surface-800-200">
+										{post.embed.external.title}
+									</div>
+								{/if}
+							</div>
+						{:else if post.embed.$type.includes('images')}
+							{#each post.embed.images as image}
+								<img class="w-full bg-surface-500/10 p-1 rounded-container overflow-hidden shadow" src={image.thumb} alt={image.alt} />
+							{/each}
+						{/if}
+					{/if}
+				</footer>
+			</article>
+		</a>
+	{/each}
+{:catch error}
+	<p class="text-center font-semibold">{error.message}</p>
+{/await}
