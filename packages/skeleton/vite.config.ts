0c9840ec2a803d0e08a8a259bd4a0c74a666b9bc
Hugos68 <hugokorteapple@gmail.com>
2025-02-02 20:38:33 +0100

    Flatten sources

--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite';
 import fg from 'fast-glob';
-import { basename, extname } from 'node:path';
+import { basename } from 'node:path';
 
 export default defineConfig({
 	build: {
 		cssCodeSplit: true,
 		lib: {
 			entry: {
-				['core/index']: 'src/core/index.css',
-				...(await fg.glob('./src/themes/*.{css,scss}')).reduce(
-					(entries, input) => {
-						Object.assign(entries, {
-							[`themes/${basename(input)}`]: input
-						});
-						return entries;
-					},
-					{} as Record<string, string>
-				)
+				['index']: 'src/index.css',
+				...Object.fromEntries((await fg.glob('./src/themes/*.{css,scss}')).map((path) => [`themes/${basename(path)}`, path]))
 			}
 		}
 	}
