d4d21f7925b8e51d896bd0a1ddf4b65ab3df5c3a
Hugos68 <hugokorteapple@gmail.com>
2025-02-02 11:11:26 +0100

    Moved legacy themes to `themes/legacy`. Moved from `preserveModules` to using output input entries.

--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 import fg from 'fast-glob';
+import { basename } from 'path';
+import { extname } from 'node:path';
 
 export default defineConfig({
 	build: {
 		cssCodeSplit: true,
 		lib: {
-			entry: ['./src/index.css', ...(await fg.glob('./src/themes/*.{css,scss}'))]
-		},
-		rollupOptions: {
-			output: {
-				preserveModules: true
+			entry: {
+				['index']: 'src/index.css',
+				...(await fg.glob('./src/themes/**/*.{css,scss}')).reduce(
+					(entries, input) => {
+						Object.assign(entries, {
+							[`themes/${basename(input, extname(input))}`]: input
+						});
+						return entries;
+					},
+					{} as Record<string, string>
+				)
 			}
 		}
 	}
