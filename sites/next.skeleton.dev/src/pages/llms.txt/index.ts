1c93d0ad79e59f2a7cefe4e850d7d2fb718281c2
Hugos68 <hugokorteapple@gmail.com>
2025-02-12 17:31:34 +0100

    Remove accidental branch stuff

--- sites/next.skeleton.dev/src/pages/llms.txt/index.ts
+++
@@ -1,7 +0,0 @@
-import { getCollection } from 'astro:content';
-
-export async function GET() {
-	const pages = await getCollection('docs');
-
-	return (await Promise.all(pages.map(async (page) => {}))).join('\n');
-}
