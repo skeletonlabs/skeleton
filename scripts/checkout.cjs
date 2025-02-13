d074b0a18dee5677eb84160193446ced3344ea78
Hugos68 <hugokorteapple@gmail.com>
2025-02-13 17:40:37 +0100

    Removed a bunch of redundant stuff

--- scripts/checkout.cjs
+++
@@ -1,9 +0,0 @@
-var shell = require('shelljs');
-
-if (!shell.which('git')) {
-	shell.echo('Sorry, this script requires git');
-	shell.exit(1);
-}
-
-shell.exec('git clone https://github.com/skeletonlabs/create-skeleton-app packages/create-skeleton-app');
-shell.exec('git clone https://github.com/skeletonlabs/skeletonlabs.co sites/skeletonlabs.co');
