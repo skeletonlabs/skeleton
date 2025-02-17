6da2bd8ad5bfd2dbc55f4e4831edee8c711df662
Hugos68 <hugokorteapple@gmail.com>
2025-02-17 20:31:32 +0100

    themes export

---
+++ packages/skeleton/src/themes/index.ts
@@ -0,0 +1,135 @@
+import catppuccin from './catppuccin.css?raw';
+import cerberus from './cerberus.css?raw';
+import concord from './concord.css?raw';
+import crimson from './crimson.css?raw';
+import fennec from './fennec.css?raw';
+import hamlindigo from './hamlindigo.css?raw';
+import legacy from './legacy.css?raw';
+import mint from './mint.css?raw';
+import modern from './modern.css?raw';
+import mona from './mona.css?raw';
+import nosh from './nosh.css?raw';
+import nouveau from './nouveau.css?raw';
+import pine from './pine.css?raw';
+import reign from './reign.css?raw';
+import rocket from './rocket.css?raw';
+import rose from './rose.css?raw';
+import sahara from './sahara.css?raw';
+import seafoam from './seafoam.css?raw';
+import terminus from './terminus.css?raw';
+import vintage from './vintage.css?raw';
+import vox from './vox.css?raw';
+import wintry from './wintry.css?raw';
+
+export const themes = [
+	{
+		name: 'catppuccin',
+		css: catppuccin,
+		emoji: undefined
+	},
+	{
+		name: 'cerberus',
+		css: cerberus,
+		emoji: undefined
+	},
+	{
+		name: 'concord',
+		css: concord,
+		emoji: undefined
+	},
+	{
+		name: 'crimson',
+		css: crimson,
+		emoji: undefined
+	},
+	{
+		name: 'fennec',
+		css: fennec,
+		emoji: undefined
+	},
+	{
+		name: 'hamlindigo',
+		css: hamlindigo,
+		emoji: undefined
+	},
+	{
+		name: 'legacy',
+		css: legacy,
+		emoji: undefined
+	},
+	{
+		name: 'mint',
+		css: mint,
+		emoji: undefined
+	},
+	{
+		name: 'modern',
+		css: modern,
+		emoji: undefined
+	},
+	{
+		name: 'mona',
+		css: mona,
+		emoji: undefined
+	},
+	{
+		name: 'nosh',
+		css: nosh,
+		emoji: undefined
+	},
+	{
+		name: 'nouveau',
+		css: nouveau,
+		emoji: undefined
+	},
+	{
+		name: 'pine',
+		css: pine,
+		emoji: undefined
+	},
+	{
+		name: 'reign',
+		css: reign,
+		emoji: undefined
+	},
+	{
+		name: 'rocket',
+		css: rocket,
+		emoji: undefined
+	},
+	{
+		name: 'rose',
+		css: rose,
+		emoji: undefined
+	},
+	{
+		name: 'sahara',
+		css: sahara,
+		emoji: undefined
+	},
+	{
+		name: 'seafoam',
+		css: seafoam,
+		emoji: undefined
+	},
+	{
+		name: 'terminus',
+		css: terminus,
+		emoji: undefined
+	},
+	{
+		name: 'vintage',
+		css: vintage,
+		emoji: undefined
+	},
+	{
+		name: 'vox',
+		css: vox,
+		emoji: undefined
+	},
+	{
+		name: 'wintry',
+		css: wintry,
+		emoji: undefined
+	}
+] as const;
