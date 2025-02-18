49200fb7311a171bd02bfde52619c2eb473dbd2a
endigo9740 <gundamx9740@gmail.com>
2025-02-18 15:12:01 -0600

    Temporary RC1 importer added

--- sites/themes.skeleton.dev/src/routes/(app)/themes/import/+page.svelte
+++ sites/themes.skeleton.dev/src/routes/(app)/themes/import/+page.svelte
@@ -5,6 +5,7 @@
 	// Utils
 	import { importThemeV2 } from '$lib/utils/importer/import-theme-v2';
 	import { importThemeV3 } from '$lib/utils/importer/import-theme-v3';
+	import { importThemeV3Rc1 } from '$lib/utils/importer/import-theme-v3-rc1';
 	// Componets (skeleton)
 	import { FileUpload } from '@skeletonlabs/skeleton-svelte';
 	// Icons
@@ -38,12 +39,18 @@
 		const file = event.acceptedFiles[0];
 		const fileText = await file.text();
 		const isCssFormat = fileName.includes('.css');
-		// Run Importer
+
+		// // Run Importer
 		if (isCssFormat) {
 			importThemeV3(fileText, fileName);
 		} else {
 			importThemeV2(fileText, fileName);
 		}
+
+		// **** TEMPORARY ****
+		// importThemeV3Rc1(fileText, fileName);
+		// ******** / ********
+
 		// Redirect to Generator page
 		goto('/themes/create');
 	}
