9e74bc87b97ab48b98401d7351a416dbd666e1fd
Hugos68 <hugokorteapple@gmail.com>
2025-02-18 10:02:53 +0100

    format && lint

--- packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.html.ts
+++ packages/skeleton-cli/src/commands/migrate/migrations/skeleton-3/transformers/transform-app.html.ts
@@ -1,42 +1,29 @@
-import { type AST, parse } from 'svelte/compiler';
-import MagicString from 'magic-string';
-import { walk } from 'zimmerframe';
+import { parse } from 'node-html-parser';
 import { THEME_MAPPINGS } from '../utility/theme-mappings';
 
 function transformAppHtml(code: string) {
-	const s = new MagicString(code);
-	const ast = parse(code, {
-		modern: true
-	});
-	let theme: string | undefined;
-	walk(
-		ast.fragment as AST.SvelteNode,
-		{},
-		{
-			RegularElement(node, ctx) {
-				if (node.name === 'body') {
-					for (const attribute of node.attributes) {
-						if (
-							!(
-								attribute.type === 'Attribute' &&
-								attribute.name === 'data-theme' &&
-								Array.isArray(attribute.value) &&
-								attribute.value.length === 1 &&
-								attribute.value[0].type === 'Text'
-							)
-						) {
-							continue;
-						}
-						const theme = attribute.value[0].data;
-						if (Object.hasOwn(THEME_MAPPINGS, theme)) {
-							s.update(attribute.start, attribute.end, `data-theme="${THEME_MAPPINGS[theme]}"`);
-						} else {
-			$('html').attr('data-theme', theme);
+	const parsed = parse(code);
+	const html = parsed.querySelector('html');
+	const body = parsed.querySelector('body');
+	if (!(html && body)) {
+		return {
+			code: code,
+			meta: {
+				theme: undefined
 			}
+		};
+	}
+	const theme = body.getAttribute('data-theme');
+	if (theme) {
+		body.removeAttribute('data-theme');
+		if (Object.hasOwn(THEME_MAPPINGS, theme)) {
+			html.setAttribute('data-theme', THEME_MAPPINGS[theme]);
+		} else {
+			html.setAttribute('data-theme', theme);
 		}
-	);
+	}
 	return {
-		code: s.toString(),
+		code: parsed.toString(),
 		meta: {
 			theme: theme
 		}
