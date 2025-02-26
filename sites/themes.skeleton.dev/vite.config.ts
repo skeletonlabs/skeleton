a02f24bfdf930316d33f8b816c4f781113bf7d10
Hugos68 <hugokorteapple@gmail.com>
2025-02-26 19:42:57 +0100

    revert themes.skeleton.dev changes

--- sites/themes.skeleton.dev/vite.config.ts
+++ sites/themes.skeleton.dev/vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [sveltekit(), tailwind()]
 });
