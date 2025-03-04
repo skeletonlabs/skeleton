d84a98010a13f5a93a51611341197eb2a426a054
endigo9740 <gundamx9740@gmail.com>
2025-03-04 15:05:47 -0600

    Header popovers added

---
+++ sites/next.skeleton.dev/src/components/docs/HeaderSponsors.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useState } from 'react';
+// Floating UI React
+import {
+	useFloating,
+	autoUpdate,
+	offset,
+	flip,
+	shift,
+	useClick,
+	useDismiss,
+	useRole,
+	useInteractions,
+	FloatingFocusManager
+} from '@floating-ui/react';
+// Icons
+import { ChevronDown as IconChevron } from 'lucide-react';
+import { Heart as IconHeart } from 'lucide-react';
+
+export const HeaderSponsors: React.FC = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const { refs, floatingStyles, context } = useFloating({
+		open: isOpen,
+		onOpenChange: setIsOpen,
+		middleware: [offset(8), flip(), shift()],
+		placement: 'top-end',
+		whileElementsMounted: autoUpdate
+	});
+
+	const click = useClick(context);
+	const dismiss = useDismiss(context);
+	const role = useRole(context);
+
+	const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
+
+	return (
+		<div>
+			{/* Trigger */}
+			<button className="hidden xl:flex btn hover:preset-tonal" title="Sponsor Us" ref={refs.setReference} {...getReferenceProps()}>
+				<IconHeart className="size-5" />
+				<IconChevron className="size-4 opacity-60" />
+			</button>
+			{/* Popup */}
+			{isOpen && (
+				<FloatingFocusManager context={context} modal={false}>
+					<div
+						className="card bg-surface-50-950 border border-surface-200-800 flex flex-col gap-1 p-4"
+						ref={refs.setFloating}
+						style={floatingStyles}
+						{...getFloatingProps()}
+					>
+						<a
+							className="btn hover:preset-tonal justify-start"
+							href="https://github.com/sponsors/skeletonlabs"
+							target="_blank"
+							rel="noreferrer"
+							onClick={() => setIsOpen(false)}
+						>
+							<span>GitHub Sponsors</span>
+						</a>
+						<a
+							className="btn hover:preset-tonal justify-start"
+							href="https://ko-fi.com/skeletonlabs"
+							target="_blank"
+							rel="noreferrer"
+							onClick={() => setIsOpen(false)}
+						>
+							<span>Ko-Fi</span>
+						</a>
+					</div>
+				</FloatingFocusManager>
+			)}
+		</div>
+	);
+};
---
+++ sites/next.skeleton.dev/src/components/docs/HeaderThemes.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+// Floating UI React
+import {
+	useFloating,
+	autoUpdate,
+	offset,
+	flip,
+	shift,
+	useClick,
+	useDismiss,
+	useRole,
+	useInteractions,
+	FloatingFocusManager
+} from '@floating-ui/react';
+// Themes
+import { themes } from '@skeletonlabs/skeleton-common';
+// Icons
+import { ChevronDown as IconChevron } from 'lucide-react';
+import { SwatchBook as IconTheme } from 'lucide-react';
+
+export const HeaderThemes: React.FC = () => {
+	const defaultTheme = 'cerberus';
+
+	const [isOpen, setIsOpen] = useState(false);
+	const [activeTheme, setActiveTheme] = useState(defaultTheme);
+
+	useEffect(() => {
+		setActiveTheme(localStorage.getItem('theme') || defaultTheme);
+	}, []);
+
+	const { refs, floatingStyles, context } = useFloating({
+		open: isOpen,
+		onOpenChange: setIsOpen,
+		middleware: [offset(8), flip(), shift()],
+		// placement: 'top-end',
+		whileElementsMounted: autoUpdate
+	});
+
+	const click = useClick(context);
+	const dismiss = useDismiss(context);
+	const role = useRole(context);
+
+	const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
+
+	function onSelection(themeName: string) {
+		setActiveTheme(themeName);
+
+		document.body.classList.add('transition-all');
+		document.body.classList.add('duration-200');
+
+		document.documentElement.setAttribute('data-theme', themeName);
+		localStorage.setItem('theme', themeName);
+		setIsOpen(false);
+
+		setTimeout(() => {
+			document.body.classList.remove('transition-all');
+			document.body.classList.remove('duration-200');
+		}, 200);
+	}
+
+	return (
+		<div>
+			{/* Trigger */}
+			<button className="btn hover:preset-tonal" title="Choose Theme" ref={refs.setReference} {...getReferenceProps()}>
+				<IconTheme className="xl:hidden size-5" />
+				<span className="hidden xl:inline">Theme</span>
+				<IconChevron className="size-4 opacity-60" />
+			</button>
+			{/* Popup */}
+			{isOpen && (
+				<FloatingFocusManager context={context} modal={false}>
+					<div
+						className="card max-h-72 md:max-h-96 xl:max-h-none overflow-y-auto xl:overflow-y-visible bg-surface-50-950 border border-surface-200-800 grid grid-cols-1 xl:grid-cols-3 gap-2 p-4 pr-2"
+						ref={refs.setFloating}
+						style={floatingStyles}
+						{...getFloatingProps()}
+					>
+						{themes.map((theme) => (
+							<button
+								key={theme.name}
+								data-theme={theme.name}
+								className={`w-full bg-surface-50-950 p-3 preset-outlined-surface-100-900 hover:preset-outlined-surface-950-50 rounded-md grid grid-cols-[auto_1fr_auto] items-center gap-4 ${activeTheme === theme.name ? 'preset-outlined-surface-500' : ''}`}
+								onClick={() => onSelection(theme.name)}
+							>
+								<span>{theme.emoji}</span>
+								<h3 className="text-sm capitalize font-bold text-left">{theme.name}</h3>
+								<div className="flex justify-center items-center -space-x-1.5">
+									<div className="aspect-square w-4 bg-primary-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-secondary-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-tertiary-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-success-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-warning-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-error-500 border-[1px] border-black/10 rounded-full" />
+									<div className="aspect-square w-4 bg-surface-500 border-[1px] border-black/10 rounded-full" />
+								</div>
+							</button>
+						))}
+					</div>
+				</FloatingFocusManager>
+			)}
+		</div>
+	);
+};
