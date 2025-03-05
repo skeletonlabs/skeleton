45dd41d09c2a77cc81df46feaf3f053c786eeb0f
Hugos68 <hugokorteapple@gmail.com>
2025-03-05 07:48:39 +0100

    test consistency

--- packages/necroparser/test/samples.test.ts
+++ packages/necroparser/test/samples.test.ts
@@ -4,19 +4,20 @@ import { fileURLToPath, resolve } from 'node:url';
 import { describe, expect, test } from 'vitest';
 import { getInterfaces } from '../src/index.js';
 
-const fixturesTest = import.meta.glob('./fixtures/*.ts', {
+const samples = import.meta.glob('./samples/**/input.ts', {
 	query: '?raw'
 });
 
 describe('fixtures', () => {
-	for (const path of Object.keys(fixturesTest)) {
-		const name = path.split('/').at(-1);
+	for (const path of Object.keys(samples)) {
+		const parts = path.split('/');
+		const name = parts.at(-2);
 		if (!name) {
 			continue;
 		}
 		test(name, async () => {
 			const actual = getInterfaces(resolve(fileURLToPath(import.meta.url), path));
-			const expected = await import(path.replace('fixtures', 'results').replace('.ts', '.json')).then((module) => module.default);
+			const expected = await import(path.replace('input.ts', 'output.json')).then((module) => module.default);
 			expect(actual).toStrictEqual(expected);
 		});
 	}
