f84afb84b495b6d9b20b602bf62647f39f3897cd
Hugos68 <hugokorteapple@gmail.com>
2025-03-07 23:37:08 +0100

    remove mock stuff

--- sites/next.skeleton.dev/scripts/generate-schemas.ts
+++ sites/next.skeleton.dev/scripts/generate-schemas.ts
@@ -5,7 +5,7 @@ import { join, resolve } from 'path';
 import { performance } from 'perf_hooks';
 
 function log(message: string) {
-	console.log(`[${new Date().toLocaleTimeString()}] ${message}`);
+	console.log(`[${new Date().toLocaleTimeString()}] [generate-schemas] ${message}`);
 }
 
 async function generateSchemas() {
@@ -14,20 +14,36 @@ async function generateSchemas() {
 	const inputPaths = await glob('./node_modules/@skeletonlabs/skeleton-**/src/components/**/types.ts');
 	const outputPath = resolve(import.meta.dirname, '../src/content/schemas');
 	const project = new Project(inputPaths);
-	await rm(outputPath, { recursive: true, force: true });
-	await Promise.all(
+	await rm(outputPath, {
+		recursive: true,
+		force: true
+	});
+	await Promise.allSettled(
 		inputPaths.map(async (path) => {
 			const framework = path.match(/@skeletonlabs\/skeleton-(\w+)/)?.[1];
 			const component = path.match(/\/components\/(\w+)/)?.[1];
-			if (!framework || !component) return;
-
+			if (!(framework && component)) {
+				return;
+			}
+			log(`Generating schema for ${framework}/${component}`);
 			const interfaces = project.getInterfaces(path, {
-				filter: (node) => /\w+Props/.test(node.name.getText()),
-				transformProperty: (property) => (property.type.startsWith('Snippet') ? { ...property, typeKind: 'primitive' } : property)
+				filter(node) {
+					return /\w+Props/.test(node.name.getText());
+				},
+				transformProperty(property) {
+					if (property.type.startsWith('Snippet')) {
+						return {
+							...property,
+							typeKind: 'primitive'
+						};
+					}
+					return property;
+				}
 			});
-
 			const frameworkPath = join(outputPath, framework);
-			await mkdir(frameworkPath, { recursive: true });
+			await mkdir(frameworkPath, {
+				recursive: true
+			});
 			await writeFile(join(frameworkPath, `${component}.json`), JSON.stringify(interfaces, null, 4));
 		})
 	);
