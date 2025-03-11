85e611983c475ab778b1a113863e2c060d8c5bd2
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2025-03-11 21:53:50 +0100

    docs(react-avatar-examples): Fix the casing for the prop name `colorInterpolationFilters` (#3373)

--- sites/skeleton.dev/src/examples/components/avatars/ExampleFilter.tsx
+++ sites/skeleton.dev/src/examples/components/avatars/ExampleFilter.tsx
@@ -10,7 +10,7 @@ export const Page: React.FC = () => {
 			{/* SVG Filter */}
 			{/* Ideally you have as single instance of this in your application. */}
 			<svg id="svg-filter-apollo" className="absolute -left-full w-0 h-0">
-				<filter id="Apollo" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+				<filter id="Apollo" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
 					<feColorMatrix
 						values="0.8 0.6 -0.4 0.1 0,
 					0 1.2 0.05 0 0,
