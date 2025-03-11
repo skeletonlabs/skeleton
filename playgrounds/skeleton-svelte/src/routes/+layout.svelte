d3168475b4fac07d35e6b646648df01c8aca8ac7
Hugos68 <hugokorteapple@gmail.com>
2025-03-11 22:51:34 +0100

    docs(homepage-switch): Fix bind: still being used

--- playgrounds/skeleton-svelte/src/routes/+layout.svelte
+++ playgrounds/skeleton-svelte/src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 		<div class="bg-surface-100-900 space-y-8 overflow-y-auto p-8">
 			<a class="text-sm bg-orange-500 p-2 font-mono font-bold text-white" href="/">skeleton-svelte</a>
 			<hr class="hr" />
-			<Switch name="mode" controlActive="bg-surface-200" bind:checked={modeState} onCheckedChange={onModeChange}>
+			<Switch name="mode" controlActive="bg-surface-200" checked={modeState} onCheckedChange={onModeChange}>
 				{#snippet inactiveChild()}<IconMoon size="14" />{/snippet}
 				{#snippet activeChild()}<IconSun size="14" />{/snippet}
 				<p>Toggle Mode</p>
