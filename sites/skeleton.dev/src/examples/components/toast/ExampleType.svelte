c6ab1e54a8b65c24badf091a3f81fc7abc73e651
Hugos68 <hugokorteapple@gmail.com>
2025-03-21 19:41:31 +0100

    example type

---
+++ sites/skeleton.dev/src/examples/components/toast/ExampleType.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { Toaster, createToaster } from '@skeletonlabs/skeleton-svelte';
+
+	const toaster = createToaster();
+</script>
+
+<Toaster {toaster}></Toaster>
+
+<div class="grid gap-2">
+	<button
+		class="btn preset-filled"
+		onclick={() => {
+			toaster.info({
+				type: 'info',
+				title: 'This is a toast!'
+			});
+		}}>Info</button
+	>
+
+	<button
+		class="btn preset-tonal-success"
+		onclick={() => {
+			toaster.info({
+				type: 'error',
+				title: 'This is a toast!'
+			});
+		}}>Success</button
+	>
+
+	<button
+		class="btn preset-tonal-error"
+		onclick={() => {
+			toaster.info({
+				type: 'error',
+				title: 'This is a toast!'
+			});
+		}}>Toast!</button
+	>
+</div>
---
+++ sites/skeleton.dev/src/examples/components/toast/ExampleType.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { Toaster, createToaster } from '@skeletonlabs/skeleton-react';
+
+export default function () {
+	const toaster = createToaster();
+	return (
+		<>
+			<Toaster toaster={toaster}></Toaster>
+			<div className="grid gap-2">
+				<button
+					className="btn preset-filled"
+					onClick={() => {
+						toaster.info({
+							type: 'info',
+							title: 'This is a toast!'
+						});
+					}}
+				>
+					Info
+				</button>
+
+				<button
+					className="btn preset-tonal-success"
+					onClick={() => {
+						toaster.info({
+							type: 'error',
+							title: 'This is a toast!'
+						});
+					}}
+				>
+					Success
+				</button>
+
+				<button
+					className="btn preset-tonal-error"
+					onClick={() => {
+						toaster.info({
+							type: 'error',
+							title: 'This is a toast!'
+						});
+					}}
+				>
+					Toast!
+				</button>
+			</div>
+		</>
+	);
+}
