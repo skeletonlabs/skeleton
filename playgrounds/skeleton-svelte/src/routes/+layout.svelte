af3a91bfdaadb286168e8f688bea0e56f2b0ea42
Hugos68 <hugokorteapple@gmail.com>
2025-03-31 15:45:39 +0200

    merge main

--- playgrounds/skeleton-svelte/src/routes/+layout.svelte
+++ playgrounds/skeleton-svelte/src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	// Components
-	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
-	import { Switch } from '@skeletonlabs/skeleton-svelte';
-	// Icons
 	import LightSwitch from './light-switch.svelte';
 
 	let { children } = $props();
@@ -35,6 +31,7 @@
 					<a class="anchor" href="/components/switch">Switch</a>
 					<a class="anchor" href="/components/tabs">Tabs</a>
 					<a class="anchor" href="/components/tags-input">Tags Input</a>
+					<a class="anchor" href="/components/toast">Toast</a>
 				</nav>
 			</div>
 			<!-- Svelte-Only -->
@@ -45,63 +42,9 @@
 					<a class="anchor" href="/components/tooltip">Tooltip</a>
 					<a class="anchor" href="/components/combobox">Combobox</a>
 					<a class="anchor" href="/components/modal">Modal</a>
-					<a class="anchor" href="/components/toast">Toast</a>
 				</nav>
 			</div>
-		</div>
-		<!-- Page -->
-		<main class="space-y-8 overflow-y-auto p-8">
-			{@render children?.()}
-		</main>
-	</div>
-</ToastProvider>
 		</header>
-		<hr class="hr" />
-		<Switch
-			name="mode"
-			controlActive="bg-surface-200"
-			checked={modeState}
-			onCheckedChange={(e) => {
-				modeState = e.checked;
-				toggleMode();
-			}}
-		>
-			{#snippet inactiveChild()}<IconMoon size="14" />{/snippet}
-			{#snippet activeChild()}<IconSun size="14" />{/snippet}
-			<p>Toggle Mode</p>
-		</Switch>
-		<hr class="hr" />
-		<!-- Components -->
-		<div class="space-y-8">
-			<div class="font-bold">Components</div>
-			<nav class="text-sm flex flex-col gap-2">
-				<a class="anchor" href="/components/accordions">Accordions</a>
-				<a class="anchor" href="/components/app-bars">App Bars</a>
-				<a class="anchor" href="/components/avatars">Avatars</a>
-				<a class="anchor" href="/components/file-upload">File Upload</a>
-				<a class="anchor" href="/components/navigation">Navigation</a>
-				<a class="anchor" href="/components/pagination">Pagination</a>
-				<a class="anchor" href="/components/progress">Progress</a>
-				<a class="anchor" href="/components/progress-rings">Progress Rings</a>
-				<a class="anchor" href="/components/ratings">Ratings</a>
-				<a class="anchor" href="/components/segment-control">Segment Control</a>
-				<a class="anchor" href="/components/slider">Slider</a>
-				<a class="anchor" href="/components/switch">Switch</a>
-				<a class="anchor" href="/components/tabs">Tabs</a>
-				<a class="anchor" href="/components/tags-input">Tags Input</a>
-				<a class="anchor" href="/components/toast">Toast</a>
-			</nav>
-		</div>
-		<!-- Svelte-Only -->
-		<div class="space-y-8">
-			<div class="font-bold">Svelte-Only</div>
-			<nav class="text-sm flex flex-col gap-2">
-				<a class="anchor" href="/components/popover">Popover</a>
-				<a class="anchor" href="/components/tooltip">Tooltip</a>
-				<a class="anchor" href="/components/combobox">Combobox</a>
-				<a class="anchor" href="/components/modal">Modal</a>
-			</nav>
-		</div>
 	</div>
 	<!-- Page -->
 	<main class="space-y-8 overflow-y-auto p-8">
