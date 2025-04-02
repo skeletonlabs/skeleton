296ba4fb54fa14ae4643fb7555fc9d437cfa2fb7
Louis Li <lee178lee@gmail.com>
2025-04-01 20:15:31 -0700

    fix

--- sites/skeleton.dev/src/components/docs/Drawer.tsx
+++ sites/skeleton.dev/src/components/docs/Drawer.tsx
@@ -22,8 +22,6 @@ export const Drawer: React.FC<DrawerProps> = ({ navigation, children }) => {
 		open: isOpen,
 		onOpenChange: setIsOpen,
 		middleware: [offset(0)],
-		placement: 'left-start',
-		strategy: 'fixed',
 		whileElementsMounted: autoUpdate
 	});
 
@@ -34,48 +32,58 @@ export const Drawer: React.FC<DrawerProps> = ({ navigation, children }) => {
 
 	return (
 		<div>
-			{/* Trigger (Hamburger icon) */}
+			{/* Trigger */}
 			<button className="btn hover:preset-tonal gap-1" {...getReferenceProps({ ref: refs.setReference })} onClick={() => setIsOpen(true)}>
 				<IconMenu className="size-4" />
 			</button>
 
 			{isOpen && (
 				<>
-					<div className="fixed inset-0 bg-surface-50-950" onClick={() => setIsOpen(false)} />
-
 					<FloatingFocusManager context={context} modal={false}>
 						<div
 							ref={refs.setFloating}
-							style={{ position: 'fixed', top: 0, left: 0 }}
 							{...getFloatingProps()}
-							className="z-50 h-screen overflow-y-auto bg-surface-50-950 p-4 
-                         w-full sm:w-80 md:w-96"
+							className="fixed top-0 left-0 bottom-0 z-50 h-screen overflow-y-auto bg-surface-50-950 p-4 w-72"
 						>
 							{/* Header row */}
 							<div className="flex items-center justify-between">
-								<h1 className="text-xl font-bold">Skeleton</h1>
+								<h3 className="h3">Skeleton</h3>
 								<button onClick={() => setIsOpen(false)} aria-label="Close Drawer">
 									<IconClose className="size-5" />
 								</button>
 							</div>
 
-							{/* Navigation links */}
-							<div className="mt-4">
-								<h2 className="h2">Navigation</h2>
-								<a className="mt-2 flex flex-col gap-2">
-									{navigation.map((link) => (
-										<a
-											key={link.label}
-											href={link.href}
-											target={link.target}
-											className="btn hover:preset-tonal justify-between"
-											onClick={() => setIsOpen(false)}
-										>
-											{link.label}
-										</a>
-									))}
-								</a>
+							{/* Version Section */}
+							<div className="mt-4 w-full">
+								<span className="text-sm font-bold capitalize ml-2">Version</span>
+								<div className="mt-2 flex w-full justify-between gap-2">
+									<a className="btn flex-1 text-center cursor-not-allowed opacity-50 pointer-events-none">v3</a>
+									<a href="https://v2.skeleton.dev/" target="_blank" rel="noreferrer" className="btn hover:preset-tonal flex-1 text-center">
+										v2
+									</a>
+									<a href="https://v1.skeleton.dev/" target="_blank" rel="noreferrer" className="btn hover:preset-tonal flex-1 text-center">
+										v1
+									</a>
+								</div>
 							</div>
+
+							{/* Navigation links */}
+							<div className="mt-4 mb-10">
+								<span className="text-sm font-bold capitalize ml-2">Navigation</span>
+								<nav className="mt-2">
+									<ul className="flex flex-col gap-1">
+										{navigation.map((link) => (
+											<li key={link.label}>
+												<a href={link.href} target={link.target} className="px-2 py-1 rounded-base anchor" onClick={() => setIsOpen(false)}>
+													{link.label}
+												</a>
+											</li>
+										))}
+									</ul>
+								</nav>
+							</div>
+
+							{/* Child content */}
 							{children && <div className="mt-auto">{children}</div>}
 						</div>
 					</FloatingFocusManager>
