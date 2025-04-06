da3bd029385f65141bb221bf092cb0c233b669d4
Hugos68 <hugokorteapple@gmail.com>
2025-04-06 19:11:41 +0200

    fix

--- packages/skeleton-svelte/src/components-overhaul/avatar/modules/types.ts
+++ packages/skeleton-svelte/src/components-overhaul/avatar/modules/types.ts
@@ -1,6 +1,6 @@
 import * as avatar from '@zag-js/avatar';
 import type { HTMLAttributes, HTMLImgAttributes } from 'svelte/elements';
-import type { PropsWithChildren } from '../../../../internal/props-with-children.js';
+import type { PropsWithChildren } from '../../../internal/props-with-children.js';
 
 interface AvatarRootProps extends PropsWithChildren, Omit<avatar.Props, 'id'>, Omit<HTMLAttributes<HTMLElement>, 'id' | 'dir'> {}
 type AvatarImageProps = HTMLImgAttributes;
