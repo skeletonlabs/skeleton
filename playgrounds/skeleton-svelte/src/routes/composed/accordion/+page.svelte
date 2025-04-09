0ac37236ccf54d04fdcc1e253950f8d79b851f1e
Hugos68 <hugokorteapple@gmail.com>
2025-04-09 20:50:23 +0200

    fix

--- playgrounds/skeleton-svelte/src/routes/composed/accordion/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/composed/accordion/+page.svelte
@@ -6,21 +6,21 @@
 	<Accordion.Item value="item-1">
 		<Accordion.Heading>
 			<Accordion.Trigger>
-				{#snippet child({ attributes })}
+				{#snippet element({ attributes })}
 					<button {...attributes}>My Own Button</button>
 				{/snippet}
 			</Accordion.Trigger>
 			<Accordion.Content>Content for Item 1</Accordion.Content>
 		</Accordion.Heading>
 	</Accordion.Item>
-	<hr class="hr">
+	<hr class="hr" />
 	<Accordion.Item value="item-2">
 		<Accordion.Heading>
 			<Accordion.Trigger>Item 2</Accordion.Trigger>
 			<Accordion.Content>Content for Item 2</Accordion.Content>
 		</Accordion.Heading>
 	</Accordion.Item>
-	<hr class="hr">
+	<hr class="hr" />
 	<Accordion.Item value="item-3">
 		<Accordion.Heading>
 			<Accordion.Trigger>Item 3</Accordion.Trigger>
