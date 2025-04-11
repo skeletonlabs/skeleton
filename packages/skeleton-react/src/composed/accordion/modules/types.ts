92c10f42b45333e93cee625ebde8d476f5b751e4
Hugos68 <hugokorteapple@gmail.com>
2025-04-11 17:34:15 +0200

    use `ComponentProps`

--- packages/skeleton-react/src/composed/accordion/modules/types.ts
+++ packages/skeleton-react/src/composed/accordion/modules/types.ts
@@ -1,15 +1,15 @@
 import * as accordion from '@zag-js/accordion';
-import type { ButtonHTMLAttributes, HTMLAttributes, PropsWithChildren } from 'react';
+import type { PropsWithChildren, ComponentProps } from 'react';
 import type { PropsWithElement } from '../../../internal/props-with-child.js';
 
 interface AccordionRootProps
 	extends PropsWithChildren,
 		PropsWithElement,
 		Omit<accordion.Props, 'id'>,
-		Omit<HTMLAttributes<HTMLDivElement>, 'id' | 'defaultValue' | 'dir'> {}
-interface AccordionItemProps extends PropsWithChildren, PropsWithElement, accordion.ItemProps, HTMLAttributes<HTMLDivElement> {}
-interface AccordionTriggerProps extends PropsWithChildren, PropsWithElement, ButtonHTMLAttributes<HTMLButtonElement> {}
-interface AccordionHeadingProps extends PropsWithChildren, PropsWithElement, HTMLAttributes<HTMLHeadingElement> {
+		Omit<ComponentProps<'div'>, 'id' | 'defaultValue' | 'dir'> {}
+interface AccordionItemProps extends PropsWithChildren, PropsWithElement, accordion.ItemProps, ComponentProps<'div'> {}
+interface AccordionTriggerProps extends PropsWithChildren, PropsWithElement, ComponentProps<'button'> {}
+interface AccordionHeadingProps extends PropsWithChildren, PropsWithElement, ComponentProps<'h3'> {
 	/**
 	 * The level of the heading. This is used to determine the heading level for accessibility purposes.
 	 *
@@ -17,8 +17,8 @@ interface AccordionHeadingProps extends PropsWithChildren, PropsWithElement, HTM
 	 */
 	level?: 1 | 2 | 3 | 4 | 5 | 6;
 }
-interface AccordionIndicatorProps extends PropsWithChildren, PropsWithElement, HTMLAttributes<HTMLDivElement> {}
-interface AccordionContentProps extends PropsWithChildren, PropsWithElement, HTMLAttributes<HTMLDivElement> {}
+interface AccordionIndicatorProps extends PropsWithChildren, PropsWithElement, ComponentProps<'div'> {}
+interface AccordionContentProps extends PropsWithChildren, PropsWithElement, ComponentProps<'div'> {}
 interface AccordionContext {
 	api: accordion.Api;
 }
--- packages/skeleton-react/src/composed/avatar/modules/types.ts
+++ packages/skeleton-react/src/composed/avatar/modules/types.ts
@@ -1,14 +1,14 @@
 import * as avatar from '@zag-js/avatar';
-import type { HTMLAttributes, ImgHTMLAttributes, PropsWithChildren } from 'react';
+import type { ComponentProps, PropsWithChildren } from 'react';
 import type { PropsWithElement } from '../../../internal/props-with-child.js';
 
 interface AvatarRootProps
 	extends PropsWithChildren,
 		PropsWithElement,
 		Omit<avatar.Props, 'id'>,
-		Omit<HTMLAttributes<HTMLDivElement>, 'id' | 'dir'> {}
-interface AvatarImageProps extends PropsWithElement, ImgHTMLAttributes<HTMLImageElement> {}
-interface AvatarFallbackProps extends PropsWithChildren, PropsWithElement, HTMLAttributes<HTMLSpanElement> {}
+		Omit<ComponentProps<'div'>, 'id' | 'dir'> {}
+interface AvatarImageProps extends PropsWithElement, ComponentProps<'img'> {}
+interface AvatarFallbackProps extends PropsWithChildren, PropsWithElement, ComponentProps<'span'> {}
 interface AvatarRootContext {
 	api: avatar.Api;
 }
