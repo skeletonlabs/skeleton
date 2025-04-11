bf5eb15b0e9540a36f5b7afa0d6b097cf400c5a0
Chris Simmons <gundamx9740@gmail.com>
2025-04-11 00:56:29 -0500

    Update Toast component docs (#3513)

---
+++ sites/skeleton.dev/src/examples/components/toast/ExampleReusePage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { toaster } from './toaster-svelte';
+</script>
+
+<button
+	class="btn preset-filled"
+	onclick={() => {
+		toaster.info({
+			title: 'This is a toast!'
+		});
+	}}
+>
+	Toast
+</button>
