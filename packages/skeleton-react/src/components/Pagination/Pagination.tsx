e1c60ccbacf93a485ca840cf51cacb5f8631bfaa
Ross <git@ross.ch>
2025-05-19 17:48:05 +0200

    Segment/Pagination: move default border padding to padding prop

--- packages/skeleton-react/src/components/Pagination/Pagination.tsx
+++ packages/skeleton-react/src/components/Pagination/Pagination.tsx
@@ -20,9 +20,9 @@ export const Pagination: FC<PaginationProps> = ({
 	// Root
 	base = 'inline-flex items-stretch overflow-hidden',
 	background = 'preset-outlined-surface-200-800',
-	border = 'p-2',
+	border = '',
 	gap = 'gap-2',
-	padding = '',
+	padding = 'p-2',
 	rounded = 'rounded-container',
 	classes = '',
 	// Buttons
