39ae970f682ffd027055109fae779b7e0506ff4e
dev-viinz <42003446+dev-viinz@users.noreply.github.com>
2025-07-04 20:36:59 +0200

    fix: remove unused context

--- packages/skeleton-svelte/src/components/TreeView/context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from '../../internal/create-context.js';
-import type { TreeViewContext } from './types.js';
-
-// @ts-expect-error - Defaults for context don't make sense, `createContext` should just throw TBH
-export const [setTreeViewContext, getTreeViewContext, key] = createContext<TreeViewContext>();
