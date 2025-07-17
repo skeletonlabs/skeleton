97f0df36077a7069db874818be7e5216006d75da
Hugos68 <hugokorteapple@gmail.com>
2025-07-17 19:38:02 +0200

    Fix resize observer mock

--- packages/skeleton-react/vitest.setup.js
+++ packages/skeleton-react/vitest.setup.js
@@ -1,4 +1,4 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import '@testing-library/user-event';
@@ -6,3 +6,9 @@ import '@testing-library/user-event';
 afterEach(() => {
 	cleanup();
 });
+
+globalThis.ResizeObserver = class MockedResizeObserver {
+	observe = vi.fn();
+	unobserve = vi.fn();
+	disconnect = vi.fn();
+};
