3092699feeec022407424346d2eb0b34ff5c5d54
Hugos68 <hugokorteapple@gmail.com>
2025-08-14 19:37:32 +0200

    Add accordion examples

---
+++ sites/skeleton.dev/src/examples/components-composed/svelte/accordion/multiple.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Accordion } from '@skeletonlabs/skeleton-svelte/composed';
+</script>
+
+<Accordion multiple>
+	<Accordion.Item value="item-1">
+		<Accordion.Heading>
+			<Accordion.Trigger>Item 1</Accordion.Trigger>
+			<Accordion.Content>Content for Item 1</Accordion.Content>
+		</Accordion.Heading>
+	</Accordion.Item>
+	<Accordion.Item value="item-2">
+		<Accordion.Heading>
+			<Accordion.Trigger>Item 2</Accordion.Trigger>
+			<Accordion.Content>Content for Item 2</Accordion.Content>
+		</Accordion.Heading>
+	</Accordion.Item>
+	<Accordion.Item value="item-3">
+		<Accordion.Heading>
+			<Accordion.Trigger>Item 3</Accordion.Trigger>
+			<Accordion.Content>Content for Item 3</Accordion.Content>
+		</Accordion.Heading>
+	</Accordion.Item>
+</Accordion>
