064434d763d8559b6e95a47bfccb701da1891a73
Hugos68 <hugokorteapple@gmail.com>
2025-08-26 20:04:40 +0200

    Add skb: to all strings with a rollup plugin

--- packages/skeleton-common/build.config.ts
+++ packages/skeleton-common/build.config.ts
@@ -1,7 +1,5 @@
 import { defineBuildConfig } from 'unbuild';
-import { join, relative } from 'node:path';
-import { walk } from 'estree-walker';
-import MagicString from 'magic-string';
+import prefixStringLiterals from './build/rollup-plugin-prefix-string-literals.js';
 
 export default defineBuildConfig({
 	entries: ['./src/index.ts'],
@@ -13,45 +11,11 @@ export default defineBuildConfig({
 	},
 	hooks: {
 		'rollup:options'(_, options) {
-			options.plugins?.push({
-				name: 'prefix-string-literals',
-				transform(code, id) {
-					const path = relative(import.meta.dirname, id);
-					if (!path.startsWith(join('src', 'classes'))) {
-						return null;
-					}
-					const s = new MagicString(code);
-
-					const ast = this.parse(code);
-					walk(ast, {
-						enter(node) {
-							if (
-								node.type === 'Literal' &&
-								typeof node.value === 'string' &&
-								node.value.length > 0 &&
-								'start' in node &&
-								'end' in node &&
-								typeof node.start === 'number' &&
-								typeof node.end === 'number' &&
-								node
-							) {
-								s.overwrite(
-									node.start + 1,
-									node.end - 1,
-									node.value
-										.split(' ')
-										.map((v) => `skb:${v}`)
-										.join(' ')
-								);
-							}
-						}
-					});
-					return {
-						code: s.toString(),
-						map: s.generateMap()
-					};
-				}
-			});
+			options.plugins?.push(
+				prefixStringLiterals('skb', {
+					include: ['**/src/classes/**']
+				})
+			);
 		}
 	}
 });
