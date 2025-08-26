58563bcc22f1173e5d7f9a66033690de941811c8
Hugos68 <hugokorteapple@gmail.com>
2025-08-26 20:05:52 +0200

    formatter

--- packages/skeleton-common/build/rollup-plugin-prefix-string-literals.ts
+++ packages/skeleton-common/build/rollup-plugin-prefix-string-literals.ts
@@ -13,39 +13,39 @@ export default function(prefix: string, options: PrefixStringLiteralOptions): Ro
     const filter = createFilter(options.include, options.exclude);
     return {
         name: 'prefix-string-literals',
-                transform(code, id) {
-                    if (!filter(id)) return null;
-    
-                    const s = new MagicString(code);
-                    const ast = this.parse(code);
+        transform(code, id) {
+            if (!filter(id)) return null;
 
-                    walk(ast, {
-                        enter(node) {
-                            if (
-                                node.type === 'Literal' &&
-                                typeof node.value === 'string' &&
-                                node.value.length > 0 &&
-                                'start' in node &&
-                                'end' in node &&
-                                typeof node.start === 'number' &&
-                                typeof node.end === 'number' &&
-                                node
-                            ) {
-                                s.overwrite(
-                                    node.start + 1,
-                                    node.end - 1,
-                                    node.value
-                                        .split(' ')
-                                        .map((v) => `${prefix}:${v}`)
-                                        .join(' ')
-                                );
-                            }
-                        }
-                    });
-                    return {
-                        code: s.toString(),
-                        map: s.generateMap()
-                    };
+            const s = new MagicString(code);
+            const ast = this.parse(code);
+
+            walk(ast, {
+                enter(node) {
+                    if (
+                        node.type === 'Literal' &&
+                        typeof node.value === 'string' &&
+                        node.value.length > 0 &&
+                        'start' in node &&
+                        'end' in node &&
+                        typeof node.start === 'number' &&
+                        typeof node.end === 'number' &&
+                        node
+                    ) {
+                        s.overwrite(
+                            node.start + 1,
+                            node.end - 1,
+                            node.value
+                                .split(' ')
+                                .map((v) => `${prefix}:${v}`)
+                                .join(' ')
+                        );
+                    }
                 }
+            });
+            return {
+                code: s.toString(),
+                map: s.generateMap()
+            };
+        }
     }
 } 
