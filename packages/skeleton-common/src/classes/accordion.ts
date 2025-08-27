2a7ae324ff5526c79d057fdb844b3074a5736b87
Hugos68 <hugokorteapple@gmail.com>
2025-08-27 18:52:39 +0200

    remove prefix test

--- packages/skeleton-common/src/classes/accordion.ts
+++ packages/skeleton-common/src/classes/accordion.ts
@@ -1,5 +1,5 @@
 export const classesAccordion = {
-	root: 'w-full grid !!gap-2',
+	root: 'w-full grid gap-2',
 	content: 'py-2 px-4',
 	heading: '',
 	trigger: 'w-full grid items-center gap-4 py-2 px-4 rounded-base text-left hover:preset-tonal-primary',
