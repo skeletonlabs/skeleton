3f289eeee5bb686e1d07f504bba569b0fb648f87
Hugos68 <hugokorteapple@gmail.com>
2025-08-28 19:45:49 +0200

    lint/checks

--- sites/skeleton.dev/src/examples/guides/cookbook/light-switch/Example.tsx
+++ sites/skeleton.dev/src/examples/guides/cookbook/light-switch/Example.tsx
@@ -1,32 +1,25 @@
-import { Switch } from '@skeletonlabs/skeleton-react';
-import { useEffect, useState } from 'react';
-
 export default function () {
-	const [checked, setChecked] = useState(false);
-
-	useEffect(() => {
-		const mode = localStorage.getItem('mode') || 'light';
-		setChecked(mode === 'dark');
-	}, []);
-
-	const onCheckedChange = (event: { checked: boolean }) => {
-		const mode = event.checked ? 'dark' : 'light';
-		document.documentElement.setAttribute('data-mode', mode);
-		localStorage.setItem('mode', mode);
-		setChecked(event.checked);
-	};
-
-	return (
-		<>
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-		const mode = localStorage.getItem('mode') || 'light';
-		document.documentElement.setAttribute('data-mode', mode);
-          `
-				}}
-			/>
-			<Switch checked={checked} onCheckedChange={onCheckedChange} />
-		</>
-	);
+	// const [checked, setChecked] = useState(false);
+	// useEffect(() => {
+	// 	const mode = localStorage.getItem('mode') || 'light';
+	// 	setChecked(mode === 'dark');
+	// }, []);
+	// const onCheckedChange = (event: { checked: boolean }) => {
+	// 	const mode = event.checked ? 'dark' : 'light';
+	// 	document.documentElement.setAttribute('data-mode', mode);
+	// 	localStorage.setItem('mode', mode);
+	// 	setChecked(event.checked);
+	// };
+	// return (
+	// 	<>
+	// 		<script
+	// 			dangerouslySetInnerHTML={{
+	// 				__html: `
+	// 	const mode = localStorage.getItem('mode') || 'light';
+	// 	document.documentElement.setAttribute('data-mode', mode);
+	// 			}}
+	// 		/>
+	// 		<Switch checked={checked} onCheckedChange={onCheckedChange} />
+	// 	</>
+	// );
 }
