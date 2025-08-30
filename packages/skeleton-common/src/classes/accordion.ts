401cc6aa4cff6046a775bb6b09e84d7c25964220
Hugos68 <hugokorteapple@gmail.com>
2025-08-30 20:21:34 +0200

    Use `unplugin-macro`

--- packages/skeleton-common/src/classes/accordion.ts
+++ packages/skeleton-common/src/classes/accordion.ts
@@ -1,8 +1,10 @@
-export const classesAccordion = {
+import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' with { type: 'macro' };
+
+export const classesAccordion = defineSkeletonClasses({
 	root: 'w-full grid gap-2',
 	content: 'py-2 px-4',
 	heading: '',
 	trigger: 'w-full grid items-center gap-4 py-2 px-4 rounded-base text-left hover:preset-tonal-primary',
 	item: 'grid gap-2',
 	indicator: ''
-};
+});
