c3150c1e1dacb45338da44ef7c11c32f52a6ccca
Hugos68 <hugokorteapple@gmail.com>
2025-09-01 17:41:52 +0200

    comments

--- packages/skeleton-react/src/internal/props-with-element.ts
+++ packages/skeleton-react/src/internal/props-with-element.ts
@@ -1,5 +1,8 @@
 import type { JSX } from 'react';
 
 export interface PropsWithElement {
+	/**
+	 * Render the element yourself
+	 */
 	element?: (arg: { attributes: Record<string, unknown> }) => JSX.Element | null;
 }
