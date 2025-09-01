7f85aadc23fa3f1763520b1079e30ab2ebedb299
Hugos68 <hugokorteapple@gmail.com>
2025-09-01 13:57:56 +0200

    improvement

--- sites/skeleton.dev/scripts/generate-type-documentation.ts
+++ sites/skeleton.dev/scripts/generate-type-documentation.ts
@@ -1,4 +1,4 @@
-import { mkdir, rm, writeFile } from 'node:fs/promises';
+import { mkdir, readFile, rm, writeFile } from 'node:fs/promises';
 import { basename, dirname, join } from 'node:path';
 import { fileURLToPath, pathToFileURL } from 'node:url';
 import { glob } from 'tinyglobby';
@@ -35,55 +35,28 @@ interface Interface {
 }
 
 class SourceFile {
-	private framework: (typeof frameworks)[number];
-	private sourceFile: tsMorph.SourceFile;
-
-	constructor(framework: (typeof frameworks)[number], sourceFile: tsMorph.SourceFile) {
-		this.framework = framework;
-		this.sourceFile = sourceFile;
-	}
+	constructor(
+		private framework: (typeof frameworks)[number],
+		private sourceFile: tsMorph.SourceFile
+	) {}
 
 	private isFunctionType(type: tsMorph.Type) {
-		if (type.getCallSignatures().length > 0) {
-			return true;
-		}
-		if (type.getUnionTypes().some((t) => t.getCallSignatures().length > 0)) {
-			return true;
-		}
-		return false;
+		return type.getCallSignatures().length > 0 || type.getUnionTypes().some((t) => t.getCallSignatures().length > 0);
 	}
 
 	private isArrayType(type: tsMorph.Type) {
-		if (type.isArray()) {
-			return true;
-		}
-		if (type.getUnionTypes().some((t) => t.isArray())) {
-			return true;
-		}
-		return false;
+		return type.isArray() || type.getUnionTypes().some((t) => t.isArray());
 	}
 
 	private isObjectType(type: tsMorph.Type) {
-		if (type.isObject()) {
-			return true;
-		}
-		if (type.getUnionTypes().some((t) => t.isObject())) {
-			return true;
-		}
-		return false;
+		return type.isObject() || type.getUnionTypes().some((t) => t.isObject());
 	}
 
 	private getTypeKind(symbol: tsMorph.Symbol): TypeKind {
 		const type = symbol.getTypeAtLocation(this.sourceFile);
-		if (this.isFunctionType(type)) {
-			return 'function';
-		}
-		if (this.isArrayType(type)) {
-			return 'array';
-		}
-		if (this.isObjectType(type)) {
-			return 'object';
-		}
+		if (this.isFunctionType(type)) return 'function';
+		if (this.isArrayType(type)) return 'array';
+		if (this.isObjectType(type)) return 'object';
 		return 'primitive';
 	}
 
@@ -94,19 +67,11 @@ class SourceFile {
 			.flatMap((decl) => decl.getJsDocs())
 			.at(0);
 
-		if (!jsDoc) {
-			return {
-				description: null,
-				tags: []
-			};
-		}
+		if (!jsDoc) return { description: null, tags: [] };
 
 		return {
 			description: jsDoc.getDescription().trim(),
-			tags: jsDoc.getTags().map((tag) => ({
-				name: tag.getTagName(),
-				value: tag.getCommentText() ?? null
-			}))
+			tags: jsDoc.getTags().map((tag) => ({ name: tag.getTagName(), value: tag.getCommentText() ?? null }))
 		};
 	}
 
@@ -123,14 +88,14 @@ class SourceFile {
 
 	public getInterface(name: string): Interface {
 		const interface_ = this.sourceFile.getInterface(name);
-		if (!interface_) {
-			throw new Error(`Interface not found: ${name}`);
-		}
+		if (!interface_) throw new Error(`Interface not found: ${name}`);
+
 		interface_.getExtends().forEach((ext) => {
 			if (this.framework.config.extendsBlacklist.some((pattern) => pattern.test(ext.getText()))) {
 				interface_.removeExtends(ext);
 			}
 		});
+
 		return {
 			name: interface_.getName(),
 			properties: interface_
@@ -142,25 +107,22 @@ class SourceFile {
 }
 
 class Parser {
-	private framework: (typeof frameworks)[number];
 	private project: tsMorph.Project;
 
-	constructor(framework: (typeof frameworks)[number]) {
-		this.framework = framework;
+	constructor(private framework: (typeof frameworks)[number]) {
 		this.project = new tsMorph.Project({
 			skipAddingFilesFromTsConfig: true,
-			tsConfigFilePath: join(MONOREPO_ROOT, 'packages', `skeleton-${this.framework.name}`, 'tsconfig.json')
+			tsConfigFilePath: join(MONOREPO_ROOT, 'packages', `skeleton-${framework.name}`, 'tsconfig.json')
 		});
+
 		this.project.addSourceFilesAtPaths(
-			join(MONOREPO_ROOT, 'packages', `skeleton-${this.framework.name}`, 'dist', 'components/*/anatomy/*.d.ts')
+			join(MONOREPO_ROOT, 'packages', `skeleton-${framework.name}`, 'dist', 'components/*/anatomy/*.d.ts')
 		);
 	}
 
 	public getSourceFile(path: string): SourceFile {
 		const sourceFile = this.project.getSourceFile(path);
-		if (!sourceFile) {
-			throw new Error(`Source file not found: ${path}`);
-		}
+		if (!sourceFile) throw new Error(`Source file not found: ${path}`);
 		return new SourceFile(this.framework, sourceFile);
 	}
 }
@@ -173,17 +135,8 @@ function kebabToPascal(str: string) {
 	return str.split('-').map(capitalize).join('');
 }
 
-async function getClassValue(component: string, part: string) {
-	const { [`classes${kebabToPascal(component)}`]: classes } = await import(
-		pathToFileURL(join(CLASSES_DIRECTORY, `${component}.js`)).toString()
-	);
-	const key = part
-		.replace(new RegExp(`^${kebabToPascal(component).charAt(0).toUpperCase() + kebabToPascal(component).slice(1)}`), '')
-		.replace(/^./, (c) => c.toLowerCase());
-	if (!Object.hasOwn(classes, key)) {
-		return;
-	}
-	return classes[key];
+function pascalToKebab(str: string) {
+	return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
 const frameworks = [
@@ -205,14 +158,90 @@ const frameworks = [
 	}
 ] as const;
 
+async function getPartOrderFromAnatomy(framework: string, component: string) {
+	const project = new tsMorph.Project({ useInMemoryFileSystem: true });
+	const sourceFile = project.createSourceFile(
+		`${component}-anatomy.js`,
+		await readFile(
+			join(MONOREPO_ROOT, 'packages', `skeleton-${framework}`, 'dist', 'components', component, 'modules', `${component}-anatomy.js`),
+			'utf-8'
+		)
+	);
+	const componentNode = sourceFile.getFirstDescendantByKindOrThrow(tsMorph.SyntaxKind.ObjectLiteralExpression);
+	const order = componentNode
+		.getProperties()
+		.filter(tsMorph.Node.isPropertyAssignment)
+		.map((p) => `${kebabToPascal(component)}${p.getName()}`);
+	project.removeSourceFile(sourceFile);
+	return order;
+}
+
+async function getClassValue(component: string, part: string) {
+	const { [`classes${kebabToPascal(component)}`]: classes } = await import(
+		pathToFileURL(join(CLASSES_DIRECTORY, `${component}.js`)).toString()
+	);
+
+	const key = part
+		.replace(new RegExp(`^${kebabToPascal(component).charAt(0).toUpperCase() + kebabToPascal(component).slice(1)}`), '')
+		.replace(/^./, (c) => c.toLowerCase());
+
+	return Object.hasOwn(classes, key) ? classes[key] : undefined;
+}
+
+async function getComponentEntries(framework: (typeof frameworks)[number], parser: Parser, components: Record<string, string[]>) {
+	const entries = [];
+
+	for (const [component, files] of Object.entries(components)) {
+		const order = await getPartOrderFromAnatomy(framework.name, component);
+		const filesInOrder = [
+			...files.filter((f) => !order.some((o) => f.toLowerCase().endsWith(pascalToKebab(o) + framework.config.declarationExtension))),
+			...order.flatMap((o) => files.filter((f) => f.toLowerCase().endsWith(pascalToKebab(o) + framework.config.declarationExtension)))
+		];
+
+		const contentObj = Object.assign(
+			{},
+			...(await Promise.all(
+				filesInOrder.map(async (file) => {
+					const sourceFile = parser.getSourceFile(file);
+					const part = kebabToPascal(basename(file).replace(framework.config.declarationExtension, ''));
+					const _interface = sourceFile.getInterface(`${part}Props`);
+					const classValue = await getClassValue(component, part);
+
+					if (classValue) {
+						_interface.properties.push({
+							name: framework.config.classPropertyName,
+							type: 'string | undefined',
+							typeKind: 'primitive',
+							optional: true,
+							JSDoc: {
+								description: 'The base classes provided by Skeleton',
+								tags: [{ name: 'default', value: classValue }]
+							}
+						});
+					}
+
+					return { [_interface.name]: _interface.properties };
+				})
+			))
+		);
+
+		entries.push({
+			path: join(OUTPUT_DIRECTORY, framework.name, `${component}.json`),
+			content: JSON.stringify(contentObj, null, 2)
+		});
+	}
+
+	return entries;
+}
+
 async function main() {
 	for (const framework of frameworks) {
-		const components = (
-			await glob(`**/anatomy/*.d.ts`, {
-				cwd: join(MONOREPO_ROOT, 'packages', `skeleton-${framework.name}`, 'dist', 'components'),
-				absolute: true
-			})
-		).reduce(
+		const componentFiles = await glob(`**/anatomy/*.d.ts`, {
+			cwd: join(MONOREPO_ROOT, 'packages', `skeleton-${framework.name}`, 'dist', 'components'),
+			absolute: true
+		});
+
+		const components = componentFiles.reduce(
 			(acc, file) => {
 				const parent = dirname(file).split(/[/\\]/).slice(-2, -1)[0];
 				(acc[parent] = acc[parent] || []).push(file);
@@ -220,47 +249,10 @@ async function main() {
 			},
 			{} as Record<string, string[]>
 		);
+
 		const parser = new Parser(framework);
+		const entries = await getComponentEntries(framework, parser, components);
 
-		const entries = await Promise.all(
-			Object.entries(components).map(async ([component, files]) => ({
-				path: join(OUTPUT_DIRECTORY, framework.name, `${component}.json`),
-				content: JSON.stringify(
-					Object.assign(
-						{},
-						...(await Promise.all(
-							files.map(async (file) => {
-								const sourceFile = parser.getSourceFile(file);
-								const part = kebabToPascal(basename(file).replace(framework.config.declarationExtension, ''));
-								const _interface = sourceFile.getInterface(`${part}Props`);
-								const classValue = await getClassValue(component, part);
-								if (classValue) {
-									_interface.properties.push({
-										name: framework.config.classPropertyName,
-										type: 'string | undefined',
-										typeKind: 'primitive',
-										optional: true,
-										JSDoc: {
-											description: 'The base classes provided by Skeleton',
-											tags: [
-												{
-													name: 'default',
-													value: classValue
-												}
-											]
-										}
-									});
-								}
-
-								return { [_interface.name]: _interface.properties };
-							})
-						))
-					),
-					null,
-					2
-				)
-			}))
-		);
 		await rm(join(OUTPUT_DIRECTORY, framework.name), { recursive: true, force: true });
 		await mkdir(join(OUTPUT_DIRECTORY, framework.name), { recursive: true });
 		await Promise.all(entries.map((entry) => writeFile(entry.path, entry.content)));
