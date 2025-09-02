111f42824218069735ea0661f92b157b1920215d
Hugos68 <hugokorteapple@gmail.com>
2025-09-03 01:15:56 +0200

    add spacing

--- packages/skeleton-common/src/classes/tabs.ts
+++ packages/skeleton-common/src/classes/tabs.ts
@@ -2,7 +2,7 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with
 
 export const classesTabs = defineSkeletonClasses({
 	root: '',
-	list: 'flex gap-2 border-b-1 border-surface-200-800',
+	list: 'flex gap-2 border-b-1 border-surface-200-800 mb-4',
 	trigger: 'pb-2',
 	label: 'btn hover:preset-tonal-primary',
 	content: ''
