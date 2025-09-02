7eeff13fd0be42b4425620ab829d19d0ddf84a55
Hugos68 <hugokorteapple@gmail.com>
2025-09-02 17:51:00 +0200

    list styles

--- packages/skeleton-common/src/classes/tabs.ts
+++ packages/skeleton-common/src/classes/tabs.ts
@@ -2,7 +2,7 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes';
 
 export const classesTabs = defineSkeletonClasses({
 	root: '',
-	list: '',
+	list: 'flex gap-2 border-b-1 border-surface-200-800',
 	trigger: 'btn preset-tonal-primary',
 	content: ''
 });
