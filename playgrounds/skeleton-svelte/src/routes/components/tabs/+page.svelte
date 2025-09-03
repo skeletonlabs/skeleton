1048638a8128f30bce0174182f0baf57f261d119
Hugos68 <hugokorteapple@gmail.com>
2025-09-03 19:26:53 +0200

    use padding instead of margin

--- playgrounds/skeleton-svelte/src/routes/components/tabs/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/tabs/+page.svelte
@@ -2,7 +2,7 @@
 	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<Tabs defaultValue="tab-1" orientation="vertical" dir="rtl">
+<Tabs defaultValue="tab-1">
 	<Tabs.List>
 		<Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
 		<Tabs.Trigger value="tab-2">Tab 2</Tabs.Trigger>
