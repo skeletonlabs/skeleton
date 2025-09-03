d283cf331885ef4949262b9a8784eb8375df983a
Hugos68 <hugokorteapple@gmail.com>
2025-09-03 20:33:47 +0200

    generated sucks

--- packages/skeleton-svelte/svelte.config.js
+++ packages/skeleton-svelte/svelte.config.js
@@ -2,11 +2,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		alias: {
-			'@/': './src/'
-		}
-	}
+	preprocess: vitePreprocess()
 };
+
 export default config;
