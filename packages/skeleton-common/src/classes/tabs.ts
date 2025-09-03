2a2cb169b6c5a419d71f5f167c12f074a3a25094
Hugos68 <hugokorteapple@gmail.com>
2025-09-03 12:27:33 +0200

    more doc examples

--- packages/skeleton-common/src/classes/tabs.ts
+++ packages/skeleton-common/src/classes/tabs.ts
@@ -1,9 +1,9 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with { type: 'macro' };
 
 export const classesTabs = defineSkeletonClasses({
-	root: '',
+	root: 'w-full',
 	list: 'flex gap-2 border-b border-surface-200-800 mb-4 relative',
-	trigger: 'btn hover:preset-tonal-primary mb-2',
+	trigger: 'btn hover:preset-tonal-primary mb-2 data-disabled:opacity-50 data-disabled:pointer-events-none',
 	indicator: 'h-0.5 bottom-0 w-(--width) bg-surface-950-50',
 	content: ''
 });
