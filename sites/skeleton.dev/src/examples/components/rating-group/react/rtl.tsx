5d517a840ac27bea4d5fd2fd2b150abf779075b0
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2025-09-04 16:51:54 +0200

    refinement: rating group (#3681)

--- sites/skeleton.dev/src/examples/components/rating-group/react/rtl.tsx
+++ sites/skeleton.dev/src/examples/components/rating-group/react/rtl.tsx
@@ -1,23 +1,14 @@
 'use client';
 
 import { RatingGroup } from '@skeletonlabs/skeleton-react';
-import { StarIcon } from 'lucide-react';
 
-export default function Page() {
+export default function () {
 	return (
-		<RatingGroup count={5} dir="rtl">
+		<RatingGroup count={5}>
 			<RatingGroup.Control>
-				<RatingGroup.Context>
-					{(ctx) =>
-						ctx.api.items.map((index) => (
-							<RatingGroup.Item key={index} index={index}>
-								<RatingGroup.ItemContext>
-									{(ctx) => (ctx.itemState.highlighted ? <StarIcon fill="currentColor" /> : <StarIcon />)}
-								</RatingGroup.ItemContext>
-							</RatingGroup.Item>
-						))
-					}
-				</RatingGroup.Context>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<RatingGroup.Item key={index} index={index + 1} />
+				))}
 			</RatingGroup.Control>
 			<RatingGroup.HiddenInput />
 		</RatingGroup>
--- sites/skeleton.dev/src/examples/components/rating-group/svelte/disabled.svelte
+++ sites/skeleton.dev/src/examples/components/rating-group/svelte/disabled.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
 	import { RatingGroup } from '@skeletonlabs/skeleton-svelte';
-	import StarIcon from '@lucide/svelte/icons/star';
 </script>
 
 <RatingGroup count={5} disabled={true}>
 	<RatingGroup.Control>
-		<RatingGroup.Context>
-			{#snippet children(ctx)}
-				{#each ctx.api.items as index (index)}
-					<RatingGroup.Item {index}>
-						<RatingGroup.ItemContext>
-							{#snippet children(ctx)}
-								{#if ctx.itemState.highlighted}
-									<StarIcon fill="currentColor" />
-								{:else}
-									<StarIcon />
-								{/if}
-							{/snippet}
-						</RatingGroup.ItemContext>
-					</RatingGroup.Item>
-				{/each}
-			{/snippet}
-		</RatingGroup.Context>
+		{#each Array.from({ length: 5 }), index}
+			<RatingGroup.Item index={index + 1} />
+		{/each}
 	</RatingGroup.Control>
 	<RatingGroup.HiddenInput />
 </RatingGroup>
