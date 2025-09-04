b5467c651e8c56e5add1bdd3397d1db2c01993b0
Hugos68 <hugokorteapple@gmail.com>
2025-09-04 11:53:50 +0200

    space

--- packages/skeleton-common/src/classes/tabs.ts
+++ packages/skeleton-common/src/classes/tabs.ts
@@ -3,7 +3,7 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with
 export const classesTabs = defineSkeletonClasses({
 	root: 'w-full flex data-[orientation=horizontal]:flex-col data-[orientation=vertical]:flex-row',
 	list: 'relative data-[orientation=horizontal]:mb-4 data-[orientation=horizontal]:pb-2 data-[orientation=vertical]:pe-2 data-[orientation=vertical]:me-4 flex data-[orientation=horizontal]:flex-row data-[orientation=vertical]:flex-col gap-2 data-[orientation=horizontal]:border-b data-[orientation=vertical]:border-e border-surface-200-800',
-	trigger: 'btn hover:preset-tonal-primary  data-disabled:opacity-50 data-disabled:pointer-events-none',
+	trigger: 'btn hover:preset-tonal-primary data-disabled:opacity-50 data-disabled:pointer-events-none',
 	indicator:
 		'bg-surface-950-50 data-[orientation=horizontal]:w-(--width) data-[orientation=horizontal]:h-0.5 data-[orientation=horizontal]:bottom-0 data-[orientation=vertical]:w-0.5 data-[orientation=vertical]:h-(--height) data-[orientation=vertical]:end-0',
 	content: ''
