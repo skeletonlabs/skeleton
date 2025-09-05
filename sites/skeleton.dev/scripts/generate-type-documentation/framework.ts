cbb66b12ca7335f4206194ae7ba43bcc6ee56be9
Chris Simmons <gundamx9740@gmail.com>
2025-09-05 09:46:03 -0500

    API Ref Table Improvements (#3690)
    Co-authored-by: Hugos68 <hugokorteapple@gmail.com>

--- sites/skeleton.dev/scripts/generate-type-documentation/framework.ts
+++ sites/skeleton.dev/scripts/generate-type-documentation/framework.ts
@@ -2,16 +2,12 @@ export const frameworks = [
 	{
 		name: 'svelte',
 		config: {
-			classPropertyName: 'class',
-			declarationExtension: '.svelte.d.ts',
 			extendsBlacklist: [/^HTML.*Attributes(?:<.*>)?$/, /^Omit<\s*HTML.*Attributes.*>$/] as RegExp[]
 		}
 	},
 	{
 		name: 'react',
 		config: {
-			classPropertyName: 'className',
-			declarationExtension: '.d.ts',
 			extendsBlacklist: [/^ComponentProps(?:<.*>)?$/, /^Omit<\s*ComponentProps.*>$/] as RegExp[]
 		}
 	}
