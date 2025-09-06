005a33c691199a4d49fd946718bd8c2e0af3551f
Hugos68 <hugokorteapple@gmail.com>
2025-09-06 23:25:41 +0200

    cleanup tests

--- packages/skeleton-svelte/test/components/switch/index.test.ts
+++ packages/skeleton-svelte/test/components/switch/index.test.ts
@@ -1,39 +1,39 @@
 import { describe, expect, it } from 'vitest';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import Switch from './switch.svelte';
 
 describe('switch', () => {
 	describe('root', () => {
 		it('renders', () => {
-			const screen = render(Switch);
+			render(Switch);
 			expect(screen.getByTestId('root')).toBeInTheDocument();
 		});
 	});
 
 	describe('hidden input', () => {
 		it('renders', () => {
-			const screen = render(Switch);
+			render(Switch);
 			expect(screen.getByTestId('hidden-input')).toBeInTheDocument();
 		});
 	});
 
 	describe('control', () => {
 		it('renders', () => {
-			const screen = render(Switch);
+			render(Switch);
 			expect(screen.getByTestId('control')).toBeInTheDocument();
 		});
 	});
 
 	describe('thumb', () => {
 		it('renders', () => {
-			const screen = render(Switch);
+			render(Switch);
 			expect(screen.getByTestId('thumb')).toBeInTheDocument();
 		});
 	});
 
 	describe('label', () => {
 		it('renders', () => {
-			const screen = render(Switch);
+			render(Switch);
 			expect(screen.getByTestId('label')).toBeInTheDocument();
 		});
 	});
