d1a03abc2bf19a0c5254a3ea54d8b84dbd3a53fa
Hugos68 <hugokorteapple@gmail.com>
2025-09-06 23:17:23 +0200

    fix

--- packages/skeleton-react/src/components/tabs/modules/tabs-anatomy.ts
+++ packages/skeleton-react/src/components/tabs/modules/tabs-anatomy.ts
@@ -1,9 +1,9 @@
-import TabsRoot from '../anatomy/tabs-rootx';
-import TabsRootContext from '../anatomy/tabs-root-contextx';
-import TabsList from '../anatomy/tabs-listx';
-import TabsTrigger from '../anatomy/tabs-triggerx';
+import TabsRoot from '../anatomy/tabs-root';
+import TabsRootContext from '../anatomy/tabs-root-context';
+import TabsList from '../anatomy/tabs-list';
+import TabsTrigger from '../anatomy/tabs-trigger';
 import tabsIndicator from '../anatomy/tabs-indicator';
-import TabsContent from '../anatomy/tabs-contentx';
+import TabsContent from '../anatomy/tabs-content';
 
 export const Tabs = Object.assign(TabsRoot, {
 	Context: TabsRootContext,
