7d534495bff3ade4933f6b0c5c36a76d4777e3c1
Hugos68 <hugokorteapple@gmail.com>
2025-09-06 23:23:09 +0200

    cleanup tests

--- packages/skeleton-svelte/test/components/file-upload/index.test.ts
+++ packages/skeleton-svelte/test/components/file-upload/index.test.ts
@@ -1,67 +1,67 @@
 import { describe, expect, it } from 'vitest';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import FileUpload from './file-upload.svelte';
 
 describe('file-upload', () => {
 	describe('root', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('root')).toBeInTheDocument();
 		});
 	});
 
 	describe('dropzone', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('dropzone')).toBeInTheDocument();
 		});
 	});
 
 	describe('trigger', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('trigger')).toBeInTheDocument();
 		});
 	});
 
 	describe('item-group', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('item-group')).toBeInTheDocument();
 		});
 	});
 
 	describe('item', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('item')).toBeInTheDocument();
 		});
 	});
 
 	describe('item-name', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('item-name')).toBeInTheDocument();
 		});
 	});
 
 	describe('item-size-text', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('item-size-text')).toBeInTheDocument();
 		});
 	});
 
 	describe('item-delete-trigger', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('item-delete-trigger')).toBeInTheDocument();
 		});
 	});
 
 	describe('hidden-input', () => {
 		it('renders', () => {
-			const screen = render(FileUpload);
+			render(FileUpload);
 			expect(screen.getByTestId('hidden-input')).toBeInTheDocument();
 		});
 	});
