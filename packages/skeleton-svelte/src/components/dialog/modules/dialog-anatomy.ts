e1233ecda1431d546ff9149f448b6e6b52ba6dbf
Hugos68 <hugokorteapple@gmail.com>
2025-09-07 12:30:47 +0200

    dialog

--- packages/skeleton-svelte/src/components/dialog/modules/dialog-anatomy.ts
+++ packages/skeleton-svelte/src/components/dialog/modules/dialog-anatomy.ts
@@ -1,6 +1,7 @@
 import DialogRoot from '../anatomy/dialog-root.svelte';
 import DialogRootContext from '../anatomy/dialog-root-context.svelte';
 import DialogTrigger from '../anatomy/dialog-trigger.svelte';
+import DialogBackdrop from '../anatomy/dialog-backdrop.svelte';
 import DialogPositioner from '../anatomy/dialog-positioner.svelte';
 import DialogContent from '../anatomy/dialog-content.svelte';
 import DialogTitle from '../anatomy/dialog-title.svelte';
@@ -10,6 +11,7 @@ import DialogCloseTrigger from '../anatomy/dialog-close-trigger.svelte';
 export const Dialog = Object.assign(DialogRoot, {
 	Context: DialogRootContext,
 	Trigger: DialogTrigger,
+	Backdrop: DialogBackdrop,
 	Positioner: DialogPositioner,
 	Content: DialogContent,
 	Title: DialogTitle,
--- playgrounds/skeleton-react/src/app/layout.tsx
+++ playgrounds/skeleton-react/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 								<a className="anchor" href="/components/avatar">
 									Avatar
 								</a>
+								<a className="anchor" href="/components/dialog">
+									Dialog
+								</a>
 								<a className="anchor" href="/components/rating-group">
 									Rating Group
 								</a>
