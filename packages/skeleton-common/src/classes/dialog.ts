75244446f1d53aefb0b700d771ba19511b429ee9
Hugos68 <hugokorteapple@gmail.com>
2025-09-07 14:29:05 +0200

    styling

--- packages/skeleton-common/src/classes/dialog.ts
+++ packages/skeleton-common/src/classes/dialog.ts
@@ -1,11 +1,11 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' with { type: 'macro' };
 
 export const classesDialog = defineSkeletonClasses({
-	trigger: '',
-	backdrop: '',
-	positioner: '',
-	content: '',
+	trigger: 'btn preset-filled',
+	backdrop: 'fixed inset-0 bg-black/50',
+	positioner: 'fixed inset-0 flex justify-center items-center',
+	content: 'bg-surface-100-900 rounded-container p-4',
 	title: '',
 	description: '',
-	closeTrigger: ''
+	closeTrigger: 'btn preset-filled'
 });
