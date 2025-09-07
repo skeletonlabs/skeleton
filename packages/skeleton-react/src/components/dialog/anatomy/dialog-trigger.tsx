5416318a26e456a9f3b93e86cc824ce5b0f80e80
Hugos68 <hugokorteapple@gmail.com>
2025-09-07 11:49:33 +0200

    stuff

---
+++ packages/skeleton-react/src/components/dialog/anatomy/dialog-trigger.tsx
@@ -0,0 +1,20 @@
+import { useContext, type ComponentProps } from 'react';
+import { mergeProps } from '@zag-js/react';
+import { DialogRootContext } from '../modules/dialog-root-context';
+import { classesDialog } from '@skeletonlabs/skeleton-common';
+import type { PropsWithElement } from '@/internal/props-with-element';
+
+export interface DialogTriggerProps extends PropsWithElement, ComponentProps<'button'> {}
+
+export default function (props: DialogTriggerProps) {
+	const rootContext = useContext(DialogRootContext);
+	const { element, children, ...restAttributes } = props;
+	const attributes = mergeProps(
+		rootContext.api.getTriggerProps(),
+		{
+			className: classesDialog.trigger
+		},
+		restAttributes
+	);
+	return element ? element({ attributes }) : <button {...attributes}>{children}</button>;
+}
