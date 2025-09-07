967cfdeee8c3a5c755aad2ad334c2ef9da984fc2
Hugos68 <hugokorteapple@gmail.com>
2025-09-08 01:01:33 +0200

    combobox start

---
+++ packages/skeleton-svelte/src/components/combobox/anatomy/combobox-root.svelte
@@ -0,0 +1,41 @@
+<script lang="ts" module>
+	import type { HTMLAttributes } from 'svelte/elements';
+	import type { PropsWithElement } from '@/internal/props-with-element';
+	import type { Props } from '@zag-js/combobox';
+
+	export interface ComboboxRootProps
+		extends PropsWithElement,
+			Omit<Props, 'id'>,
+			Omit<HTMLAttributes<HTMLDivElement>, 'id' | 'dir' | 'placeholder'> {}
+</script>
+
+<script lang="ts">
+	import { useMachine, normalizeProps, mergeProps } from '@zag-js/svelte';
+	import { classesCombobox } from '@skeletonlabs/skeleton-common';
+	import { ComboboxRootContext } from '../modules/combobox-root-context';
+	import { connect, machine, splitProps } from '@zag-js/combobox';
+
+	const props: ComboboxRootProps = $props();
+	const [machineProps, componentProps] = $derived(splitProps(props));
+	const { element, children, ...restAttributes } = $derived(componentProps);
+	const id = $props.id();
+	const service = useMachine(machine, () => ({
+		id: id,
+		...machineProps
+	}));
+	const api = $derived(connect(service, normalizeProps));
+	const attributes = $derived(mergeProps(api.getRootProps(), { class: classesCombobox.root }, restAttributes));
+	ComboboxRootContext.provide({
+		get api() {
+			return api;
+		}
+	});
+</script>
+
+{#if element}
+	{@render element({ attributes: restAttributes })}
+{:else}
+	<div {...attributes}>
+		{@render children?.()}
+	</div>
+{/if}
---
+++ packages/skeleton-svelte/test/components/combobox/combobox.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Combobox } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<Combobox data-testid="root">
+	<Combobox.Label data-testid="label" />
+	<Combobox.Control data-testid="control">
+		<Combobox.Input data-testid="input" />
+		<Combobox.Trigger data-testid="trigger" />
+	</Combobox.Control>
+	<Combobox.Positioner data-testid="positioner">
+		<Combobox.Content data-testid="content">
+			<Combobox.ItemGroup data-testid="item-group">
+				<Combobox.ItemGroupLabel data-testid="item-group-label" />
+				<Combobox.Item item="item" data-testid="item" />
+			</Combobox.ItemGroup>
+		</Combobox.Content>
+	</Combobox.Positioner>
+</Combobox>
