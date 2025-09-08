76522dffbb86125e8f9f5f26181b91a92d1fb5d3
Hugos68 <hugokorteapple@gmail.com>
2025-09-08 21:59:51 +0200

    fix style

--- packages/skeleton-common/src/classes/toast.ts
+++ packages/skeleton-common/src/classes/toast.ts
@@ -1,7 +1,7 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with { type: 'macro' };
 
 export const classesToast = defineSkeletonClasses({
-	root: 'flex justify-between items-center gap-3 p-3 rounded-container data-[type=info]:preset-outlined-surface-200-800 data-[type=success]:preset-filled-success-500 data-[type=error]:preset-filled-error-500',
+	root: 'flex justify-between items-center gap-3 p-3 rounded-container preset-outlined-surface-200-800 data-[type=success]:preset-filled-success-500 data-[type=error]:preset-filled-error-500',
 	group: '',
 	title: '',
 	description: '',
