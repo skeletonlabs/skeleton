89956855ef9fac8d983aa891bb9e785af988ff9c
Hugos68 <hugokorteapple@gmail.com>
2025-09-08 15:11:29 +0200

    stuff

--- packages/skeleton-svelte/test/components/toast/index.test.ts
+++ packages/skeleton-svelte/test/components/toast/index.test.ts
@@ -1,39 +1,39 @@
 import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import Toast from './toast';
+import { render, screen } from '@testing-library/svelte';
+import Toast from './toast.svelte';
 
 describe('toast', () => {
 	describe('group', () => {
 		it('renders', () => {
-			render(<Toast />);
+			render(Toast);
 			expect(screen.getByTestId('group')).toBeInTheDocument();
 		});
 	});
 
 	describe('root', () => {
 		it('renders', async () => {
-			render(<Toast />);
+			render(Toast);
 			expect(screen.getByTestId('root')).toBeInTheDocument();
 		});
 	});
 
 	describe('title', () => {
 		it('renders', () => {
-			render(<Toast />);
+			render(Toast);
 			expect(screen.getByTestId('title')).toBeInTheDocument();
 		});
 	});
 
 	describe('description', () => {
 		it('renders', () => {
-			render(<Toast />);
+			render(Toast);
 			expect(screen.getByTestId('description')).toBeInTheDocument();
 		});
 	});
 
 	describe('close trigger', () => {
 		it('renders', () => {
-			render(<Toast />);
+			render(Toast);
 			expect(screen.getByTestId('close-trigger')).toBeInTheDocument();
 		});
 	});
---
+++ packages/skeleton-svelte/test/components/toast/toast.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Toast, createToaster } from '../../../src/index';
+	const toaster = createToaster({});
+	toaster.create({});
+</script>
+
+<Toast.Group {toaster} data-testid="group">
+	{#snippet children(toast)}
+		<Toast key={toast.id} {toast} data-testid="root">
+			<Toast.Title data-testid="title" />
+			<Toast.Description data-testid="description" />
+			<Toast.CloseTrigger data-testid="close-trigger" />
+		</Toast>
+	{/snippet}
+</Toast.Group>
