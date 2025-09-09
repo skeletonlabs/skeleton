c167f56881c7274700798c084268aa29b203e669
Hugos68 <hugokorteapple@gmail.com>
2025-09-09 15:21:15 +0200

    fix

--- packages/skeleton-svelte/src/components/progress-linear/index.ts
+++ packages/skeleton-svelte/src/components/progress-linear/index.ts
@@ -1,11 +1,7 @@
-export { ProgressLinear } from './modules/progress-linear-anatomy.js';
-
-// Components
-export type { ProgressLinearRootProps } from './anatomy/progress-linear-root.svelte';
-export type { ProgressLinearRootContextProps } from './anatomy/progress-linear-root-context.svelte';
-export type { ProgressLinearLabelProps } from './anatomy/progress-linear-label.svelte';
-export type { ProgressLinearTrackProps } from './anatomy/progress-linear-track.svelte';
-export type { ProgressLinearRangeProps } from './anatomy/progress-linear-range.svelte';
-
-// Context
-export type { ProgressLinearRootContextType as ProgressLinearRootContext } from './modules/progress-linear-root-context.js';
+export { ProgressLinear } from './modules/anatomy';
+export type { ProgressLinearRootProps } from './anatomy/root.svelte';
+export type { ProgressLinearRootContextProps } from './anatomy/root-context.svelte';
+export type { ProgressLinearLabelProps } from './anatomy/label.svelte';
+export type { ProgressLinearTrackProps } from './anatomy/track.svelte';
+export type { ProgressLinearRangeProps } from './anatomy/range.svelte';
+export type { ProgressLinearRootContextType as ProgressLinearRootContext } from './modules/root-context';
