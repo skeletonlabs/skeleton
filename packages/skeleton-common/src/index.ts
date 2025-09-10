a9c651adac035dc4613f0118bca05f0cfb0b22b5
Chris Simmons <gundamx9740@gmail.com>
2025-09-10 12:08:08 -0500

    feat: progress (linear) (#3694)
    Co-authored-by: Hugos68 <hugokorteapple@gmail.com>
    Co-authored-by: Hugo Korte <63101006+Hugos68@users.noreply.github.com>

--- packages/skeleton-common/src/index.ts
+++ packages/skeleton-common/src/index.ts
@@ -1,5 +1,6 @@
 export * from './themes';
 export * from './classes/accordion';
 export * from './classes/avatar';
+export * from './classes/progress-linear';
 export * from './classes/rating-group';
 export * from './classes/tabs';
---
+++ sites/skeleton.dev/src/examples/components/progress-linear/svelte/indeterminate.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { ProgressLinear } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<ProgressLinear value={null}>
+	<ProgressLinear.Track>
+		<ProgressLinear.Range />
+	</ProgressLinear.Track>
+</ProgressLinear>
