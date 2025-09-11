963569efce9ecb6366c911b6591dad095513a1ce
Hugos68 <hugokorteapple@gmail.com>
2025-09-11 21:53:59 +0200

    order

--- packages/skeleton-common/src/classes/switch.ts
+++ packages/skeleton-common/src/classes/switch.ts
@@ -2,7 +2,6 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' w
 
 export const classesSwitch = defineSkeletonClasses({
 	root: 'cursor-pointer data-disabled:cursor-not-allowed data-disabled:opacity-50 flex gap-2',
-	hiddenInput: '',
 	control: [
 		'block h-6 w-10 preset-filled-surface-200-800 data-[state=checked]:preset-filled-primary-500 p-0.5 rounded-full flex justify-start',
 		'data-focus:ring-2 data-focus:ring-surface-950-50'
@@ -11,5 +10,6 @@ export const classesSwitch = defineSkeletonClasses({
 		'rounded-full block preset-filled-surface-50-950 h-full aspect-square transition-transform grid place-items-center',
 		'data-[state=checked]:bg-surface-950-50 data-[state=checked]:translate-x-4 rtl:data-[state=checked]:-translate-x-4'
 	],
-	label: ''
+	label: '',
+	hiddenInput: ''
 });
