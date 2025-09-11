670e1d4b8925823bd23bf85efb5c1999187191e7
Hugos68 <hugokorteapple@gmail.com>
2025-09-11 22:10:05 +0200

    clean

--- packages/skeleton-common/src/classes/tooltip.ts
+++ packages/skeleton-common/src/classes/tooltip.ts
@@ -3,7 +3,7 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with
 export const classesTooltip = defineSkeletonClasses({
 	trigger: '',
 	positioner: '',
-	content: 'preset-filled-surface-100-900 border-1 border-surface-200-800 p-2 rounded-container shadow-2xs',
+	content: '',
 	arrow: '',
-	arrowTip: 'border-t-1 border-l-1 border-surface-200-800 text-red-500'
+	arrowTip: ''
 });
