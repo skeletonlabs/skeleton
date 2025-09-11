d16d01a46eaacf6f7f1f18ca61de591779a44f58
Hugos68 <hugokorteapple@gmail.com>
2025-09-11 22:08:06 +0200

    remove opinionated styles

--- packages/skeleton-common/src/classes/popover.ts
+++ packages/skeleton-common/src/classes/popover.ts
@@ -1,12 +1,12 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' with { type: 'macro' };
 
 export const classesPopover = defineSkeletonClasses({
-	trigger: 'btn preset-filled',
+	trigger: '',
 	positioner: '',
-	content: 'preset-filled-surface-100-900 border-1 border-surface-200-800 p-4 rounded-container',
+	content: '',
 	arrow: '',
-	arrowTip: 'border-t-1 border-l-1 border-surface-200-800',
-	title: 'h4',
+	arrowTip: '',
+	title: '',
 	description: '',
 	closeTrigger: ''
 });
