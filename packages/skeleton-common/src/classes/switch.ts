6c01b07c38037077224fbdb2aaac913f191375a3
Hugos68 <hugokorteapple@gmail.com>
2025-09-11 19:00:36 +0200

    add focus styles

--- packages/skeleton-common/src/classes/switch.ts
+++ packages/skeleton-common/src/classes/switch.ts
@@ -3,9 +3,13 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' w
 export const classesSwitch = defineSkeletonClasses({
 	root: 'cursor-pointer data-disabled:cursor-not-allowed data-disabled:opacity-50 flex gap-2',
 	hiddenInput: '',
-	control:
+	control: [
 		'block h-6 w-10 preset-filled-surface-200-800 data-[state=checked]:preset-filled-primary-500 p-0.5 rounded-full flex justify-start',
-	thumb:
-		'rounded-full block bg-surface-50-950 h-full aspect-square data-[state=checked]:bg-surface-950-50 data-[state=checked]:translate-x-4 rtl:data-[state=checked]:-translate-x-4 transition-transform',
+		'data-focus:ring-2 data-focus:ring-surface-950-50'
+	],
+	thumb: [
+		'rounded-full block bg-surface-50-950 h-full aspect-square transition-transform',
+		'data-[state=checked]:bg-surface-950-50 data-[state=checked]:translate-x-4 rtl:data-[state=checked]:-translate-x-4'
+	],
 	label: ''
 });
