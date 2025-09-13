f04e8d69ed77052f1d9824c390de02d45a0803ed
Hugos68 <hugokorteapple@gmail.com>
2025-09-13 23:59:11 +0200

    update types

--- packages/skeleton-svelte/src/components/dialog/modules/use-dialog.svelte.ts
+++ packages/skeleton-svelte/src/components/dialog/modules/use-dialog.svelte.ts
@@ -1,7 +1,7 @@
-import { connect, machine, type Props } from '@zag-js/dialog';
+import { connect, machine, type Api, type Props } from '@zag-js/dialog';
 import { useMachine, normalizeProps } from '@zag-js/svelte';
 
-export function useDialog(props: () => Props) {
+export function useDialog(props: Props | (() => Props)): () => Api {
 	const service = useMachine(machine, props);
 	const dialog = $derived(connect(service, normalizeProps));
 	return () => dialog;
