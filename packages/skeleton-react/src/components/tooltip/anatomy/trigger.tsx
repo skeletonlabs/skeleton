011e7bd79ce5f25588eaab5282f572df2163f492
Hugos68 <hugokorteapple@gmail.com>
2025-09-13 20:06:10 +0200

    fix

--- packages/skeleton-react/src/components/tooltip/anatomy/trigger.tsx
+++ packages/skeleton-react/src/components/tooltip/anatomy/trigger.tsx
@@ -1,18 +1,20 @@
 import { mergeProps } from '@zag-js/react';
 import { useContext } from 'react';
-import type { HTMLAttributes } from '@/internal/html-attributes';
-import { TooltipRootContext } from '../modules/root-context.js';
+import { TooltipRootContext } from '../modules/root-context';
 import { classesTooltip } from '@skeletonlabs/skeleton-common';
-import type { PropsWithElement } from '@/internal/props-with-element.js';
+import type { PropsWithElement } from '@/internal/props-with-element';
+import type { HTMLAttributes } from '@/internal/html-attributes';
 
-export interface TooltipTriggerProps extends PropsWithElement, HTMLAttributes<'button'> {}
+export interface TooltipTriggerProps extends PropsWithElement<'button'>, HTMLAttributes<'button'> {}
 
 export default function (props: TooltipTriggerProps) {
 	const tooltip = useContext(TooltipRootContext);
 
 	const { element, children, ...rest } = props;
 
-	const attributes = mergeProps(tooltip.api.getTriggerProps(), { className: classesTooltip.trigger }, rest);
+	const attributes = mergeProps(tooltip.getTriggerProps(), rest, {
+		className: classesTooltip.trigger
+	});
 
 	return element ? element(attributes) : <button {...attributes}>{children}</button>;
 }
