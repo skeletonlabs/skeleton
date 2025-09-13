454dd83f2337dcf7ba2149e1bb32c36fd6ccd5aa
Hugos68 <hugokorteapple@gmail.com>
2025-09-14 00:25:14 +0200

    update types

--- packages/skeleton-react/src/components/popover/modules/use-popover.ts
+++ packages/skeleton-react/src/components/popover/modules/use-popover.ts
@@ -1,8 +1,8 @@
-import { machine, connect, type Props } from '@zag-js/popover';
+import { machine, connect, type Api, type Props } from '@zag-js/popover';
 import { normalizeProps, useMachine } from '@zag-js/react';
 import { useId } from 'react';
 
-export function usePopover(props: Omit<Props, 'id'>) {
+export function usePopover(props: Omit<Props, 'id'>): Api {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props
--- packages/skeleton-svelte/src/components/popover/modules/use-popover.svelte.ts
+++ packages/skeleton-svelte/src/components/popover/modules/use-popover.svelte.ts
@@ -1,7 +1,7 @@
-import { connect, machine, type Props } from '@zag-js/popover';
+import { connect, machine, type Api, type Props } from '@zag-js/popover';
 import { useMachine, normalizeProps } from '@zag-js/svelte';
 
-export function usePopover(props: () => Props) {
+export function usePopover(props: Props | (() => Props)): () => Api {
 	const service = useMachine(machine, props);
 	const popover = $derived(connect(service, normalizeProps));
 	return () => popover;
