8be03635d30a59bac1c72cb850d23ee95a1c5a0b
Hugos68 <hugokorteapple@gmail.com>
2025-09-14 00:32:51 +0200

    update types

--- packages/skeleton-react/src/components/tooltip/modules/use-tooltip.ts
+++ packages/skeleton-react/src/components/tooltip/modules/use-tooltip.ts
@@ -1,8 +1,8 @@
-import { machine, connect, type Props } from '@zag-js/tooltip';
+import { machine, connect, type Api, type Props } from '@zag-js/tooltip';
 import { normalizeProps, useMachine } from '@zag-js/react';
 import { useId } from 'react';
 
-export function useTooltip(props: Omit<Props, 'id'>) {
+export function useTooltip(props: Omit<Props, 'id'>): Api {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props
--- packages/skeleton-svelte/src/components/tooltip/modules/use-tooltip.svelte.ts
+++ packages/skeleton-svelte/src/components/tooltip/modules/use-tooltip.svelte.ts
@@ -1,7 +1,7 @@
-import { connect, machine, type Props } from '@zag-js/tooltip';
+import { connect, machine, type Api, type Props } from '@zag-js/tooltip';
 import { useMachine, normalizeProps } from '@zag-js/svelte';
 
-export function useTooltip(props: () => Props) {
+export function useTooltip(props: Props | (() => Props)): () => Api {
 	const service = useMachine(machine, props);
 	const tooltip = $derived(connect(service, normalizeProps));
 	return () => tooltip;
