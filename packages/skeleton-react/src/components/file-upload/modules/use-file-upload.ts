7b25b72437f1c0116c6d70cb27e63ca063f5d821
Hugos68 <hugokorteapple@gmail.com>
2025-09-14 00:23:47 +0200

    update types

--- packages/skeleton-react/src/components/file-upload/modules/use-file-upload.ts
+++ packages/skeleton-react/src/components/file-upload/modules/use-file-upload.ts
@@ -1,8 +1,8 @@
-import { connect, machine, type Props } from '@zag-js/file-upload';
+import { connect, machine, type Api, type Props } from '@zag-js/file-upload';
 import { normalizeProps, useMachine } from '@zag-js/react';
 import { useId } from 'react';
 
-export function useFileUpload(props: Omit<Props, 'id'> = {}) {
+export function useFileUpload(props: Omit<Props, 'id'> = {}): Api {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props
--- packages/skeleton-svelte/src/components/file-upload/modules/use-file-upload.svelte.ts
+++ packages/skeleton-svelte/src/components/file-upload/modules/use-file-upload.svelte.ts
@@ -1,7 +1,7 @@
-import { connect, machine, type Props } from '@zag-js/file-upload';
+import { connect, machine, type Api, type Props } from '@zag-js/file-upload';
 import { normalizeProps, useMachine } from '@zag-js/svelte';
 
-export function useFileUpload(props: () => Props) {
+export function useFileUpload(props: Props | (() => Props)): () => Api {
 	const service = useMachine(machine, props);
 	const api = $derived(connect(service, normalizeProps));
 	return () => api;
