0d01d9b7884355af5e6946807cd51edc402d2ad8
Hugos68 <hugokorteapple@gmail.com>
2025-09-13 23:22:32 +0200

    fix dts

--- packages/skeleton-svelte/src/components/tabs/modules/use-tabs.svelte.ts
+++ packages/skeleton-svelte/src/components/tabs/modules/use-tabs.svelte.ts
@@ -1,7 +1,7 @@
-import { connect, machine, type Props } from '@zag-js/tabs';
+import { connect, machine, type Api, type Props } from '@zag-js/tabs';
 import { useMachine, normalizeProps } from '@zag-js/svelte';
 
-export function useTabs(props: () => Props) {
+export function useTabs(props: Props | (() => Props)): () => Api {
 	const service = useMachine(machine, props);
 	const tabs = $derived(connect(service, normalizeProps));
 	return () => tabs;
