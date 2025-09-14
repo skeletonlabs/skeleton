8425f090c33d38a746950f31376dd241579f8cd7
Hugos68 <hugokorteapple@gmail.com>
2025-09-14 15:01:16 +0200

    add disabled example

---
+++ sites/skeleton.dev/src/examples/components/file-upload/react/disabled.tsx
@@ -0,0 +1,28 @@
+import { FileUpload } from '@skeletonlabs/skeleton-react';
+import { X } from 'lucide-react';
+
+export default function () {
+	return (
+		<FileUpload disabled={true} className="w-full">
+			<FileUpload.Dropzone>
+				<FileUpload.Trigger>Upload</FileUpload.Trigger>
+				<FileUpload.HiddenInput />
+			</FileUpload.Dropzone>
+			<FileUpload.ItemGroup>
+				<FileUpload.Context>
+					{(fileUpload) =>
+						fileUpload.acceptedFiles.map((file) => (
+							<FileUpload.Item key={file.name} file={file}>
+								<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
+								<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
+								<FileUpload.ItemDeleteTrigger>
+									<X />
+								</FileUpload.ItemDeleteTrigger>
+							</FileUpload.Item>
+						))
+					}
+				</FileUpload.Context>
+			</FileUpload.ItemGroup>
+		</FileUpload>
+	);
+}
---
+++ sites/skeleton.dev/src/examples/components/file-upload/svelte/disabled.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { FileUpload } from '@skeletonlabs/skeleton-svelte';
+	import X from '@lucide/svelte/icons/x';
+</script>
+
+<FileUpload disabled={true} class="w-full">
+	<FileUpload.Dropzone>
+		<FileUpload.Trigger>Upload</FileUpload.Trigger>
+		<FileUpload.HiddenInput />
+	</FileUpload.Dropzone>
+	<FileUpload.ItemGroup>
+		<FileUpload.Context>
+			{#snippet children(fileUpload)}
+				{#each fileUpload().acceptedFiles as file (file.name)}
+					<FileUpload.Item {file}>
+						<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
+						<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
+						<FileUpload.ItemDeleteTrigger><X /></FileUpload.ItemDeleteTrigger>
+					</FileUpload.Item>
+				{/each}
+			{/snippet}
+		</FileUpload.Context>
+	</FileUpload.ItemGroup>
+</FileUpload>
