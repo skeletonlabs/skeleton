dfd149b7c47bfa98b92b2e486058f76b7fb6b004
Hugos68 <hugokorteapple@gmail.com>
2025-09-14 14:59:12 +0200

    fix

--- packages/skeleton-common/src/classes/file-upload.ts
+++ packages/skeleton-common/src/classes/file-upload.ts
@@ -4,7 +4,7 @@ export const classesFileUpload = defineSkeletonClasses({
 	root: ['flex flex-col gap-2', 'data-disabled:disabled'],
 	dropzone: [
 		'px-4 py-10 rounded border-1 border-dashed border-surface-200-800 flex justify-center items-center',
-		'data-dragging:preset-tonal '
+		'data-dragging:preset-tonal'
 	],
 	hiddenInput: '',
 	trigger: 'btn preset-filled',
