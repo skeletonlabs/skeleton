18603dd00fd83b947ffe24e48250d0119b3f8933
Hugos68 <hugokorteapple@gmail.com>
2025-09-15 17:51:27 +0200

    add button example

--- playgrounds/skeleton-react/src/app/components/file-upload/page.tsx
+++ playgrounds/skeleton-react/src/app/components/file-upload/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { FileUpload, useFileUpload } from '@skeletonlabs/skeleton-react';
-import { X } from 'lucide-react';
+import { X., } from 'lucide-react';
 
 export default function Page() {
 	const fileUpload = useFileUpload();
