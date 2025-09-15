3e59fb4d1774fd91a33a580759c9abb8d4750282
Hugos68 <hugokorteapple@gmail.com>
2025-09-15 22:51:14 +0200

    add missing label-text classes

--- packages/skeleton-common/src/classes/rating-group.ts
+++ packages/skeleton-common/src/classes/rating-group.ts
@@ -2,7 +2,7 @@ import { defineSkeletonClasses } from '../internal/define-skeleton-classes' with
 
 export const classesRatingGroup = defineSkeletonClasses({
 	root: 'flex flex-col gap-1',
-	label: '',
+	label: 'label-text',
 	control: 'flex gap-2 data-disabled:cursor-not-allowed data-disabled:opacity-50',
 	item: '',
 	hiddenInput: '',
