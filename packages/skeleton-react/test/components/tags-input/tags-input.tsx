ca383d077e23105963b11fde1d781678bcb5f62d
Hugos68 <hugokorteapple@gmail.com>
2025-09-15 10:53:48 +0200

    simplify test

--- packages/skeleton-react/test/components/tags-input/tags-input.tsx
+++ packages/skeleton-react/test/components/tags-input/tags-input.tsx
@@ -5,17 +5,11 @@ export default function TagsInputTest() {
 		<TagsInput data-testid="root">
 			<TagsInput.Label data-testid="label" />
 			<TagsInput.Control data-testid="control">
-				<TagsInput.Context>
-					{(tagsInput) =>
-						tagsInput.value.map((value, index) => (
-							<TagsInput.Item key={value} index={index} value={value} data-testid="item">
-								<TagsInput.ItemPreview data-testid="item-preview" />
-								<TagsInput.ItemDeleteTrigger data-testid="item-delete-trigger" />
-								<TagsInput.ItemInput data-testid="item-input" />
-							</TagsInput.Item>
-						))
-					}
-				</TagsInput.Context>
+				<TagsInput.Item index={1} value="test" data-testid="item">
+					<TagsInput.ItemPreview data-testid="item-preview" />
+					<TagsInput.ItemDeleteTrigger data-testid="item-delete-trigger" />
+					<TagsInput.ItemInput data-testid="item-input" />
+				</TagsInput.Item>
 			</TagsInput.Control>
 			<TagsInput.Input data-testid="input" />
 			<TagsInput.ClearTrigger data-testid="clear-trigger" />
