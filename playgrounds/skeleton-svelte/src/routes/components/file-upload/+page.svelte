870828e2ffda8be48b219d1503bf207d40788dd5
endigo9740 <gundamx9740@gmail.com>
2025-09-15 10:28:08 -0500

    Playground and style updates

--- playgrounds/skeleton-svelte/src/routes/components/file-upload/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/file-upload/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import X from '@lucide/svelte/icons/x';
+	import IconX from '@lucide/svelte/icons/circle-x';
 	import { FileUpload, useFileUpload } from '@skeletonlabs/skeleton-svelte';
 
 	const id = $props.id();
-	const fileUpload = useFileUpload({ id, disabled: true });
+	const fileUpload = useFileUpload({ id, disabled: false });
 </script>
 
 <FileUpload.Provider value={fileUpload}>
 	<FileUpload.Dropzone>
-		<FileUpload.Trigger>Upload</FileUpload.Trigger>
+		<div>Select file or drag here.</div>
+		<FileUpload.Trigger>Browse Files</FileUpload.Trigger>
 		<FileUpload.HiddenInput />
 	</FileUpload.Dropzone>
 	<FileUpload.ItemGroup>
@@ -18,12 +19,34 @@
 					<FileUpload.Item {file}>
 						<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
 						<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
-						<FileUpload.ItemDeleteTrigger><X /></FileUpload.ItemDeleteTrigger>
+						<FileUpload.ItemDeleteTrigger><IconX class="size-4" /></FileUpload.ItemDeleteTrigger>
 					</FileUpload.Item>
 				{/each}
 			{/snippet}
 		</FileUpload.Context>
 	</FileUpload.ItemGroup>
 </FileUpload.Provider>
+
+<!-- <FileUpload.Provider value={fileUpload}>
+	<FileUpload.Dropzone>
+		{#snippet element({ attributes })}
+			<button type="button" {...attributes} class="btn preset-filled">Browse Files</button>
+		{/snippet}
+		<FileUpload.HiddenInput />
+	</FileUpload.Dropzone>
+	<FileUpload.ItemGroup>
+		<FileUpload.Context>
+			{#snippet children(fileUpload)}
+				{#each fileUpload().acceptedFiles as file (file.name)}
+					<FileUpload.Item {file}>
+						<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
+						<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
+						<FileUpload.ItemDeleteTrigger><IconX class="size-4" /></FileUpload.ItemDeleteTrigger>
+					</FileUpload.Item>
+				{/each}
+			{/snippet}
+		</FileUpload.Context>
+	</FileUpload.ItemGroup>
+</FileUpload.Provider> -->
 
 <button class="btn preset-outlined-error-500" onclick={() => fileUpload().clearFiles()}> Clear Files </button>
