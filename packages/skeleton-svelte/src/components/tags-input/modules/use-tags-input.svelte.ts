96b6f160e45900f261ea3864c11909fa3a533bbb
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 01:21:19 +0200

    fix

--- packages/skeleton-svelte/src/components/tags-input/modules/use-tags-input.svelte.ts
+++ packages/skeleton-svelte/src/components/tags-input/modules/use-tags-input.svelte.ts
@@ -2,9 +2,7 @@ import { normalizeProps, useMachine, type PropTypes } from '@zag-js/svelte';
 import { type Api, connect, machine, type Props } from '@zag-js/tags-input';
 
 export function useTagsInput(props: Props | (() => Props)): () => Api<PropTypes> {
-	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	const service = useMachine(machine, props);
-	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	const tagsInput = $derived(connect(service, normalizeProps));
 	return () => tagsInput;
 }
