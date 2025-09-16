1924a300fd55fb3d12b89af58da1264359f9212f
Hugos68 <hugokorteapple@gmail.com>
2025-09-16 13:18:57 +0200

    ts-expect-error with link to zag issue

--- packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
+++ packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
@@ -1,11 +1,13 @@
-import { normalizeProps, useMachine } from '@zag-js/react';
+import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { type Api, connect, machine, type Props } from '@zag-js/tags-input';
 import { useId } from 'react';
 
 export function useTagsInput(props: Omit<Props, 'id'>): Api<PropTypes> {
+	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
 	});
+	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	return connect(service, normalizeProps);
 }
