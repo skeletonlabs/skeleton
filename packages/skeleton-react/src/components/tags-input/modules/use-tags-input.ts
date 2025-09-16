1dfcb05e03f643d9ce469d083c698ee415010275
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 01:17:07 +0200

    test ci

--- packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
+++ packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
@@ -3,11 +3,9 @@ import { type Api, connect, machine, type Props } from '@zag-js/tags-input';
 import { useId } from 'react';
 
 export function useTagsInput(props: Omit<Props, 'id'>): Api<PropTypes> {
-	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
 	});
-	// @ts-expect-error - https://github.com/chakra-ui/zag/issues/2716
 	return connect(service, normalizeProps);
 }
