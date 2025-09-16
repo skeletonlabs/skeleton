29690fd4ac6bbd066023b10a7bc3aabce75fab9e
Hugos68 <hugokorteapple@gmail.com>
2025-09-16 23:41:02 +0200

    fix

--- sites/skeleton.dev/src/examples/components/tooltip/react/arrow.tsx
+++ sites/skeleton.dev/src/examples/components/tooltip/react/arrow.tsx
@@ -8,15 +8,15 @@ export default function Arrow() {
 				<Tooltip.Content className="card max-w-md p-2 bg-surface-100-900 shadow-xl">
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente magni distinctio explicabo quisquam. Rerum impedit culpa
 					nesciunt enim.
+					<Tooltip.Arrow
+						style={{
+							['--arrow-size' as string]: 'calc(var(--spacing) * 2)',
+							['--arrow-background' as string]: 'var(--color-surface-100-900)',
+						}}
+					>
+						<Tooltip.ArrowTip />
+					</Tooltip.Arrow>
 				</Tooltip.Content>
-				<Tooltip.Arrow
-					style={{
-						['--arrow-size' as string]: 'calc(var(--spacing) * 2)',
-						['--arrow-background' as string]: 'var(--color-surface-100-900)',
-					}}
-				>
-					<Tooltip.ArrowTip />
-				</Tooltip.Arrow>
 			</Tooltip.Positioner>
 		</Tooltip>
 	);
