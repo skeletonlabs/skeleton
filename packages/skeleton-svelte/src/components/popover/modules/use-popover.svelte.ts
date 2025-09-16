e48f0024e4347aea986825a2deded0c96cf3fd6d
Hugos68 <hugokorteapple@gmail.com>
2025-09-16 13:06:49 +0200

    fix

--- packages/skeleton-svelte/src/components/popover/modules/use-popover.svelte.ts
+++ packages/skeleton-svelte/src/components/popover/modules/use-popover.svelte.ts
@@ -1,7 +1,7 @@
 import { type Api, connect, machine, type Props } from '@zag-js/popover';
-import { normalizeProps, useMachine } from '@zag-js/svelte';
+import { normalizeProps, useMachine, type PropTypes } from '@zag-js/svelte';
 
-export function usePopover(props: Props | (() => Props)): () => Api {
+export function usePopover(props: Props | (() => Props)): () => Api<PropTypes> {
 	const service = useMachine(machine, props);
 	const popover = $derived(connect(service, normalizeProps));
 	return () => popover;
