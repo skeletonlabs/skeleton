ae25d34c9e4cf7c8cc0b80187c1f8a1810caa227
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 10:00:29 +0200

    fix

--- packages/skeleton-react/src/components/popover/modules/use-popover.ts
+++ packages/skeleton-react/src/components/popover/modules/use-popover.ts
@@ -2,7 +2,7 @@ import { type Api, connect, machine, type Props } from '@zag-js/popover';
 import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { useId } from 'react';
 
-export function usePopover(props: Omit<Props, 'id'>): Api<PropTypes> {
+export function usePopover(props: Omit<Props, 'id'> = {}): Api<PropTypes> {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
