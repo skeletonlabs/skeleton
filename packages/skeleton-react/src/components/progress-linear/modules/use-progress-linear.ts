ac383e5b8dd849c1a17f4d65a024d47a3c0f4d5a
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2025-09-17 09:58:46 +0200

    chore: make props optional (#3756)

--- packages/skeleton-react/src/components/progress-linear/modules/use-progress-linear.ts
+++ packages/skeleton-react/src/components/progress-linear/modules/use-progress-linear.ts
@@ -3,7 +3,7 @@ import type { Api, Props } from '@zag-js/progress';
 import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { useId } from 'react';
 
-export function useProgressLinear(props: Omit<Props, 'id'>): Api<PropTypes> {
+export function useProgressLinear(props: Omit<Props, 'id'> = {}): Api<PropTypes> {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
