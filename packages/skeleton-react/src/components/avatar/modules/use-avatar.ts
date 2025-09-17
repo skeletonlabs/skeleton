03bb3734e57698c9f270d91d8f6ac9fab6549ec6
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 09:56:23 +0200

    make props optional

--- packages/skeleton-react/src/components/avatar/modules/use-avatar.ts
+++ packages/skeleton-react/src/components/avatar/modules/use-avatar.ts
@@ -3,7 +3,7 @@ import type { Api, Props } from '@zag-js/avatar';
 import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { useId } from 'react';
 
-export function useAvatar(props: Omit<Props, 'id'>): Api<PropTypes> {
+export function useAvatar(props: Omit<Props, 'id'> = {}): Api<PropTypes> {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
