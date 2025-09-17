8f8c6a8bc46a6ac643e16ed2be82af603b3705a5
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 09:59:26 +0200

    fix

--- packages/skeleton-react/src/components/combobox/modules/use-combobox.ts
+++ packages/skeleton-react/src/components/combobox/modules/use-combobox.ts
@@ -2,7 +2,7 @@ import { type Api, connect, machine, type Props } from '@zag-js/combobox';
 import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { useId } from 'react';
 
-export function useCombobox(props: Omit<Props, 'id'>): Api<PropTypes> {
+export function useCombobox(props: Omit<Props, 'id'> = {}): Api<PropTypes> {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
