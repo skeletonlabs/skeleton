c78dbdd86a07e685d3c3393b7d5448ce8807e2c6
Hugos68 <hugokorteapple@gmail.com>
2025-09-17 10:01:06 +0200

    fix

--- packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
+++ packages/skeleton-react/src/components/tags-input/modules/use-tags-input.ts
@@ -2,7 +2,7 @@ import { normalizeProps, useMachine, type PropTypes } from '@zag-js/react';
 import { type Api, connect, machine, type Props } from '@zag-js/tags-input';
 import { useId } from 'react';
 
-export function useTagsInput(props: Omit<Props, 'id'>): Api<PropTypes> {
+export function useTagsInput(props: Omit<Props, 'id'> = {}): Api<PropTypes> {
 	const service = useMachine(machine, {
 		id: useId(),
 		...props,
