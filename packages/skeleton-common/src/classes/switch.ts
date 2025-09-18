c01009b5bc3cb551d61bc7a08244496438d66901
Hugos68 <hugokorteapple@gmail.com>
2025-09-19 01:14:19 +0200

    improve focus styles

--- packages/skeleton-common/src/classes/switch.ts
+++ packages/skeleton-common/src/classes/switch.ts
@@ -1,11 +1,13 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' with { type: 'macro' };
 
 export const classesSwitch = defineSkeletonClasses({
-	root: 'cursor-pointer data-disabled:cursor-not-allowed data-disabled:opacity-50 flex gap-2',
+	root: [
+		'cursor-pointer data-disabled:cursor-not-allowed data-disabled:opacity-50 inline-flex gap-2 items-center rounded p-1',
+		'data-focus-visible:ring data-focus-visible:ring-primary-500',
+	],
 	control: [
 		'block preset-filled-surface-200-800 h-6 w-10 p-0.5 rounded-full flex justify-start',
 		'data-[state=checked]:preset-filled-primary-500',
-		'data-focus:ring-2 data-focus:ring-surface-950-50',
 	],
 	thumb: [
 		'rounded-full block preset-filled-surface-50-950 h-full aspect-square transition-transform grid place-items-center',
