e87a38a498e5bab2fc20f4d825ed581dd2304136
Hugos68 <hugokorteapple@gmail.com>
2025-09-18 10:58:00 +0200

    fix sidebar

--- playgrounds/skeleton-svelte/src/routes/+layout.server.ts
+++ playgrounds/skeleton-svelte/src/routes/+layout.server.ts
@@ -1,18 +1,17 @@
 import { globSync } from 'node:fs';
 import { dirname, relative, sep } from 'node:path';
 
-function kebabToPascalCase(str: string) {
-	return str
-		.split('-')
-		.map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-		.join('');
+function capitalize(str: string) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-const components = globSync('./**/components/*/+page.svelte').map((route) => {
-	const href = '/' + relative('src/routes', dirname(route)).split(sep).join('/');
-	const name = kebabToPascalCase(href.split('/').pop()!);
-	return { href, name };
-});
+const components = globSync('./**/components/*/+page.svelte')
+	.toSorted((a, b) => a.localeCompare(b))
+	.map((route) => {
+		const href = '/' + relative('src/routes', dirname(route)).split(sep).join('/');
+		const name = href.split('/').pop()!.split('-').map(capitalize).join(' ');
+		return { href, name };
+	});
 
 export function load() {
 	return {
