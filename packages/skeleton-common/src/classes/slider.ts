6dd19850d62ab91bf041bec275502f6d00571558
endigo9740 <gundamx9740@gmail.com>
2025-09-19 11:55:12 -0500

    Minor style adjustments

--- packages/skeleton-common/src/classes/slider.ts
+++ packages/skeleton-common/src/classes/slider.ts
@@ -1,15 +1,17 @@
 import { defineSkeletonClasses } from '../internal/define-skeleton-classes.js' with { type: 'macro' };
 
 export const classesSlider = defineSkeletonClasses({
-	root: 'flex flex-col gap-2',
+	root: 'space-y-3',
 	label: 'label-text',
 	valueText: '',
 	control: 'flex items-center',
-	track: 'bg-surface-200-800 rounded-full h-2 flex-1',
-	range: 'bg-surface-950-50 rounded-container h-full',
-	thumb:
-		'size-5 bg-surface-50-950 ring-inset ring-2 ring-surface-950-50 rounded-full focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-surface-950-50',
+	track: 'bg-surface-200-800 rounded-base h-2 overflow-hidden flex-1',
+	range: 'bg-surface-950-50 h-full',
+	thumb: [
+		'size-5 bg-surface-50-950 ring-inset ring-2 ring-surface-950-50 rounded-full',
+		'focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-surface-950-50',
+	],
 	hiddenInput: '',
 	markerGroup: '',
-	marker: 'text-sm text-surface-800-200',
+	marker: 'text-xs text-surface-600-400',
 });
--- playgrounds/skeleton-svelte/src/routes/components/slider/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/slider/+page.svelte
@@ -2,21 +2,27 @@
 	import { Slider } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<Slider>
-	<Slider.Label>Label</Slider.Label>
-	<Slider.Control>
-		<Slider.Track>
-			<Slider.Range />
-		</Slider.Track>
-		<Slider.Thumb index={0}>
-			<Slider.HiddenInput />
-		</Slider.Thumb>
-	</Slider.Control>
-	<Slider.MarkerGroup>
-		<Slider.Marker value={0} />
-		<Slider.Marker value={25} />
-		<Slider.Marker value={50} />
-		<Slider.Marker value={75} />
-		<Slider.Marker value={100} />
-	</Slider.MarkerGroup>
-</Slider>
+<div class="space-y-8">
+	<p>Above</p>
+
+	<Slider>
+		<Slider.Label>Label</Slider.Label>
+		<Slider.Control>
+			<Slider.Track>
+				<Slider.Range />
+			</Slider.Track>
+			<Slider.Thumb index={0}>
+				<Slider.HiddenInput />
+			</Slider.Thumb>
+		</Slider.Control>
+		<Slider.MarkerGroup>
+			<Slider.Marker value={0} />
+			<Slider.Marker value={25} />
+			<Slider.Marker value={50} />
+			<Slider.Marker value={75} />
+			<Slider.Marker value={100} />
+		</Slider.MarkerGroup>
+	</Slider>
+
+	<p>Below</p>
+</div>
