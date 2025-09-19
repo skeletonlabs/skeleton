ff9bdcba147fafdaf97a62123908c3480c4d0fb3
Hugos68 <hugokorteapple@gmail.com>
2025-09-19 10:40:43 +0200

    ZIndex example

---
+++ sites/skeleton.dev/src/examples/components/combobox/react/z-index.tsx
@@ -0,0 +1,53 @@
+import { Combobox, type ComboboxRootProps, useListCollection } from '@skeletonlabs/skeleton-react';
+import { useState } from 'react';
+
+const data = [
+	{ label: 'Apple', value: 'apple' },
+	{ label: 'Banana', value: 'banana' },
+	{ label: 'Orange', value: 'orange' },
+	{ label: 'Carrot', value: 'carrot' },
+	{ label: 'Broccoli', value: 'broccoli' },
+	{ label: 'Spinach', value: 'spinach' },
+];
+
+export default function ZIndex() {
+	const [items, setItems] = useState(data);
+
+	const collection = useListCollection({
+		items: items,
+		itemToString: (item) => item.label,
+		itemToValue: (item) => item.value,
+	});
+
+	const onOpenChange = () => {
+		setItems(data);
+	};
+
+	const onInputValueChange: ComboboxRootProps['onInputValueChange'] = (event) => {
+		const filtered = data.filter((item) => item.value.toLowerCase().includes(event.inputValue.toLowerCase()));
+		if (filtered.length > 0) {
+			setItems(filtered);
+		} else {
+			setItems(data);
+		}
+	};
+
+	return (
+		<Combobox placeholder="Search..." collection={collection} onOpenChange={onOpenChange} onInputValueChange={onInputValueChange}>
+			<Combobox.Label>Label</Combobox.Label>
+			<Combobox.Control>
+				<Combobox.Input />
+				<Combobox.Trigger />
+			</Combobox.Control>
+			<Combobox.Positioner className="z-50">
+				<Combobox.Content>
+					{items.map((item) => (
+						<Combobox.Item key={item.value} item={item}>
+							{item.label}
+						</Combobox.Item>
+					))}
+				</Combobox.Content>
+			</Combobox.Positioner>
+		</Combobox>
+	);
+}
