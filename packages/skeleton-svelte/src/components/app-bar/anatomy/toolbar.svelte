24526affc00c70b08af0ba1299c82ba64ec9d58a
Chris Simmons <gundamx9740@gmail.com>
2025-09-23 11:30:49 -0500

    feat: app bar (#3783)
    Co-authored-by: Hugo Korte <63101006+Hugos68@users.noreply.github.com>

---
+++ packages/skeleton-svelte/src/components/app-bar/anatomy/toolbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" module>
+	import type { HTMLAttributes } from '@/internal/html-attributes';
+	import type { PropsWithElement } from '@/internal/props-with-element';
+
+	export interface AppBarToolbarProps extends PropsWithElement<'div'>, HTMLAttributes<'div'> {}
+</script>
+
+<script lang="ts">
+	import { classesAppBar } from '@skeletonlabs/skeleton-common';
+	import { mergeProps } from '@zag-js/svelte';
+
+	const props: AppBarToolbarProps = $props();
+	const { element, children, ...rest } = $derived(props);
+
+	const attributes = $derived(
+		mergeProps(rest, {
+			class: classesAppBar.toolbar,
+		}),
+	);
+</script>
+
+{#if element}
+	{@render element(attributes)}
+{:else}
+	<div {...attributes}>
+		{@render children?.()}
+	</div>
+{/if}
