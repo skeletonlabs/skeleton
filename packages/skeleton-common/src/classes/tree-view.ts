2c3c561aba241f2c9007040f049160d4e6a8443e
Hugos68 <hugokorteapple@gmail.com>
2025-09-25 15:35:58 +0200

    fix spacing

--- packages/skeleton-common/src/classes/tree-view.ts
+++ packages/skeleton-common/src/classes/tree-view.ts
@@ -5,18 +5,17 @@ export const classesTreeView = defineSkeletonClasses({
 	tree: '',
 	label: 'label-text',
 	item: [
-		'flex gap-2 items-center ps-[calc(var(--depth)*var(--spacing)*6+calc(var(--spacing)*6))] pe-4 py-2 hover:preset-tonal rounded select-none',
+		'flex gap-2 items-center ps-[calc(var(--depth)*var(--spacing)*6+calc(var(--spacing)*4))] pe-4 py-2 hover:preset-tonal rounded select-none',
 		'data-selected:preset-tonal',
 		'[&>svg]:size-4',
 	],
 	branch: 'grid',
 	branchControl: [
-		'flex gap-2 items-center ps-[calc(var(--depth)*var(--spacing)*6)] pe-4 py-2 hover:preset-tonal rounded select-none',
+		'flex gap-2 items-center ps-[calc(var(--depth)*var(--spacing)*6-calc(var(--spacing)*2))] pe-4 py-2 hover:preset-tonal rounded select-none',
 		'data-selected:preset-tonal',
 	],
 	branchIndicator: ['[&>svg]:size-4'],
 	branchText: ['flex gap-2 items-center', '[&>svg]:size-4'],
 	branchContent: 'grid relative',
-	branchIndentGuide:
-		'absolute preset-filled-surface-500 w-0.5 left-[calc(var(--depth)*var(--spacing)*6+calc(var(--spacing)*2))] -translate-x-1/2 h-full rounded',
+	branchIndentGuide: 'absolute preset-filled-surface-500 w-0.5 left-[calc(var(--depth)*var(--spacing)*6)] -translate-x-1/2 h-full rounded',
 });
