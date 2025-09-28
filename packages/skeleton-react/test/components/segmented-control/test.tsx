f8092cf263023c42efafad827b3c85485b4c33a3
Hugos68 <hugokorteapple@gmail.com>
2025-09-28 16:05:34 +0200

    add missing tests

--- packages/skeleton-react/test/components/segmented-control/test.tsx
+++ packages/skeleton-react/test/components/segmented-control/test.tsx
@@ -3,11 +3,14 @@ import { SegmentedControl } from '@/index';
 export default function Test() {
 	return (
 		<SegmentedControl data-testid="root">
-			<SegmentedControl.Indicator data-testid="indicator" />
-			<SegmentedControl.Item value="item-1" data-testid="item">
-				<SegmentedControl.ItemText data-testid="item-text" />
-				<SegmentedControl.ItemHiddenInput data-testid="item-hidden-input" />
-			</SegmentedControl.Item>
+			<SegmentedControl.Label data-testid="label" />
+			<SegmentedControl.Control>
+				<SegmentedControl.Indicator data-testid="indicator" />
+				<SegmentedControl.Item value="item-1" data-testid="item">
+					<SegmentedControl.ItemText data-testid="item-text" />
+					<SegmentedControl.ItemHiddenInput data-testid="item-hidden-input" />
+				</SegmentedControl.Item>
+			</SegmentedControl.Control>
 		</SegmentedControl>
 	);
 }
