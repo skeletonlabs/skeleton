91c9bb5908c6c132f7b0bdeff928adf88094e952
Chris Simmons <gundamx9740@gmail.com>
2025-09-28 05:13:59 -0500

    feat: navigation (#3800)
    Co-authored-by: Hugo Korte <63101006+Hugos68@users.noreply.github.com>
    Co-authored-by: Hugos68 <hugokorteapple@gmail.com>

---
+++ packages/skeleton-svelte/src/components/navigation/anatomy/header.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+	import type { HTMLAttributes } from '@/internal/html-attributes';
+	import type { PropsWithElement } from '@/internal/props-with-element';
+
+	export interface NavigationHeaderProps extends PropsWithElement<'header'>, HTMLAttributes<'header'> {}
+</script>
+
+<script lang="ts">
+	import { RootContext } from '../modules/root-context';
+	import { classesNavigation } from '@skeletonlabs/skeleton-common';
+	import { mergeProps } from '@zag-js/svelte';
+
+	const props: NavigationHeaderProps = $props();
+
+	const navigation = RootContext.consume();
+
+	const { element, children, ...rest } = $derived(props);
+
+	const attributes = $derived(
+		mergeProps(
+			{
+				'data-layout': navigation().layout,
+				class: classesNavigation.header,
+			},
+			rest,
+		),
+	);
+</script>
+
+{#if element}
+	{@render element(attributes)}
+{:else}
+	<header {...attributes}>
+		{@render children?.()}
+	</header>
+{/if}
---
+++ packages/skeleton-svelte/src/components/navigation/anatomy/root.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" module>
+	import type { HTMLAttributes } from '@/internal/html-attributes';
+	import type { PropsWithElement } from '@/internal/props-with-element';
+
+	export interface NavigationRootProps extends PropsWithElement<'div'>, HTMLAttributes<'div'> {
+		/**
+		 * Sets the data-layout attribute, which modifies the visual presentation of the component set.
+		 *
+		 * @default bar
+		 * */
+		layout?: 'bar' | 'rail' | 'sidebar';
+	}
+</script>
+
+<script lang="ts">
+	import { RootContext } from '../modules/root-context';
+	import { classesNavigation } from '@skeletonlabs/skeleton-common';
+	import { mergeProps } from '@zag-js/svelte';
+
+	const props: NavigationRootProps = $props();
+	const { layout = 'bar', element, children, ...rest } = $derived(props);
+
+	const attributes = $derived(
+		mergeProps(
+			{
+				'data-layout': layout,
+				class: classesNavigation.root,
+			},
+			rest,
+		),
+	);
+
+	RootContext.provide(() => ({ layout }));
+</script>
+
+{#if element}
+	{@render element(attributes)}
+{:else}
+	<div {...attributes}>
+		{@render children?.()}
+	</div>
+{/if}
