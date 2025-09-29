a00c9d625af356e50fec911d14e98ef7e844142c
Hugos68 <hugokorteapple@gmail.com>
2025-09-29 15:24:44 +0200

    custom format

--- sites/skeleton.dev/src/examples/components/progress-circular/react/custom-format.tsx
+++ sites/skeleton.dev/src/examples/components/progress-circular/react/custom-format.tsx
@@ -2,19 +2,14 @@ import { Progress } from '@skeletonlabs/skeleton-react';
 
 export default function CustomFormat() {
 	return (
-		<Progress
-			className="items-center w-fit"
-			translations={{
-				value(details) {
-					return `${details.value} of ${details.max}`;
-				},
-			}}
-		>
+		<Progress className="items-center w-fit">
 			<Progress.Circle style={{ ['--size' as string]: '4rem', ['--thickness' as string]: '0.5rem' }}>
 				<Progress.CircleTrack />
 				<Progress.CircleRange />
 			</Progress.Circle>
-			<Progress.ValueText />
+			<Progress.ValueText>
+				<Progress.Context>{(progress) => `${progress.value} of ${progress.max}`}</Progress.Context>
+			</Progress.ValueText>
 		</Progress>
 	);
 }
--- sites/skeleton.dev/src/examples/components/progress-circular/svelte/custom-format.svelte
+++ sites/skeleton.dev/src/examples/components/progress-circular/svelte/custom-format.svelte
@@ -2,17 +2,16 @@
 	import { Progress } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<Progress
-	class="items-center w-fit"
-	translations={{
-		value(details) {
-			return `${details.value} of ${details.max}`;
-		},
-	}}
->
+<Progress class="items-center w-fit">
 	<Progress.Circle --size="4rem" --thickness="0.5rem">
 		<Progress.CircleTrack />
 		<Progress.CircleRange />
 	</Progress.Circle>
-	<Progress.ValueText />
+	<Progress.ValueText>
+		<Progress.Context>
+			{#snippet children(progress)}
+				{progress().value} of {progress().max}
+			{/snippet}
+		</Progress.Context>
+	</Progress.ValueText>
 </Progress>
