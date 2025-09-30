b722bad930b592417bee0b04f91802c49e214c5e
Hugos68 <hugokorteapple@gmail.com>
2025-10-01 00:58:52 +0200

    Add tests

---
+++ packages/skeleton-react/test/components/date-picker/test.tsx
@@ -0,0 +1,42 @@
+import { DatePicker } from '@/index';
+
+export default function Test() {
+	return (
+		<DatePicker data-testid="root">
+			<DatePicker.Label data-testid="label" />
+			<DatePicker.Control data-testid="control">
+				<DatePicker.Input data-testid="input" />
+				<DatePicker.Trigger data-testid="trigger" />
+			</DatePicker.Control>
+			<DatePicker.Positioner data-testid="positioner">
+				<DatePicker.Content data-testid="content">
+					<DatePicker.YearSelect data-testid="year-select" />
+					<DatePicker.MonthSelect data-testid="month-select" />
+					<DatePicker.View data-testid="view">
+						<DatePicker.ViewControl data-testid="view-control">
+							<DatePicker.PrevTrigger data-testid="prev-trigger" />
+							<DatePicker.ViewTrigger data-testid="view-trigger">
+								<DatePicker.RangeText data-testid="range-text" />
+							</DatePicker.ViewTrigger>
+							<DatePicker.NextTrigger data-testid="next-trigger" />
+						</DatePicker.ViewControl>
+						<DatePicker.Table data-testid="table">
+							<DatePicker.TableHead data-testid="table-head">
+								<DatePicker.TableRow data-testid="table-row">
+									<DatePicker.TableHeader data-testid="table-header" />
+								</DatePicker.TableRow>
+							</DatePicker.TableHead>
+							<DatePicker.TableBody data-testid="table-body">
+								<DatePicker.TableRow data-testid="table-row">
+									<DatePicker.TableCell value={0} data-testid="table-cell">
+										<DatePicker.TableCellTrigger data-testid="table-cell-trigger" />
+									</DatePicker.TableCell>
+								</DatePicker.TableRow>
+							</DatePicker.TableBody>
+						</DatePicker.Table>
+					</DatePicker.View>
+				</DatePicker.Content>
+			</DatePicker.Positioner>
+		</DatePicker>
+	);
+}
