b2fcdbe051e037559015c3995fee5d4d4a3cea76
Hugos68 <hugokorteapple@gmail.com>
2025-10-02 17:49:56 +0200

    fix excessive logging

--- packages/skeleton-common/tsdown.config.ts
+++ packages/skeleton-common/tsdown.config.ts
@@ -3,6 +3,7 @@ import Macros from 'unplugin-macros/rolldown';
 import Raw from 'unplugin-raw/rolldown';
 
 export default defineConfig({
+	logLevel: 'error',
 	unbundle: true,
 	copy: ['src/index.css'],
 	plugins: [Raw(), Macros()],
