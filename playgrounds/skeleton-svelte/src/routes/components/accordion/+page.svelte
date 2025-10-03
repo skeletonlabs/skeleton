4baa2f90a0c6bdd82c65d6567ed4dd9cba674850
Hugos68 <hugokorteapple@gmail.com>
2025-10-03 09:54:07 +0200

    fix

--- playgrounds/skeleton-svelte/src/routes/components/accordion/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/accordion/+page.svelte
@@ -4,15 +4,21 @@
 
 <Accordion>
 	<Accordion.Item value="item-1">
-		<Accordion.ItemTrigger>Item 1</Accordion.ItemTrigger>
+		<h3>
+			<Accordion.ItemTrigger>Item 1</Accordion.ItemTrigger>
+		</h3>
 		<Accordion.ItemContent>Content 1</Accordion.ItemContent>
 	</Accordion.Item>
 	<Accordion.Item value="item-2">
-		<Accordion.ItemTrigger>Item 2</Accordion.ItemTrigger>
+		<h3>
+			<Accordion.ItemTrigger>Item 2</Accordion.ItemTrigger>
+		</h3>
 		<Accordion.ItemContent>Content 2</Accordion.ItemContent>
 	</Accordion.Item>
 	<Accordion.Item value="item-3">
-		<Accordion.ItemTrigger>Item 3</Accordion.ItemTrigger>
+		<h3>
+			<Accordion.ItemTrigger>Item 3</Accordion.ItemTrigger>
+		</h3>
 		<Accordion.ItemContent>Content 3</Accordion.ItemContent>
 	</Accordion.Item>
 </Accordion>
