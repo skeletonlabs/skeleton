7a77a3b1b2c24fd0de47c026164e3030a6639236
Hugos68 <hugokorteapple@gmail.com>
2025-10-04 22:30:34 +0200

    add skeleton integration

--- sites/skeleton.dev/astro.config.ts
+++ sites/skeleton.dev/astro.config.ts
@@ -4,6 +4,7 @@ import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
 import svelte, { vitePreprocess } from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import { AstroIntegration } from 'astro';
 import AutoImport from 'astro-auto-import';
 import expressiveCode from 'astro-expressive-code';
 import icon from 'astro-icon';
@@ -13,8 +14,47 @@ import { fileURLToPath } from 'node:url';
 import { createIndex } from 'pagefind';
 import { pagefind } from 'vite-plugin-pagefind';
 
+function skeleton(): AstroIntegration {
+	return {
+		name: 'skeleton',
+		hooks: {
+			'astro:build:start': async (ctx) => {
+				ctx.logger.info('Generating type documentation...');
+				await generateTypeDocumentation();
+				ctx.logger.info('Type documentation generated.');
+			},
+			'astro:build:done': async (ctx) => {
+				ctx.logger.info('Removing meta pages...');
+				for (const page of ctx.pages) {
+					if (!page.pathname.endsWith('/meta/')) {
+						continue;
+					}
+					await rm(fileURLToPath(new URL(page.pathname, ctx.dir).href), {
+						recursive: true,
+					});
+					ctx.logger.info(`Removed ${page.pathname}`);
+				}
+				ctx.logger.info('Meta pages removed.');
+				ctx.logger.info('Generating search index...');
+				const { index, errors } = await createIndex({
+					excludeSelectors: ['.expressive-code'],
+				});
+				if (!index) {
+					throw new Error(`Failed to create search index: ${errors?.join(', ')}`);
+				}
+				await index.addDirectory({
+					path: fileURLToPath(ctx.dir),
+				});
+				await index.writeFiles();
+				ctx.logger.info('Search index generated.');
+			},
+		},
+	};
+}
+
 export default defineConfig({
 	integrations: [
+		skeleton(),
 		partytown(),
 		svelte({
 			preprocess: vitePreprocess(),
@@ -52,41 +92,6 @@ export default defineConfig({
 		 * Note: Keep this as the last integration so that framework components are available in MDX.
 		 */
 		mdx(),
-		{
-			name: 'skeleton',
-			hooks: {
-				'astro:build:start': async (ctx) => {
-					ctx.logger.info('Generating type documentation...');
-					await generateTypeDocumentation();
-					ctx.logger.info('Type documentation generated.');
-				},
-				'astro:build:done': async (ctx) => {
-					ctx.logger.info('Removing meta pages...');
-					for (const page of ctx.pages) {
-						if (!page.pathname.endsWith('/meta/')) {
-							continue;
-						}
-						await rm(fileURLToPath(new URL(page.pathname, ctx.dir).href), {
-							recursive: true,
-						});
-						ctx.logger.info(`Removed ${page.pathname}`);
-					}
-					ctx.logger.info('Meta pages removed.');
-					ctx.logger.info('Generating search index...');
-					const { index, errors } = await createIndex({
-						excludeSelectors: ['.expressive-code'],
-					});
-					if (!index) {
-						throw new Error(`Failed to create search index: ${errors?.join(', ')}`);
-					}
-					await index.addDirectory({
-						path: fileURLToPath(ctx.dir),
-					});
-					await index.writeFiles();
-					ctx.logger.info('Search index generated.');
-				},
-			},
-		},
 	],
 	vite: {
 		plugins: [
