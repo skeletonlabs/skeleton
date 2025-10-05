5c82454f75a753b0f60a60906810af4f826631c8
Hugos68 <hugokorteapple@gmail.com>
2025-10-05 13:26:12 +0200

    migrate to our own popvoers

--- sites/skeleton.dev/src/components/ui/header-sponsors.tsx
+++ sites/skeleton.dev/src/components/ui/header-sponsors.tsx
@@ -1,83 +1,46 @@
-// Floating UI React
-import {
-	FloatingFocusManager,
-	autoUpdate,
-	flip,
-	offset,
-	shift,
-	useClick,
-	useDismiss,
-	useFloating,
-	useInteractions,
-	useRole,
-} from '@floating-ui/react';
+import { Popover, Portal } from '@skeletonlabs/skeleton-react';
 import { ArrowUpRightIcon, ChevronDownIcon, HeartIcon } from 'lucide-react';
-import React from 'react';
-import { useState } from 'react';
 
-export const HeaderSponsors: React.FC = () => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const { refs, floatingStyles, context } = useFloating({
-		open: isOpen,
-		onOpenChange: setIsOpen,
-		middleware: [offset(8), flip(), shift()],
-		placement: 'top-end',
-		whileElementsMounted: autoUpdate,
-	});
-
-	const click = useClick(context);
-	const dismiss = useDismiss(context);
-	const role = useRole(context);
-
-	const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
-
+export default function HeaderSponsors() {
 	return (
 		<nav className="h-full flex items-stretch">
-			{/* Trigger */}
-			<button className="hidden xl:flex btn hover:preset-tonal gap-1" title="Sponsor Us" ref={refs.setReference} {...getReferenceProps()}>
-				<HeartIcon className="size-5" />
-				<ChevronDownIcon className="size-4 opacity-60" />
-			</button>
-			{/* Popup */}
-			{isOpen && (
-				<FloatingFocusManager context={context} modal={false}>
-					<div
-						className="card max-w-72 bg-surface-50-950 border border-surface-200-800 p-4 space-y-4"
-						ref={refs.setFloating}
-						style={floatingStyles}
-						{...getFloatingProps()}
-					>
-						<header className="space-y-2">
-							<p className="font-bold">Sponsor Us</p>
-							<p className="text-xs opacity-60">Sponsor Skeleton and help support the project. Available on the following services.</p>
-						</header>
-						<hr className="hr" />
-						<div className="flex flex-col gap-1">
-							<a
-								className="btn hover:preset-tonal justify-between"
-								href="https://github.com/sponsors/skeletonlabs"
-								target="_blank"
-								rel="noreferrer"
-								onClick={() => setIsOpen(false)}
-							>
-								<span>Via GitHub</span>
-								<ArrowUpRightIcon className="size-4 opacity-60" />
-							</a>
-							<a
-								className="btn hover:preset-tonal justify-between"
-								href="https://ko-fi.com/skeletonlabs"
-								target="_blank"
-								rel="noreferrer"
-								onClick={() => setIsOpen(false)}
-							>
-								<span>Via Ko-Fi</span>
-								<ArrowUpRightIcon className="size-4 opacity-60" />
-							</a>
-						</div>
-					</div>
-				</FloatingFocusManager>
-			)}
+			<Popover positioning={{ placement: 'top-end' }}>
+				<Popover.Trigger className="hidden xl:flex btn hover:preset-tonal gap-1" title="Sponsor Us">
+					<HeartIcon className="size-5" />
+					<ChevronDownIcon className="size-4 opacity-60" />
+				</Popover.Trigger>
+				<Portal>
+					<Popover.Positioner>
+						<Popover.Content className="card max-w-72 bg-surface-50-950 border border-surface-200-800 p-4 space-y-4 z-50">
+							<header className="space-y-2">
+								<p className="font-bold">Sponsor Us</p>
+								<p className="text-xs opacity-60">Sponsor Skeleton and help support the project. Available on the following services.</p>
+							</header>
+							<hr className="hr" />
+							<div className="flex flex-col gap-1">
+								<a
+									className="btn hover:preset-tonal justify-between"
+									href="https://github.com/sponsors/skeletonlabs"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<span>Via GitHub</span>
+									<ArrowUpRightIcon className="size-4 opacity-60" />
+								</a>
+								<a
+									className="btn hover:preset-tonal justify-between"
+									href="https://ko-fi.com/skeletonlabs"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<span>Via Ko-Fi</span>
+									<ArrowUpRightIcon className="size-4 opacity-60" />
+								</a>
+							</div>
+						</Popover.Content>
+					</Popover.Positioner>
+				</Portal>
+			</Popover>
 		</nav>
 	);
-};
+}
