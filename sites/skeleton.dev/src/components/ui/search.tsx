ae7c369975ed37cf517bc16235b859383bf64fe0
endigo9740 <gundamx9740@gmail.com>
2025-10-06 11:40:06 -0500

    QA and Doc improvements

--- sites/skeleton.dev/src/components/ui/search.tsx
+++ sites/skeleton.dev/src/components/ui/search.tsx
@@ -91,9 +91,9 @@ export default function Search() {
 					<SearchIcon className="w-5 h-5" />
 					<span>Search...</span>
 				</div>
-				<div className="flex items-center gap-1 leading-4">
-					<kbd className="kbd text-xs">⌘</kbd>
-					<kbd className="kbd">K</kbd>
+				<div className="hidden md:flex items-center gap-1 leading-4">
+					<kbd className="kbd text-[10px]">⌘</kbd>
+					<kbd className="kbd text-[10px]">K</kbd>
 				</div>
 			</Dialog.Trigger>
 			<Portal>
