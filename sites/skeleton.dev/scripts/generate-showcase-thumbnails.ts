4b9fe28bdc462d6666e3161196a93638e5fad9d0
Hugos68 <hugokorteapple@gmail.com>
2025-10-10 20:50:32 +0200

    dom content

--- sites/skeleton.dev/scripts/generate-showcase-thumbnails.ts
+++ sites/skeleton.dev/scripts/generate-showcase-thumbnails.ts
@@ -35,7 +35,7 @@ export async function generateShowcaseThumbnails() {
 				viewport: { width: 1920, height: 1080 },
 			});
 			await page.goto(project.url, {
-				waitUntil: 'networkidle',
+				waitUntil: 'domcontentloaded',
 			});
 			await page.screenshot({ path: join(OUTPUT_DIRECTORY, `${project.slug}.png`) });
 		}),
