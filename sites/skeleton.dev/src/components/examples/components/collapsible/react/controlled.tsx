4de251c93ab4c65656dc5e4efb1ffa521ddfed27
Hugos68 <hugokorteapple@gmail.com>
2025-10-10 17:27:45 +0200

    add alignment and controlled examples, commented out direction

---
+++ sites/skeleton.dev/src/components/examples/components/collapsible/react/controlled.tsx
@@ -0,0 +1,16 @@
+import { Collapsible } from '@skeletonlabs/skeleton-react';
+import { useState } from 'react';
+
+export default function Controlled() {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<Collapsible open={open} onOpenChange={(details) => setOpen(details.open)}>
+			<Collapsible.Trigger className="btn preset-filled">Toggle</Collapsible.Trigger>
+			<Collapsible.Content>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur error soluta voluptate, accusantium modi repudiandae omnis
+				eligendi, suscipit repellat impedit architecto neque sequi mollitia autem dicta quae ipsum et?
+			</Collapsible.Content>
+		</Collapsible>
+	);
+}
