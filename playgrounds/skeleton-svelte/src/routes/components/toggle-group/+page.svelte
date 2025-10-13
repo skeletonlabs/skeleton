84f455072aeabd626d3543ea1e55dd334bb7dde0
Hugos68 <hugokorteapple@gmail.com>
2025-10-14 00:40:08 +0200

    idk

--- playgrounds/skeleton-svelte/src/routes/components/toggle-group/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/toggle-group/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { ToggleGroup } from '@skeletonlabs/skeleton-svelte';
-
-	let value = $state(['center']);
 </script>
 
-<ToggleGroup {value} onValueChange={(details) => (value = details.value)}>
+<ToggleGroup defaultValue={['center']}>
 	<ToggleGroup.Item value="left">Left</ToggleGroup.Item>
 	<ToggleGroup.Item value="center">Center</ToggleGroup.Item>
 	<ToggleGroup.Item value="right">Right</ToggleGroup.Item>
