f94bcf84539200ff432f245c76f683af8ba10bb8
Hugos68 <hugokorteapple@gmail.com>
2025-10-14 00:55:54 +0200

    Fix missing $derived causing stale state

--- packages/skeleton-svelte/src/components/toggle-group/index.ts
+++ packages/skeleton-svelte/src/components/toggle-group/index.ts
@@ -1,17 +1,6 @@
-import Item from './anatomy/item.svelte';
-import RootContext from './anatomy/root-context.svelte';
-import RootProvider from './anatomy/root-provider.svelte';
-import Root from './anatomy/root.svelte';
-import { useToggleGroup } from './modules/provider.svelte';
-
-export const ToggleGroup = Object.assign(Root, {
-	Provider: RootProvider,
-	Context: RootContext,
-	Item: Item,
-});
-
-export { useToggleGroup };
+export type { ToggleGroupItemProps } from './anatomy/item.svelte';
 export type { ToggleGroupRootProps } from './anatomy/root.svelte';
 export type { ToggleGroupRootContextProps } from './anatomy/root-context.svelte';
 export type { ToggleGroupRootProviderProps } from './anatomy/root-provider.svelte';
-export type { ToggleGroupItemProps } from './anatomy/item.svelte';
+export { ToggleGroup } from './modules/anatomy';
+export { useToggleGroup } from './modules/provider.svelte';
--- packages/skeleton-svelte/src/components/toggle-group/modules/provider.svelte.ts
+++ packages/skeleton-svelte/src/components/toggle-group/modules/provider.svelte.ts
@@ -1,11 +1,8 @@
 import { normalizeProps, useMachine, type PropTypes } from '@zag-js/svelte';
-import { connect, machine } from '@zag-js/toggle-group';
-import type { Api, Props } from '@zag-js/toggle-group';
+import { type Api, connect, machine, type Props } from '@zag-js/toggle-group';
 
 export function useToggleGroup(props: Props | (() => Props)): () => Api<PropTypes> {
-	const service = useMachine(machine, {
-		...(typeof props === 'function' ? props() : props),
-	});
-	const toggleGroup = connect(service, normalizeProps);
+	const service = useMachine(machine, props);
+	const toggleGroup = $derived(connect(service, normalizeProps));
 	return () => toggleGroup;
 }
