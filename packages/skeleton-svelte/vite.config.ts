819c90b9471df6311aa913750045eac5719d61d0
Hugos68 <hugokorteapple@gmail.com>
2025-10-14 14:13:02 +0200

    reduce dep and config

--- packages/skeleton-svelte/vite.config.ts
+++ packages/skeleton-svelte/vite.config.ts
@@ -1,11 +1,16 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	logLevel: 'error',
-	plugins: [svelte(), svelteTesting()],
+	plugins: [
+		svelte({
+			preprocess: vitePreprocess(),
+		}),
+		svelteTesting(),
+	],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
