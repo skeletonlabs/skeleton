c73ff8347f8212e81c4b4f6bca8abce9741b2551
Hugos68 <hugokorteapple@gmail.com>
2025-10-15 15:54:59 +0200

    stackblitz yippy

---
+++ sites/overhaul.skeleton.dev/src/modules/stackblitz/stackblitz.ts
@@ -0,0 +1,14 @@
+import { openReactStackblitzProject } from './stackblitz-react';
+import { openSvelteStackblitzProject } from './stackblitz-svelte';
+import type { CollectionEntry } from 'astro:content';
+
+export function openStackblitzProject(code: string, framework: CollectionEntry<'frameworks'>['id']) {
+	switch (framework) {
+		case 'react':
+			return openReactStackblitzProject(code);
+		case 'svelte':
+			return openSvelteStackblitzProject(code);
+		default:
+			throw new Error(`Unsupported framework: ${framework}`);
+	}
+}
