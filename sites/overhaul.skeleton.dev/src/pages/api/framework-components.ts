ce9cb3bfb807a6952e4f229a28cea1d4434f7f99
Hugos68 <hugokorteapple@gmail.com>
2025-10-15 14:34:28 +0200

    update

--- sites/overhaul.skeleton.dev/src/pages/api/framework-components.ts
+++ sites/overhaul.skeleton.dev/src/pages/api/framework-components.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
 export const GET: APIRoute = async () => {
-	const components = await getCollection('docs', (docs) => docs.id.startsWith('components/'));
+	const components = await getCollection('docs', (docs) => docs.id.startsWith('framework-components/'));
 
 	return Response.json(
 		components.map((component) => ({
