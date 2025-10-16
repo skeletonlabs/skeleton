606910fa0db9f7107ab14092febd986b84ea8f36
Hugos68 <hugokorteapple@gmail.com>
2025-10-16 15:21:38 +0200

    cleanup from testing

--- sites/overhaul.skeleton.dev/src/components/ui/code.svelte
+++ sites/overhaul.skeleton.dev/src/components/ui/code.svelte
@@ -2,7 +2,7 @@
 	import { createHighlighterCore } from 'shiki/core';
 	import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 
-	const highligher = await createHighlighterCore({
+	const highlighter = await createHighlighterCore({
 		themes: [import('@shikijs/themes/dark-plus')],
 		langs: [import('@shikijs/langs/typescript'), import('@shikijs/langs/tsx'), import('@shikijs/langs/svelte')],
 		engine: createOnigurumaEngine(import('shiki/wasm')),
@@ -17,13 +17,13 @@
 
 	const props: Props = $props();
 	const { code, lang } = $derived(props);
-	const html = $derived(highligher.codeToHtml(code, { lang, theme: 'dark-plus' }));
+
+	const html = $derived(
+		highlighter.codeToHtml(code, {
+			lang,
+			theme: 'dark-plus',
+		}),
+	);
 </script>
 
 {@html html}
-
-<style>
-	:global(.shiki) {
-		white-space: pre-wrap;
-	}
-</style>
