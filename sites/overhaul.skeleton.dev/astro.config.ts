a6d4b8c696a378d9128f09f6b566782c241ac71d
Hugos68 <hugokorteapple@gmail.com>
2025-10-16 15:46:15 +0200

    fix

--- sites/overhaul.skeleton.dev/astro.config.ts
+++ sites/overhaul.skeleton.dev/astro.config.ts
@@ -11,7 +11,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	prefetch: true,
 	site: `https://${process.env.VERCEL_ENV === 'production' ? process.env.VERCEL_PROJECT_PRODUCTION_URL : process.env.VERCEL_URL}`,
-	adapter: vercel(),
 	integrations: [
 		react(),
 		svelte({
@@ -30,4 +29,5 @@ export default defineConfig({
 		/* @ts-expect-error vite version mistmatch */
 		plugins: [tailwindcss()],
 	},
+	adapter: vercel(),
 });
