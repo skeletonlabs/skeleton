cfac86812198b1d8bb45d5354ba4f8fa9fc72c08
Eric Branson <eric.branson@gmail.com>
2025-10-18 09:38:59 -0600

    first prev next last

--- packages/skeleton-react/src/components/pagination/modules/anatomy.ts
+++ packages/skeleton-react/src/components/pagination/modules/anatomy.ts
@@ -2,6 +2,8 @@ import Ellipsis from '../anatomy/ellipsis';
 import Item from '../anatomy/item';
 import NextTrigger from '../anatomy/next-trigger';
 import PrevTrigger from '../anatomy/prev-trigger';
+import FirstTrigger from '../anatomy/first-trigger';
+import LastTrigger from '../anatomy/last-trigger';
 import Root from '../anatomy/root';
 import RootContext from '../anatomy/root-context';
 import RootProvider from '../anatomy/root-provider';
@@ -9,8 +11,10 @@ import RootProvider from '../anatomy/root-provider';
 export const Pagination = Object.assign(Root, {
 	Provider: RootProvider,
 	Context: RootContext,
+	FirstTrigger: FirstTrigger,
 	PrevTrigger: PrevTrigger,
 	Item: Item,
 	Ellipsis: Ellipsis,
 	NextTrigger: NextTrigger,
+	LastTrigger: LastTrigger,
 });
