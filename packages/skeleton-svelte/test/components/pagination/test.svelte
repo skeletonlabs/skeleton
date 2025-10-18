d642b1d7eaa2fc7ca37a70822e2c3da52bc95573
Eric Branson <eric.branson@gmail.com>
2025-10-18 10:17:02 -0600

    added tests

--- packages/skeleton-svelte/test/components/pagination/test.svelte
+++ packages/skeleton-svelte/test/components/pagination/test.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <Pagination data-testid="root">
+	<Pagination.FirstTrigger data-testid="first-trigger" />
 	<Pagination.PrevTrigger data-testid="prev-trigger" />
 	<Pagination.Item type="page" value={0} data-testid="item" />
 	<Pagination.Ellipsis index={0} data-testid="ellipsis" />
 	<Pagination.NextTrigger data-testid="next-trigger" />
+	<Pagination.LastTrigger data-testid="last-trigger" />
 </Pagination>
