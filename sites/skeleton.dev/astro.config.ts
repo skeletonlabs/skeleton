f0eae1b158a2d391d28e174c8165895ff6ee6893
Hugos68 <hugokorteapple@gmail.com>
2025-10-19 19:26:24 +0200

    try out static

--- sites/skeleton.dev/astro.config.ts
+++ sites/skeleton.dev/astro.config.ts
@@ -39,7 +39,6 @@ export default defineConfig({
 			}),
 		],
 	},
-	output: 'server',
 	adapter: vercel(),
 });
 
--- sites/skeleton.dev/src/modules/get-active-framework.ts
+++ sites/skeleton.dev/src/modules/get-active-framework.ts
@@ -1,18 +1,19 @@
-import type { AstroCookies } from 'astro';
+import type { AstroGlobal } from 'astro';
 import { getCollection, getEntry } from 'astro:content';
 
 const frameworks = await getCollection('frameworks');
 
 const DEFAULT_FRAMEWORK = frameworks.at(0)!;
 
-export async function getActiveFramework(cookies: AstroCookies) {
-	const frameworkCookie = cookies.get('framework');
-	if (!frameworkCookie) {
-		return DEFAULT_FRAMEWORK;
-	}
-	const framework = await getEntry('frameworks', frameworkCookie.value);
-	if (!framework) {
-		return DEFAULT_FRAMEWORK;
-	}
-	return framework;
+export async function getActiveFramework(astro: AstroGlobal) {
+	return DEFAULT_FRAMEWORK;
+	// const frameworkCookie = astro.cookies.get('framework');
+	// if (!frameworkCookie) {
+	// 	return DEFAULT_FRAMEWORK;
+	// }
+	// const framework = await getEntry('frameworks', frameworkCookie.value);
+	// if (!framework) {
+	// 	return DEFAULT_FRAMEWORK;
+	// }
+	// return framework;
 }
