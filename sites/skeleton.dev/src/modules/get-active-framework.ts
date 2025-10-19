c7ebd043c5af52a5a01f2e5f0ad9e115a8e070c4
Hugos68 <hugokorteapple@gmail.com>
2025-10-20 00:56:41 +0200

    test

--- sites/skeleton.dev/src/modules/get-active-framework.ts
+++ sites/skeleton.dev/src/modules/get-active-framework.ts
@@ -5,15 +5,13 @@ const frameworks = await getCollection('frameworks');
 
 const DEFAULT_FRAMEWORK = frameworks.at(0)!;
 
-export async function getActiveFramework(astro: AstroGlobal) {
-	return DEFAULT_FRAMEWORK;
-	// const frameworkCookie = astro.cookies.get('framework');
-	// if (!frameworkCookie) {
-	// 	return DEFAULT_FRAMEWORK;
-	// }
-	// const framework = await getEntry('frameworks', frameworkCookie.value);
-	// if (!framework) {
-	// 	return DEFAULT_FRAMEWORK;
-	// }
-	// return framework;
+export async function getActiveFramework(Astro: AstroGlobal) {
+	if (!Astro.params.framework) {
+		return DEFAULT_FRAMEWORK;
+	}
+	const framework = await getEntry('frameworks', Astro.params.framework);
+	if (!framework) {
+		return DEFAULT_FRAMEWORK;
+	}
+	return framework;
 }
