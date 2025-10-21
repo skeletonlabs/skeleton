44f42640546d40a527e0e8791da4bfc5776879e1
Hugos68 <hugokorteapple@gmail.com>
2025-10-21 12:46:48 +0200

    framework picker

--- sites/skeleton.dev/src/modules/framework.ts
+++ sites/skeleton.dev/src/modules/framework.ts
@@ -1,15 +1,21 @@
 import type { AstroGlobal } from 'astro';
 import { getEntry } from 'astro:content';
 
-const DEFAULT_FRAMEWORK = await getEntry('frameworks', 'svelte');
-
 export async function getActiveFramework(Astro: AstroGlobal) {
 	if (!Astro.params.framework) {
-		return DEFAULT_FRAMEWORK;
+		return await getDefaultFramework();
 	}
 	const framework = await getEntry('frameworks', Astro.params.framework);
 	if (!framework) {
-		return DEFAULT_FRAMEWORK;
+		return await getDefaultFramework();
+	}
+	return framework;
+}
+
+export async function getDefaultFramework() {
+	const framework = await getEntry('frameworks', 'svelte');
+	if (!framework) {
+		throw new Error('Default framework "svelte" not found in content collection "frameworks".');
 	}
 	return framework;
 }
