bec02d0e5e77d502df056bdadaae287d6106f15c
Hugos68 <hugokorteapple@gmail.com>
2025-10-23 14:21:30 +0200

    nice looking TOC

--- sites/skeleton.dev/src/components/ui/table-of-contents.svelte
+++ sites/skeleton.dev/src/components/ui/table-of-contents.svelte
@@ -1,22 +1,22 @@
 <script lang="ts" module>
-	function useActiveHeading(headings: () => MarkdownHeading[]) {
-		let activeHeading = $state<MarkdownHeading | undefined>(headings()[0]);
+	function useActiveHeading(headings: MarkdownHeading[]) {
+		let activeHeading = $state<MarkdownHeading | undefined>(headings[0]);
 		$effect(() => {
 			const observer = new IntersectionObserver(
 				(entries) => {
 					for (const entry of entries) {
 						const id = `#${entry.target.getAttribute('id')}`;
-						const heading = headings().find((heading) => `#${heading.slug}` === id);
+						const heading = headings.find((heading) => `#${heading.slug}` === id);
 						if (entry?.isIntersecting) {
 							activeHeading = heading;
 						}
 					}
 				},
 				{
-					rootMargin: '0px 0px -60% 0px',
+					rootMargin: '0px 0px -85% 0px',
 				},
 			);
-			for (const element of headings()
+			for (const element of headings
 				.map((heading) => document.getElementById(heading.slug))
 				.filter((element): element is HTMLElement => element !== null)) {
 				observer.observe(element);
@@ -28,6 +28,7 @@
 </script>
 
 <script lang="ts">
+	import { TableOfContentsIcon } from '@lucide/svelte';
 	import { SegmentedControl } from '@skeletonlabs/skeleton-svelte';
 	import type { MarkdownHeading } from 'astro';
 
@@ -37,35 +38,42 @@
 
 	const { headings }: Props = $props();
 
-	const activeHeading = useActiveHeading(() => headings);
+	const activeHeading = useActiveHeading(headings);
 
 	function getPaddingFromDepth(depth: number) {
 		return {
 			1: 'ps-0',
-			2: 'ps-2',
-			3: 'ps-4',
-			4: 'ps-6',
-			5: 'ps-8',
-			6: 'ps-10',
+			2: 'ps-4',
+			3: 'ps-8',
+			4: 'ps-12',
+			5: 'ps-16',
+			6: 'ps-20',
 		}[depth];
 	}
 </script>
 
 {#if headings.length > 0}
 	<nav class="flex flex-col gap-2">
-		<span class="font-bold">On This Page</span>
-		<SegmentedControl value={activeHeading()?.slug} orientation="vertical">
+		<span class="opacity-50 flex items-center gap-2">
+			<TableOfContentsIcon class="size-4" />
+			On This Page
+		</span>
+		<SegmentedControl value={activeHeading()?.slug} orientation="vertical" class="shadow-[inset_2px_0_0_0_var(--color-surface-200-800)]">
 			<SegmentedControl.Control class="border-none p-0">
 				<SegmentedControl.Indicator class="w-0.5" />
 				{#each headings as heading (heading)}
-					<a href={`#${heading.slug}`}>
-						<SegmentedControl.Item value={heading.slug} class="justify-start text-sm p-0 {getPaddingFromDepth(heading.depth)}">
-							<SegmentedControl.ItemText class="data-[state=checked]:text-surface-contrast-50-950 data-[state=checked]:font-semibold">
-								{heading.text}
-							</SegmentedControl.ItemText>
-							<SegmentedControl.ItemHiddenInput />
-						</SegmentedControl.Item>
-					</a>
+					<SegmentedControl.Item value={heading.slug} class="justify-start p-0 {getPaddingFromDepth(heading.depth)}">
+						{#snippet element(attributes: Record<string, unknown>)}
+							<a {...attributes} href={`#${heading.slug}`}>
+								<SegmentedControl.ItemText
+									class="text-sm text-surface-contrast-50-950/50 data-[state=checked]:text-surface-contrast-50-950"
+								>
+									{heading.text}
+								</SegmentedControl.ItemText>
+								<SegmentedControl.ItemHiddenInput />
+							</a>
+						{/snippet}
+					</SegmentedControl.Item>
 				{/each}
 			</SegmentedControl.Control>
 		</SegmentedControl>
