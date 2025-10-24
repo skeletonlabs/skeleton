80fb014b0b8b1bd039a733c6938266140f785f32
Hugos68 <hugokorteapple@gmail.com>
2025-10-25 00:14:00 +0200

    fix

--- sites/skeleton.dev/src/components/ui/header/search.svelte
+++ sites/skeleton.dev/src/components/ui/header/search.svelte
@@ -215,9 +215,9 @@
 					{/if}
 					<hr class="hidden lg:block hr" />
 					<div class="hidden lg:flex gap-2 px-4 pb-4 pt-2">
-						<span class="text-xs opacity-50"><kbd class="kbd">Esc</kbd> to close</span>
-						<span class="text-xs opacity-50"><kbd class="kbd">Enter</kbd> to select</span>
 						<span class="text-xs opacity-50"><kbd class="kbd">↑</kbd> <kbd class="kbd">↓</kbd> to navigate</span>
+						<span class="text-xs opacity-50"><kbd class="kbd">Enter</kbd> to select</span>
+						<span class="text-xs opacity-50"><kbd class="kbd">Esc</kbd> to close</span>
 					</div>
 				</Combobox>
 			</Dialog.Content>
