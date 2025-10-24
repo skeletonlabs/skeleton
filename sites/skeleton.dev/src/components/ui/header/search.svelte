8e737c6036cf72b64c3207135d723971ad9fd2bc
Hugos68 <hugokorteapple@gmail.com>
2025-10-24 21:11:52 +0200

    fix

--- sites/skeleton.dev/src/components/ui/header/search.svelte
+++ sites/skeleton.dev/src/components/ui/header/search.svelte
@@ -164,7 +164,7 @@
 		<div class="space-y-1">
 			<Combobox.ItemText>{item.title}</Combobox.ItemText>
 			<p class="text-xs text-surface-600-400 break-words [&>mark]:mark">
-				{@html item.description}
+				{@html item.excerpt}
 			</p>
 		</div>
 		<ChevronRightIcon class="size-4 opacity-50" />
