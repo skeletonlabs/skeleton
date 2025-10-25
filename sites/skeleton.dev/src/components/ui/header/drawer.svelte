07ca89c75c040f84a21325896133ce6c6db5ba59
endigo9740 <gundamx9740@gmail.com>
2025-10-25 14:45:43 -0500

    Small changes

--- sites/skeleton.dev/src/components/ui/header/drawer.svelte
+++ sites/skeleton.dev/src/components/ui/header/drawer.svelte
@@ -16,7 +16,7 @@
 		/>
 		<Dialog.Positioner class="fixed inset-0 z-50 flex justify-start">
 			<Dialog.Content
-				class="h-screen card bg-surface-100-900 w-sm p-4 space-y-4 shadow-xl transition transition-discrete opacity-0 -translate-x-full starting:data-[state=open]:opacity-0 starting:data-[state=open]:-translate-x-full data-[state=open]:opacity-100 data-[state=open]:translate-x-0"
+				class="card bg-surface-100-900 w-sm !h-screen p-4 space-y-4 shadow-xl overflow-y-auto transition transition-discrete opacity-0 -translate-x-full starting:data-[state=open]:opacity-0 starting:data-[state=open]:-translate-x-full data-[state=open]:opacity-100 data-[state=open]:translate-x-0"
 			>
 				<header class="flex justify-between items-center gap-4">
 					<Dialog.Title class="text-2xl font-bold">Skeleton</Dialog.Title>
