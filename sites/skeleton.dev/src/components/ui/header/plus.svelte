1f86d7efacb5507761b63475b9fadd76ffdddfda
Hugos68 <hugokorteapple@gmail.com>
2025-10-27 14:34:03 +0100

    improve

--- sites/skeleton.dev/src/components/ui/header/plus.svelte
+++ sites/skeleton.dev/src/components/ui/header/plus.svelte
@@ -1,13 +1,36 @@
-<script>
+<script lang="ts">
 	import { ArrowUpRightIcon, ChevronDownIcon } from '@lucide/svelte';
 	import { Popover, Portal } from '@skeletonlabs/skeleton-svelte';
 
-	const links = [{ href: 'https://themes.skeleton.dev/', title: 'Theme Generator', target: '_blank' }];
-	const linksMore = [
-		{ href: 'https://www.etesie.dev/figma', title: 'Figma Kit', target: '_blank' },
-		{ href: 'https://www.etesie.dev/guides/figma/01_basics', title: 'Figma Kit Tutorials', target: '_blank' },
-	];
+	const skeletonTools = [
+		{
+			href: 'https://themes.skeleton.dev/',
+			title: 'Theme Generator',
+		},
+	] as const;
+
+	const communityTools = [
+		{
+			href: 'https://www.etesie.dev/figma',
+			title: 'Figma Kit',
+		},
+		{
+			href: 'https://www.etesie.dev/guides/figma/01_basics',
+			title: 'Figma Kit Tutorials',
+		},
+	] as const;
 </script>
+
+{#snippet links(links: typeof skeletonTools | typeof communityTools)}
+	<div class="grid grid-cols-1 gap-2">
+		{#each links as link (link)}
+			<a href={link.href} target="_blank" rel="noopener noreferrer" class="btn justify-between hover:preset-tonal">
+				<span>{link.title}</span>
+				<ArrowUpRightIcon class="size-4" />
+			</a>
+		{/each}
+	</div>
+{/snippet}
 
 <Popover>
 	<Popover.Trigger class="btn hover:preset-tonal data-[state=open]:preset-tonal">
@@ -15,26 +38,12 @@
 		<ChevronDownIcon class="size-4 opacity-50" />
 	</Popover.Trigger>
 	<Portal>
-		<Popover.Positioner class="z-[51]!">
+		<Popover.Positioner class="z-51!">
 			<Popover.Content class="card bg-surface-50-950 border border-surface-200-800 p-4 space-y-4 shadow-xl">
-				<div class="grid grid-cols-1 gap-2">
-					{#each links as link (link)}
-						<a href={link.href} target={link.target} class="btn justify-between hover:preset-tonal">
-							<span>{link.title}</span>
-							{#if link.target === '_blank'}<ArrowUpRightIcon class="size-4" />{/if}
-						</a>
-					{/each}
-				</div>
+				{@render links(skeletonTools)}
 				<hr class="hr" />
 				<p class="text-xs ml-4 opacity-60">Community Tools</p>
-				<div class="grid grid-cols-1 gap-2">
-					{#each linksMore as link (link)}
-						<a href={link.href} target={link.target} class="btn justify-between hover:preset-tonal">
-							<span>{link.title}</span>
-							{#if link.target === '_blank'}<ArrowUpRightIcon class="size-4" />{/if}
-						</a>
-					{/each}
-				</div>
+				{@render links(communityTools)}
 			</Popover.Content>
 		</Popover.Positioner>
 	</Portal>
