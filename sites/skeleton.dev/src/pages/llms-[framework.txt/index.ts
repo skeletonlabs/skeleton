d874ac81e7596ad694935a06e782e5af956c5165
Hugos68 <hugokorteapple@gmail.com>
2025-10-27 21:26:04 +0100

    static paths

---
+++ sites/skeleton.dev/src/pages/llms-[framework.txt/index.ts
@@ -0,0 +1,25 @@
+import { getCollection } from '@/modules/content';
+import { getMarkdownFromDoc } from '@/modules/llms';
+import { commonSections } from '@/modules/navigation';
+import type { APIRoute } from 'astro';
+
+export async function getStaticPaths() {
+	const frameworks = await getCollection('frameworks');
+	return frameworks.map((framework) => ({
+		params: {
+			framework: framework.id,
+		},
+	}));
+}
+
+// TODO: Prune sections based on framework param
+export const GET: APIRoute = () => {
+	return new Response(
+		`# Skeleton Documentation\n\n${commonSections.map((section) => `## ${section.label}\n\n${section.items.map(getMarkdownFromDoc).join('\n')}`).join('\n')}`,
+		{
+			headers: {
+				'Content-Type': 'text/plain',
+			},
+		},
+	);
+};
