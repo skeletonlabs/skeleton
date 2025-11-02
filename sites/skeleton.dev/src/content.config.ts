b482adbcb3fbba39215017082ceb7728339028d2
Hugos68 <hugokorteapple@gmail.com>
2025-11-02 23:47:21 +0100

    update content

--- sites/skeleton.dev/src/content.config.ts
+++ sites/skeleton.dev/src/content.config.ts
@@ -85,7 +85,7 @@ export const collections = {
 		}),
 	}),
 	components: defineCollection({
-		loader: process.env.VERCEL_ENV === 'preview' ? componentsLoader : async () => [],
+		loader: ['preview', 'production'].includes(process.env.VERCEL_ENV ?? '') ? componentsLoader : async () => [],
 		schema: z.object({
 			name: z.string(),
 			types: z.array(
