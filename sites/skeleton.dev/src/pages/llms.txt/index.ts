0c64381f26eb24dc461c1ebdbcad4ba4b7f6eb1e
Hugos68 <hugokorteapple@gmail.com>
2025-11-03 00:27:32 +0100

    add llms

--- sites/skeleton.dev/src/pages/llms.txt/index.ts
+++ sites/skeleton.dev/src/pages/llms.txt/index.ts
@@ -1,12 +1,23 @@
-import { getCollection } from '@/modules/content';
-import { getMarkdownFromDoc } from '@/modules/llms/get-markdown-from-doc';
 import { commonSections } from '@/modules/navigation';
+import { resolvePath } from '@/modules/resolve-path';
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async () => {
-	const frameworks = await getCollection('frameworks');
+export const GET: APIRoute = async (context) => {
 	return new Response(
-		commonSections.map((section) => section.docs.map((doc) => getMarkdownFromDoc(doc, frameworks)).join('\n')).join('\n'),
+		[
+			'# Skeleton Documentation',
+			'',
+			...commonSections.map((section) => {
+				return [
+					`## ${section.label}`,
+					'',
+					section.docs.map((doc) => `- [${doc.data.title}](${resolvePath(context.params, `/docs/[framework]/${doc.id}.md`)})`).join('\n'),
+					'',
+				].join('\n');
+			}),
+		]
+			.flat()
+			.join('\n'),
 		{
 			headers: {
 				'Content-Type': 'text/plain',
