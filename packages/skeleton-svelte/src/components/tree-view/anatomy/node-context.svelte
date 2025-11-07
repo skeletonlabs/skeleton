c8018ca62e87c80ec5697f886073626a6cd59a85
viinz <42003446+dev-viinz@users.noreply.github.com>
2025-11-07 19:18:03 +0100

    feat: treeview node context (#4058)

---
+++ packages/skeleton-svelte/src/components/tree-view/anatomy/node-context.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" module>
+	import type { NodeProps } from '@zag-js/tree-view';
+	import type { Snippet } from 'svelte';
+
+	export interface TreeViewNodeContextProps {
+		children: Snippet<[() => NodeProps]>;
+	}
+</script>
+
+<script lang="ts">
+	import { NodeContext } from '../modules/node-context.js';
+
+	const props: TreeViewNodeContextProps = $props();
+
+	const nodeProps = NodeContext.consume();
+
+	const { children } = $derived(props);
+</script>
+
+{@render children(nodeProps)}
