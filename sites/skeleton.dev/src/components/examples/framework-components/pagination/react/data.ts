94d024961ea8c9d54efb475f7af4c22fe7881325
Hugos68 <hugokorteapple@gmail.com>
2025-11-10 15:28:34 +0100

    seed data

--- sites/skeleton.dev/src/components/examples/framework-components/pagination/react/data.ts
+++ sites/skeleton.dev/src/components/examples/framework-components/pagination/react/data.ts
@@ -1,4 +1,13 @@
-import { faker } from '@faker-js/faker';
+import { en_US, Faker, generateMersenne32Randomizer } from '@faker-js/faker';
+
+const randomizer = generateMersenne32Randomizer();
+
+randomizer.seed(0);
+
+const faker = new Faker({
+	locale: en_US,
+	randomizer: randomizer,
+});
 
 export const users = Array.from({ length: 500 }, (_, i) => ({
 	id: i + 1,
