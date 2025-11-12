b050fc11ed997d603a5025e75bc6032b3da98c9b
Hugos68 <hugokorteapple@gmail.com>
2025-11-12 23:11:21 +0100

    nested

---
+++ sites/skeleton.dev/src/components/examples/framework-components/menu/react/nested-menu.tsx
@@ -0,0 +1,43 @@
+import { Menu, Portal } from '@skeletonlabs/skeleton-react';
+
+export default function Default() {
+	return (
+		<Menu>
+			<Menu.Trigger className="btn preset-filled">Open Menu</Menu.Trigger>
+			<Portal>
+				<Menu.Positioner>
+					<Menu.Content className="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
+						<Menu>
+							<Menu.TriggerItem className="menu-item">New</Menu.TriggerItem>
+							<Portal>
+								<Menu.Positioner>
+									<Menu.Content className="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
+										<Menu.Item value="project" className="menu-item">
+											New Project
+										</Menu.Item>
+										<Menu.Item value="file" className="menu-item">
+											New File
+										</Menu.Item>
+										<Menu.Item value="folder" className="menu-item">
+											New Folder
+										</Menu.Item>
+									</Menu.Content>
+								</Menu.Positioner>
+							</Portal>
+						</Menu>
+						<Menu.Item value="open" className="menu-item">
+							Open File
+						</Menu.Item>
+						<Menu.Separator className="border-t border-surface-300-700 my-1" />
+						<Menu.Item value="save" className="menu-item">
+							Save
+						</Menu.Item>
+						<Menu.Item value="export" className="menu-item">
+							Export
+						</Menu.Item>
+					</Menu.Content>
+				</Menu.Positioner>
+			</Portal>
+		</Menu>
+	);
+}
---
+++ sites/skeleton.dev/src/components/examples/framework-components/menu/svelte/nested-menu.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Menu, Portal } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<Menu>
+	<Menu.Trigger class="btn preset-filled">Open Menu</Menu.Trigger>
+	<Portal>
+		<Menu.Positioner>
+			<Menu.Content class="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
+				<Menu>
+					<Menu.TriggerItem class="menu-item">New</Menu.TriggerItem>
+					<Portal>
+						<Menu.Positioner>
+							<Menu.Content class="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
+								<Menu.Item value="project" class="menu-item">New Project</Menu.Item>
+								<Menu.Item value="file" class="menu-item">New File</Menu.Item>
+								<Menu.Item value="folder" class="menu-item">New Folder</Menu.Item>
+							</Menu.Content>
+						</Menu.Positioner>
+					</Portal>
+				</Menu>
+				<Menu.Item value="open" class="menu-item">Open File</Menu.Item>
+				<Menu.Separator class="border-t border-surface-300-700 my-1" />
+				<Menu.Item value="save" class="menu-item">Save</Menu.Item>
+				<Menu.Item value="export" class="menu-item">Export</Menu.Item>
+			</Menu.Content>
+		</Menu.Positioner>
+	</Portal>
+</Menu>
