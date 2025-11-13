934565f343e44b5db3ed1d0ff4e55be8d48acabb
Hugos68 <hugokorteapple@gmail.com>
2025-11-13 23:20:16 +0100

    fixup playgrounds

--- playgrounds/skeleton-react/app/components/menu/page.tsx
+++ playgrounds/skeleton-react/app/components/menu/page.tsx
@@ -3,15 +3,23 @@ import { Menu, Portal } from '@skeletonlabs/skeleton-react';
 export default function Page() {
 	return (
 		<Menu>
-			<Menu.Trigger className="btn preset-filled">Open Menu</Menu.Trigger>
+			<Menu.Trigger>Open Menu</Menu.Trigger>
 			<Portal>
 				<Menu.Positioner>
-					<Menu.Content className="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
-						<Menu.Item value="new">New File</Menu.Item>
-						<Menu.Item value="open">Open File</Menu.Item>
-						<Menu.Separator className="hr" />
-						<Menu.Item value="save">Save</Menu.Item>
-						<Menu.Item value="export">Export</Menu.Item>
+					<Menu.Content>
+						<Menu.Item value="new">
+							<Menu.ItemText>New File</Menu.ItemText>
+						</Menu.Item>
+						<Menu.Item value="open">
+							<Menu.ItemText>Open File</Menu.ItemText>
+						</Menu.Item>
+						<Menu.Separator />
+						<Menu.Item value="save">
+							<Menu.ItemText>Save</Menu.ItemText>
+						</Menu.Item>
+						<Menu.Item value="export">
+							<Menu.ItemText>Export</Menu.ItemText>
+						</Menu.Item>
 					</Menu.Content>
 				</Menu.Positioner>
 			</Portal>
--- playgrounds/skeleton-svelte/src/routes/components/menu/+page.svelte
+++ playgrounds/skeleton-svelte/src/routes/components/menu/+page.svelte
@@ -3,15 +3,23 @@
 </script>
 
 <Menu>
-	<Menu.Trigger class="btn preset-filled">Open Menu</Menu.Trigger>
+	<Menu.Trigger>Open Menu</Menu.Trigger>
 	<Portal>
 		<Menu.Positioner>
-			<Menu.Content class="card w-56 p-2 bg-surface-100-900 shadow-xl space-y-1">
-				<Menu.Item value="new">New File</Menu.Item>
-				<Menu.Item value="open">Open File</Menu.Item>
-				<Menu.Separator class="hr" />
-				<Menu.Item value="save">Save</Menu.Item>
-				<Menu.Item value="export">Export</Menu.Item>
+			<Menu.Content>
+				<Menu.Item value="new">
+					<Menu.ItemText>New File</Menu.ItemText>
+				</Menu.Item>
+				<Menu.Item value="open">
+					<Menu.ItemText>Open File</Menu.ItemText>
+				</Menu.Item>
+				<Menu.Separator />
+				<Menu.Item value="save">
+					<Menu.ItemText>Save</Menu.ItemText>
+				</Menu.Item>
+				<Menu.Item value="export">
+					<Menu.ItemText>Export</Menu.ItemText>
+				</Menu.Item>
 			</Menu.Content>
 		</Menu.Positioner>
 	</Portal>
