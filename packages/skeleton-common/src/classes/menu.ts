c239d0724255a694f67ee9d002efc969d1ec4989
Hugos68 <hugokorteapple@gmail.com>
2025-11-13 23:57:28 +0100

    style tweak

--- packages/skeleton-common/src/classes/menu.ts
+++ packages/skeleton-common/src/classes/menu.ts
@@ -11,7 +11,7 @@ export const classesMenu = defineSkeletonClasses({
 	itemGroup: '',
 	itemGroupLabel: '',
 	item: [
-		'btn cursor-pointer hover:preset-tonal',
+		'flex justify-between items-center px-2 py-1 rounded cursor-pointer',
 		'data-highlighted:outline-2 data-highlighted:outline-offset-1 data-highlighted:outline-surface-950-50 data-highlighted:preset-tonal',
 	],
 	itemText: '',
