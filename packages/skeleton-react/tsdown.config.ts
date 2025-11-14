6010ad198b6b00498cf034b021c4970fec40f52f
Hugos68 <hugokorteapple@gmail.com>
2025-11-14 14:21:39 +0100

    remove unbundle and clean and whatnot

--- packages/skeleton-react/tsdown.config.ts
+++ packages/skeleton-react/tsdown.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
 	logLevel: 'error',
-	clean: false,
 	copy: ['src/index.css'],
-	unbundle: true,
 });
--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
 	build: {
 		cssCodeSplit: true,
 		cssMinify: false,
-		emptyOutDir: false,
 		lib: {
 			entry: {
 				['index']: 'src/index.css',
