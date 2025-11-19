8f8c247cef967862de3c55d924c8608584b464f8
Hugos68 <hugokorteapple@gmail.com>
2025-11-19 16:44:19 +0100

    remove unused styles

--- packages/skeleton-common/src/classes/floating-panel.ts
+++ packages/skeleton-common/src/classes/floating-panel.ts
@@ -6,7 +6,7 @@ export const classesFloatingPanel = defineSkeletonClasses({
 	content: 'card overflow-hidden shadow-lg border border-surface-300-800',
 	dragTrigger: '',
 	header: 'p-2 flex justify-between items-center bg-surface-200-800 border-b border-surface-300-800',
-	title: 'flex truncate ',
+	title: '',
 	control: 'flex',
 	stageTrigger: 'rounded p-2 hover:preset-tonal',
 	closeTrigger: 'rounded p-2 hover:preset-tonal',
