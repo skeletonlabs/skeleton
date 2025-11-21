779787c05ed673b1bb6e0bc0f938053640918309
Hugos68 <hugokorteapple@gmail.com>
2025-11-22 00:50:30 +0100

    test

--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -1,24 +1,14 @@
-import { basename, dirname, join } from 'node:path';
-import { glob } from 'tinyglobby';
+import { globSync } from 'node:fs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	logLevel: 'error',
 	build: {
 		cssCodeSplit: true,
-		cssMinify: false,
 		emptyOutDir: false,
 		lib: {
-			entry: {
-				['index']: 'src/index.css',
-				...Object.fromEntries(
-					(await glob('./src/themes/*.{css,scss}')).map((path) => {
-						const directory = basename(dirname(path));
-						const filename = basename(path);
-						return [join(directory, filename), path];
-					}),
-				),
-			},
+			formats: ['es'],
+			entry: ['src/index.css', ...globSync('src/themes/*.css')],
 		},
 	},
 });
