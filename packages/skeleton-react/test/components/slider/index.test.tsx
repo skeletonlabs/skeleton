5d4c48075254d39e207354930d185162d22dd7a3
Hugos68 <hugokorteapple@gmail.com>
2025-11-21 19:40:14 +0100

    remove setup, use act

--- packages/skeleton-react/test/components/slider/index.test.tsx
+++ packages/skeleton-react/test/components/slider/index.test.tsx
@@ -1,74 +1,75 @@
 import Test from './test.jsx';
+import { act } from 'react';
 import { describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-react';
 
 describe('Slider', () => {
 	describe('Root', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('root')).toBeInTheDocument();
 		});
 	});
 
 	describe('Label', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('label')).toBeInTheDocument();
 		});
 	});
 
 	describe('ValueText', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('value-text')).toBeInTheDocument();
 		});
 	});
 
 	describe('Control', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('control')).toBeInTheDocument();
 		});
 	});
 
 	describe('Track', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('track')).toBeInTheDocument();
 		});
 	});
 
 	describe('Range', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('range')).toBeInTheDocument();
 		});
 	});
 
 	describe('Thumb', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('thumb')).toBeInTheDocument();
 		});
 	});
 
 	describe('HiddenInput', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('hidden-input')).toBeInTheDocument();
 		});
 	});
 
 	describe('MarkerGroup', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('marker-group')).toBeInTheDocument();
 		});
 	});
 
 	describe('Marker', () => {
 		it('renders', async () => {
-			const { getByTestId } = await render(<Test />);
+			const { getByTestId } = await act(() => render(<Test />));
 			expect(getByTestId('marker')).toBeInTheDocument();
 		});
 	});
