c8f2aa873af51725e893f7ddb704888f27b80d5f
Hugos68 <hugokorteapple@gmail.com>
2025-11-22 18:15:24 +0100

    react

--- playgrounds/skeleton-react/vite.config.ts
+++ playgrounds/skeleton-react/vite.config.ts
@@ -4,8 +4,5 @@ import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
 	plugins: [tailwindcss(), tanstackStart(), viteReact()],
 });
