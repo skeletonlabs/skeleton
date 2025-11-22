675769ce159c917faddb229e8606ccf739ad22fe
Hugos68 <hugokorteapple@gmail.com>
2025-11-22 14:12:06 +0100

    remove

--- packages/skeleton-react/vite.config.ts
+++ packages/skeleton-react/vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [react()],
 	test: {
-		globalSetup: './vitest.setup.ts',
 		browser: {
 			enabled: true,
 			provider: playwright(),
