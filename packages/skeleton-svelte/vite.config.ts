9d1c29ae7f21735e6019796f985332314ed9a718
Hugos68 <hugokorteapple@gmail.com>
2025-11-22 13:23:58 +0100

    test

--- packages/skeleton-svelte/vite.config.ts
+++ packages/skeleton-svelte/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [svelte()],
+	optimizeDeps: {
+		exclude: ['@zag-js/svelte'],
+	},
 	test: {
 		browser: {
 			enabled: true,
