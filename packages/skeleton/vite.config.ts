d95165e343177cd2309c230ec4ff9a2ab8e8d854
Hugos68 <hugokorteapple@gmail.com>
2025-11-23 20:42:10 +0100

    fix up some small issues introduced in the monorepo performance PR

--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
 	build: {
 		cssCodeSplit: true,
 		cssMinify: false,
-		emptyOutDir: false,
 		lib: {
 			formats: ['es'],
 			entry: ['src/index.css', ...globSync('src/themes/*.css')],
