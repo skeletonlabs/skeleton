4d5a818c5dd25a1b79b15b7a9f4e0a101e6db965
Hugos68 <hugokorteapple@gmail.com>
2025-11-26 18:03:50 +0100

    (unrelated, but fixed floating panel not being in correct directory, small effort)

--- playgrounds/skeleton-react/src/routes/components/file-upload/index.tsx
+++ playgrounds/skeleton-react/src/routes/components/file-upload/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FileUpload, useFileUpload } from '@skeletonlabs/skeleton-react';
+import { FileUpload } from '@skeletonlabs/skeleton-react';
 import { createFileRoute } from '@tanstack/react-router';
 import { CircleXIcon } from 'lucide-react';
 
@@ -9,35 +9,29 @@ export const Route = createFileRoute('/components/file-upload/')({
 });
 
 function Page() {
-	const fileUpload = useFileUpload();
-
 	return (
-		<>
-			<FileUpload.Provider value={fileUpload}>
-				<FileUpload.Label>Upload your files</FileUpload.Label>
-				<FileUpload.Dropzone>
-					<FileUpload.Trigger>Browse Files</FileUpload.Trigger>
-					<FileUpload.HiddenInput />
-				</FileUpload.Dropzone>
-				<FileUpload.ItemGroup>
-					<FileUpload.Context>
-						{(fileUpload) =>
-							fileUpload.acceptedFiles.map((file) => (
-								<FileUpload.Item key={file.name} file={file}>
-									<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
-									<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
-									<FileUpload.ItemDeleteTrigger>
-										<CircleXIcon />
-									</FileUpload.ItemDeleteTrigger>
-								</FileUpload.Item>
-							))
-						}
-					</FileUpload.Context>
-				</FileUpload.ItemGroup>
-			</FileUpload.Provider>
-			<button className="btn preset-outlined-error-500" onClick={() => fileUpload.clearFiles()}>
-				Clear Files
-			</button>
-		</>
+		<FileUpload>
+			<FileUpload.Label>Upload your files</FileUpload.Label>
+			<FileUpload.Dropzone>
+				<FileUpload.Trigger>Browse Files</FileUpload.Trigger>
+				<FileUpload.HiddenInput />
+			</FileUpload.Dropzone>
+			<FileUpload.ItemGroup>
+				<FileUpload.Context>
+					{(fileUpload) =>
+						fileUpload.acceptedFiles.map((file) => (
+							<FileUpload.Item key={file.name} file={file}>
+								<FileUpload.ItemName>{file.name}</FileUpload.ItemName>
+								<FileUpload.ItemSizeText>{file.size} bytes</FileUpload.ItemSizeText>
+								<FileUpload.ItemDeleteTrigger>
+									<CircleXIcon />
+								</FileUpload.ItemDeleteTrigger>
+							</FileUpload.Item>
+						))
+					}
+				</FileUpload.Context>
+			</FileUpload.ItemGroup>
+			<FileUpload.ClearTrigger>Clear Files</FileUpload.ClearTrigger>
+		</FileUpload>
 	);
 }
