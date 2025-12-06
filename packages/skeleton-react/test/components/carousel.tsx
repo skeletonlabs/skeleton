fd5143424b3460014929f267b847a1b661451b2e
Hugos68 <hugokorteapple@gmail.com>
2025-12-06 01:00:25 +0100

    fix test

--- packages/skeleton-react/test/components/carousel.tsx
+++ packages/skeleton-react/test/components/carousel.tsx
@@ -4,21 +4,15 @@ export default function Test() {
 	return (
 		<Carousel slideCount={3} data-testid="root">
 			<Carousel.Control data-testid="control">
-				<Carousel.PrevTrigger data-testid="prev-trigger">Prev</Carousel.PrevTrigger>
-				<Carousel.NextTrigger data-testid="next-trigger">Next</Carousel.NextTrigger>
-				<Carousel.AutoplayTrigger data-testid="autoplay-trigger">Autoplay</Carousel.AutoplayTrigger>
+				<Carousel.PrevTrigger data-testid="prev-trigger" />
+				<Carousel.NextTrigger data-testid="next-trigger" />
+				<Carousel.AutoplayTrigger data-testid="autoplay-trigger" />
 			</Carousel.Control>
 			<Carousel.ItemGroup data-testid="item-group">
-				<Carousel.Item index={0} data-testid="item">
-					Slide 1
-				</Carousel.Item>
-				<Carousel.Item index={1}>Slide 2</Carousel.Item>
-				<Carousel.Item index={2}>Slide 3</Carousel.Item>
+				<Carousel.Item index={0} data-testid="item" />
 			</Carousel.ItemGroup>
 			<Carousel.IndicatorGroup data-testid="indicator-group">
 				<Carousel.Indicator index={0} data-testid="indicator" />
-				<Carousel.Indicator index={1} />
-				<Carousel.Indicator index={2} />
 			</Carousel.IndicatorGroup>
 		</Carousel>
 	);
