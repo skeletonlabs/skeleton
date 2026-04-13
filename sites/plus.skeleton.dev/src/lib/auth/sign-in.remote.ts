632fd9f9f17f7eb6e6dc6988fd513734a4851681
Hugos68 <hugokorteapple@gmail.com>
2026-04-14 00:02:55 +0200

    fix redirectTo in production

--- sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
+++ sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
@@ -2,6 +2,17 @@ import * as v from 'valibot';
 import { form, getRequestEvent } from '$app/server';
 import { SUPPORTED_PROVIDERS } from '$lib/auth/supported-providers';
 import { error, redirect } from '@sveltejs/kit';
+import { dev } from '$app/environment';
+
+function getRedirectTo() {
+	const callbackPath = '/auth/callback';
+	if (dev) {
+		return new URL(callbackPath, 'http://localhost:5173').toString();
+	} else if (process.env.VERCEL_ENV === 'preview') {
+		return new URL(callbackPath, `https://${process.env.VERCEL_URL}`).toString();
+	}
+	return new URL(callbackPath, `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`).toString();
+}
 
 export const signIn = form(
 	v.object({
@@ -13,7 +24,7 @@ export const signIn = form(
 		const signInResult = await event.locals.supabase.auth.signInWithOAuth({
 			provider: data.provider,
 			options: {
-				redirectTo: new URL('/auth/callback', event.url).toString(),
+				redirectTo: getRedirectTo(),
 			},
 		});
 
