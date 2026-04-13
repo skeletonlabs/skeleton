88773345f0b1716d2f93e0143e8d2047a6cd47f3
Hugos68 <hugokorteapple@gmail.com>
2026-04-14 01:05:39 +0200

    fix

--- sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
+++ sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
@@ -2,6 +2,19 @@ import * as v from 'valibot';
 import { form, getRequestEvent } from '$app/server';
 import { SUPPORTED_PROVIDERS } from '$lib/auth/supported-providers';
 import { error, redirect } from '@sveltejs/kit';
+import { dev } from '$app/environment';
+// @ts-expect-error variables are only present in the preview/production environment
+import { VERCEL_URL, VERCEL_PROJECT_PRODUCTION_URL } from '$env/dynamic/private';
+
+function getRedirectTo() {
+	const callbackPath = '/auth/callback';
+	if (dev) {
+		return new URL(callbackPath, 'http://localhost:5173').toString();
+	} else if (process.env.VERCEL_ENV === 'preview') {
+		return new URL(callbackPath, `https://${VERCEL_URL}`).toString();
+	}
+	return new URL(callbackPath, `https://${VERCEL_PROJECT_PRODUCTION_URL}`).toString();
+}
 
 export const signIn = form(
 	v.object({
@@ -13,7 +26,7 @@ export const signIn = form(
 		const signInResult = await event.locals.supabase.auth.signInWithOAuth({
 			provider: data.provider,
 			options: {
-				redirectTo: 'https://plus.skeleton.dev/auth/callback',
+				redirectTo: getRedirectTo(),
 			},
 		});
 
