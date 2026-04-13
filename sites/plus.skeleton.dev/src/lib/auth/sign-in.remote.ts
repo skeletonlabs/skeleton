b31241fb27e23fc6da3541b0dd23c05b3e2996e6
Hugos68 <hugokorteapple@gmail.com>
2026-04-14 01:06:44 +0200

    test

--- sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
+++ sites/plus.skeleton.dev/src/lib/auth/sign-in.remote.ts
@@ -3,17 +3,15 @@ import { form, getRequestEvent } from '$app/server';
 import { SUPPORTED_PROVIDERS } from '$lib/auth/supported-providers';
 import { error, redirect } from '@sveltejs/kit';
 import { dev } from '$app/environment';
-// @ts-expect-error variables are only present in the preview/production environment
-import { VERCEL_URL, VERCEL_PROJECT_PRODUCTION_URL } from '$env/dynamic/private';
 
 function getRedirectTo() {
 	const callbackPath = '/auth/callback';
 	if (dev) {
 		return new URL(callbackPath, 'http://localhost:5173').toString();
 	} else if (process.env.VERCEL_ENV === 'preview') {
-		return new URL(callbackPath, `https://${VERCEL_URL}`).toString();
+		return new URL(callbackPath, `https://${process.env.VERCEL_URL}`).toString();
 	}
-	return new URL(callbackPath, `https://${VERCEL_PROJECT_PRODUCTION_URL}`).toString();
+	return new URL(callbackPath, `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`).toString();
 }
 
 export const signIn = form(
