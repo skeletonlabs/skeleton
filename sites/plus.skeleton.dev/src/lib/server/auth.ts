2dfa9ef893b317c13a2f610ce7caadcd778320f1
Hugos68 <hugokorteapple@gmail.com>
2026-04-19 22:29:28 +0200

    update

--- sites/plus.skeleton.dev/src/lib/server/auth.ts
+++ sites/plus.skeleton.dev/src/lib/server/auth.ts
@@ -1,22 +1,21 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { database } from '$lib/server/database';
-import { BETTER_AUTH_URL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import * as schema from '$lib/server/schema';
 import { getRequestEvent } from '$app/server';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 
 export const auth = betterAuth({
-	baseURL: BETTER_AUTH_URL,
 	database: drizzleAdapter(database, {
 		provider: 'pg',
 		schema: schema,
 	}),
 	socialProviders: {
 		github: {
-			clientId: GITHUB_CLIENT_ID,
-			clientSecret: GITHUB_CLIENT_SECRET,
-			redirectURI: 'http://localhost:5173/api/auth/callback/github',
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET,
+			redirectURI: `${env.BETTER_AUTH_URL}/api/auth/callback/github`,
 		},
 	},
 	plugins: [sveltekitCookies(getRequestEvent)],
--- sites/plus.skeleton.dev/src/lib/server/database.ts
+++ sites/plus.skeleton.dev/src/lib/server/database.ts
@@ -1,9 +1,9 @@
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 export const database = drizzle({
-	client: postgres(DATABASE_URL, {
+	client: postgres(env.DATABASE_URL, {
 		prepare: false,
 	}),
 });
