da9af32ac272795de48515084521d12ec0b25c1e
Hugos68 <hugokorteapple@gmail.com>
2026-04-20 02:03:52 +0200

    add schema for types safety

--- sites/plus.skeleton.dev/src/lib/server/database.ts
+++ sites/plus.skeleton.dev/src/lib/server/database.ts
@@ -1,9 +1,11 @@
 import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import * as schema from '$lib/server/schema';
 
 export const database = drizzle({
 	client: postgres(env.DATABASE_URL, {
 		prepare: false,
 	}),
+	schema,
 });
