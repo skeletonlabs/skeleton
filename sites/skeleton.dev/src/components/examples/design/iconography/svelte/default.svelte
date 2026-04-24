47a7146dfcaacfdba296e6f98924f9d7c0cbcf0c
endigo9740 <gundamx9740@gmail.com>
2026-04-23 19:39:07 -0500

    Icon size adjustments

---
+++ sites/skeleton.dev/src/components/examples/design/iconography/svelte/default.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import HeartIcon from '@lucide/svelte/icons/heart';
+	import UserRoundIcon from '@lucide/svelte/icons/user-round';
+	import RocketIcon from '@lucide/svelte/icons/rocket';
+	import CrownIcon from '@lucide/svelte/icons/crown';
+	import CompassIcon from '@lucide/svelte/icons/compass';
+	import SparklesIcon from '@lucide/svelte/icons/sparkles';
+	import FlameIcon from '@lucide/svelte/icons/flame';
+	import LeafIcon from '@lucide/svelte/icons/leaf';
+	import MusicIcon from '@lucide/svelte/icons/music';
+	import GemIcon from '@lucide/svelte/icons/gem';
+	import CameraIcon from '@lucide/svelte/icons/camera';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import SkullIcon from '@lucide/svelte/icons/skull';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import FeatherIcon from '@lucide/svelte/icons/feather';
+	import AnchorIcon from '@lucide/svelte/icons/anchor';
+	import KeyIcon from '@lucide/svelte/icons/key';
+	import BellIcon from '@lucide/svelte/icons/bell';
+	import StarIcon from '@lucide/svelte/icons/star';
+	import GiftIcon from '@lucide/svelte/icons/gift';
+	import CloudIcon from '@lucide/svelte/icons/cloud';
+	import CoffeeIcon from '@lucide/svelte/icons/coffee';
+	import ZapIcon from '@lucide/svelte/icons/zap';
+	import GhostIcon from '@lucide/svelte/icons/ghost';
+	import SwordsIcon from '@lucide/svelte/icons/swords';
+</script>
+
+<div class="grid grid-cols-5 gap-4 place-items-center">
+	<HeartIcon class="size-12" />
+	<UserRoundIcon class="size-12" />
+	<RocketIcon class="size-12" />
+	<CrownIcon class="size-12" />
+	<CompassIcon class="size-12" />
+	<SparklesIcon class="size-12" />
+	<FlameIcon class="size-12" />
+	<LeafIcon class="size-12" />
+	<MusicIcon class="size-12" />
+	<GemIcon class="size-12" />
+	<CameraIcon class="size-12" />
+	<MoonIcon class="size-12" />
+	<SkullIcon class="size-12" />
+	<SunIcon class="size-12" />
+	<FeatherIcon class="size-12" />
+	<AnchorIcon class="size-12" />
+	<KeyIcon class="size-12" />
+	<BellIcon class="size-12" />
+	<StarIcon class="size-12" />
+	<GiftIcon class="size-12" />
+	<CloudIcon class="size-12" />
+	<CoffeeIcon class="size-12" />
+	<ZapIcon class="size-12" />
+	<GhostIcon class="size-12" />
+	<SwordsIcon class="size-12" />
+</div>
