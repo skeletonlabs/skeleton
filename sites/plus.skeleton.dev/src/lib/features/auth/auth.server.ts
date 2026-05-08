7e7b035d6fa2824d4008bf582262224b0d0fb06e
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2026-05-09 00:01:46 +0200

    feature: better auth fix (test) (#4414)

--- sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
+++ sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
@@ -6,11 +6,13 @@ import * as schema from '$lib/infrastructure/database/schema';
 import { getRequestEvent } from '$app/server';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import type { SupportedOAuthProvider } from '$lib/features/auth/supported-oauth-providers';
-import { getAppOrigin } from '$lib/infrastructure/http/get-app-origin';
 import { oAuthProxy } from 'better-auth/plugins';
 
 export const auth = betterAuth({
-	baseURL: getAppOrigin(),
+	baseURL: {
+		allowedHosts: ['localhost:*', 'plusskeleton-*-skeleton-labs.vercel.app', 'plus.skeleton.dev'],
+		protocol: 'https',
+	},
 	database: drizzleAdapter(db, {
 		provider: 'pg',
 		schema,
@@ -25,11 +27,10 @@ export const auth = betterAuth({
 			clientSecret: env.DISCORD_CLIENT_SECRET!,
 		},
 	} satisfies Record<SupportedOAuthProvider['id'], unknown>,
-	trustedOrigins: ['http://localhost:5173', 'https://plusskeleton-*-skeleton-labs.vercel.app', 'https://plus.skeleton.dev'],
 	plugins: [
+		sveltekitCookies(getRequestEvent),
 		oAuthProxy({
 			productionURL: 'https://plus.skeleton.dev',
 		}),
-		sveltekitCookies(getRequestEvent),
 	],
 });
