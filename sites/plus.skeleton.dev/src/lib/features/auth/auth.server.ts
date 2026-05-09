d9505ef065493cdc657cebb7eb8d403aae39535a
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2026-05-09 21:51:39 +0200

    fdas (#4427)

--- sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
+++ sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
@@ -10,7 +10,7 @@ import { oAuthProxy } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	baseURL: {
-		allowedHosts: ['localhost:*', 'plus.skeleton.dev'],
+		allowedHosts: ['localhost:*', '*.vercel.app','plus.skeleton.dev'],
 	},
 	database: drizzleAdapter(db, {
 		provider: 'pg',
