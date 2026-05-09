77c7fd0d3d29d9bb45807039910a2c6048e2fe13
Hugos68 <hugokorteapple@gmail.com>
2026-05-09 21:06:35 +0200

    test

--- sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
+++ sites/plus.skeleton.dev/src/lib/features/auth/auth.server.ts
@@ -28,11 +28,7 @@ export const auth = betterAuth({
 	} satisfies Record<SupportedOAuthProvider['id'], unknown>,
 	plugins: [
 		oAuthProxy({
-			currentURL: import.meta.env.DEV
-				? 'http://localhost:5173'
-				: process.env.VERCEL_ENV === 'production'
-					? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-					: `https://${process.env.VERCEL_URL}`,
+			currentURL: import.meta.env.DEV ? 'http://localhost:5173' : 'https://plus.skeleton.dev',
 			productionURL: 'https://plus.skeleton.dev',
 		}),
 		/**
