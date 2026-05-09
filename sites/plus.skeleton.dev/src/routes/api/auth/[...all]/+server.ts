ae0b61c14e85e8a13553b996fb1979b701445fea
Hugos68 <hugokorteapple@gmail.com>
2026-05-09 20:45:41 +0200

    cleanup

--- sites/plus.skeleton.dev/src/routes/api/auth/[...all]/+server.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from '$lib/features/auth/auth.server';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = (event) => auth.handler(event.request);
-export const POST: RequestHandler = (event) => auth.handler(event.request);
