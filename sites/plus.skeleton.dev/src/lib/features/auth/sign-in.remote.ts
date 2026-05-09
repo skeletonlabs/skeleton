ea7adc7a54d9c3589dd823b24c7ed72eceb84003
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2026-05-09 20:56:07 +0200

    fix signIn (#4423)

--- sites/plus.skeleton.dev/src/lib/features/auth/sign-in.remote.ts
+++ sites/plus.skeleton.dev/src/lib/features/auth/sign-in.remote.ts
@@ -1,5 +1,5 @@
 import * as v from 'valibot';
-import { form } from '$app/server';
+import { form, getRequestEvent } from '$app/server';
 import { error, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/features/auth/auth.server';
 
@@ -8,7 +8,10 @@ export const signIn = form(
 		provider: v.picklist(Object.keys(auth.options.socialProviders)),
 	}),
 	async (data) => {
+		const event = getRequestEvent();
+
 		const signIn = await auth.api.signInSocial({
+			headers: event.request.headers,
 			body: {
 				provider: data.provider,
 			},
