e150e179039dff3218ea67a073f71fdc633ccba2
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2026-05-13 16:42:13 +0200

    deps: upgrade drizzle (#4450)

--- sites/plus.skeleton.dev/src/lib/server/database/database.ts
+++ sites/plus.skeleton.dev/src/lib/server/database/database.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
-import * as schema from '$lib/server/database/schema';
+import { relations } from '$lib/server/database/relations';
 import { neon } from '@neondatabase/serverless';
 import { drizzle as drizzleProduction } from 'drizzle-orm/neon-http';
 import { drizzle as drizzleLocal } from 'drizzle-orm/postgres-js';
@@ -9,9 +9,9 @@ import postgres from 'postgres';
 export const database = dev
 	? drizzleLocal({
 			client: postgres(env.DATABASE_URL!),
-			schema,
+			relations,
 		})
 	: drizzleProduction({
 			client: neon(env.DATABASE_URL!),
-			schema,
+			relations,
 		});
