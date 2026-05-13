5bdff42d37f130405b32751986c5765a730c9388
Hugos68 <hugokorteapple@gmail.com>
2026-05-13 10:56:06 +0200

    Revert "upgrade drizzle"
    This reverts commit 6247c9efc151e59d0e87e7c4bd1e8f4039b8c135.

--- sites/plus.skeleton.dev/src/lib/server/database/database.ts
+++ sites/plus.skeleton.dev/src/lib/server/database/database.ts
@@ -1,19 +1,17 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
-import * as relations from '$lib/server/database/relations';
+import * as schema from '$lib/server/database/schema';
 import { neon } from '@neondatabase/serverless';
 import { drizzle as drizzleProduction } from 'drizzle-orm/neon-http';
 import { drizzle as drizzleLocal } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 export const database = dev
-	? // @ts-expect-error - https://github.com/drizzle-team/drizzle-orm/issues/5693
-		drizzleLocal({
+	? drizzleLocal({
 			client: postgres(env.DATABASE_URL!),
-			relations,
+			schema,
 		})
-	: // @ts-expect-error - https://github.com/drizzle-team/drizzle-orm/issues/5693
-		drizzleProduction({
+	: drizzleProduction({
 			client: neon(env.DATABASE_URL!),
-			relations,
+			schema,
 		});
