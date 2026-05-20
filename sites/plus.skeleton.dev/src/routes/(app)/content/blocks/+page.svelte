c95ab31edc27e3a94b01c21f32024686ed176823
Hugos68 <hugokorteapple@gmail.com>
2026-05-20 22:38:24 +0200

    Implement tier blocks

--- sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
+++ sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import FreeBlock from '$lib/tiers/free/free-block.svelte';
+	import PremiumBlock from '$lib/tiers/premium/premium-block.svelte';
+</script>
+
 <div class="container-page">
 	<p>Now viewing <code class="code">blocks</code></p>
+
+	<FreeBlock />
+
+	<PremiumBlock />
 </div>
