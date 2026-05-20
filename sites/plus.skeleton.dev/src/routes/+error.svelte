717575b843772a8e1edfc8dac139b4744f6f96d4
endigo9740 <gundamx9740@gmail.com>
2026-05-20 09:35:54 -0500

    Design updates

--- sites/plus.skeleton.dev/src/routes/+error.svelte
+++ sites/plus.skeleton.dev/src/routes/+error.svelte
@@ -3,28 +3,33 @@
 	import Skeleton from '$lib/components/branding/skeleton.svelte';
 	import Footer from '$lib/components/layout/footer.svelte';
 	import Header from '$lib/components/layout/header.svelte';
+	import HomeIcon from '@lucide/svelte/icons/home';
+	import RefreshCwIcon from '@lucide/svelte/icons/refresh-cw';
 </script>
 
-<div class="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+<div class="min-h-dvh grid grid-rows-[auto_1fr_auto]">
 	<Header />
-	<main class="container mx-auto flex w-full items-center justify-center border-x border-surface-200-800 px-6 py-16">
-		<div class="flex w-full max-w-xl flex-col justi gap-4">
-			<div class="opacity-70">
-				<Skeleton />
-			</div>
-
-			<p class="text-sm font-medium tracking-wide text-surface-500 uppercase">
-				Status {page.status}
-			</p>
-
-			<p class="max-w-md text-surface-600-300">{page.error?.message ?? 'An unknown error occurred.'}</p>
-
-			<div class="mt-2 flex gap-3">
-				<a href={page.url.toString()} class="btn preset-filled"> Reload page </a>
-
-				<a href="/" class="btn preset-tonal"> Go home </a>
-			</div>
-		</div>
+	<main class="container container-page mx-auto flex w-full items-center justify-center border-x border-surface-200-800">
+		<article class="w-full max-w-xl flex flex-col justify-center items-center gap-4">
+			<header>
+				<Skeleton class="size-elem-7xl" />
+				<h1 class="sr-only">Error</h1>
+			</header>
+			<article class="space-y-2 text-center">
+				<p class="opacity-60">Status {page.status}</p>
+				<h2 class="h3">{page.error?.message ?? 'An unknown error occurred.'}</h2>
+			</article>
+			<footer class="flex gap-2">
+				<a href={page.url.toString()} class="btn preset-outlined-surface-200-800">
+					<RefreshCwIcon />
+					<span>Reload Page</span>
+				</a>
+				<a href="/" class="btn preset-filled">
+					<HomeIcon />
+					<span>Go Home</span>
+				</a>
+			</footer>
+		</article>
 	</main>
 	<Footer />
 </div>
