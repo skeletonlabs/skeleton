a6ceb8272755a8be49a216fe3ed78a82c8c451eb
Hugo Korte <63101006+Hugos68@users.noreply.github.com>
2026-05-21 17:57:21 +0200

    task: add plus submodules (#4470)

--- sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
+++ sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
@@ -1,3 +1,30 @@
+<script lang="ts">
+	import FreeBlockReact from '$lib/content/free/blocks/example/react/free-block?raw';
+	import FreeBlock from '$lib/content/free/blocks/example/svelte/free-block.svelte';
+	import PremiumBlockReact from '$lib/content/premium/blocks/example/react/premium-block?raw';
+	import PremiumBlock from '$lib/content/premium/blocks/example/svelte/premium-block.svelte';
+</script>
+
 <div class="container-page">
 	<p>Now viewing <code class="code">blocks</code></p>
+
+	<h2>Free Block (Svelte + Rendered)</h2>
+
+	<FreeBlock />
+
+	<h2>Free Block (React)</h2>
+
+	<code class="code">
+		{FreeBlockReact}
+	</code>
+
+	<h2>Premium Block (Svelte + Rendered)</h2>
+
+	<PremiumBlock />
+
+	<h2>Premium Block (React)</h2>
+
+	<code class="code">
+		{PremiumBlockReact}
+	</code>
 </div>
