237806b5e254b1d8af4788f72c90d03790787ee5
Hugos68 <hugokorteapple@gmail.com>
2026-05-21 09:32:47 +0200

    import new blocks structure

--- sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
+++ sites/plus.skeleton.dev/src/routes/(app)/content/blocks/+page.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
-	import FreeBlock from '$lib/tiers/free/free-block.svelte';
-	import PremiumBlock from '$lib/tiers/premium/premium-block.svelte';
+	import FreeBlockReact from '$lib/tiers/free/blocks/example/react/free-block?raw';
+	import FreeBlock from '$lib/tiers/free/blocks/example/svelte/free-block.svelte';
+	import PremiumBlockReact from '$lib/tiers/premium/blocks/example/react/premium-block?raw';
+	import PremiumBlock from '$lib/tiers/premium/blocks/example/svelte/premium-block.svelte';
 </script>
 
 <div class="container-page">
 	<p>Now viewing <code class="code">blocks</code></p>
 
+	<h2>Free Block (Svelte + Rendered)</h2>
+
 	<FreeBlock />
 
+	<h2>Free Block (React)</h2>
+
+	<code class="code">
+		{FreeBlockReact}
+	</code>
+
+	<h2>Premium Block (Svelte + Rendered)</h2>
+
 	<PremiumBlock />
+
+	<h2>Premium Block (React)</h2>
+
+	<code class="code">
+		{PremiumBlockReact}
+	</code>
 </div>
