2b171660b2295cae866ec6fed7b5fb6cf274bb36
Hugos68 <hugokorteapple@gmail.com>
2026-05-23 21:45:13 +0200

    fix

--- sites/plus.skeleton.dev/src/routes/(app)/content/themes/+page.svelte
+++ sites/plus.skeleton.dev/src/routes/(app)/content/themes/+page.svelte
@@ -1,3 +1,17 @@
+<script>
+	import Code from '$lib/components/code/code.svelte';
+</script>
+
 <div class="container-page">
 	<p>Now viewing <code class="code">themes (repo)</code></p>
+
+	<Code code="console.log('Hello, World!');" lang="ts" />
 </div>
+
+<style>
+	@reference "../../../layout.css";
+
+	:global(.shiki) {
+		/* Override shiki styles here */
+	}
+</style>
