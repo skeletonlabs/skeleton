62fe57ece2e121d20e4bb63db47a208a2f7c4a83
Hugos68 <hugokorteapple@gmail.com>
2026-05-25 01:02:17 +0200

    update

--- packages/skeleton-svelte/src/components/tree-view/modules/provider.svelte.ts
+++ packages/skeleton-svelte/src/components/tree-view/modules/provider.svelte.ts
@@ -3,11 +3,11 @@ import { normalizeProps, useMachine, type PropTypes } from '@zag-js/svelte';
 import { connect, machine } from '@zag-js/tree-view';
 import type { Api, Props, TreeNode } from '@zag-js/tree-view';
 
-export function useTreeView<T extends TreeNode = TreeNode>(props: Omit<Props<T>, 'id'> = {}): () => Api<PropTypes> {
+export function useTreeView<T extends TreeNode = TreeNode>(props: Props<T> | (() => Props<T>)): () => Api<PropTypes> {
 	const local = LocaleProviderContext.consume();
 	const service = useMachine(machine, () => ({
 		dir: local().dir,
-		...props,
+		...(typeof props === 'function' ? props() : props),
 	}));
 	const treeView = $derived(connect(service, normalizeProps));
 	return () => treeView;
