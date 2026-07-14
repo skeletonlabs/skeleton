cad72316583d704ebcc1221d9132929680482974
endigo9740 <gundamx9740@gmail.com>
2026-07-14 13:39:59 -0500

    CI fix

--- sites/plus.skeleton.dev/scripts/init-git-submodules.sh
+++ sites/plus.skeleton.dev/scripts/init-git-submodules.sh
@@ -8,10 +8,10 @@ git config --global url."https://${SKELETONLABS_PLUS_REPOSITORIES_PAT}@github.co
 cd "$(git rev-parse --show-toplevel)"
 git submodule deinit -f --all 2>/dev/null || true
 
-while IFS= read -r path; do
+git config --file .gitmodules --get-regexp 'submodule\..*\.path' | awk '{print $2}' | while IFS= read -r path; do
   rm -rf ".git/modules/$path"
   rm -rf "$path"
-done < <(git config --file .gitmodules --get-regexp 'submodule\..*\.path' | awk '{print $2}')
+done
 
 git submodule sync --recursive
 git submodule update --init --recursive
